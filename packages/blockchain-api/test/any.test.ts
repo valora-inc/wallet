35a00715bb7d0c523af77ea09a1e329823aba0e5
Conner Swann <me@connerswann.me>
2021-03-12 10:22:10 -0800

    formatting

--- packages/blockchain-api/test/any.test.ts
+++ packages/blockchain-api/test/any.test.ts
@@ -1,13 +1,6 @@
 import { Any } from '../src/events/Any'
 import { metrics } from '../src/metrics'
 
-// Wrap counters in a class like this: https://github.com/celo-org/celo-oracle/blob/main/test/exchange_adapters/base.test.ts#L78
-
-// mock the input context (or something)
-// finish this describe block
-// call any.getEvent
-// expect the counter to be incremented
-
 jest.mock('../src/metrics')
 
 describe('AnyType', () => {
