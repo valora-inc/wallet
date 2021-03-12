cb8ae27937656d5089673b737a0f2cd5d542b702
Conner Swann <me@connerswann.me>
2021-03-12 08:52:20 -0800

    added prometheus metrics

---
+++ packages/blockchain-api/src/metrics.ts
@@ -0,0 +1,18 @@
+import { Counter } from 'prom-client'
+
+export class apiMetrics {
+  private unknownTransactionCounter: Counter<string>
+
+  constructor() {
+    this.unknownTransactionCounter = new Counter({
+      name: 'get_unknown_transaction_type',
+      help: 'Increments when an unknown transaction type is ingested.',
+    })
+  }
+
+  unknownTransaction() {
+    this.unknownTransactionCounter.inc()
+  }
+}
+
+export enum Context {}
