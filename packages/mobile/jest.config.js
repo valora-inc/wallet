32ade7c6c93a80129dd8566366293a0690d7b8f1
Jean Regisser <jean.regisser@gmail.com>
2021-03-18 10:22:25 +0100

    typo

--- packages/mobile/jest.config.js
+++ packages/mobile/jest.config.js
@@ -26,7 +26,7 @@ module.exports = {
     'react-native-bip39': '<rootDir>/../../node_modules/react-native-bip39/src',
     'react-native-svg': '<rootDir>/../../node_modules/react-native-svg-mock',
     // For some reason jest doesn't pick it up automatically from the __mocks__ folder
-    // like the other modules, dding it here fixes it
+    // like the other modules, adding it here fixes it
     'secrets.json': '<rootDir>/__mocks__/secrets.json',
   },
   modulePathIgnorePatterns: ['<rootDir>/node_modules/(.*)/node_modules/react-native'],
