b86428aec53ce37977b4ec1efa912d74da0acead
Tarik Bellamine <tarikbellamine@protonmail.com>
2021-04-02 13:04:06 -0700

    use reselect

--- packages/mobile/src/account/selectors.ts
+++ packages/mobile/src/account/selectors.ts
@@ -1,5 +1,6 @@
 import { Countries } from '@celo/utils/lib/countries'
 import * as RNLocalize from 'react-native-localize'
+import { createSelector } from 'reselect'
 import i18n from 'src/i18n'
 import { RootState } from 'src/redux/reducers'
 
@@ -17,8 +18,11 @@ export const devModeSelector = (state: RootState) => state.account.devModeActive
 export const nameSelector = (state: RootState) => state.account.name
 export const e164NumberSelector = (state: RootState) => state.account.e164PhoneNumber
 export const pictureSelector = (state: RootState) => state.account.pictureUri
-export const defaultCountryCodeSelector = (state: RootState) =>
-  state.account.defaultCountryCode || inferCountryCode()
+export const defaultCountryCodeSelector = () =>
+  createSelector(
+    (state: RootState) => state.account.defaultCountryCode,
+    (defaultCountryCode) => defaultCountryCode || inferCountryCode()
+  )
 export const userContactDetailsSelector = (state: RootState) => state.account.contactDetails
 export const pincodeTypeSelector = (state: RootState) => state.account.pincodeType
 export const promptFornoIfNeededSelector = (state: RootState) => state.account.promptFornoIfNeeded
