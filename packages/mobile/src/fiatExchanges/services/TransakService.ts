9b895822a0e7dbbc902feeed47f9b892d18ba20e
Pedro Gutiérrez <pedrogp9693@gmail.com>
2021-04-12 16:14:14 +0200

    Transak policies fees

--- packages/mobile/src/fiatExchanges/services/TransakService.ts
+++ packages/mobile/src/fiatExchanges/services/TransakService.ts
@@ -1,4 +1,5 @@
 import networkConfig from 'src/geth/networkConfig'
+import { PaymentMethod } from 'src/fiatExchanges/FiatExchangeOptions'
 import { CicoService } from 'src/fiatExchanges/services/CicoService.abstract'
 
 export class TransakService extends CicoService {
@@ -17,6 +18,22 @@ export class TransakService extends CicoService {
     super()
 
     this.baseUrl = networkConfig.transakApiUrl
+  }
+
+  getFeesPolicy(paymentMethod: PaymentMethod) {
+    // From: https://www.notion.so/On-ramp-Buy-Crypto-976ee96fc0764628ba990b550b1310d3
+    if (paymentMethod === PaymentMethod.CARD) {
+      return {
+        percentage: 3.9,
+        extraPercentage: 0.3,
+        minimum: 5,
+      }
+    }
+    if (paymentMethod === PaymentMethod.BANK) {
+      return {
+        percentage: [0.25, 0.5],
+      }
+    }
   }
 
   getFees(cryptoAsset: string, fiatAsset: string, requestedFiatAmount: number) {
