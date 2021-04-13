774b6788a5dd4f831ad58f9515a0ad89415b9390
michellewong793 <michellewong793@gmail.com>
2021-04-12 17:40:19 -0700

    Add test WIP in Settings

--- packages/mobile/src/account/Settings.test.tsx
+++ packages/mobile/src/account/Settings.test.tsx
@@ -1,13 +1,23 @@
 import * as React from 'react'
 import 'react-native'
+import { fireEvent, render } from 'react-native-testing-library'
 import { Provider } from 'react-redux'
 import * as renderer from 'react-test-renderer'
 import Settings from 'src/account/Settings'
+import { ensurePincode, navigate } from 'src/navigator/NavigationService'
 import { Screens } from 'src/navigator/Screens'
 import { createMockStore, getMockStackScreenProps } from 'test/utils'
 import { mockE164Number, mockE164NumberPepper } from 'test/values'
 
 describe('Account', () => {
+  const mockedNavigate = navigate as jest.Mock
+  const mockedEnsurePincode = ensurePincode as jest.Mock
+  // Clear mocks before each test so that navigate is called with fresh copy
+  beforeEach(() => {
+    jest.clearAllMocks()
+    mockedNavigate.mockReset()
+  })
+
   beforeAll(() => {
     jest.useFakeTimers()
   })
@@ -59,5 +69,23 @@ describe('Account', () => {
       </Provider>
     )
     expect(tree).toMatchSnapshot()
+  })
+
+  it('navigates to PincodeSet screen if entered PIN is correct', async () => {
+    const tree = render(
+      <Provider store={createMockStore({})}>
+        <Settings {...getMockStackScreenProps(Screens.Settings)} />
+      </Provider>
+    )
+    fireEvent.press(tree.getByTestId('ChangePIN'))
+    try {
+      await mockedEnsurePincode()
+      expect(navigate).toHaveBeenCalledWith(Screens.PincodeSet, {
+        isVerifying: false,
+        changePin: true,
+      })
+    } catch (error) {
+      expect(mockedNavigate).not.toHaveBeenCalled()
+    }
   })
 })
