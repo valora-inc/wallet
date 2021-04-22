191186f9d05ad4f5867bef87acb12ef03575f580
Gonzalo Nardini <gnardini@clabs.co>
2021-04-22 17:32:42 -0300

    Add analytics events when navigating to rewards screen

--- packages/mobile/src/analytics/Events.tsx
+++ packages/mobile/src/analytics/Events.tsx
@@ -388,6 +388,11 @@ export enum NavigationEvents {
   navigator_not_ready = 'navigator_not_ready',
 }
 
+export enum RewardsEvents {
+  rewards_screen_opened = 'rewards_screen_opened',
+  rewards_screen_cta_pressed = 'rewards_screen_cta_pressed',
+}
+
 export type AnalyticsEventType =
   | AppEvents
   | HomeEvents
@@ -407,3 +412,4 @@ export type AnalyticsEventType =
   | NetworkEvents
   | PerformanceEvents
   | NavigationEvents
+  | RewardsEvents
