d25ec8920c9a8fbace01cacc349f40cf1dc03b1d
Gonzalo Nardini <gnardini@clabs.co>
2021-04-26 00:37:49 -0300

    Fix sending of escrow notification

--- packages/notification-service/src/invites/invites.ts
+++ packages/notification-service/src/invites/invites.ts
@@ -1,3 +1,4 @@
+import { EventLog } from 'web3-core'
 import {
   getLastInviteBlockNotified,
   sendInviteNotification,
@@ -7,27 +8,47 @@ import { getContractKit } from '../util/utils'
 
 const TAG = 'INVITES'
 
-export async function handleInvites() {
-  const kit = await getContractKit()
-  const fromBlock = getLastInviteBlockNotified() + 1
-  if (fromBlock <= 0) {
+let sendingInvites = false
+
+function updateLastInviteBlock(fromBlock: number, events: EventLog[]) {
+  if (events.length === 0) {
     return
   }
-  console.debug(TAG, `Starting to fetch invites from block ${fromBlock}`)
+  const maxBlock = events
+    .map((event) => event.blockNumber)
+    .reduce((max, current) => Math.max(max, current), fromBlock)
+  setLastInviteBlockNotified(maxBlock)
+}
 
-  const escrow = await kit.contracts.getEscrow()
-  const events = await escrow.getPastEvents('Withdrawal', {
-    fromBlock,
-  })
-  console.debug(TAG, `Got ${events.length} escrow withdrawal events`)
-  let maxBlock = fromBlock
-  for (const event of events) {
-    maxBlock = Math.max(event.blockNumber, fromBlock)
-    const inviter = event.returnValues.to.toLowerCase()
-    console.debug(TAG, `Sending invite notification to ${inviter}`)
-    await sendInviteNotification(inviter)
-  }
-  if (maxBlock > fromBlock) {
-    setLastInviteBlockNotified(maxBlock)
+export async function handleInvites() {
+  try {
+    if (sendingInvites) {
+      return
+    }
+    sendingInvites = true
+
+    const kit = await getContractKit()
+    const fromBlock = getLastInviteBlockNotified() + 1
+    if (fromBlock <= 0) {
+      return
+    }
+    console.debug(TAG, `Starting to fetch invites from block ${fromBlock}`)
+
+    const escrow = await kit.contracts.getEscrow()
+    const events = await escrow.getPastEvents('Withdrawal', {
+      fromBlock,
+    })
+    updateLastInviteBlock(fromBlock, events)
+    console.debug(TAG, `Got ${events.length} escrow withdrawal events`)
+
+    for (const event of events) {
+      const inviter = event.returnValues.to.toLowerCase()
+      console.debug(TAG, `Sending invite notification to ${inviter}`)
+      await sendInviteNotification(inviter)
+    }
+  } catch (error) {
+    console.error(TAG, 'Error while sending invite notifications', error)
+  } finally {
+    sendingInvites = false
   }
 }
