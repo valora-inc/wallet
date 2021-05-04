fc61c78760085ae5d9eea47c64799ef7ba0a8183
Jean Regisser <jean.regisser@gmail.com>
2021-05-04 16:20:20 +0200

    Fix incorrect analytic event for choosing restore account

---
+++ packages/mobile/src/analytics/Events.test.tsx
@@ -0,0 +1,16 @@
+import * as AnalyticsEvents from 'src/analytics/Events'
+
+describe('AnalyticsEvents', () => {
+  it('all events keys should match their values', () => {
+    expect.hasAssertions()
+    const eventsEnums = Object.keys(AnalyticsEvents)
+
+    for (const eventEnumKey of eventsEnums) {
+      const eventEnum = AnalyticsEvents[eventEnumKey]
+
+      for (const [key, value] of Object.entries(eventEnum)) {
+        expect(value).toBe(key)
+      }
+    }
+  })
+})
