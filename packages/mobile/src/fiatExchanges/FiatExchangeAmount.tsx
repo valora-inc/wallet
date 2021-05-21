ca3505d627901cba0f8c5334b22d28eeae64529e
Tarik Bellamine <tarikbellamine@protonmail.com>
2021-05-21 12:45:48 -0700

    fix cashout limit balance calculation and error dialog

--- packages/mobile/src/fiatExchanges/FiatExchangeAmount.tsx
+++ packages/mobile/src/fiatExchanges/FiatExchangeAmount.tsx
@@ -32,7 +32,6 @@ import {
 import { fetchExchangeRate } from 'src/exchange/actions'
 import { ExchangeRatePair, exchangeRatePairSelector } from 'src/exchange/reducer'
 import { CURRENCIES, CURRENCY_ENUM } from 'src/geth/consts'
-import { celoTokenBalanceSelector } from 'src/goldToken/selectors'
 import i18n, { Namespaces } from 'src/i18n'
 import { LocalCurrencyCode, LocalCurrencySymbol } from 'src/localCurrency/consts'
 import {
@@ -106,7 +105,6 @@ function FiatExchangeAmount({ route }: Props) {
   const exchangeRatePair = useSelector(exchangeRatePairSelector)
   const localCurrencyExchangeRate = useSelector(getLocalCurrencyExchangeRate)
   const cUSDBalance = useSelector(stableTokenBalanceSelector)
-  const celoBalance = useSelector(celoTokenBalanceSelector)
   const localCurrencyCode = useLocalCurrencyCode()
   const currencySymbol = LocalCurrencySymbol[localCurrencyCode]
 
@@ -117,14 +115,6 @@ function FiatExchangeAmount({ route }: Props) {
   const dollarAmount = useDollarAmount(
     currency,
     parsedInputAmount,
-    localCurrencyExchangeRate,
-    localCurrencyCode,
-    exchangeRatePair
-  )
-
-  const dollarBalance = useDollarAmount(
-    currency,
-    new BigNumber((currency === CURRENCY_ENUM.DOLLAR ? cUSDBalance : celoBalance) || 0),
     localCurrencyExchangeRate,
     localCurrencyCode,
     exchangeRatePair
@@ -170,7 +160,11 @@ function FiatExchangeAmount({ route }: Props) {
   }
 
   function onPressContinue() {
-    Logger.debug(`Input: ${dollarAmount}`)
+    if (currency === CURRENCY_ENUM.GOLD) {
+      Logger.debug('Got to FiatExchangeAmountScreen with CELO as asset - should never happen')
+      return
+    }
+
     if (route.params.isCashIn) {
       if (
         dollarAmount.isLessThan(DOLLAR_ADD_FUNDS_MIN_AMOUNT) ||
@@ -190,16 +184,18 @@ function FiatExchangeAmount({ route }: Props) {
       ValoraAnalytics.track(FiatExchangeEvents.cico_add_funds_amount_continue, {
         dollarAmount,
       })
-    } else {
-      if (dollarAmount.isGreaterThan(dollarBalance)) {
-        dispatch(
-          showError(ErrorMessages.CASH_OUT_LIMIT_EXCEEDED, ALERT_BANNER_DURATION, {
-            dollarBalance,
-            currency: currency === CURRENCY_ENUM.DOLLAR ? 'cUSD' : 'CELO',
-          })
-        )
-        return
-      }
+    } else if (dollarAmount.isGreaterThan(cUSDBalance || 0)) {
+      const localBalance =
+        cUSDBalance && localCurrencyExchangeRate
+          ? new BigNumber(cUSDBalance).times(localCurrencyExchangeRate)
+          : new BigNumber(0)
+      dispatch(
+        showError(ErrorMessages.CASH_OUT_LIMIT_EXCEEDED, ALERT_BANNER_DURATION, {
+          balance: localBalance.toFixed(2),
+          currency: localCurrencyCode,
+        })
+      )
+      return
     }
 
     ValoraAnalytics.track(FiatExchangeEvents.cico_add_funds_amount_continue, {
