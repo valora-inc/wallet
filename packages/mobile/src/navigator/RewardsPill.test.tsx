9fe88d07197de625f3f62da7c8989262d1527fa8
Isabelle Wei <isabelle@clabs.co>
2021-05-27 15:58:01 -0400

    update test

--- packages/mobile/src/navigator/RewardsPill.test.tsx
+++ packages/mobile/src/navigator/RewardsPill.test.tsx
@@ -1,17 +1,35 @@
 import * as React from 'react'
 import { fireEvent, render } from 'react-native-testing-library'
+import { Provider } from 'react-redux'
 import { navigate } from 'src/navigator/NavigationService'
 import RewardsPill from 'src/navigator/RewardsPill'
 import { Screens } from 'src/navigator/Screens'
+import { createMockStore } from 'test/utils'
 
 describe('RewardsPill', () => {
   it('renders correctly', () => {
-    const tree = render(<RewardsPill />)
+    const tree = render(
+      <Provider
+        store={createMockStore({
+          account: { rewardsEnabled: true },
+        })}
+      >
+        <RewardsPill />)
+      </Provider>
+    )
     expect(tree).toMatchSnapshot()
   })
 
   it('opens the consumer incentives screen when pressed', () => {
-    const { getByTestId } = render(<RewardsPill />)
+    const { getByTestId } = render(
+      <Provider
+        store={createMockStore({
+          account: { rewardsEnabled: true },
+        })}
+      >
+        <RewardsPill />)
+      </Provider>
+    )
     fireEvent.press(getByTestId('EarnRewards'))
     expect(navigate).toBeCalledWith(Screens.ConsumerIncentivesHomeScreen)
   })
