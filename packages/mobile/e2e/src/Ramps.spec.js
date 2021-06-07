b84d738886e1dd39d9a1ba2f7d7be40f4201de25
Tom McGuire <Mcgtom10@gmail.com>
2021-06-07 14:49:53 -0700

    Tomm/detox update (#260)
    ### Description
    
    The goal of this PR is to reduce test flake within our CICD pipeline, to setup e2e test parallelization and add additional screen comparison utilities.
    
    This PR also includes an update to the Detox Framework  from `17.13.2` to `18.14.1`
    
    Refactored / Added Tests:
    | Test Suite | Use cases |
    | ----- | ----- |
    | Account Setup | NewAccountOnboarding |
    | Account Setup | RestoreAccountOnboarding |
    | AccountManagement | Support |
    | AccountManagement | Settings |
    | AccountManagement | Reset Account |
    | Ramps | Cashin |
    
    
    ### Other changes
    
    Changes to test utilities: `enterPinUiIfNecessary`, `enterPinUi` and `isElementVisible`
    
    Added to test utilities: `pixelDiff`, `setDemoMode`, `scrollIntoView`, `quickOnboarding` and `isElementExistent`
    
    Updated `e2e/README.md` to reflect e2e test framework updates.
    
    Android Command Line tools were upgraded.
    
    ### Tested
    
    Ran locally on Android and iOS emulators - Circle Ci Results Passing
    
    Run unit tests - Circle Ci Results Passing
    
    ### Backwards compatibility
    
    This should only impact e2e tests. Snapshots have been updated for mobile tests.

---
+++ packages/mobile/e2e/src/Ramps.spec.js
@@ -0,0 +1,10 @@
+import { quickOnboarding } from './utils/utils'
+import CashIn from './usecases/CashIn'
+
+describe('Ramps', () => {
+  beforeAll(async () => {
+    await quickOnboarding()
+  })
+
+  describe('Cash In', CashIn)
+})
