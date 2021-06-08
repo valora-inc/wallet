fc146fe305f1deffdae1167c8a4b53e73c6352be
Gonzalo Nardini <nardinigonzalom@gmail.com>
2021-06-08 16:52:12 -0300

    Move payment request notifications to Cloud Function (#430)
    ### Description
    
    Moved the payment requests notification sending from the notification service to a database-triggered Cloud Function.
    
    See https://www.notion.so/clabsco/Notification-Service-Indexer-064ca4f58be64c3384ac8fb5f00fca56 for more information on the motivation for doing this.
    
    ### Other changes
    
    N/A
    
    ### Tested
    
    Manually and added unit test. This is deployed in Alfajores.
    
    ### How others should test
    
    Send a payment request to an address. See that the requested address receives a push notification.
    
    ### Related issues
    
    - Fixes #428
    
    ### Backwards compatibility
    
    N/A

--- packages/notification-service/src/polling.ts
+++ packages/notification-service/src/polling.ts
@@ -1,13 +1,11 @@
 import AsyncPolling from 'async-polling'
 import { handleTransferNotifications } from './blockscout/transfers'
 import { INVITES_POLLING_INTERVAL, POLLING_INTERVAL } from './config'
-import { handlePaymentRequests } from './handlers'
 import { handleInvites } from './invites/invites'
 
 export const notificationPolling = AsyncPolling(async (end) => {
   try {
     await handleTransferNotifications()
-    await handlePaymentRequests()
   } catch (e) {
     console.error('Notifications polling failed', e)
   } finally {
