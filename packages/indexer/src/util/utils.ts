5863e8b125e4fb838b8bed0a459fd0b79337bd7d
Silas Boyd-Wickizer <silasbw@gmail.com>
2021-06-09 08:13:05 -0700

    Remove more unused

--- packages/indexer/src/util/utils.ts
+++ packages/indexer/src/util/utils.ts
@@ -1,37 +1,6 @@
-import { CeloContract, ContractKit, newKitFromWeb3 } from '@celo/contractkit'
+import { ContractKit, newKitFromWeb3 } from '@celo/contractkit'
 import Web3 from 'web3'
 import { WEB3_PROVIDER_URL } from '../config'
-
-interface ObjectWithStringsAndUndefined {
-  [key: string]: string | undefined
-}
-
-interface ObjectWithStrings {
-  [key: string]: string
-}
-
-export function removeEmptyValuesFromObject(obj: ObjectWithStringsAndUndefined) {
-  const newObj: ObjectWithStrings = {}
-  Object.keys(obj)
-    // @ts-ignore
-    .filter((k) => obj[k] !== null && obj[k] !== undefined)
-    // @ts-ignore
-    .forEach((k) => (newObj[k] = obj[k]))
-  return newObj
-}
-
-let goldTokenAddress: string
-let stableTokenAddress: string
-export async function getTokenAddresses() {
-  if (goldTokenAddress && stableTokenAddress) {
-    return { goldTokenAddress, stableTokenAddress }
-  } else {
-    const kit = await getContractKit()
-    goldTokenAddress = await kit.registry.addressFor(CeloContract.GoldToken)
-    stableTokenAddress = await kit.registry.addressFor(CeloContract.StableToken)
-    return { goldTokenAddress, stableTokenAddress }
-  }
-}
 
 let contractKit: ContractKit
 export async function getContractKit(): Promise<ContractKit> {
@@ -44,10 +13,4 @@ export async function getContractKit(): Promise<ContractKit> {
     contractKit = newKitFromWeb3(web3)
     return contractKit
   }
-}
-
-// Flat single level array
-// TODO: remove this once we upgrade to node >= 11 which natively supports Array.prototype.flat
-export function flat<T>(arr: T[][]): T[] {
-  return ([] as T[]).concat(...arr)
 }
