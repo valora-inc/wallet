274687aadbc42cb7b3792df18d59dad0829adf5c
MuckT <mcgtom10@gmail.com>
2021-06-11 19:20:19 -0700

    test: increase send test timeout

--- packages/mobile/e2e/src/usecases/Send.js
+++ packages/mobile/e2e/src/usecases/Send.js
@@ -59,7 +59,7 @@ export default Send = () => {
     // Look for the latest transaction and assert
     await waitFor(element(by.text(`${randomContent}`)))
       .toBeVisible()
-      .withTimeout(45000)
+      .withTimeout(60000)
   })
 
   // TODO(tomm): debug why error is thrown in e2e tests
