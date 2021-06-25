3edf85698e61f648885a69051ff7b088c0274e1e
Kara (Luna) <luna.graysen@valoraapp.com>
2021-06-25 11:05:39 -0400

    Disable Ramps test suite for right now

--- packages/mobile/e2e/src/Ramps.spec.js
+++ packages/mobile/e2e/src/Ramps.spec.js
@@ -1,10 +1,10 @@
 import { quickOnboarding } from './utils/utils'
 import CashIn from './usecases/CashIn'
 
-describe('Ramps', () => {
-  beforeAll(async () => {
-    await quickOnboarding()
-  })
+// describe('Ramps', () => {
+//   beforeAll(async () => {
+//     await quickOnboarding()
+//   })
 
-  describe('Cash In', CashIn)
-})
+//   describe('Cash In', CashIn)
+// })
