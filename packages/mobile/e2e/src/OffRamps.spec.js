8cca1ce04b926e2d251a4dfabd7238f8ba076370
Tom McGuire <Mcgtom10@gmail.com>
2021-06-29 01:36:39 -0700

    Tomm/e2e ramps (#451)
    ### Description
    
    Increases the automated coverage of our on and off ramps in the Valora. Tests focus on in app behavior along with checking integration with 3rd party providers. PNG snapshots are used to confirm that 3rd navigating away from the app behaves as expected.
    
    ### Other changes
    
    - [x] Updates Jest Snapshots with new testID's.
    - [x] Add associated png snapshots for Android and iOS.
    
    ### Tested
    
    - [x] All tests running locally on iOS and Android devices matching Circle Ci Configurations
    - [x] Run in Circle Ci 
    - [x] Individual specs run locally
    
    ### How others should test
    
    * Navigate to the `wallet/packages/mobiles` directory.
    
    * All Tests can be run
    
      ```sh
      # Using Multiple Emulated Devices
      yarn test:e2e:ios -w 4
      
      # Runs One Emulated Device
      yarn test:android -w  1
      ```
    
    * A Specific spec.js can be run 
    
      ```sh
      yarn test:e2e:ios -w 1 -f OnRamps.spec.js
      ```
    
    * Rerunning with out rebuilding
    
      ```sh
      # Both 'OnRamps.spec.js' and 'OffRamps.spec.js' will run
      yarn test:e2e:ios -w 2 -d -f Ramps.spec.js
      ```
    
    This does not need to be tested by QA, but monitoring build times prior to merge to main will be important as the asset folder grows.
    
    ### Backwards compatibility
    
    This PR can be merged alone and also contains all of the contents of PR #260.

--- packages/mobile/e2e/src/OffRamps.spec.js
+++ packages/mobile/e2e/src/OffRamps.spec.js
@@ -1,10 +1,10 @@
+import offRamps from './usecases/OffRamps'
 import { quickOnboarding } from './utils/utils'
-import CashIn from './usecases/CashIn'
 
 describe('Ramps', () => {
   beforeAll(async () => {
     await quickOnboarding()
   })
 
-  describe('Cash In', CashIn)
+  describe('Given Cash Out', offRamps)
 })
---
+++ packages/mobile/e2e/src/usecases/OffRamps.js
@@ -0,0 +1,112 @@
+import { dismissBanners } from '../utils/banners'
+import { pixelDiff, sleep, enterPinUiIfNecessary, getDeviceModel } from '../utils/utils'
+import { DEFAULT_RECIPIENT_ADDRESS } from '../utils/consts'
+
+export default offRamps = () => {
+  beforeEach(async () => {
+    await device.reloadReactNative()
+    await dismissBanners()
+    await element(by.id('Hamburger')).tap()
+    await element(by.id('add-and-withdraw')).tap()
+    await element(by.id('cashOut')).tap()
+  })
+
+  describe('cUSD', () => {
+    beforeEach(async () => {
+      await element(by.id('radio/cUSD')).tap()
+    })
+
+    describe('When Bank Account Selected', () => {
+      beforeEach(async () => {
+        await element(by.id('receiveWithBank')).tap()
+        await element(by.text('Next')).tap()
+      })
+
+      it('Then Should Be Able To Navigate To Providers', async () => {
+        await element(by.id('FiatExchangeInput')).replaceText('2')
+        await element(by.id('FiatExchangeNextButton')).tap()
+        await expect(element(by.id('Provider/Xanpool'))).toBeVisible()
+        await expect(element(by.id('Icon/Xanpool'))).toExist()
+        const imagePath = await device.takeScreenshot('cUSD Out Providers')
+        await pixelDiff(imagePath, `./e2e/assets/${await getDeviceModel()}/cUSD Out Providers.png`)
+      })
+    })
+
+    describe.skip('When Gift Cards and Mobile Top Up Selected', () => {
+      beforeEach(async () => {
+        await element(by.id('receiveWithBidali')).tap()
+        await element(by.text('Next')).tap()
+      })
+
+      it('Then Bidali Should Display', async () => {
+        await expect(element(by.text('Bidali'))).toBeVisible()
+        // TODO: Include Check of Screenshot in Nightly Tests
+        // await sleep(15000)
+        // const imagePath = await device.takeScreenshot('Bidali')
+        // await pixelDiff(imagePath, `./e2e/assets/${await getDeviceModel()}/Bidali.png`)
+      })
+    })
+
+    describe('When Cryptocurrency Exchanges Selected', () => {
+      beforeEach(async () => {
+        await element(by.id('withExchange')).tap()
+        await element(by.id('GoToProviderButton')).tap()
+      })
+
+      it('Then Should Display Exchanges & Account Key', async () => {
+        await expect(element(by.id('Bittrex'))).toBeVisible()
+        await expect(element(by.id('CoinList Pro'))).toBeVisible()
+        await expect(element(by.id('OKCoin'))).toBeVisible()
+        await expect(element(by.id('accountBox'))).toBeVisible()
+        const imagePath = await device.takeScreenshot('cUSD Exchanges')
+        await pixelDiff(imagePath, `./e2e/assets/${await getDeviceModel()}/cUSD Exchanges.png`)
+      })
+    })
+  })
+
+  describe('CELO', () => {
+    beforeEach(async () => {
+      await element(by.id('radio/CELO')).tap()
+    })
+
+    describe('When Address Selected', () => {
+      const randomAmount = `${Math.random().toFixed(3)}`
+
+      beforeEach(async () => {
+        await element(by.id('receiveOnAddress')).tap()
+        await element(by.text('Next')).tap()
+        await element(by.id('AccountAddress')).replaceText(DEFAULT_RECIPIENT_ADDRESS)
+        await element(by.id('CeloAmount')).replaceText(randomAmount)
+        //TODO: Investigate why sleep is needed
+        await sleep(1000)
+        await element(by.id('WithdrawReviewButton')).tap()
+      })
+
+      it('Then Should Be Able To Send To Address', async () => {
+        // console.log('randomAmount: ', `-${randomAmount} CELO`)
+        await element(by.id('ConfirmWithdrawButton')).tap()
+        await enterPinUiIfNecessary()
+        await expect(element(by.text(`-${randomAmount} CELO`))).toBeVisible()
+      })
+    })
+
+    describe('When Cryptocurrency Exchanges Selected', () => {
+      beforeEach(async () => {
+        await element(by.id('withExchange')).tap()
+        await element(by.id('GoToProviderButton')).tap()
+      })
+
+      it('Then Should Display Exchanges & Account Key', async () => {
+        await expect(element(by.id('Binance'))).toBeVisible()
+        await expect(element(by.id('Bittrex'))).toBeVisible()
+        await expect(element(by.id('Coinbase (CELO as CGLD)'))).toBeVisible()
+        await expect(element(by.id('Coinbase Pro (CELO as CGLD)'))).toBeVisible()
+        await expect(element(by.id('OKCoin'))).toBeVisible()
+        await expect(element(by.id('OKEx'))).toBeVisible()
+        await expect(element(by.id('accountBox'))).toBeVisible()
+        const imagePath = await device.takeScreenshot('CELO Exchanges')
+        await pixelDiff(imagePath, `./e2e/assets/${await getDeviceModel()}/CELO Exchanges.png`)
+      })
+    })
+  })
+}
