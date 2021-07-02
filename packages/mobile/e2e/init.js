e4583f815816b79c507941ab09c61efaa4013244
MuckT <mcgtom10@gmail.com>
2021-07-01 20:30:08 -0700

    test: install app if not present

--- packages/mobile/e2e/init.js
+++ packages/mobile/e2e/init.js
@@ -2,6 +2,8 @@ const detox = require('detox')
 import { setDemoMode } from './src/utils/utils'
 
 beforeAll(async () => {
+  // Install app if not present
+  await device.installApp()
   await device.launchApp({
     newInstance: false,
     permissions: { notifications: 'YES', contacts: 'YES' },
