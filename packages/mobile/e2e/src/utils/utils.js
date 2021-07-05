2dd349d2aaae25c543a7fe3d1aa3031d2acbc78d
MuckT <mcgtom10@gmail.com>
2021-07-05 09:20:09 -0700

    test: add sleep before wallet backup key input

--- packages/mobile/e2e/src/utils/utils.js
+++ packages/mobile/e2e/src/utils/utils.js
@@ -163,10 +163,9 @@ export async function quickOnboarding() {
     await waitFor(element(by.id('connectingToCelo')))
       .not.toBeVisible()
       .withTimeout(20000)
+
     // Input Wallet Backup Key
-    await waitFor(element(by.id('ImportWalletBackupKeyInputField')))
-      .toBeVisible()
-      .withTimeout(20000)
+    await sleep(3000)
     await element(by.id('ImportWalletBackupKeyInputField')).tap()
     await element(by.id('ImportWalletBackupKeyInputField')).replaceText(`${SAMPLE_BACKUP_KEY}`)
     if (device.getPlatform() === 'ios') {
