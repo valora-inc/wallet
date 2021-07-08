6b1175bc476b3965a6a8c5b483b7b9a58d3f19a4
MuckT <mcgtom10@gmail.com>
2021-07-08 15:28:54 -0700

    test: remove detox url black list to reduce test flake

--- packages/mobile/e2e/init.js
+++ packages/mobile/e2e/init.js
@@ -8,6 +8,7 @@ beforeAll(async () => {
     newInstance: false,
     permissions: { notifications: 'YES', contacts: 'YES' },
   })
-  await device.setURLBlacklist(['.*blockchain-api-dot-celo-mobile-alfajores.appspot.com.*'])
+  // Disabled url blacklist while test take longer this results in less flakey tests in CI
+  // await device.setURLBlacklist(['.*blockchain-api-dot-celo-mobile-alfajores.appspot.com.*'])
   await setDemoMode()
 })
