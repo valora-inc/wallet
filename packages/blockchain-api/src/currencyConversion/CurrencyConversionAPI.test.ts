valora-inc/wallet

commit fa8031f4ba4f9c70437d55431b180129ffd24a6b

Support stable tokens in blockchain-api (#259)
### Description

It allow the extension of different stable tokens. Currently is pending to determine how we are going to get the value of cEUR from firebase or another mechanism.

Pending of https://github.com/celo-org/wallet/pull/365

### Other changes

N/A

### Tested

Added

### Related issues

- Fixes https://github.com/celo-org/wallet/issues/230

### Backwards compatibility

N/A

packages/blockchain-api/src/currencyConversion/CurrencyConversionAPI.ts
@@ -1,14 +1,21 @@
 import { DataSource, DataSourceConfig } from 'apollo-datasource'
 import BigNumber from 'bignumber.js'
 import { CurrencyConversionArgs, MoneyAmount } from '../schema'
-import { CGLD, CUSD, USD } from './consts'
+import {
+  CGLD,
+  CUSD,
+  stablePairs,
+  supportedCurrencies,
+  supportedPairs,
+  supportedStableTokens,
+  USD,
+} from './consts'
 import ExchangeRateAPI from './ExchangeRateAPI'
 import GoldExchangeRateAPI from './GoldExchangeRateAPI'
 
 function insertIf<T>(condition: boolean, element: T) {
   return condition ? [element] : []
 }
-
 export default class CurrencyConversionAPI<TContext = any> extends DataSource {
   exchangeRateAPI = new ExchangeRateAPI()
   goldExchangeRateAPI = new GoldExchangeRateAPI()
@@ -24,11 +31,10 @@ export default class CurrencyConversionAPI<TContext = any> extends DataSource {
     timestamp,
     impliedExchangeRates,
   }: CurrencyConversionArgs): Promise<BigNumber> {
-    const fromCode = sourceCurrencyCode || USD
+    const fromCode = sourceCurrencyCode!
     const toCode = currencyCode
 
     const steps = this.getConversionSteps(fromCode, toCode)
-
     const ratesPromises = []
     for (let i = 1; i < steps.length; i++) {
       const prevCode = steps[i - 1]
@@ -52,26 +58,55 @@ export default class CurrencyConversionAPI<TContext = any> extends DataSource {
       // Same code, nothing to do
       return []
     } else if (fromCode === CGLD || toCode === CGLD) {
-      // cGLD -> X (where X !== cUSD)
-      if (fromCode === CGLD && toCode !== CUSD) {
+      // cGLD -> X (where X !== celoStableToken)
+      if (fromCode === CGLD && !this.enumContains(supportedStableTokens, toCode.toUpperCase())) {
+        if (this.enumContains(supportedCurrencies, toCode) && toCode !== USD) {
+          return [CGLD, this.getStableToken(toCode), toCode]
+        }
         return [CGLD, CUSD, ...insertIf(toCode !== USD, USD), toCode]
       }
-      // X -> cGLD (where X !== cUSD)
-      else if (fromCode !== CUSD && toCode === CGLD) {
+      // Currency -> cGLD (where X !== celoStableToken)
+      else if (
+        !this.enumContains(supportedStableTokens, fromCode.toUpperCase()) &&
+        toCode === CGLD
+      ) {
+        if (this.enumContains(supportedCurrencies, fromCode) && fromCode !== USD) {
+          return [fromCode, this.getStableToken(fromCode), CGLD]
+        }
         return [fromCode, ...insertIf(fromCode !== USD, USD), CUSD, CGLD]
       }
     } else {
-      // cUSD -> X (where X !== USD)
-      if (fromCode === CUSD && toCode !== USD) {
-        return [CUSD, USD, toCode]
+      // celoStableToken -> X (where X!== currency)
+      if (
+        this.enumContains(supportedStableTokens, fromCode.toUpperCase()) &&
+        this.getCurrency(fromCode) !== toCode
+      ) {
+        if (this.enumContains(supportedStableTokens, toCode.toUpperCase())) {
+          return [fromCode, this.getCurrency(fromCode), this.getCurrency(toCode), toCode]
+        }
+        return [fromCode, this.getCurrency(fromCode), toCode]
       }
-      // X -> cUSD (where X !== USD)
-      else if (fromCode !== USD && toCode === CUSD) {
-        return [fromCode, USD, CUSD]
+      // currency -> X (where X!== celoStableToken)
+      else if (
+        this.getCurrency(toCode) !== fromCode &&
+        this.enumContains(supportedStableTokens, toCode.toUpperCase())
+      ) {
+        return [fromCode, this.getCurrency(toCode), toCode]
       }
     }
-
     return [fromCode, toCode]
+  }
+
+  private enumContains(x: any, code: string) {
+    return Object.values(x).includes(code)
+  }
+
+  private getStableToken(code: string) {
+    return 'c' + code
+  }
+
+  private getCurrency(code: string) {
+    return code.substring(1)
   }
 
   private getSupportedExchangeRate(
@@ -81,15 +116,13 @@ export default class CurrencyConversionAPI<TContext = any> extends DataSource {
     impliedExchangeRates?: MoneyAmount['impliedExchangeRates']
   ): BigNumber | Promise<BigNumber> {
     const pair = `${fromCode}/${toCode}`
-
     if (impliedExchangeRates && impliedExchangeRates[pair]) {
       return new BigNumber(impliedExchangeRates[pair])
     }
-
-    if (pair === 'cUSD/USD' || pair === 'USD/cUSD') {
+    if (this.enumContains(stablePairs, pair)) {
       // TODO: use real rates once we have the data
       return new BigNumber(1)
-    } else if (pair === 'cGLD/cUSD' || pair === 'cUSD/cGLD') {
+    } else if (this.enumContains(supportedPairs, pair)) {
       return this.goldExchangeRateAPI.getExchangeRate({
         sourceCurrencyCode: fromCode,
         currencyCode: toCode,

packages/blockchain-api/src/currencyConversion/CurrencyConversionAPI.test.ts
@@ -138,4 +138,27 @@ describe('CurrencyConversionAPI', () => {
     expect(mockDefaultGetExchangeRate).toHaveBeenCalledTimes(0)
     expect(mockGoldGetExchangeRate).toHaveBeenCalledTimes(0)
   })
+
+  it('should retrieve rate for cGLD/EUR', async () => {
+    const impliedExchangeRates = { 'cGLD/cEUR': new BigNumber(10) }
+    const result = await currencyConversionAPI.getExchangeRate({
+      sourceCurrencyCode: 'cGLD',
+      currencyCode: 'EUR',
+      impliedExchangeRates,
+    })
+    expect(result).toEqual(new BigNumber(10))
+
+    expect(mockDefaultGetExchangeRate).toHaveBeenCalledTimes(0)
+    expect(mockGoldGetExchangeRate).toHaveBeenCalledTimes(0)
+  })
+
+  it('should retrieve rate for EUR/cGLD', async () => {
+    const result = await currencyConversionAPI.getExchangeRate({
+      sourceCurrencyCode: 'EUR',
+      currencyCode: 'cGLD',
+    })
+    expect(result).toEqual(new BigNumber(10))
+    expect(mockDefaultGetExchangeRate).toHaveBeenCalledTimes(0)
+    expect(mockGoldGetExchangeRate).toHaveBeenCalledTimes(1)
+  })
 })

packages/blockchain-api/src/blockscout.ts
@@ -1,6 +1,7 @@
 import { RESTDataSource } from 'apollo-datasource-rest'
 import { performance } from 'perf_hooks'
 import { BLOCKSCOUT_API, FAUCET_ADDRESS } from './config'
+import { CGLD, CUSD } from './currencyConversion/consts'
 import {
   Any,
   ContractCall,
@@ -149,12 +150,13 @@ export class BlockscoutAPI extends RESTDataSource {
 
   async getTokenTransactions(args: TokenTransactionArgs) {
     const userAddress = args.address.toLowerCase()
-    const token = args.token
+    const { token, tokens: receivedTokens } = args
     const rawTransactions = await this.getRawTokenTransactions(userAddress)
-
+    // cUSD/cGLD is the default for legacy reasons. Can be removed once most users updated to Valora >= 1.16
+    const tokens = receivedTokens ?? (token ? [token!] : [CUSD, CGLD])
     const context = {
       userAddress,
-      token,
+      tokens,
     }
 
     const transactionClassifier = new TransactionClassifier([
@@ -194,7 +196,7 @@ export class BlockscoutAPI extends RESTDataSource {
 
     return events
       .filter((e) => e)
-      .filter((event) => (token ? event.amount.currencyCode === token : true))
+      .filter((event) => tokens.includes(event.amount.currencyCode))
       .sort((a, b) => b.timestamp - a.timestamp)
   }
 }

packages/blockchain-api/src/currencyConversion/consts.ts
@@ -2,4 +2,26 @@ import { CURRENCIES, CURRENCY_ENUM } from '@celo/utils'
 
 export const CGLD = CURRENCIES[CURRENCY_ENUM.GOLD].code
 export const CUSD = CURRENCIES[CURRENCY_ENUM.DOLLAR].code
+export const CEUR = CURRENCIES[CURRENCY_ENUM.EURO].code
+export const EUR = 'EUR'
 export const USD = 'USD'
+export enum supportedStableTokens {
+  CEUR,
+  CUSD,
+}
+export enum supportedCurrencies {
+  EUR,
+  USD,
+}
+export enum supportedPairs {
+  'cGLD/cUSD',
+  'cUSD/cGLD',
+  'cGLD/cEUR',
+  'cEUR/cGLD',
+}
+export enum stablePairs {
+  'cUSD/USD',
+  'USD/cUSD',
+  'EUR/cEUR',
+  'cEUR/EUR',
+}

packages/blockchain-api/src/events/ExchangeCeloToToken.ts
@@ -1,4 +1,3 @@
-import { CUSD } from '../currencyConversion/consts'
 import { EventBuilder } from '../helpers/EventBuilder'
 import { Transaction } from '../transaction/Transaction'
 import { TransactionType } from '../transaction/TransactionType'
@@ -29,9 +28,7 @@ export class ExchangeCeloToToken extends TransactionType {
       transaction,
       inTransfer,
       outTransfer,
-      // TODO: this will need to be updated once cEUR is introduced
-      // see https://github.com/celo-org/wallet/pull/90#discussion_r587308620
-      this.context.token || CUSD,
+      this.context.tokens,
       transaction.fees
     )
   }

packages/blockchain-api/src/events/ExchangeTokenToCelo.ts
@@ -1,4 +1,3 @@
-import { CUSD } from '../currencyConversion/consts'
 import { EventBuilder } from '../helpers/EventBuilder'
 import { Transaction } from '../transaction/Transaction'
 import { TransactionType } from '../transaction/TransactionType'
@@ -30,7 +29,7 @@ export class ExchangeTokenToCelo extends TransactionType {
       transaction,
       inTransfer,
       outTransfer,
-      this.context.token || CUSD,
+      this.context.tokens,
       transaction.fees
     )
   }

packages/blockchain-api/src/helpers/EventBuilder.ts
@@ -42,13 +42,21 @@ export class EventBuilder {
     }
   }
 
+  static chooseTokenToShowInExchange(possibleTokens: string[]) {
+    if (possibleTokens.length === 1) {
+      return possibleTokens[0]
+    }
+    return possibleTokens.filter((token) => token !== CGLD)[0]
+  }
+
   static exchangeEvent(
     transaction: Transaction,
     inTransfer: BlockscoutCeloTransfer,
     outTransfer: BlockscoutCeloTransfer,
-    token: string,
+    tokens: string[],
     fees?: Fee[]
   ) {
+    const token = this.chooseTokenToShowInExchange(tokens)
     const hash = transaction.transactionHash
     const block = transaction.blockNumber
     const timestamp = transaction.timestamp
@@ -58,7 +66,6 @@ export class EventBuilder {
     if (!tokenTransfer) {
       return undefined
     }
-
     const impliedExchangeRates: MoneyAmount['impliedExchangeRates'] = {}
     if (inTransfer!.token === CGLD && outTransfer!.token === CUSD) {
       impliedExchangeRates['cGLD/cUSD'] = new BigNumber(outTransfer!.value).dividedBy(

packages/blockchain-api/src/schema.ts
@@ -1,6 +1,7 @@
 import { gql } from 'apollo-server-express'
 import BigNumber from 'bignumber.js'
 import { DataSources } from './apolloServer'
+import { USD } from './currencyConversion/consts'
 
 export enum EventTypes {
   EXCHANGE = 'EXCHANGE',
@@ -62,11 +63,12 @@ export interface EventArgs {
   offset?: number
 }
 
-export type Token = 'cUSD' | 'cGLD'
+export type Token = 'cUSD' | 'cGLD' | 'cEUR'
 
 export interface TokenTransactionArgs {
   address: string
   token: Token | null
+  tokens?: Token[]
   localCurrencyCode: string
 }
 
@@ -114,6 +116,7 @@ export const typeDefs = gql`
 
   enum Token {
     cUSD
+    cEUR
     cGLD
   }
 
@@ -212,6 +215,7 @@ export const typeDefs = gql`
     tokenTransactions(
       address: Address!
       token: Token
+      tokens: [Token]
       localCurrencyCode: String
       # pagination
       before: String
@@ -258,7 +262,12 @@ export const resolvers = {
       args: CurrencyConversionArgs,
       { dataSources }: Context
     ) => {
-      const rate = await dataSources.currencyConversionAPI.getExchangeRate(args)
+      const rate = await dataSources.currencyConversionAPI.getExchangeRate({
+        ...args,
+        // This field is optional for legacy reasons. Remove default value after Valora 1.16 is
+        // released and most users update.
+        sourceCurrencyCode: args.sourceCurrencyCode ?? USD,
+      })
       return { rate: rate.toNumber() }
     },
   },

commit 42dc8962faf1cad60efe0a422235012ec759f855

[BlockchainApi] Fix feed error when local currency is EUR (#853)
### Description

Fixed regression introduced in #259 causing Valora clients to get an error loading their transaction feed when the local currency is set to `EUR` and they have `CELO` transactions in their history that were done before May.

Specifically, when converting from `EUR` to `CELO` (or vice versa), it tried to use the `cEUR/cGLD` (or `cGLD/cEUR`) rate we store in firebase.
However we only have data for these pairs since May or so, and hence can't use that for `EUR` to `CELO` prior to that date.

The fix implemented here is simple. An improvement would be to check the requested timestamp to determine whether we can use the `cEUR/cGLD` (or `cGLD/cEUR`) rate or fallback to the current fix.

### Tested

Tested on Alfajores.

### How others should test

Have some `CELO` transactions in your feed created before May and local currency set to `EUR`, check that the feeds load properly.

### Related issues

[Thread on Slack](https://valora-app.slack.com/archives/CL7BVQPHB/p1625690953274500)

### Backwards compatibility

Yes

packages/blockchain-api/src/currencyConversion/CurrencyConversionAPI.ts
@@ -1,15 +1,7 @@
 import { DataSource, DataSourceConfig } from 'apollo-datasource'
 import BigNumber from 'bignumber.js'
 import { CurrencyConversionArgs, MoneyAmount } from '../schema'
-import {
-  CGLD,
-  CUSD,
-  stablePairs,
-  supportedCurrencies,
-  supportedPairs,
-  supportedStableTokens,
-  USD,
-} from './consts'
+import { CGLD, CUSD, stablePairs, supportedPairs, supportedStableTokens, USD } from './consts'
 import ExchangeRateAPI from './ExchangeRateAPI'
 import GoldExchangeRateAPI from './GoldExchangeRateAPI'
 
@@ -60,9 +52,8 @@ export default class CurrencyConversionAPI<TContext = any> extends DataSource {
     } else if (fromCode === CGLD || toCode === CGLD) {
       // cGLD -> X (where X !== celoStableToken)
       if (fromCode === CGLD && !this.enumContains(supportedStableTokens, toCode.toUpperCase())) {
-        if (this.enumContains(supportedCurrencies, toCode) && toCode !== USD) {
-          return [CGLD, this.getStableToken(toCode), toCode]
-        }
+        // TODO, we could optimize this and use the cGLD/cEUR rate for instance
+        // but it would only be supported from the date when we started storing it
         return [CGLD, CUSD, ...insertIf(toCode !== USD, USD), toCode]
       }
       // Currency -> cGLD (where X !== celoStableToken)
@@ -70,9 +61,8 @@ export default class CurrencyConversionAPI<TContext = any> extends DataSource {
         !this.enumContains(supportedStableTokens, fromCode.toUpperCase()) &&
         toCode === CGLD
       ) {
-        if (this.enumContains(supportedCurrencies, fromCode) && fromCode !== USD) {
-          return [fromCode, this.getStableToken(fromCode), CGLD]
-        }
+        // TODO, we could optimize this and use the cEUR/cGLD rate for instance
+        // but it would only be supported from the date when we started storing it
         return [fromCode, ...insertIf(fromCode !== USD, USD), CUSD, CGLD]
       }
     } else {
@@ -99,10 +89,6 @@ export default class CurrencyConversionAPI<TContext = any> extends DataSource {
 
   private enumContains(x: any, code: string) {
     return Object.values(x).includes(code)
-  }
-
-  private getStableToken(code: string) {
-    return 'c' + code
   }
 
   private getCurrency(code: string) {

packages/blockchain-api/src/currencyConversion/CurrencyConversionAPI.test.ts
@@ -140,16 +140,20 @@ describe('CurrencyConversionAPI', () => {
   })
 
   it('should retrieve rate for cGLD/EUR', async () => {
-    const impliedExchangeRates = { 'cGLD/cEUR': new BigNumber(10) }
     const result = await currencyConversionAPI.getExchangeRate({
       sourceCurrencyCode: 'cGLD',
       currencyCode: 'EUR',
-      impliedExchangeRates,
     })
-    expect(result).toEqual(new BigNumber(10))
+    expect(result).toEqual(new BigNumber(200))
 
-    expect(mockDefaultGetExchangeRate).toHaveBeenCalledTimes(0)
-    expect(mockGoldGetExchangeRate).toHaveBeenCalledTimes(0)
+    expect(mockGoldGetExchangeRate).toHaveBeenCalledWith({
+      sourceCurrencyCode: 'cGLD',
+      currencyCode: 'cUSD',
+    })
+    expect(mockDefaultGetExchangeRate).toHaveBeenCalledWith({
+      sourceCurrencyCode: 'USD',
+      currencyCode: 'EUR',
+    })
   })
 
   it('should retrieve rate for EUR/cGLD', async () => {
@@ -157,8 +161,15 @@ describe('CurrencyConversionAPI', () => {
       sourceCurrencyCode: 'EUR',
       currencyCode: 'cGLD',
     })
-    expect(result).toEqual(new BigNumber(10))
-    expect(mockDefaultGetExchangeRate).toHaveBeenCalledTimes(0)
-    expect(mockGoldGetExchangeRate).toHaveBeenCalledTimes(1)
+    expect(result).toEqual(new BigNumber(200))
+
+    expect(mockDefaultGetExchangeRate).toHaveBeenCalledWith({
+      sourceCurrencyCode: 'EUR',
+      currencyCode: 'USD',
+    })
+    expect(mockGoldGetExchangeRate).toHaveBeenCalledWith({
+      sourceCurrencyCode: 'cUSD',
+      currencyCode: 'cGLD',
+    })
   })
 })