01bb40bfdc87d9db1f42c2699e3af643a64a2837
MuckT <mcgtom10@gmail.com>
2021-07-13 15:49:38 -0700

    test: force app reinstall on android

--- packages/mobile/e2e/init.js
+++ packages/mobile/e2e/init.js
@@ -3,6 +3,10 @@ import { setDemoMode } from './src/utils/utils'
 import { launchApp } from './src/utils/retries'
 
 beforeAll(async () => {
+  // This is a hack to ensure android app state stays correct. Causes the app to reinstall for every spec
+  if (device.getPlatform() === 'android') {
+    await device.uninstallApp()
+  }
   // Install app if not present
   await device.installApp()
   await launchApp({
