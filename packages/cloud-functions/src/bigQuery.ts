b6ba55bbcdea13750b836cab17d8274f126aaf9f
Tarik Bellamine <tarikbellamine@protonmail.com>
2021-07-26 23:02:40 +0100

    remove extra console.error

--- packages/cloud-functions/src/bigQuery.ts
+++ packages/cloud-functions/src/bigQuery.ts
@@ -15,7 +15,6 @@ export const trackEvent = async (table: string, row: any) => {
 
     await bigQuery.dataset(bigQueryDataset).table(table).insert(row)
   } catch (error) {
-    console.error(error)
     console.error(`BigQuery error:`, JSON.stringify(error))
     throw error
   }
