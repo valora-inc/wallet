ea3b13bb383f97034d7be173d3a9b373ea700167
Victor Graf <victor@clabs.co>
2021-08-26 16:13:49 -0700

    change e2e test PIN

--- packages/mobile/e2e/src/utils/consts.js
+++ packages/mobile/e2e/src/utils/consts.js
@@ -1,5 +1,5 @@
 export const DEFAULT_RECIPIENT_ADDRESS = '0x22c8a9178841ba95a944afd1a1faae517d3f5daa'
 export const SAMPLE_BACKUP_KEY =
   'general debate dial flock want basket local machine effort monitor stomach purity attend brand extend salon obscure soul open floor useful like cause exhaust'
-export const DEFAULT_PIN = '364141' // Last 6 hexidecimal values of the secp256k1 group order https://en.bitcoin.it/wiki/Secp256k1
+export const DEFAULT_PIN = '323122'
 export const EXAMPLE_NAME = 'Test Name'
