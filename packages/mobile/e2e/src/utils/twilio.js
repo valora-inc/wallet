53806459357128c5b355b921e1a784517bd79de4
MuckT <mcgtom10@gmail.com>
2021-09-01 19:01:52 -0700

    test: use try catch, comment out console log and adjust balance check

--- packages/mobile/e2e/src/utils/twilio.js
+++ packages/mobile/e2e/src/utils/twilio.js
@@ -9,23 +9,27 @@ export const receiveSms = async (
   secondsAfter = 3 * 60 * 1000,
   existingCodes = []
 ) => {
-  let client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-  let tryNumber = 0
+  try {
+    let client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+    let tryNumber = 0
 
-  while (tryNumber < MAX_TRIES) {
-    const messages = await client.messages.list({
-      dateSentAfter: new Date(Date.now() - secondsAfter),
-      limit: 3,
-    })
-    const codes = messages.map((message) => message.body.split(': ')[1])
-    console.log('Codes received:', codes)
-    if (codes.filter((code) => !existingCodes.includes(code)).length >= numCodes) {
-      return codes
+    while (tryNumber < MAX_TRIES) {
+      const messages = await client.messages.list({
+        dateSentAfter: new Date(Date.now() - secondsAfter),
+        limit: 3,
+      })
+      const codes = messages.map((message) => message.body.split(': ')[1])
+      // console.log('Codes received:', codes)
+      if (codes.filter((code) => !existingCodes.includes(code)).length >= numCodes) {
+        return codes
+      }
+      tryNumber += 1
+      await sleep(1000)
     }
-    tryNumber += 1
-    await sleep(1000)
+    return []
+  } catch (error) {
+    console.log('Error with Twilio SMS', error)
   }
-  return []
 }
 
 export const checkBalance = async () => {
@@ -34,11 +38,7 @@ export const checkBalance = async () => {
     const twilioBalance = await client.balance.fetch()
     console.log(`Twilio Balance is ${twilioBalance.balance} ${twilioBalance.currency}`)
     // Convert balance to number and check
-    if (+twilioBalance.balance > 0.0675) {
-      return true
-    } else {
-      return false
-    }
+    return +twilioBalance.balance > 0.0675
   } catch (error) {
     console.log('Error fetching Twilio Credit Balance', error)
   }
