7e59fcb1ccd17df215437529b3b2ce8b616b7935
MuckT <mcgtom10@gmail.com>
2021-09-07 14:43:36 -0700

    test: adjust e2e consts

--- packages/mobile/e2e/src/utils/consts.js
+++ packages/mobile/e2e/src/utils/consts.js
@@ -1,8 +1,6 @@
-import { VERIFICATION_PHONE_NUMBER } from '@env'
-
 export const DEFAULT_RECIPIENT_ADDRESS = '0x22c8a9178841ba95a944afd1a1faae517d3f5daa'
 export const SAMPLE_BACKUP_KEY =
   'general debate dial flock want basket local machine effort monitor stomach purity attend brand extend salon obscure soul open floor useful like cause exhaust'
 export const DEFAULT_PIN = '112233'
 export const EXAMPLE_NAME = 'Test Name'
-export const EXAMPLE_PHONE_NUMBER = VERIFICATION_PHONE_NUMBER || '+1 306-555-5555'
+export const EXAMPLE_PHONE_NUMBER = '+1 306-555-5555'
