2f985f6c0b22789a4e802a3d908fdc1820f59904
Jean Regisser <jean.regisser@gmail.com>
2021-09-17 12:18:42 +0200

    Fix release bundle error with numeric separator

--- packages/mobile/babel.config.js
+++ packages/mobile/babel.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   plugins: [
     [require('@babel/plugin-proposal-decorators').default, { legacy: true }],
+    // Remove @babel/plugin-proposal-numeric-separator when we upgrade metro with the upstream fix (https://github.com/facebook/metro/pull/681)
+    '@babel/plugin-proposal-numeric-separator',
     'react-native-reanimated/plugin',
     // NOTE: Reanimated plugin has to be listed last.
   ],
