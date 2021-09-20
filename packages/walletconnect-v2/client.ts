fa119f7bae74f6c23c4f09f06624065745beeded
Jean Regisser <jean.regisser@gmail.com>
2021-09-20 15:11:28 +0200

    Clean up

--- packages/walletconnect-v2/client.ts
+++ packages/walletconnect-v2/client.ts
@@ -1,7 +1,2 @@
-// import WalletConnectClient from '@walletconnect/client'
-// import { IClientMeta } from '@walletconnect/types'
-
-// export default WalletConnectClient
-
 export * from '@walletconnect/client'
 export { default } from '@walletconnect/client'
