d5ca77829d5b2548f2e347ca5b9bef2996a6b4b9
Lisa Li <lisali@Lisas-MacBook-Pro.local>
2021-09-23 10:02:33 -0700

    resolve build err

--- packages/mobile/src/import/ImportWallet.test.tsx
+++ packages/mobile/src/import/ImportWallet.test.tsx
@@ -3,9 +3,9 @@ import * as React from 'react'
 import 'react-native'
 import { fireEvent, render } from 'react-native-testing-library'
 import { Provider } from 'react-redux'
-import ImportWallet, { ImportWallet as ImportWalletClass } from 'src/import/ImportWallet'
+import ImportWallet from 'src/import/ImportWallet'
 import { Screens } from 'src/navigator/Screens'
-import { createMockStore, getMockI18nProps, getMockStackScreenProps } from 'test/utils'
+import { createMockStore, getMockStackScreenProps } from 'test/utils'
 import { mockMnemonic } from 'test/values'
 
 jest.mock('src/geth/GethAwareButton', () => {
@@ -31,16 +31,7 @@ describe('ImportWallet', () => {
 
     const wrapper = render(
       <Provider store={createMockStore()}>
-        <ImportWalletClass
-          importBackupPhrase={importFn}
-          hideAlert={jest.fn()}
-          isImportingWallet={false}
-          connected={true}
-          isRecoveringFromStoreWipe={false}
-          accountToRecoverFromStoreWipe={undefined}
-          {...mockScreenProps}
-          {...getMockI18nProps()}
-        />
+        <ImportWallet {...mockScreenProps} />
       </Provider>
     )
 
