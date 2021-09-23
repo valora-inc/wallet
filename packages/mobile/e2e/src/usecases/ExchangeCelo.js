820a967c9ef5399699548c1028de485b2a189fd9
MuckT <mcgtom10@gmail.com>
2021-09-23 12:00:49 -0700

    test: assert of amount and time instead of balance

--- packages/mobile/e2e/src/usecases/ExchangeCelo.js
+++ packages/mobile/e2e/src/usecases/ExchangeCelo.js
@@ -1,3 +1,4 @@
+import { format } from 'date-fns'
 import {
   enterPinUiIfNecessary,
   isElementVisible,
@@ -9,10 +10,10 @@ import { celoEducation } from '../utils/celoEducation'
 import { dismissBanners } from '../utils/banners'
 import { reloadReactNative } from '../utils/retries'
 
-const jestExpect = require('expect')
-const CELO_TO_SELL = 0.002
-const CELO_TO_BUY = 0.045
-const CELO_TO_WITHDRAW = 0.02
+const CELO_TO_SELL = 0.045
+const CELO_TO_BUY = 0.025
+const CELO_TO_WITHDRAW = 0.015
+const CELO_TO_SELL_MIN = 0.002
 const FEES = 0.001
 
 export default ExchangeCelo = () => {
@@ -28,14 +29,12 @@ export default ExchangeCelo = () => {
     await celoEducation()
   })
 
-  jest.retryTimes(4)
+  jest.retryTimes(2)
   it('Then Buy CELO', async () => {
     // Wait for buy button
     await waitFor(element(by.text('Buy')))
       .toBeVisible()
       .withTimeout(5000)
-    // Get starting balance
-    let celoBalanceBefore = await element(by.id('CeloBalance')).getAttributes()
     // Tap Buy
     await element(by.text('Buy')).tap()
     // Fill in the amount
@@ -54,33 +53,28 @@ export default ExchangeCelo = () => {
     await element(by.id('ConfirmExchange')).tap()
     // Enter PIN
     await enterPinUiIfNecessary()
+    // Get transaction time
+    let transactionTime = new Date()
     // Scroll to top of feed
     await element(by.id('ExchangeScrollView')).scroll(200, 'down')
     // Wait 10 seconds checking that error banner is not visible each second
     await waitForExpectNotVisible('errorBanner')
-    // Assert balance is updated
-    let endingBalance = element(by.id('CeloBalance')).getAttributes()
-    jestExpect(+celoBalanceBefore.text + CELO_TO_BUY).toBeCloseTo(+endingBalance.text)
-    // Rounding was causing errors
-    // await waitFor(element(by.id('CeloBalance')))
-    //   .toHaveText(`${(+celoBalanceBefore.text + CELO_TO_BUY).toFixed(3)}`)
-    //   .withTimeout(10 * 1000)
-    // Assert buy amount is present - displays amount after fees
-    await expect(
-      element(
-        by.text(`${(CELO_TO_BUY - FEES).toFixed(3)}`).withAncestor(by.id('TransactionList'))
-      ).atIndex(0)
-    ).toBeVisible()
+    // Wait up to 45 second and assert the transaction correctly appears
+    await waitFor(element(by.text(`${(CELO_TO_BUY - FEES).toFixed(3)}`)).atIndex(0))
+      .toBeVisible()
+      .withTimeout(15 * 10000)
+    await waitFor(element(by.text(`${format(transactionTime, "MMM d 'at' h':'mm a")}`)).atIndex(0))
+      .toBeVisible()
+      .withTimeout(30 * 1000)
+    // TODO Check that transaction appears in home feed
   })
 
-  jest.retryTimes(4)
+  jest.retryTimes(2)
   it('Then Sell CELO', async () => {
     // Wait for buy button
     await waitFor(element(by.text('Sell')))
       .toBeVisible()
       .withTimeout(5000)
-    // Get starting balance
-    let celoBalanceBefore = await element(by.id('CeloBalance')).getAttributes()
     // Tap Sell
     await element(by.text('Sell')).tap()
     // Fill in the amount
@@ -99,28 +93,82 @@ export default ExchangeCelo = () => {
     await element(by.id('ConfirmExchange')).tap()
     // Enter PIN
     await enterPinUiIfNecessary()
+    // Get transaction time
+    let transactionTime = new Date()
     // Scroll to top of feed
     await element(by.id('ExchangeScrollView')).scroll(200, 'down')
     // Wait 10 seconds checking that error banner is not visible each second
     await waitForExpectNotVisible('errorBanner')
-    // Assert balance is updated
-    let endingBalance = element(by.id('CeloBalance')).getAttributes()
-    jestExpect(+celoBalanceBefore.text - CELO_TO_SELL).toBeCloseTo(+endingBalance.text)
-    // Rounding was causing errors
-    // await waitFor(element(by.id('CeloBalance')))
-    //   .toHaveText(`${(+celoBalanceBefore.text - CELO_TO_SELL).toFixed(3)}`)
-    //   .withTimeout(10 * 1000)
-    // Assert buy amount is present - displays amount sans fees
-    await expect(
+    // Wait up to 30 second and assert the transaction correctly appears
+    await waitFor(
       element(
         by.text(`-${CELO_TO_SELL.toFixed(3)}`).withAncestor(by.id('TransactionList'))
       ).atIndex(0)
-    ).toBeVisible()
+    )
+      .toBeVisible()
+      .withTimeout(15 * 10000)
+    await waitFor(
+      element(
+        by
+          .text(`${format(transactionTime, "MMM d 'at' h':'mm a")}`)
+          .withAncestor(by.id('TransactionList'))
+      ).atIndex(0)
+    )
+      .toBeVisible()
+      .withTimeout(15 * 1000)
+    // TODO Check that transaction appears in home feed
   })
 
-  jest.retryTimes(4)
+  jest.retryTimes(2)
+  it('Then Sell Minimum CELO', async () => {
+    // Wait for buy button
+    await waitFor(element(by.text('Sell')))
+      .toBeVisible()
+      .withTimeout(5000)
+    // Tap Sell
+    await element(by.text('Sell')).tap()
+    // Fill in the amount
+    await element(by.id('ExchangeInput')).replaceText(`${CELO_TO_SELL_MIN}`)
+    // TODO - Get first exchange rate
+    // Send return key to close keyboard if review button is obscured by keyboard
+    if (!(await isElementVisible('ExchangeReviewButton'))) {
+      await element(by.id('ExchangeInput')).typeText('\n')
+    }
+    // TODO - Get second exchange rate
+    // Sleep for 3 seconds on review
+    await sleep(3 * 1000)
+    // Tap Review
+    await element(by.id('ExchangeReviewButton')).tap()
+    // Tap Confirm
+    await element(by.id('ConfirmExchange')).tap()
+    // Enter PIN
+    await enterPinUiIfNecessary()
+    // Get transaction time
+    let transactionTime = new Date()
+    // Scroll to top of feed
+    await element(by.id('ExchangeScrollView')).scroll(200, 'down')
+    // Wait 10 seconds checking that error banner is not visible each second
+    await waitForExpectNotVisible('errorBanner')
+    // Wait up to 30 second and assert the transaction correctly appears
+    await waitFor(
+      element(by.text(`-${CELO_TO_SELL_MIN}`).withAncestor(by.id('TransactionList'))).atIndex(0)
+    )
+      .toBeVisible()
+      .withTimeout(15 * 10000)
+    await waitFor(
+      element(
+        by
+          .text(`${format(transactionTime, "MMM d 'at' h':'mm a")}`)
+          .withAncestor(by.id('TransactionList'))
+      ).atIndex(0)
+    )
+      .toBeVisible()
+      .withTimeout(15 * 1000)
+    // TODO Check that transaction appears in home feed
+  })
+
+  jest.retryTimes(2)
   it('Then Withdraw CELO', async () => {
-    let celoBalanceBefore = await element(by.id('CeloBalance')).getAttributes()
     // Scroll to the withdraw button
     await waitFor(element(by.id('WithdrawCELO')))
       .toBeVisible()
@@ -150,22 +198,29 @@ export default ExchangeCelo = () => {
     await element(by.id('ConfirmWithdrawButton')).tap()
     // Enter PIN
     await enterPinUiIfNecessary()
+    // Get transaction time
+    let transactionTime = new Date()
     // Scroll to top of feed
     await element(by.id('ExchangeScrollView')).scroll(200, 'down')
     // Wait 10 seconds checking that error banner is not visible each second
     await waitForExpectNotVisible('errorBanner')
-    // Assert balance is updated
-    let endingBalance = element(by.id('CeloBalance')).getAttributes()
-    jestExpect(+celoBalanceBefore.text - CELO_TO_WITHDRAW).toBeCloseTo(+endingBalance.text)
-    // Rounding was causing errors
-    // await waitFor(element(by.id('CeloBalance')))
-    //   .toHaveText(`${(+celoBalanceBefore.text - CELO_TO_WITHDRAW).toFixed(3)}`)
-    //   .withTimeout(10 * 1000)
-    // Assert withdrawal amount is present
-    await expect(
+    // Wait up to 30 second and assert the transaction correctly appears
+    await waitFor(
       element(
         by.text(`-${CELO_TO_WITHDRAW.toFixed(3)}`).withAncestor(by.id('TransactionList'))
       ).atIndex(0)
-    ).toBeVisible()
+    )
+      .toBeVisible()
+      .withTimeout(15 * 10000)
+    await waitFor(
+      element(
+        by
+          .text(`${format(transactionTime, "MMM d 'at' h':'mm a")}`)
+          .withAncestor(by.id('TransactionList'))
+      ).atIndex(0)
+    )
+      .toBeVisible()
+      .withTimeout(15 * 1000)
+    // TODO Check that transaction appears in home feed
   })
 }
