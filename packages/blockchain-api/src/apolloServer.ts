f81f81741853ac42e14859077a610470482ac335
Gonzalo Nardini <nardinigonzalom@gmail.com>
2021-09-29 19:51:59 -0300

    Improve Blockchain API logging

--- packages/blockchain-api/src/apolloServer.ts
+++ packages/blockchain-api/src/apolloServer.ts
@@ -1,6 +1,7 @@
 import { ApolloServer } from 'apollo-server-express'
 import { BlockscoutAPI } from './blockscout'
 import CurrencyConversionAPI from './currencyConversion/CurrencyConversionAPI'
+import { logger } from './logger'
 import { resolvers, typeDefs } from './schema'
 
 export interface DataSources {
@@ -16,5 +17,12 @@ export const server = new ApolloServer({
       blockscoutAPI: new BlockscoutAPI(),
       currencyConversionAPI: new CurrencyConversionAPI(),
     }
+  },
+  formatError: (error) => {
+    logger.error({
+      type: 'UNHANDLED_ERROR',
+      error: error?.message,
+    })
+    return error
   },
 })
