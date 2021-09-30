95fd6d4c5bc709dc32038471d8989a6b324736fe
Gonzalo Nardini <nardinigonzalom@gmail.com>
2021-09-30 12:59:47 -0300

    Reenable WalletConnect v2

--- packages/mobile/src/walletConnect/saga.ts
+++ packages/mobile/src/walletConnect/saga.ts
@@ -1,17 +1,14 @@
 import '@react-native-firebase/database'
 import '@react-native-firebase/messaging'
-import { call, put, select, spawn } from 'redux-saga/effects'
-import { showError } from 'src/alert/actions'
+import { call, select, spawn } from 'redux-saga/effects'
 import { WalletConnectPairingOrigin } from 'src/analytics/types'
-import { ErrorMessages } from 'src/app/ErrorMessages'
 import { walletConnectEnabledSelector } from 'src/app/selectors'
-import { navigateBack } from 'src/navigator/NavigationService'
 import { initialiseWalletConnectV1, walletConnectV1Saga } from 'src/walletConnect/v1/saga'
+import { initialiseWalletConnectV2, walletConnectV2Saga } from 'src/walletConnect/v2/saga'
 
 export function* walletConnectSaga() {
   yield spawn(walletConnectV1Saga)
-  // TODO: Add back once it's working
-  // yield spawn(walletConnectV2Saga)
+  yield spawn(walletConnectV2Saga)
 }
 
 export function* initialiseWalletConnect(uri: string, origin: WalletConnectPairingOrigin) {
@@ -26,10 +23,7 @@ export function* initialiseWalletConnect(uri: string, origin: WalletConnectPairi
       yield call(initialiseWalletConnectV1, uri, origin)
       break
     case '2':
-      yield put(showError(ErrorMessages.WC2_UNSUPPORTED))
-      navigateBack()
-      // TODO: Add back once it's working
-      // yield call(initialiseWalletConnectV2, uri, origin)
+      yield call(initialiseWalletConnectV2, uri, origin)
       break
     default:
       throw new Error(`Unsupported WalletConnect version '${version}'`)
