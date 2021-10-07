581908a6e9243ff6f2515b5d3e68de3d62264d9b
Jean Regisser <jean.regisser@gmail.com>
2021-10-07 18:20:46 +0200

    Add support for rich push notification on iOS

---
+++ packages/mobile/ios/NotificationService/NotificationService.m
@@ -0,0 +1,13 @@
+//
+//  NotificationService.m
+//  NotificationService
+//
+//  Created by Jean Regisser on 07/10/2021.
+//  Copyright © 2021 Facebook. All rights reserved.
+//
+
+#import "NotificationService.h"
+
+@implementation NotificationService
+
+@end
