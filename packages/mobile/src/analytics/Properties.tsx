0f80baa03f82d1d0483524b7d901fd9a772b483c
Isabelle Wei <isabelle@clabs.co>
2021-11-01 23:49:04 -0400

    PR changes

--- packages/mobile/src/analytics/Properties.tsx
+++ packages/mobile/src/analytics/Properties.tsx
@@ -125,7 +125,7 @@ interface HomeEventsProperties {
   }
   [HomeEvents.transaction_feed_item_select]: undefined
   [HomeEvents.transaction_feed_address_copy]: undefined
-  [HomeEvents.viewTokenBalances]: undefined
+  [HomeEvents.viewTokenBalances]: string
 }
 
 interface SettingsEventsProperties {
--- packages/mobile/src/home/HomeTokenBalance.tsx
+++ packages/mobile/src/home/HomeTokenBalance.tsx
@@ -2,74 +2,96 @@ import Colors from '@celo/react-components/styles/colors'
 import fontStyles from '@celo/react-components/styles/fonts'
 import variables from '@celo/react-components/styles/variables'
 import BigNumber from 'bignumber.js'
-import React from 'react'
+import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { useSelector } from 'react-redux'
 import { HomeEvents } from 'src/analytics/Events'
 import ValoraAnalytics from 'src/analytics/ValoraAnalytics'
+import Dialog from 'src/components/Dialog'
 import { Namespaces } from 'src/i18n'
 import InfoIcon from 'src/icons/InfoIcon'
 import ProgressArrow from 'src/icons/ProgressArrow'
 import { getLocalCurrencySymbol } from 'src/localCurrency/selectors'
 import { navigate } from 'src/navigator/NavigationService'
 import { Screens } from 'src/navigator/Screens'
-import { tokenBalancesSelector, totalTokenBalanceSelector } from 'src/tokens/reducer'
+import { tokensWithBalanceSelector, totalTokenBalanceSelector } from 'src/tokens/selectors'
+
+function TokenBalance() {
+  const tokenBalances = useSelector(tokensWithBalanceSelector)
+  const localCurrencySymbol = useSelector(getLocalCurrencySymbol)
+  const totalBalance = useSelector(totalTokenBalanceSelector)
+  if (tokenBalances.length == 0) {
+    return (
+      <Text style={styles.balance}>
+        {localCurrencySymbol}
+        {'0.00'}
+      </Text>
+    )
+  } else if (tokenBalances.length == 1) {
+    return (
+      <View style={styles.oneBalance}>
+        <Image source={{ uri: tokenBalances[0].imageUrl }} style={styles.tokenImg} />
+        <View style={styles.column}>
+          <Text style={styles.balance}>
+            {localCurrencySymbol}
+            {totalBalance}
+          </Text>
+          <Text style={styles.tokenBalance}>
+            {new BigNumber(tokenBalances[0].balance).toFixed(2)} {tokenBalances[0].name}
+          </Text>
+        </View>
+      </View>
+    )
+  } else {
+    return (
+      <Text style={styles.balance}>
+        {localCurrencySymbol}
+        {totalBalance}
+      </Text>
+    )
+  }
+}
 
 function HomeTokenBalance() {
   const { t } = useTranslation(Namespaces.walletFlow5)
-  const tokenBalances = useSelector(tokenBalancesSelector)
-  const totalBalance = useSelector(totalTokenBalanceSelector)
-  const localCurrencySymbol = useSelector(getLocalCurrencySymbol)
-  const balances = Object.values(tokenBalances).filter((token) =>
-    token?.balance ? new BigNumber(token?.balance).gt(0) : false
-  )
-  const multipleBalances: boolean = balances.length > 1
-  const noBalances: boolean = balances.length == 0
+  const totalBalance = useSelector(totalTokenBalanceSelector) ?? '-'
+  const tokenBalances = useSelector(tokensWithBalanceSelector)
 
   const onViewBalances = () => {
-    ValoraAnalytics.track(HomeEvents.viewTokenBalances)
+    ValoraAnalytics.track(HomeEvents.viewTokenBalances, totalBalance)
     navigate(Screens.TokenBalances)
   }
+
+  const [infoVisible, setInfoVisible] = useState(false)
 
   return (
     <View style={styles.container}>
       <View style={styles.title}>
         <View style={styles.row}>
           <Text style={styles.totalValue}>{t('totalValue')}</Text>
-          {!noBalances && <InfoIcon size={14} color={Colors.gray3} />}
+          {tokenBalances.length > 0 && (
+            <TouchableOpacity onPress={() => setInfoVisible(true)} hitSlop={variables.iconHitslop}>
+              <InfoIcon size={14} color={Colors.gray3} />
+            </TouchableOpacity>
+          )}
         </View>
-        {multipleBalances && (
+        <Dialog
+          title={t('whatTotalValue.title')}
+          isVisible={infoVisible}
+          actionText={t('whatTotalValue.dismiss')}
+          actionPress={() => setInfoVisible(false)}
+        >
+          {t('whatTotalValue.body')}
+        </Dialog>
+        {tokenBalances.length > 1 && (
           <TouchableOpacity style={styles.row} onPress={onViewBalances}>
             <Text style={styles.viewBalances}>{t('viewBalances')}</Text>
             <ProgressArrow style={styles.arrow} color={Colors.greenUI} />
           </TouchableOpacity>
         )}
       </View>
-      {multipleBalances ? (
-        <Text style={styles.balance}>
-          {localCurrencySymbol}
-          {totalBalance}
-        </Text>
-      ) : balances[0] && balances[0].balance ? (
-        <View style={styles.oneBalance}>
-          <Image source={{ uri: balances[0].imageUrl }} style={styles.tokenImg} />
-          <View style={styles.column}>
-            <Text style={styles.balance}>
-              {localCurrencySymbol}
-              {totalBalance}
-            </Text>
-            <Text style={styles.tokenBalance}>
-              {new BigNumber(balances[0].balance).toFixed(2)} {balances[0].name}
-            </Text>
-          </View>
-        </View>
-      ) : (
-        <Text style={styles.balance}>
-          {localCurrencySymbol}
-          {'0.00'}
-        </Text>
-      )}
+      <TokenBalance />
     </View>
   )
 }
