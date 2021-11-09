8b77c57e81cff50cb440ec4de438b3555c4daf91
Gonzalo Nardini <nardinigonzalom@gmail.com>
2021-11-09 14:51:37 -0300

    Add comment

--- packages/mobile/src/tokens/reducer.ts
+++ packages/mobile/src/tokens/reducer.ts
@@ -9,6 +9,8 @@ interface BaseToken {
   imageUrl: string
   name: string
   symbol: string
+  // This field is for tokens that are part of the core contracts that allow paying for fees and
+  // making transfers with a comment.
   isCoreToken?: boolean
 }
 
