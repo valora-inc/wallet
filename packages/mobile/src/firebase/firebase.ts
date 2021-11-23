7e0910ec30eda9b792b8687580d2bc42023ea040
Lisa Li <lisali@Lisas-MacBook-Pro.local>
2021-11-23 11:59:35 -0800

    comments, removed some changes that got included by rebase

--- packages/mobile/src/firebase/firebase.ts
+++ packages/mobile/src/firebase/firebase.ts
@@ -343,12 +343,17 @@ export async function cUsdDailyLimitChannel(address: string) {
 }
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 export async function kycStatusChannel(mtwAddress: string) {
   return simpleReadChannel(`inHouseLiquidity/${mtwAddress}/kycStatus`)
 =======
 export async function kycStatusChannel(address: string) {
   return simpleReadChannel(`inHouseLiquidity/${address}/kycStatus`)
 >>>>>>> fix the build
+=======
+export async function kycStatusChannel(mtwAddress: string) {
+  return simpleReadChannel(`inHouseLiquidity/${mtwAddress}/kycStatus`)
+>>>>>>> comments, removed some changes that got included by rebase
 }
 
 export function simpleReadChannel(key: string) {
