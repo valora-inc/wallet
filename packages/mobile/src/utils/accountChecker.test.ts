32296fb655b525794f59f572a1d3466ce44462d9
Jean Regisser <jean.regisser@gmail.com>
2021-11-26 11:27:39 +0100

    simplify test

--- packages/mobile/src/utils/accountChecker.test.ts
+++ packages/mobile/src/utils/accountChecker.test.ts
@@ -9,8 +9,6 @@ import { mocked } from 'ts-jest/utils'
 jest.mock('src/geth/geth')
 
 const mockedKeychain = mocked(Keychain)
-const mockedAnalytics = mocked(ValoraAnalytics)
-const mockedDeleteNodeData = mocked(deleteNodeData)
 
 beforeEach(() => {
   jest.clearAllMocks()
@@ -23,7 +21,7 @@ describe('resetStateOnInvalidStoredAccount', () => {
     const result = await resetStateOnInvalidStoredAccount(state)
 
     expect(result === state).toEqual(true)
-    expect(mockedDeleteNodeData).toHaveBeenCalledTimes(0)
+    expect(deleteNodeData).toHaveBeenCalledTimes(0)
   })
 
   it('returns the same state when given an invalid state', async () => {
@@ -31,7 +29,7 @@ describe('resetStateOnInvalidStoredAccount', () => {
     const result = await resetStateOnInvalidStoredAccount(invalidState)
 
     expect(result === invalidState).toEqual(true)
-    expect(mockedDeleteNodeData).toHaveBeenCalledTimes(0)
+    expect(deleteNodeData).toHaveBeenCalledTimes(0)
   })
 
   it("returns the same state when there's an account and a matching password hash in the keychain", async () => {
@@ -47,7 +45,7 @@ describe('resetStateOnInvalidStoredAccount', () => {
     const result = await resetStateOnInvalidStoredAccount(state)
 
     expect(result === state).toEqual(true)
-    expect(mockedDeleteNodeData).toHaveBeenCalledTimes(0)
+    expect(deleteNodeData).toHaveBeenCalledTimes(0)
   })
 
   it("returns an undefined state when there's an account and no matching password hash in the keychain", async () => {
@@ -57,8 +55,8 @@ describe('resetStateOnInvalidStoredAccount', () => {
     const result = await resetStateOnInvalidStoredAccount(state)
 
     expect(result === undefined).toEqual(true)
-    expect(mockedDeleteNodeData).toHaveBeenCalledTimes(1)
-    expect(mockedAnalytics.track).toHaveBeenCalledWith('redux_no_matching_keychain_account', {
+    expect(deleteNodeData).toHaveBeenCalledTimes(1)
+    expect(ValoraAnalytics.track).toHaveBeenCalledWith('redux_no_matching_keychain_account', {
       walletAddress: '0x0000000000000000000000000000000000007e57',
     })
   })
