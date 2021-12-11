967a222f50b4c2b7c5fa1566e04ebf594942d8d7
Silas Boyd-Wickizer <silasbw@gmail.com>
2021-12-10 20:19:38 -0800

    Remove cloud-functions
    It lives in a private repo now

--- packages/cloud-functions/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['@valora/eslint-config-typescript'],
-  parserOptions: {
-    project: './tsconfig.json',
-  },
-  ignorePatterns: ['**/__mocks__/**', '**/lcov-report/**', 'vendor', '.bundle'],
-}
