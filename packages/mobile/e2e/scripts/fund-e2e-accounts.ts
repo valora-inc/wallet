330e9b49aaac8541057799b546e57db9536566f3
MuckT <mcgtom10@gmail.com>
2021-12-14 10:47:56 -0800

    test: set process exit code for fund accounts

--- packages/mobile/e2e/scripts/fund-e2e-accounts.ts
+++ packages/mobile/e2e/scripts/fund-e2e-accounts.ts
@@ -14,11 +14,16 @@ const kit = ContractKit.newKitFromWeb3(web3)
 
 // Throw error if balance is low
 const balanceError = async (address = valoraE2ETestWallet, minBalance = 10) => {
-  let balanceObject = await getBalance(address)
-  for (const balance in balanceObject) {
-    if (balanceObject[balance] < minBalance) {
-      throw new Error(`Balance of ${address} is below ${minBalance}`)
+  try {
+    let balanceObject = await getBalance(address)
+    for (const balance in balanceObject) {
+      if (balanceObject[balance] < minBalance) {
+        throw new Error(`Balance of ${address} is below ${minBalance}`)
+      }
     }
+  } catch (err) {
+    console.log(err)
+    process.exit(1)
   }
 }
 
