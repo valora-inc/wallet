57bc68a591e30dade7d54db05186b0ec2cce65d6
Jean Regisser <jean.regisser@gmail.com>
2022-01-13 18:24:23 +0100

    Try hardcoded sleep instead of loop

--- packages/mobile/scripts/run_e2e.sh
+++ packages/mobile/scripts/run_e2e.sh
@@ -178,10 +178,11 @@ if [ $PLATFORM = "android" ]; then
       &
 
     echo "Waiting for device to connect to Wifi, this is a good proxy the device is ready"
-    until [ `adb shell dumpsys wifi | grep "mNetworkInfo" | grep "state: CONNECTED" | wc -l` -gt 0 ]
-    do
-      sleep 3
-    done
+    sleep 60
+    # until [ `adb shell dumpsys wifi | grep "mNetworkInfo" | grep "state: CONNECTED" | wc -l` -gt 0 ]
+    # do
+    #   sleep 3
+    # done
   fi
 
   # Run Detox Tests
