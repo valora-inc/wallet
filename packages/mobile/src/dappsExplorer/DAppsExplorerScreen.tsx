225fc0b3f0245b7a9d9e22880708ecd13f26ae12
Silas Boyd-Wickizer <silasbw@gmail.com>
2022-01-21 13:29:04 -0800

    Add account address to dappsListUrl API request (#1824)
    * Add account address to dappsListUrl API request

--- packages/mobile/src/dappsExplorer/DAppsExplorerScreen.tsx
+++ packages/mobile/src/dappsExplorer/DAppsExplorerScreen.tsx
@@ -21,6 +21,7 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import { useDispatch, useSelector } from 'react-redux'
 import { openUrl } from 'src/app/actions'
 import { dappsListApiUrlSelector } from 'src/app/selectors'
+import { accountAddressSelector } from 'src/web3/selectors'
 import BackButton from 'src/components/BackButton'
 import BottomSheet from 'src/components/BottomSheet'
 import Dialog from 'src/components/Dialog'
@@ -67,6 +68,7 @@ interface SectionData {
 export function DAppsExplorerScreen() {
   const { t, i18n } = useTranslation()
   const dappsListUrl = useSelector(dappsListApiUrlSelector)
+  const accountAddress = useSelector(accountAddressSelector)
   const [isHelpDialogVisible, setHelpDialogVisible] = useState(false)
   const [isBottomSheetVisible, setBottomSheetVisible] = useState(false)
   const [dappSelected, setDappSelected] = useState<Dapp>()
@@ -88,13 +90,16 @@ export function DAppsExplorerScreen() {
         throw new Error('Dapps list url is not defined')
       }
 
-      const response = await fetch(`${dappsListUrl}?language=${shortLanguage}`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
-        },
-      })
+      const response = await fetch(
+        `${dappsListUrl}?language=${shortLanguage}&address=${accountAddress}`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+          },
+        }
+      )
 
       const result = await response.json()
       try {
