98bf179975cf5d4cfc0bb0fba8c698194ba1fee8
MuckT <mcgtom10@gmail.com>
2022-01-24 16:02:46 -0800

    test: enable detox sync at the start of all tests

--- packages/mobile/e2e/init.js
+++ packages/mobile/e2e/init.js
@@ -5,6 +5,7 @@ import { launchApp } from './src/utils/retries'
 beforeAll(async () => {
   // Install app if not present
   await device.installApp()
+  await device.enableSynchronization() // Enable before all tests if disabled during a test and the test failed.
   await launchApp({
     newInstance: false,
     permissions: { notifications: 'YES', contacts: 'YES' },
