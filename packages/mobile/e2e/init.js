088fbf24a088538fcefcd46538c393f6d1f79e34
MuckT <mcgtom10@gmail.com>
2022-01-24 16:16:57 -0800

    test: move enable sync

--- packages/mobile/e2e/init.js
+++ packages/mobile/e2e/init.js
@@ -5,11 +5,11 @@ import { launchApp } from './src/utils/retries'
 beforeAll(async () => {
   // Install app if not present
   await device.installApp()
-  await device.enableSynchronization() // Enable before all tests if disabled during a test and the test failed.
   await launchApp({
     newInstance: false,
     permissions: { notifications: 'YES', contacts: 'YES' },
   })
+  await device.enableSynchronization() // Enable before all tests if disabled during a test and the test failed.
   // Uses demo mode to set a consistent top bar
   await setDemoMode()
 })
