9f06e3b0055985b07674ba0ab4c46ff28fa982fe
Kathy Luo <kathyluo18@gmail.com>
2022-01-26 09:44:46 +0100

    improve translation key name

--- packages/mobile/src/account/Settings.tsx
+++ packages/mobile/src/account/Settings.tsx
@@ -476,8 +476,8 @@ export class Account extends React.Component<Props, State> {
             />
             {this.props.biometryEnabled && this.props.supportedBiometryType && (
               <SettingsItemSwitch
-                title={t('useBiometryOption', {
-                  biometryOption: t(`biometryOption.${this.props.supportedBiometryType}`),
+                title={t('useBiometryType', {
+                  biometryType: t(`biometryType.${this.props.supportedBiometryType}`),
                 })}
                 value={this.props.pincodeType === PincodeType.PhoneAuth}
                 onValueChange={this.handleUseBiometryToggle}
