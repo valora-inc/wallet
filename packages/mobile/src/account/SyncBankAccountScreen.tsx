bcfdf429e2952785fca4d051385c1fcdb20b4509
Jacob Waterman <jacobrwaterman@gmail.com>
2022-02-01 13:00:39 -0700

    feat(plaid): Add bank accounts list screen  (#1881)
    * plaid refactor start
    
    * bank list start
    
    * a bit more
    
    * less
    
    * nits
    
    * nits
    
    * nit
    
    * respond to feedback
    
    * nits
    
    Co-authored-by: jh2oman <jacobwaterman@MacBook-Pro.local>

--- packages/mobile/src/account/SyncBankAccountScreen.tsx
+++ packages/mobile/src/account/SyncBankAccountScreen.tsx
@@ -13,6 +13,7 @@ import { StackParamList } from 'src/navigator/types'
 import useSelector from 'src/redux/useSelector'
 import { dataEncryptionKeySelector, mtwAddressSelector } from 'src/web3/selectors'
 import Logger from 'src/utils/Logger'
+import { navigate } from 'src/navigator/NavigationService'
 
 const TAG = 'SYNC_BANK_ACCOUNT'
 
@@ -49,7 +50,7 @@ const SyncBankAccountScreen = ({ route }: Props) => {
       // TODO(wallet#1447): handle errors from IHL
       return
     }
-    // TODO(wallet#1449): redirect to Bank Account List Page
+    navigate(Screens.BankAccounts, { newPublicToken: publicToken })
   }, [])
 
   return (
