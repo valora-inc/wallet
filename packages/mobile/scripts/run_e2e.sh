8b2eaa553dc5990f49843d7de63ef5f9c388ae89
MuckT <mcgtom10@gmail.com>
2022-02-03 10:00:32 -0800

    ci: assign value zero if no emulators found

--- packages/mobile/scripts/run_e2e.sh
+++ packages/mobile/scripts/run_e2e.sh
@@ -154,7 +154,7 @@ if [ $PLATFORM = "android" ]; then
     # Commented out for ci testing
     # yarn detox build -c $CONFIG_NAME
 
-    NUM_DEVICES=`adb devices -l | grep emulator- | wc -l`
+    NUM_DEVICES=`adb devices -l | grep emulator- | wc -l` || 0
     echo "Number of devices $NUM_DEVICES"
     echo "Number of workers $WORKERS"
     if [ $NUM_DEVICES -ge 1 ]; then
