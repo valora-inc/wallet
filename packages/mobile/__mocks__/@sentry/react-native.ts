772e4cf32285f7b1a49e264b7bee68cc1b199cf3
Silas Boyd-Wickizer <silasbw@gmail.com>
2022-02-25 07:32:02 -0800

    More mocking

--- packages/mobile/__mocks__/@sentry/react-native.ts
+++ packages/mobile/__mocks__/@sentry/react-native.ts
@@ -2,6 +2,7 @@ const Sentry = {
   config: () => ({ install: jest.fn() }),
   setTagsContext: jest.fn(),
   captureException: jest.fn(),
+  captureMessage: jest.fn(),
   ReactNavigationInstrumentation: jest.fn().mockImplementation(() => ({})),
   Severity: {
     Error: 'error',
