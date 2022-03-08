cc7394452c70d4f722e38d03b7dee375addb47f5
Kathy Luo <kathyluo18@gmail.com>
2022-03-08 15:07:57 +0100

    Auto bump app version test

---
+++ .github/scripts/automergeVersionBump.js
@@ -0,0 +1,51 @@
+// This script uses JSDoc annotations to get TypeScript checks
+// Why aren't we using TypeScript directly here?
+// `actions/github-script` doesn't support TypeScript scripts directly :/
+
+/**
+ * Note: we can remove typeof below once we upgrade TypeScript
+ * otherwise we get a crash, see https://github.com/microsoft/TypeScript/issues/36830
+ * @typedef {ReturnType<typeof import('@actions/github').getOctokit>} GitHub
+ * @typedef {import('@actions/github').context} Context
+ */
+
+const AUTOMERGE_LABEL = 'automerge'
+
+/**
+ * @param {Object} obj - An object.
+ * @param {GitHub} obj.github
+ * @param {Context} obj.context
+ */
+module.exports = async ({ github, context }) => {
+  const { owner, repo } = context.repo
+  const { BRANCH_NAME } = process.env
+
+  console.log('Open the version bump PR')
+  const { data: pr } = await github.rest.pulls.create({
+    owner,
+    repo,
+    head: `${context.repo.owner}:${BRANCH_NAME}`,
+    base: `${context.repo.owner}:main`,
+    title: '[KATHY TEST] Automated app version bump',
+    draft: true,
+  })
+
+  console.log(`Approving PR: ${pr.number}`)
+  await github.rest.pulls.createReview({
+    owner,
+    repo,
+    pull_number: pr.number,
+    event: 'APPROVE',
+    body: `Approved from [${context.workflow} #${context.runNumber}](${context.serverUrl}/${context.repo.owner}/${context.repo.repo}/actions/runs/${context.runId}).`,
+  })
+
+  console.log(`Adding ${AUTOMERGE_LABEL} label`)
+  await github.rest.issues.addLabels({
+    owner,
+    repo,
+    issue_number: pr.number,
+    labels: [AUTOMERGE_LABEL],
+  })
+
+  console.log('Done')
+}
---
+++ .github/scripts/create_app_version_pr.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cd packages/mobile
+
+branch_name="bump-app-version"
+app_version="$(node -p "require('./package.json').version")"
+
+echo "Create branch"
+git checkout -b $branch_name
+echo "BRANCH_NAME="$branch_name"" >> $GITHUB_ENV
+
+echo "Bump app version"
+yarn pre-deploy --minor
+
+echo "Push changes to branch"
+git add .
+git commit -m "Bump app version to $app_version"
+git push origin
