83eaebaa506b40d9b8961ee7f798fc2e8b3dfdaa
Kathy Luo <kathyluo18@gmail.com>
2022-03-09 09:19:54 +0100

    test pr creation and auto merge

--- .github/scripts/create_app_version_pr.sh
+++ .github/scripts/create_app_version_pr.sh
@@ -5,15 +5,15 @@ cd packages/mobile
 
 branch_name="bump-app-version"
 app_version="$(node -p "require('./package.json').version")"
-# echo "BRANCH_NAME="$branch_name"" >> $GITHUB_ENV
+echo "BRANCH_NAME="$branch_name"" >> $GITHUB_ENV
 
-echo "Create branch"
-git checkout -b $branch_name
+# echo "Create branch"
+# git checkout -b $branch_name
 
-echo "Bump app version"
-yarn pre-deploy --minor
+# echo "Bump app version"
+# yarn pre-deploy --minor
 
-echo "Push changes to branch"
-git add .
-git commit -m "Bump app version to $app_version"
-git push --set-upstream origin $branch_name
+# echo "Push changes to branch"
+# git add .
+# git commit -m "Bump app version to $app_version"
+# git push --set-upstream origin $branch_name
