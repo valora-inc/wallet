6e8950d03ce82fa95b8d2f7fab4ee54a72e6c5f5
Kathy Luo <kathyluo18@gmail.com>
2022-03-09 14:29:15 +0100

    test without setting ssh explicitly

--- .github/scripts/create_app_version_pr.sh
+++ .github/scripts/create_app_version_pr.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 cd packages/mobile
 
 # ensure that we are using ssh
-git remote set-url origin git@github.com:valora-inc/wallet.git
+# git remote set-url origin git@github.com:valora-inc/wallet.git
 
 # TODO populate at the workflow level
 branch_name="bump-app-version"
@@ -22,9 +22,9 @@ git add .
 git commit -m "Bump app version to $app_version"
 git push --set-upstream origin $branch_name
 
-echo "Open app version bump PR"
-curl -u "valora-bot:$VALORA_BOT_PAT" \
-  -X POST \
-  -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/valora-inc/wallet/pulls \
-  -d '{ "head": "'$branch_name'", "base": "main", "title": "[KATHY TEST] Automated app version bump", "draft": true }'
+# echo "Open app version bump PR"
+# curl -u "valora-bot:$VALORA_BOT_PAT" \
+#   -X POST \
+#   -H "Accept: application/vnd.github.v3+json" \
+#   https://api.github.com/repos/valora-inc/wallet/pulls \
+#   -d '{ "head": "'$branch_name'", "base": "main", "title": "[KATHY TEST] Automated app version bump", "draft": true }'
