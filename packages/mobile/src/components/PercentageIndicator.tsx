3b2a88eddf7d50539c67bfeb0defc1c378934e9e
Diego Vazquez <diego.vazquez@valoraapp.com>
2022-03-09 13:51:04 -0300

    CR Changes

--- packages/mobile/src/components/PercentageIndicator.tsx
+++ packages/mobile/src/components/PercentageIndicator.tsx
@@ -4,7 +4,7 @@ import Colors from '@celo/react-components/styles/colors'
 import fontStyles from '@celo/react-components/styles/fonts'
 import BigNumber from 'bignumber.js'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TextStyle, View } from 'react-native'
 
 interface Props {
   testID?: string
@@ -12,40 +12,53 @@ interface Props {
   currentValue: BigNumber.Value
 }
 
-function renderPercentage(percentage: BigNumber, testID?: string) {
+function usePercentageInfo(
+  comparedValue: BigNumber,
+  currentValue: BigNumber,
+  testID?: string
+): {
+  indicator?: React.ReactElement
+  style: TextStyle
+  percentageString: string
+} {
+  const percentage = currentValue.dividedBy(comparedValue).multipliedBy(100).minus(100)
   const comparison = percentage.comparedTo(0)
-  const percentageString = percentage.abs().toFixed(2)
-  let style
-  let indicator: any
+  const percentageString = `${percentage.abs().toFixed(2)}%`
   switch (comparison) {
     case -1:
-      style = styles.decreasedText
-      indicator = <DownIndicator testID={`${testID}:DownIndicator`} />
-      break
+      return {
+        indicator: <DownIndicator testID={`${testID}:DownIndicator`} />,
+        style: styles.decreasedText,
+        percentageString,
+      }
     case 1:
-      style = styles.increasedText
-      indicator = <UpIndicator testID={`${testID}:UpIndicator`} />
-      break
+      return {
+        indicator: <UpIndicator testID={`${testID}:UpIndicator`} />,
+        style: styles.increasedText,
+        percentageString,
+      }
     case 0:
     default:
-      style = styles.noChangeText
-      break
+      return {
+        style: styles.noChangeText,
+        percentageString,
+      }
   }
+}
+
+function PercentageIndicator({ comparedValue, currentValue, testID }: Props) {
+  const { indicator, style, percentageString } = usePercentageInfo(
+    new BigNumber(comparedValue),
+    new BigNumber(currentValue),
+    testID
+  )
 
   return (
     <View style={{ flexDirection: 'row' }} testID={testID}>
       <View style={{ padding: 6 }}>{indicator}</View>
-      <Text style={style}>{percentageString}%</Text>
+      <Text style={style}>{percentageString}</Text>
     </View>
   )
-}
-
-function PercentageIndicator({ comparedValue, currentValue, testID }: Props) {
-  const comparedValueBN = new BigNumber(comparedValue)
-  const currentValueBN = new BigNumber(currentValue)
-  const percentage = currentValueBN.dividedBy(comparedValueBN).multipliedBy(100).minus(100)
-
-  return renderPercentage(percentage, testID)
 }
 
 const styles = StyleSheet.create({
