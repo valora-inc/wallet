f2b4ffe1a1cc6506f91714d7400a4f66041c08a4
Kathy Luo <kathyluo18@gmail.com>
2022-03-09 14:50:39 +0100

    remove more workarounds

--- packages/mobile/scripts/pre-deploy.sh
+++ packages/mobile/scripts/pre-deploy.sh
@@ -36,7 +36,7 @@ yarn react-native-version --target android --never-amend
 gradle_properties="android/gradle.properties"
 current_version_code="$(grep "VERSION_CODE" $gradle_properties | cut -d '=' -f 2)"
 new_version_code=$((current_version_code + 1))
-sed -i "" "s/^VERSION_CODE=$current_version_code/VERSION_CODE=$new_version_code/" $gradle_properties
+sed -i "" "s/^VERSION_CODE=.*/VERSION_CODE=$new_version_code/" $gradle_properties
 
 # iOS: use sed to change MARKETING_VERSION in the project (agvtool unfortunately changes the plist files which we don't want)
 sed -i '' -e "s/MARKETING_VERSION \= [^\;]*\;/MARKETING_VERSION = $new_version;/" ios/celo.xcodeproj/project.pbxproj
