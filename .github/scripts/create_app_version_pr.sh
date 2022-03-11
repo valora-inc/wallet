91417eb9fc5389ca499dd5253010ed2e68d8300a
Kathy Luo <kathyluo18@gmail.com>
2022-03-11 12:00:22 +0100

    test different pr name

--- .github/scripts/create_app_version_pr.sh
+++ .github/scripts/create_app_version_pr.sh
@@ -23,9 +23,11 @@ commit_message="Bump app version to $app_version"
 # git commit -m $commit_message
 # git push --set-upstream origin $BRANCH_NAME
 
+echo "$BRANCH_NAME"
+
 echo "Open app version bump PR"
 curl -u "valora-bot:$VALORA_BOT_TOKEN" \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/valora-inc/wallet/pulls \
-  -d '{ "head": "'$BRANCH_NAME'", "base": "main", "title": "'$commit_message'" }'
+  -d '{ "head": "'$BRANCH_NAME'", "base": "main", "title": "[Kathy test]" }'
