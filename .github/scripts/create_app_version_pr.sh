853393a3d9146dda4cad1064a65add11cd7dfb19
Kathy Luo <kathyluo18@gmail.com>
2022-03-11 12:12:05 +0100

    test end to end

--- .github/scripts/create_app_version_pr.sh
+++ .github/scripts/create_app_version_pr.sh
@@ -3,29 +3,27 @@ set -euo pipefail
 
 cd packages/mobile
 
-# # ensure that we are using ssh
-# git remote set-url origin git@github.com:valora-inc/wallet.git
+# ensure that we are using ssh
+git remote set-url origin git@github.com:valora-inc/wallet.git
+
+echo "Create version bump branch from main"
+git checkout -b $BRANCH_NAME
+
+echo "Bump app version"
+yarn pre-deploy --minor --no-disclaimer
+
+# TODO: remove this step as part of https://github.com/valora-inc/wallet/issues/1856
+echo "Generate licences and disclaimer"
+yarn licenses generate-disclaimer --prod > src/account/LicenseDisclaimer.txt && ./scripts/copy_license_to_android_assets.sh
+
 app_version="$(node -p "require('./package.json').version")"
-commit_message="Bump app version to $app_version"
 
-# echo "Create branch from main"
-# git checkout -b $BRANCH_NAME
+echo "Push changes to branch"
+git add .
+git commit -m "Bump app version to $app_version"
+git push --set-upstream origin $BRANCH_NAME
 
-# echo "Bump app version"
-# yarn pre-deploy --minor --no-disclaimer
-
-# # TODO: remove this step as part of https://github.com/valora-inc/wallet/issues/1856
-# echo "Generate disclaimer"
-# yarn licenses generate-disclaimer --prod > src/account/LicenseDisclaimer.txt && ./scripts/copy_license_to_android_assets.sh
-
-# echo "Push changes to branch"
-# git add .
-# git commit -m $commit_message
-# git push --set-upstream origin $BRANCH_NAME
-
-echo "$commit_message"
-
-echo "Open app version bump PR"
+echo "Open version bump PR"
 curl -u "valora-bot:$VALORA_BOT_TOKEN" \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
