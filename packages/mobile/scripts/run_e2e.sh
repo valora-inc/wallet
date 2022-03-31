24f867d700708a7e50cef024b61a500675b5ed48
MuckT <mcgtom10@gmail.com>
2022-03-30 20:05:56 -0700

    test(e2e-ci): adjust run e2e for ci

--- packages/mobile/scripts/run_e2e.sh
+++ packages/mobile/scripts/run_e2e.sh
@@ -168,10 +168,9 @@ if [ $PLATFORM = "android" ]; then
         -avd $VD_NAME \
         -no-boot-anim \
         -noaudio \
-        -no-snapshot-save \
         -read-only \
         -netdelay $NET_DELAY \
-        ${CI:+-snapshot ci_boot} \
+        ${CI:+-gpu swiftshader_indirect -no-window -snapshot ci_boot} \
         &
     done
 
