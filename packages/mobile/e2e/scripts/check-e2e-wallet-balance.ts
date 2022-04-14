7c42401b8af3a8a0bafe084b10d5c936b0d0102c
MuckT <mcgtom10@gmail.com>
2022-04-14 10:55:33 -0700

    test(e2e-ci): report wallet balance prior to check

--- packages/mobile/e2e/scripts/check-e2e-wallet-balance.ts
+++ packages/mobile/e2e/scripts/check-e2e-wallet-balance.ts
@@ -1,5 +1,8 @@
+var consts = require('./consts.ts')
 var utils = require('./utils.ts')
 
 ;(async () => {
+  console.log(consts.E2E_TEST_WALLET)
+  console.table(await utils.getBalance())
   await utils.balanceError()
 })()
