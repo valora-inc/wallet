bb1c6090315d34b22918407dd1816ca784348493
Tom McGuire <Mcgtom10@gmail.com>
2022-05-06 13:03:07 -0700

    Fail E2E Test Prior to Run When E2E Wallet Balance is Low (#2325)
    ### Description
    
    Fails the E2E tests prior to the run when the E2E wallet balance is low to avoid wasting developer time debugging when the source is likely a low wallet balance.
    
    ### Other changes
    
    Moved functions out of the `fund-e2e-accounts.ts` script into a utils module and the constants into a constants module to be shared between the `fund-e2e-accounts.ts` and `check-e2e-wallet-balance.ts`. This will allow extension of e2e scripts in the future.
    
    `fund-e2e-accounts.ts` now balances the test faucet and is run via cron job with GitHub Action every eight hours.
    
    ### Tested
    
    Tested locally and in CI.
    
    
    ### How others should test
    
    This does not need to be tested by QA in the next release cycle. Reviewers should attempt to run `node ./fund-e2e-accounts.ts` and `node ./check-e2e-wallet-balance.ts` from the `packages/mobile/e2e/scripts` folder.
    
    ### Related issues
    
    N/A
    
    ### Backwards compatibility
    
    Yes

--- packages/mobile/e2e/scripts/fund-e2e-accounts.ts
+++ packages/mobile/e2e/scripts/fund-e2e-accounts.ts
@@ -1,87 +1,182 @@
-const Web3 = require('web3')
-const ContractKit = require('@celo/contractkit')
-const dotenv = require('dotenv')
-dotenv.config({ path: '../.env' })
+import { newKitFromWeb3, StableToken } from '@celo/contractkit'
+import dotenv from 'dotenv'
+import Web3 from 'web3'
+import { E2E_TEST_FAUCET, E2E_TEST_WALLET } from './consts'
+import { checkBalance, getBalance } from './utils'
 
-// Constants
-const valoraE2ETestWallet = '0x6131a6d616a4be3737b38988847270a64bc10caa'
-const valoraTestFaucet = '0xe5F5363e31351C38ac82DBAdeaD91Fd5a7B08846'
-const valoraTestFaucetSecret = process.env.TEST_FAUCET_SECRET
+dotenv.config({ path: `${__dirname}/../.env` })
 
-// Init a new kit, connected to the alfajores testnet
 const web3 = new Web3('https://alfajores-forno.celo-testnet.org')
-const kit = ContractKit.newKitFromWeb3(web3)
-
-// Throw error if balance is low
-const balanceError = async (address = valoraE2ETestWallet, minBalance = 10) => {
-  let balanceObject = await getBalance(address)
-  for (const balance in balanceObject) {
-    if (balanceObject[balance] < minBalance) {
-      throw new Error(
-        `${balance} balance of ${address} is below ${minBalance}. Please refill from the faucet https://celo.org/developers/faucet 🙏`
-      )
-    }
-  }
-}
-
-// Default e2e address to lookup
-const getBalance = async (address = valoraE2ETestWallet) => {
-  try {
-    let balanceObj = {}
-
-    // Get Balances
-    let balances = await kit.celoTokens.balancesOf(address)
-
-    // Convert and add to balance object
-    for (const value in balances) {
-      balanceObj[`${value}`] = balances[`${value}`] / 10 ** 18
-    }
-
-    // Return balance object
-    return balanceObj
-  } catch (err) {
-    console.log(err)
-  }
-}
+const kit = newKitFromWeb3(web3)
+const valoraTestFaucetSecret = process.env['TEST_FAUCET_SECRET']!
 
 ;(async () => {
   // Get E2E Test Wallet Balance & Valora Faucet Balance
-  const receivingBalance = await getBalance()
-  const sendingBalance = await getBalance(valoraTestFaucet)
+  const receivingBalance = await getBalance(E2E_TEST_WALLET)
+  const sendingBalance = (await getBalance(E2E_TEST_FAUCET)) ?? {}
+  console.table(await getBalance(E2E_TEST_FAUCET))
 
   // Connect Valora E2E Test Faucet - Private Key Stored in GitHub Secrets
   kit.connection.addAccount(
-    web3.eth.accounts.privateKeyToAccount(valoraTestFaucetSecret).privateKey
+    web3.eth.accounts.privateKeyToAccount(valoraTestFaucetSecret.toString()).privateKey
   )
 
   // Get Token Contract Wrappers
-  let goldtoken = await kit.contracts.getGoldToken()
-  let cUSDtoken = await kit.contracts.getStableToken()
-  let cEURtoken = await kit.contracts.getStableToken('cEUR')
+  const celoToken = await kit.contracts.getGoldToken()
+  const cusdToken = await kit.contracts.getStableToken()
+  const ceurToken = await kit.contracts.getStableToken(StableToken.cEUR)
+  const celoExchange = await kit.contracts.getExchange()
+  const cusdExchange = await kit.contracts.getExchange(StableToken.cUSD)
+  const ceurExchange = await kit.contracts.getExchange(StableToken.cEUR)
+
+  // Balance Faucet
+  let targetTokenRegex = /[cusd|celo|ceur]/gi
+  let balances = await getBalance(E2E_TEST_FAUCET)
+  let sum = 0
+  const numOfTokens = 3 // Only cusd, celo and ceur
+  for (let balance in balances) {
+    console.log(`${balance}: ${balances[balance]}`)
+    // Sum only the target balances
+    if (targetTokenRegex.test(balance)) sum += balances[balance]
+  }
+
+  for (let balance in balances) {
+    const target = sum / numOfTokens
+    if (balances[balance] >= sum / numOfTokens) {
+      const toSell = balances[balance] - target
+      console.log(toSell)
+      const amountToExchange = kit.web3.utils.toWei(`${toSell}`, 'ether')
+      console.log(`${balance} balance higher than ${sum / numOfTokens}`)
+      switch (balance) {
+        case 'CELO':
+          try {
+            const celoApproveTx = await celoToken
+              .approve(celoExchange.address, amountToExchange)
+              .send({ from: E2E_TEST_FAUCET })
+            await celoApproveTx.waitReceipt()
+            const celoSellAmount = await celoExchange.quoteGoldSell(amountToExchange)
+            const celoSellTx = await celoExchange
+              .sellGold(amountToExchange, celoSellAmount)
+              .send({ from: E2E_TEST_FAUCET })
+            await celoSellTx.waitReceipt()
+          } catch (err) {
+            console.log('Failed to sell CELO', err)
+          } finally {
+            break
+          }
+        case 'cUSD':
+          try {
+            const cusdApproveTx = await cusdToken
+              .approve(cusdExchange.address, amountToExchange)
+              .send({ from: E2E_TEST_FAUCET })
+            await cusdApproveTx.waitReceipt()
+            const cusdSellAmount = await cusdExchange.quoteStableSell(amountToExchange)
+            const cusdSellTx = await cusdExchange
+              .sellStable(amountToExchange, cusdSellAmount)
+              .send({ from: E2E_TEST_FAUCET })
+            await cusdSellTx.waitReceipt()
+          } catch (err) {
+            console.log('Failed to sell cUSD', err)
+          } finally {
+            break
+          }
+        case 'cEUR':
+          try {
+            const ceurApproveTx = await ceurToken
+              .approve(ceurExchange.address, amountToExchange)
+              .send({ from: E2E_TEST_FAUCET })
+            await ceurApproveTx.waitReceipt()
+            const ceurSellAmount = await ceurExchange.quoteStableSell(amountToExchange)
+            const ceurSellTx = await ceurExchange
+              .sellStable(amountToExchange, ceurSellAmount)
+              .send({ from: E2E_TEST_FAUCET })
+            await ceurSellTx.waitReceipt()
+          } catch (err) {
+            console.log('Failed to sell cEUR', err)
+          } finally {
+            break
+          }
+      }
+    } else {
+      const toBuy = target - balances[balance]
+      const amountToExchange = kit.web3.utils.toWei(`${toBuy}`, 'ether')
+      console.log(`${balance} balance lower than ${sum / numOfTokens}`)
+      switch (balance) {
+        case 'CELO':
+          try {
+            const celoApproveTx = await celoToken
+              .approve(cusdExchange.address, amountToExchange)
+              .send({ from: E2E_TEST_FAUCET })
+            await celoApproveTx.waitReceipt()
+            const celoBuyAmount = await celoExchange.quoteGoldBuy(amountToExchange)
+            const celoBuyTx = await celoExchange
+              .buyGold(amountToExchange, celoBuyAmount)
+              .send({ from: E2E_TEST_FAUCET })
+            await celoBuyTx.waitReceipt()
+          } catch (err) {
+            console.log('Failed to buy CELO', err)
+          } finally {
+            break
+          }
+        case 'cUSD':
+          try {
+            const cusdApproveTx = await celoToken
+              .approve(cusdExchange.address, amountToExchange)
+              .send({ from: E2E_TEST_FAUCET })
+            await cusdApproveTx.waitReceipt()
+            const cusdBuyAmount = await cusdExchange.quoteStableBuy(amountToExchange)
+            const cusdBuyTx = await cusdExchange
+              .buyStable(amountToExchange, cusdBuyAmount)
+              .send({ from: E2E_TEST_FAUCET })
+            await cusdBuyTx.waitReceipt()
+          } catch (err) {
+            console.log('Failed to buy cUSD', err)
+          } finally {
+            break
+          }
+        case 'cEUR':
+          try {
+            const ceurApproveTx = await celoToken
+              .approve(ceurExchange.address, amountToExchange)
+              .send({ from: E2E_TEST_FAUCET })
+            await ceurApproveTx.waitReceipt()
+            const ceurBuyAmount = await ceurExchange.quoteStableBuy(amountToExchange)
+            const ceurBuyTx = await ceurExchange
+              .buyStable(amountToExchange, ceurBuyAmount)
+              .send({ from: E2E_TEST_FAUCET })
+            await ceurBuyTx.waitReceipt()
+          } catch (err) {
+            console.log('Failed to buy cEUR', err)
+          } finally {
+            break
+          }
+      }
+    }
+  }
 
   // Set Amount To Send
-  let amount = web3.utils.toWei('25', 'ether')
+  let amountToSend = web3.utils.toWei('100', 'ether')
 
   // Loop through E2E Test Wallet Balance Object
   for (const coin in receivingBalance) {
-    let tx
-    // Add funds if balance is less than 100 add 25
-    if (receivingBalance[coin] < 100 && sendingBalance[coin] > 25) {
+    let tx: any
+    // Add funds if balance is less than 100 add 100
+    if (receivingBalance[coin] < 200 && sendingBalance[coin] > 100) {
       switch (coin) {
         case 'CELO':
-          tx = await goldtoken
-            .transfer(valoraE2ETestWallet, amount)
-            .send({ from: valoraTestFaucet })
+          tx = await celoToken
+            .transfer(E2E_TEST_WALLET, amountToSend)
+            .send({ from: E2E_TEST_FAUCET })
           break
         case 'cUSD':
-          tx = await cUSDtoken
-            .transfer(valoraE2ETestWallet, amount)
-            .send({ from: valoraTestFaucet })
+          tx = await cusdToken
+            .transfer(E2E_TEST_WALLET, amountToSend)
+            .send({ from: E2E_TEST_FAUCET })
           break
         case 'cEUR':
-          tx = await cEURtoken
-            .transfer(valoraE2ETestWallet, amount)
-            .send({ from: valoraTestFaucet })
+          tx = await ceurToken
+            .transfer(E2E_TEST_WALLET, amountToSend)
+            .send({ from: E2E_TEST_FAUCET })
           break
       }
       // Wait for the transactions to be processed
@@ -93,9 +188,9 @@ const getBalance = async (address = valoraE2ETestWallet) => {
   }
 
   // Log Balances
-  console.log('E2E Test Account')
-  console.table(await getBalance())
-  console.log('Valora Test Facuet')
-  console.table(await getBalance(valoraTestFaucet))
-  await balanceError()
+  console.log('E2E Test Account:', E2E_TEST_WALLET)
+  console.table(await getBalance(E2E_TEST_WALLET))
+  console.log('Valora Test Facuet:', E2E_TEST_FAUCET)
+  console.table(await getBalance(E2E_TEST_FAUCET))
+  await checkBalance(E2E_TEST_WALLET)
 })()
