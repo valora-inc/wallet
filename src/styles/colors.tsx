9629b6edfa1004e9dadc05055f4e69426043ef31
Matthew Abraham <wasteofintel@gmail.com>
2022-05-12 16:10:31 -0400

    feat: begin replacing design system

--- src/styles/colors.tsx
+++ src/styles/colors.tsx
@@ -1,5 +1,5 @@
 // Designer Created Figma Colors
-export enum Colors {
+export enum Colors2 {
   greenFaint = '#97DFC1', // green disabled
   greenBrand = '#42D689',
   greenUI = '#1AB775',
@@ -31,6 +31,48 @@ export enum Colors {
   greenBackground = '#DEF8EA',
   greenStrong = '#08CB7A',
   lightGreen = '#E6F7EC',
+}
+
+export enum Colors {
+  // greenFaint = '#97DFC1', // green disabled
+  greenBrand = '#42D689',
+  // greenUI = '#1AB775',
+  goldFaint = '#E3C376', // gold disabled
+  goldBrand = '#FBCC5C',
+  goldUI = '#EEB93C',
+  goldDark = '#9C6E00',
+  beige = '#F1F0EB',
+  brownFaint = '#FFF9EE',
+  warning = '#EA6042',
+  informational = '#0768AE',
+  dark = '#2E3338',
+  gray6 = '#DFDBCE',
+  gray5 = '#81868B',
+  gray4 = '#9CA4A9',
+  gray3 = '#B4B9BD',
+  gray2 = '#EDEEEF',
+  gray1 = '#F8F9F9',
+  light = '#FFFFFF',
+  purple = '#D39CFE',
+  teal = '#7AD6FE',
+  orange = '#FEB45E',
+  onboardingBrownLight = '#A49B80',
+  onboardingBlue = '#0768AE',
+  onboardingLightBlue = '#D6EAF3',
+  onboardingBackground = '#F9F6F0',
+  modalBackdrop = '#1B392F',
+
+  greenBackground = '#DEF8EA',
+  greenStrong = '#08CB7A',
+  lightGreen = '#E6F7EC',
+
+  // Kolektivo Colors
+  // @todo orangeFaint color for disabled primary buttons
+  orangeFaint = '#F3C6B5',
+  orangeUI = '#E4825F',
+  // @todo greenFaint color for disabled primary2 buttons
+  greenFaint = '#A9D9D7',
+  greenUI = '#45ADA8',
 }
 
 export default Colors
