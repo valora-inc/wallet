dfc1b34875ff26db8f3100232fcabedbb1796e94
Matthew Abraham <wasteofintel@gmail.com>
2022-05-13 18:34:09 -0400

    test: optimise render test for recently used dapps

--- src/home/WalletHome.tsx
+++ src/home/WalletHome.tsx
@@ -140,13 +140,13 @@ function WalletHome() {
 
   sections.push({
     data: [{}],
-    renderItem: () => <SendOrRequestButtons />,
+    renderItem: () => <SendOrRequestButtons key={'SendOrRequestButtons'} />,
   })
 
   if (maxNumRecentDapps > 0) {
     sections.push({
       data: [{}],
-      renderItem: () => <RecentlyUsedDapps key="RecentlyUsedDapps" onSelectDapp={onSelectDapp} />,
+      renderItem: () => <RecentlyUsedDapps key={'RecentlyUsedDapps'} onSelectDapp={onSelectDapp} />,
     })
   }
 
