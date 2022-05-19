7ea532f61e8f898bfd1d3ba13ed3740578cc14fd
jh2oman <jacobwaterman@MacBook-Pro.local>
2022-05-19 13:29:45 -0700

    initial

--- src/fiatconnect/index.ts
+++ src/fiatconnect/index.ts
@@ -1,6 +1,7 @@
+import { QuoteRequestQuery } from '@fiatconnect/fiatconnect-types'
+import { CICOFlow } from 'src/fiatExchanges/utils'
 import networkConfig from 'src/geth/networkConfig'
 import Logger from '../utils/Logger'
-
 const TAG = 'FIATCONNECT'
 
 export interface FiatConnectClientConfig {
@@ -15,8 +16,13 @@ export interface FiatConnectClientConfig {
  *
  * Queries a cloud function for static information about FiatConnect providers.
  */
-export async function getFiatConnectProviders(): Promise<FiatConnectClientConfig[]> {
-  const response = await fetch(networkConfig.getFiatConnectProvidersUrl)
+export async function getFiatConnectProviders(
+  address: string,
+  providerList?: string
+): Promise<FiatConnectClientConfig[]> {
+  const response = await fetch(
+    `${networkConfig.getFiatConnectProvidersUrl}?address=${address}&providers=${providerList}`
+  )
   if (!response.ok) {
     Logger.error(
       TAG,
@@ -26,4 +32,29 @@ export async function getFiatConnectProviders(): Promise<FiatConnectClientConfig
   }
   const { providers } = await response.json()
   return providers
+}
+
+type QuotesInput = QuoteRequestQuery & {
+  providers: string
+  flow: CICOFlow
+}
+
+export async function getFiatConnectQuotes(params: QuotesInput) {
+  const { flow, ...otherParams } = params
+
+  const queryParams = new URLSearchParams({
+    ...otherParams,
+    quoteType: flow === CICOFlow.CashIn ? 'in' : 'out',
+  }).toString()
+  const response = await fetch(`${networkConfig.getFiatConnectQuotesUrl}?${queryParams}`)
+  if (!response.ok) {
+    const { errorMessage } = await response.json()
+    Logger.error(
+      TAG,
+      `Failure response fetching FiatConnect quotes: ${errorMessage} , returning empty list`
+    )
+    return []
+  }
+  const { quotes } = await response.json()
+  return quotes
 }
