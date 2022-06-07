1d3685348975edd76f1012d2ead756ecc7c45e33
Satish Ravi <satish.ravi@valoraapp.com>
2022-06-07 13:19:06 -0700

    more tests

--- src/fiatconnect/ReviewScreen.test.tsx
+++ src/fiatconnect/ReviewScreen.test.tsx
@@ -12,7 +12,7 @@ import { CICOFlow } from 'src/fiatExchanges/utils'
 import { Screens } from 'src/navigator/Screens'
 import { createMockStore, getMockStackScreenProps } from 'test/utils'
 
-function getProps(flow: CICOFlow, fee?: string) {
+function getProps(flow: CICOFlow, fee?: string, cryptoType = CryptoType.cUSD) {
   return getMockStackScreenProps(Screens.FiatConnectReview, {
     flow,
     cicoQuote: {
@@ -20,7 +20,7 @@ function getProps(flow: CICOFlow, fee?: string) {
         fiatAmount: '20',
         fiatType: FiatType.USD,
         cryptoAmount: '22.05',
-        cryptoType: CryptoType.cUSD,
+        cryptoType,
         quoteId: '1',
         guaranteedUntil: '2022-01-05',
       },
@@ -51,19 +51,31 @@ function getProps(flow: CICOFlow, fee?: string) {
 describe('ReviewScreen', () => {
   const store = createMockStore()
 
+  describe('cashIn', () => {
+    it('throws not implemented', () => {
+      expect(() => {
+        render(
+          <Provider store={store}>
+            <FiatConnectReviewScreen {...getProps(CICOFlow.CashIn, '2.00')} />
+          </Provider>
+        )
+      }).toThrowError('Not implemented')
+    })
+  })
+
   describe('cashOut', () => {
     it('shows fiat amount, transaction details and payment method', () => {
       const { queryByTestId, queryByText } = render(
         <Provider store={store}>
-          <FiatConnectReviewScreen {...getProps(CICOFlow.CashOut, '2.00')} />
+          <FiatConnectReviewScreen {...getProps(CICOFlow.CashOut, '2.00', CryptoType.cEUR)} />
         </Provider>
       )
 
       expect(queryByTestId('amount-fiat/value')?.children).toEqual(['', '$', '20.00'])
       expect(queryByText('fiatConnectReviewScreen.transactionDetails')).toBeTruthy()
       expect(queryByText('fiatConnectReviewScreen.cashOut.transactionDetailsAmount')).toBeTruthy()
-      expect(queryByTestId('txDetails-total')?.children).toEqual(['', '22.05', ' cUSD'])
-      expect(queryByTestId('txDetails-converted')?.children).toEqual(['', '20.05', ' cUSD'])
+      expect(queryByTestId('txDetails-total')?.children).toEqual(['', '22.05', ' cEUR'])
+      expect(queryByTestId('txDetails-converted')?.children).toEqual(['', '20.05', ' cEUR'])
       expect(queryByTestId('txDetails-fee')).toBeTruthy()
       expect(queryByTestId('txDetails-exchangeRate/value')?.children).toEqual(['', '$', '0.9975'])
       expect(queryByTestId('txDetails-exchangeAmount/value')?.children).toEqual(['', '$', '20.00'])
