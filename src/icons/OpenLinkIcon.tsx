d66af4a50347c8c9ef3b9c74e3a3aa4daa6263f3
Samuel Shin <samuel3.shin@gmail.com>
2022-06-23 15:28:41 -0700

    removed unused parameters in OpenLinkIcon.tsx

--- src/icons/OpenLinkIcon.tsx
+++ src/icons/OpenLinkIcon.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react'
 import Svg, { Path } from 'react-native-svg'
 
-const OpenLinkIcon = (props: any) => (
-  <Svg width={24} height={24} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+const OpenLinkIcon = () => (
+  <Svg width={24} height={24} fill="none">
     <Path
       fillRule="evenodd"
       clipRule="evenodd"
