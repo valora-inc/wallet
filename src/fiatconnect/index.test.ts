96720af08e0db1b6e97291f5de7dfd8c2d500eab
Joe Bergeron <jbergero@alum.mit.edu>
2022-06-23 12:36:58 -0400

    feat(fiatconnect): Allow wallet to login with FC providers (#2612)
    * Test branch
    
    * Workaround tslib import error
    
    * Testing
    
    * Remove unnecessary code
    
    * Put metro config changes in different PR
    
    * Merge
    
    * tmp
    
    * Test
    
    * Remove unused import
    
    * Fix tests
    
    * tests
    
    * Use clock sync version of SDK
    
    * Update constructor
    
    * Add test for signing function
    
    * Unlock in signing function
    
    Co-authored-by: Jean Regisser <jean.regisser@gmail.com>

--- src/fiatconnect/index.test.ts
+++ src/fiatconnect/index.test.ts
@@ -1,5 +1,8 @@
 import { FiatAccountSchema, FiatAccountType } from '@fiatconnect/fiatconnect-types'
 import { FetchMock } from 'jest-fetch-mock'
+import { Network } from '@fiatconnect/fiatconnect-types'
+jest.mock('src/pincode/authentication')
+
 import { CICOFlow } from 'src/fiatExchanges/utils'
 import { LocalCurrencyCode } from 'src/localCurrency/consts'
 import { CiCoCurrency } from 'src/utils/currencies'
@@ -18,7 +21,12 @@ import {
   getFiatConnectProviders,
   getFiatConnectQuotes,
   QuotesInput,
+  loginWithFiatConnectProvider,
+  getSigningFunction,
 } from './index'
+import { FiatConnectClient } from '@fiatconnect/fiatconnect-sdk'
+import { KeychainWallet } from 'src/web3/KeychainWallet'
+import { getPassword } from 'src/pincode/authentication'
 
 jest.mock('src/utils/Logger', () => ({
   __esModule: true,
@@ -29,9 +37,28 @@ jest.mock('src/utils/Logger', () => ({
   },
 }))
 
+jest.mock('src/web3/KeychainWallet', () => {
+  return {
+    KeychainWallet: () => {
+      return jest.fn(() => {
+        return {}
+      })
+    },
+  }
+})
+
+jest.mock('@fiatconnect/fiatconnect-sdk', () => {
+  return {
+    FiatConnectClient: () => {
+      return jest.fn(() => {
+        return {}
+      })
+    },
+  }
+})
+
 describe('FiatConnect helpers', () => {
   const mockFetch = fetch as FetchMock
-
   beforeEach(() => {
     mockFetch.resetMocks()
     jest.clearAllMocks()
@@ -132,6 +159,94 @@ describe('FiatConnect helpers', () => {
       await expect(
         addNewFiatAccount(fakeProviderURL, fiatAccountSchema, reqBody)
       ).rejects.toThrowError('Not implemented')
+    })
+  })
+  describe('getSigningFunction', () => {
+    const wallet = new KeychainWallet({
+      address: 'some address',
+      createdAt: new Date(),
+    })
+    beforeEach(() => {
+      wallet.getAccounts = jest.fn().mockReturnValue(['fakeAccount'])
+      wallet.isAccountUnlocked = jest.fn().mockReturnValue(true)
+      wallet.signPersonalMessage = jest.fn().mockResolvedValue('some signed message')
+      wallet.unlockAccount = jest.fn().mockResolvedValue(undefined)
+    })
+    it('returns a signing function that signs a message', async () => {
+      const signingFunction = getSigningFunction(wallet)
+      const signedMessage = await signingFunction('test')
+      expect(wallet.signPersonalMessage).toHaveBeenCalled()
+      expect(wallet.unlockAccount).not.toHaveBeenCalled()
+      expect(signedMessage).toEqual('some signed message')
+    })
+    it('returns a signing function that attempts to unlock accout if locked', async () => {
+      wallet.isAccountUnlocked = jest.fn().mockReturnValue(false)
+      const signingFunction = getSigningFunction(wallet)
+      const signedMessage = await signingFunction('test')
+      expect(wallet.signPersonalMessage).toHaveBeenCalled()
+      expect(wallet.unlockAccount).toHaveBeenCalled()
+      expect(getPassword).toHaveBeenCalled()
+      expect(signedMessage).toEqual('some signed message')
+    })
+  })
+  describe('loginWithFiatConnectProvider', () => {
+    const wallet = new KeychainWallet({
+      address: 'some address',
+      createdAt: new Date(),
+    })
+    const fiatConnectClient = new FiatConnectClient(
+      {
+        baseUrl: 'some url',
+        network: Network.Alfajores,
+        accountAddress: 'some address',
+      },
+      (message: string): Promise<string> => {
+        return Promise.resolve(message)
+      }
+    )
+
+    beforeEach(() => {
+      wallet.getAccounts = jest.fn().mockReturnValue(['fakeAccount'])
+      wallet.isAccountUnlocked = jest.fn().mockReturnValue(true)
+      wallet.unlockAccount = jest.fn().mockResolvedValue(undefined)
+
+      fiatConnectClient.isLoggedIn = jest.fn().mockReturnValue(true)
+      fiatConnectClient.login = jest.fn().mockResolvedValue({ isOk: true, value: 'success' })
+    })
+    it('Does not attempt to login if already logged in', async () => {
+      await expect(loginWithFiatConnectProvider(wallet, fiatConnectClient)).resolves.toBeUndefined()
+      expect(fiatConnectClient.login).not.toHaveBeenCalled()
+    })
+    it('Forces login attempt if already logged in', async () => {
+      await expect(
+        loginWithFiatConnectProvider(wallet, fiatConnectClient, true)
+      ).resolves.toBeUndefined()
+      expect(fiatConnectClient.login).toHaveBeenCalled()
+    })
+    it('Attempts to login and prompts PIN when account is locked', async () => {
+      wallet.isAccountUnlocked = jest.fn().mockReturnValue(false)
+      fiatConnectClient.isLoggedIn = jest.fn().mockReturnValue(false)
+      await expect(loginWithFiatConnectProvider(wallet, fiatConnectClient)).resolves.toBeUndefined()
+      expect(wallet.unlockAccount).toHaveBeenCalled()
+      expect(getPassword).toHaveBeenCalled()
+      expect(fiatConnectClient.login).toHaveBeenCalled()
+    })
+    it('Attempts to login without prompting for PIN when account is unlocked', async () => {
+      wallet.isAccountUnlocked = jest.fn().mockReturnValue(true)
+      fiatConnectClient.isLoggedIn = jest.fn().mockReturnValue(false)
+      await expect(loginWithFiatConnectProvider(wallet, fiatConnectClient)).resolves.toBeUndefined()
+      expect(wallet.unlockAccount).not.toHaveBeenCalled()
+      expect(getPassword).not.toHaveBeenCalled()
+      expect(fiatConnectClient.login).toHaveBeenCalled()
+    })
+    it('Throws an error when login fails', async () => {
+      wallet.isAccountUnlocked = jest.fn().mockReturnValue(true)
+      fiatConnectClient.isLoggedIn = jest.fn().mockReturnValue(false)
+      fiatConnectClient.login = jest.fn().mockResolvedValue({
+        isOk: false,
+        error: new Error('some error'),
+      })
+      await expect(loginWithFiatConnectProvider(wallet, fiatConnectClient)).rejects.toThrow()
     })
   })
 })
