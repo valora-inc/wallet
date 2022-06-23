88a2d3a391b57ef6b27c106370115d80bacd265e
Samuel Shin <samuel3.shin@gmail.com>
2022-06-23 09:13:15 -0700

    redux schema state update

--- src/redux/store.test.ts
+++ src/redux/store.test.ts
@@ -93,7 +93,7 @@ describe('store state', () => {
       Object {
         "_persist": Object {
           "rehydrated": true,
-          "version": 56,
+          "version": 57,
         },
         "account": Object {
           "acceptedTerms": false,
