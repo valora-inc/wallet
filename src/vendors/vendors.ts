9e1534c0ff2b0a9c8dd0fbc4107ebadd18c8dc3e
Nashmod <hello@obsidianlab.io>
2022-06-28 17:15:47 -0400

    chore: update hardcoded vendor list

--- src/vendors/vendors.ts
+++ src/vendors/vendors.ts
@@ -1,6 +1,132 @@
 import { Vendors } from 'src/vendors/types'
 
 export const CuracaoVendors: Vendors = {
+  ['Agrinature Caribbean']: {
+    title: 'Agrinature Caribbean',
+    subtitle: 'Importer Natural Goods',
+    logoURI: '',
+    siteURI: 'https://www.facebook.com/agrinaturecaribbean/',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare varius nunc vitae euismod. Donec vitae mattis purus, vestibulum hendrerit odio. Vestibulum vitae sapien risus. Mauris quis posuere elit. Nulla malesuada porta mollis. Lorem ipsum posuere.',
+    tags: ['raw elements', 'biogents', 'nuud distributor'],
+    currencies: ['USD', 'ANG'],
+    location: {
+      latitude: '',
+      longitude: '',
+    },
+  },
+  ['Alta services']: {
+    title: 'Alta services',
+    subtitle: 'Business & IT Management',
+    logoURI: '',
+    siteURI: 'https://www.altaservicesint.com/',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare varius nunc vitae euismod. Donec vitae mattis purus, vestibulum hendrerit odio. Vestibulum vitae sapien risus. Mauris quis posuere elit. Nulla malesuada porta mollis. Lorem ipsum posuere.',
+    tags: ['it', 'consultancy'],
+    currencies: ['USD', 'ANG'],
+    location: {
+      latitude: '',
+      longitude: '',
+    },
+  },
+  ['Beyglz']: {
+    title: 'Beyglz',
+    subtitle: 'Bagel Shop',
+    logoURI: '',
+    siteURI: 'https://beyglz.com/',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare varius nunc vitae euismod. Donec vitae mattis purus, vestibulum hendrerit odio. Vestibulum vitae sapien risus. Mauris quis posuere elit. Nulla malesuada porta mollis. Lorem ipsum posuere.',
+    tags: ['restaurant'],
+    currencies: ['USD', 'ANG'],
+    location: {
+      latitude: '12.108278299513987',
+      longitude: '-68.93198348666893',
+    },
+  },
+  ['BitCaribe']: {
+    title: 'BitCaribe',
+    subtitle: 'Cryptocurrency Exchange',
+    logoURI: '',
+    siteURI: 'https://bitcaribe.net/en/',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare varius nunc vitae euismod. Donec vitae mattis purus, vestibulum hendrerit odio. Vestibulum vitae sapien risus. Mauris quis posuere elit. Nulla malesuada porta mollis. Lorem ipsum posuere.',
+    tags: ['financial'],
+    currencies: ['USD', 'ANG'],
+    location: {
+      latitude: '12.103442601634036',
+      longitude: '-68.9234867381928',
+    },
+  },
+  ['Brushwork Tattoos & Art']: {
+    title: 'Brushwork Tattoos & Art',
+    subtitle: 'Tattoo Parlor & Art Studio',
+    logoURI: '',
+    siteURI: 'https://www.instagram.com/brushworktattoo/',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare varius nunc vitae euismod. Donec vitae mattis purus, vestibulum hendrerit odio. Vestibulum vitae sapien risus. Mauris quis posuere elit. Nulla malesuada porta mollis. Lorem ipsum posuere.',
+    tags: ['art'],
+    currencies: ['USD', 'ANG'],
+    location: {
+      latitude: '12.105511670353598',
+      longitude: '-68.93720278033908',
+    },
+  },
+  ['Crab Social ']: {
+    title: 'Crab Social ',
+    subtitle: 'Social Media Agency',
+    logoURI: '',
+    siteURI: 'https://www.flavinawanga.com/',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare varius nunc vitae euismod. Donec vitae mattis purus, vestibulum hendrerit odio. Vestibulum vitae sapien risus. Mauris quis posuere elit. Nulla malesuada porta mollis. Lorem ipsum posuere.',
+    tags: ['social media', 'consultancy'],
+    currencies: ['USD', 'ANG'],
+    location: {
+      latitude: '12.104794283561104',
+      longitude: '-68.88108199420098',
+    },
+  },
+  ['CuraDAI']: {
+    title: 'CuraDAI',
+    subtitle: 'Community Currency',
+    logoURI: '',
+    siteURI: 'https://www.kolektivo.co/curadai',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare varius nunc vitae euismod. Donec vitae mattis purus, vestibulum hendrerit odio. Vestibulum vitae sapien risus. Mauris quis posuere elit. Nulla malesuada porta mollis. Lorem ipsum posuere.',
+    tags: ['communtiy', 'currency'],
+    currencies: ['USD', 'ANG'],
+    location: {
+      latitude: '',
+      longitude: '',
+    },
+  },
+  ['Dcommunicates']: {
+    title: 'Dcommunicates',
+    subtitle: 'Communication & Copywriting Consultant',
+    logoURI: '',
+    siteURI: 'https://www.facebook.com/dcommunicates',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare varius nunc vitae euismod. Donec vitae mattis purus, vestibulum hendrerit odio. Vestibulum vitae sapien risus. Mauris quis posuere elit. Nulla malesuada porta mollis. Lorem ipsum posuere.',
+    tags: ['copywriting'],
+    currencies: ['USD', 'ANG'],
+    location: {
+      latitude: '',
+      longitude: '',
+    },
+  },
+  ['Dushi Cards']: {
+    title: 'Dushi Cards',
+    subtitle: 'Papiamentu Greeting cards',
+    logoURI: '',
+    siteURI: 'https://dushi-cards.com/',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare varius nunc vitae euismod. Donec vitae mattis purus, vestibulum hendrerit odio. Vestibulum vitae sapien risus. Mauris quis posuere elit. Nulla malesuada porta mollis. Lorem ipsum posuere.',
+    tags: ['cards'],
+    currencies: ['USD', 'ANG'],
+    location: {
+      latitude: '12.161383955574033',
+      longitude: '-68.95588257400681',
+    },
+  },
   ['Esperamos Noorduynweg']: {
     title: 'Esperamos Supermarket',
     subtitle: 'Esperamos Noorduynweg',
@@ -29,18 +155,200 @@ export const CuracaoVendors: Vendors = {
       longitude: '-68.88107634443152',
     },
   },
-  ['Weber & Weber']: {
-    title: 'Weber & Weber',
-    subtitle: 'Locksmith',
+  ['Fuego Esports']: {
+    title: 'Fuego Esports',
+    subtitle: 'Esports Organization',
     logoURI: '',
-    siteURI: 'https://www.weberenweberbeheer.com',
+    siteURI: 'https://fuego.gg/',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare varius nunc vitae euismod. Donec vitae mattis purus, vestibulum hendrerit odio. Vestibulum vitae sapien risus. Mauris quis posuere elit. Nulla malesuada porta mollis. Lorem ipsum posuere.',
-    tags: ['keys', 'locks', 'security'],
+    tags: ['esports'],
     currencies: ['USD', 'ANG'],
     location: {
-      latitude: '12.101489986046817',
-      longitude: '-68.92306523152337',
+      latitude: '',
+      longitude: '',
+    },
+  },
+  ['Fundashon Alton Paas']: {
+    title: 'Fundashon Alton Paas',
+    subtitle: 'Spinal Cord Injury Awareness Foundation',
+    logoURI: '',
+    siteURI: 'www.fundashonaltonpaas.org',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare varius nunc vitae euismod. Donec vitae mattis purus, vestibulum hendrerit odio. Vestibulum vitae sapien risus. Mauris quis posuere elit. Nulla malesuada porta mollis. Lorem ipsum posuere.',
+    tags: ['charity', 'foundation'],
+    currencies: ['USD', 'ANG'],
+    location: {
+      latitude: '12.111271011358689',
+      longitude: '-68.94181927400729',
+    },
+  },
+  ['Gratida']: {
+    title: 'Gratida',
+    subtitle: 'Business Consultancy',
+    logoURI: '',
+    siteURI: 'https://www.gratida.com',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare varius nunc vitae euismod. Donec vitae mattis purus, vestibulum hendrerit odio. Vestibulum vitae sapien risus. Mauris quis posuere elit. Nulla malesuada porta mollis. Lorem ipsum posuere.',
+    tags: ['consultancy'],
+    currencies: ['USD', 'ANG'],
+    location: {
+      latitude: '12.104794283561104',
+      longitude: '-68.88108199420098',
+    },
+  },
+  ['Herbology Lifestyle']: {
+    title: 'Herbology Lifestyle',
+    subtitle: 'Holistic Health & Wellness Services',
+    logoURI: '',
+    siteURI: 'https://www.facebook.com/herbologylifestyle',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare varius nunc vitae euismod. Donec vitae mattis purus, vestibulum hendrerit odio. Vestibulum vitae sapien risus. Mauris quis posuere elit. Nulla malesuada porta mollis. Lorem ipsum posuere.',
+    tags: ['wellness'],
+    currencies: ['USD', 'ANG'],
+    location: {
+      latitude: '12.125885356016822',
+      longitude: '-68.89635400018429',
+    },
+  },
+  ['Limpi Recycling']: {
+    title: 'Limpi Recycling',
+    subtitle: 'Design & Recycling',
+    logoURI: '',
+    siteURI: 'www.limpirecycling.com',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare varius nunc vitae euismod. Donec vitae mattis purus, vestibulum hendrerit odio. Vestibulum vitae sapien risus. Mauris quis posuere elit. Nulla malesuada porta mollis. Lorem ipsum posuere.',
+    tags: ['design', 'recycling'],
+    currencies: ['USD', 'ANG'],
+    location: {
+      latitude: '12.123077386481757',
+      longitude: '-68.90366436365134',
+    },
+  },
+  ['PC Racing']: {
+    title: 'PC Racing',
+    subtitle: 'Foundation Tri Curaçao',
+    logoURI: '',
+    siteURI: 'https://www.tri-curacao.com/',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare varius nunc vitae euismod. Donec vitae mattis purus, vestibulum hendrerit odio. Vestibulum vitae sapien risus. Mauris quis posuere elit. Nulla malesuada porta mollis. Lorem ipsum posuere.',
+    tags: ['design', 'recycling'],
+    currencies: ['USD', 'ANG'],
+    location: {
+      latitude: '',
+      longitude: '',
+    },
+  },
+  ['Prisca VA Services']: {
+    title: 'Prisca VA Services',
+    subtitle: 'Legal Services',
+    logoURI: '',
+    siteURI: 'http://www.priscava.com/',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare varius nunc vitae euismod. Donec vitae mattis purus, vestibulum hendrerit odio. Vestibulum vitae sapien risus. Mauris quis posuere elit. Nulla malesuada porta mollis. Lorem ipsum posuere.',
+    tags: ['legal'],
+    currencies: ['USD', 'ANG'],
+    location: {
+      latitude: '12.104794283561104',
+      longitude: '-68.88108199420098',
+    },
+  },
+  ['Ronald McDonald House Charities Curacao Foundation']: {
+    title: 'Ronald McDonald House Charities Curacao Foundation',
+    subtitle: 'Charity Foundation',
+    logoURI: '',
+    siteURI: 'https://rmhc-curacao.org/',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare varius nunc vitae euismod. Donec vitae mattis purus, vestibulum hendrerit odio. Vestibulum vitae sapien risus. Mauris quis posuere elit. Nulla malesuada porta mollis. Lorem ipsum posuere.',
+    tags: ['charity', 'foundation'],
+    currencies: ['USD', 'ANG'],
+    location: {
+      latitude: '12.110217921957478',
+      longitude: '-68.94182518958408',
+    },
+  },
+  ['The Future Proof Strategist']: {
+    title: 'The Future Proof Strategist',
+    subtitle: 'Business Strategy & Consultancy',
+    logoURI: '',
+    siteURI: 'https://www.futureproofstrategist.com',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare varius nunc vitae euismod. Donec vitae mattis purus, vestibulum hendrerit odio. Vestibulum vitae sapien risus. Mauris quis posuere elit. Nulla malesuada porta mollis. Lorem ipsum posuere.',
+    tags: ['consultancy'],
+    currencies: ['USD', 'ANG'],
+    location: {
+      latitude: '',
+      longitude: '',
+    },
+  },
+  ['The Nut House']: {
+    title: 'The Nut House',
+    subtitle: 'Health Food Store',
+    logoURI: '',
+    siteURI: 'https://nuthousecuracao.com',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare varius nunc vitae euismod. Donec vitae mattis purus, vestibulum hendrerit odio. Vestibulum vitae sapien risus. Mauris quis posuere elit. Nulla malesuada porta mollis. Lorem ipsum posuere.',
+    tags: ['retail'],
+    currencies: ['USD', 'ANG'],
+    location: {
+      latitude: '',
+      longitude: '',
+    },
+  },
+  ['The Recycled Pirate']: {
+    title: 'The Recycled Pirate',
+    subtitle: 'Facilitator • Coach • Creator',
+    logoURI: '',
+    siteURI: 'https://therecycledpirate.com/',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare varius nunc vitae euismod. Donec vitae mattis purus, vestibulum hendrerit odio. Vestibulum vitae sapien risus. Mauris quis posuere elit. Nulla malesuada porta mollis. Lorem ipsum posuere.',
+    tags: ['coaching'],
+    currencies: ['USD', 'ANG'],
+    location: {
+      latitude: '12.08367004818774',
+      longitude: '-68.82257288554159',
+    },
+  },
+  ['Tiki Mas Serbes']: {
+    title: 'Tiki Mas Serbes',
+    subtitle: 'Craft Beer Brewery',
+    logoURI: '',
+    siteURI: 'http://www.tikimasserbes.com/',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare varius nunc vitae euismod. Donec vitae mattis purus, vestibulum hendrerit odio. Vestibulum vitae sapien risus. Mauris quis posuere elit. Nulla malesuada porta mollis. Lorem ipsum posuere.',
+    tags: ['brewery'],
+    currencies: ['USD', 'ANG'],
+    location: {
+      latitude: '12.161383955574033',
+      longitude: '-68.95588257400681',
+    },
+  },
+  ['Top Notch Carwash']: {
+    title: 'Top Notch Carwash',
+    subtitle: 'Mobile Carwash Service',
+    logoURI: '',
+    siteURI: 'https://www.facebook.com/TopNotchCarWashCuracao',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare varius nunc vitae euismod. Donec vitae mattis purus, vestibulum hendrerit odio. Vestibulum vitae sapien risus. Mauris quis posuere elit. Nulla malesuada porta mollis. Lorem ipsum posuere.',
+    tags: ['carwash'],
+    currencies: ['USD', 'ANG'],
+    location: {
+      latitude: '12.101099976492248',
+      longitude: '-68.92219923724113',
+    },
+  },
+  ['Xharp Media']: {
+    title: 'Xharp Media',
+    subtitle: 'Digital Media',
+    logoURI: '',
+    siteURI: 'https://xharp.media',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare varius nunc vitae euismod. Donec vitae mattis purus, vestibulum hendrerit odio. Vestibulum vitae sapien risus. Mauris quis posuere elit. Nulla malesuada porta mollis. Lorem ipsum posuere.',
+    tags: ['design', 'media'],
+    currencies: ['USD', 'ANG'],
+    location: {
+      latitude: '',
+      longitude: '',
     },
   },
 }
