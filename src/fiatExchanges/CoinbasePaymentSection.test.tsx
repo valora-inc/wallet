fae6abe83cd92852018618c60d1ae5d21eef673c
Alvin Tsui <awtsui@uci.edu>
2022-07-01 11:42:31 -0700

    test: Testing of coinbasePayEnabled feature flag

--- src/fiatExchanges/CoinbasePaymentSection.test.tsx
+++ src/fiatExchanges/CoinbasePaymentSection.test.tsx
@@ -1,14 +1,18 @@
 import { render } from '@testing-library/react-native'
 import * as React from 'react'
+import { Provider } from 'react-redux'
+import { MockStoreEnhanced } from 'redux-mock-store'
 import {
   CoinbasePaymentSection,
   CoinbasePaymentSectionProps,
 } from 'src/fiatExchanges/CoinbasePaymentSection'
 import { PaymentMethod } from 'src/fiatExchanges/utils'
+import { createMockStore } from 'test/utils'
 import { mockProviders } from 'test/values'
 
 describe('CoinbasePaymentSection', () => {
   let props: CoinbasePaymentSectionProps
+  let mockStore: MockStoreEnhanced
   beforeEach(() => {
     props = {
       coinbaseProvider: mockProviders.find(
@@ -16,16 +20,61 @@ describe('CoinbasePaymentSection', () => {
       )!,
       setNoPaymentMethods: jest.fn(),
     }
+    mockStore = createMockStore({
+      app: {
+        coinbasePayEnabled: false,
+      },
+    })
   })
-  it('shows nothing if unique provider, IE. coinbase, is restricted', async () => {
+  it('shows nothing if coinbase is restricted and feature flag is false', async () => {
     props.coinbaseProvider!.restricted = true
-    const { queryByText } = render(<CoinbasePaymentSection {...props} />)
+    const { queryByText } = render(
+      <Provider store={mockStore}>
+        <CoinbasePaymentSection {...props} />
+      </Provider>
+    )
     expect(queryByText('Coinbase Pay')).toBeFalsy()
-    expect(props.setNoPaymentMethods).toHaveBeenCalledWith(true)
+    expect(props.setNoPaymentMethods).toHaveBeenCalled()
   })
-  it('shows a card if unique provider is not restricted', async () => {
+  it('shows nothing if coinbase is not restricted but feature flag is false', async () => {
     props.coinbaseProvider!.restricted = false
-    const { queryByText } = render(<CoinbasePaymentSection {...props} />)
+    const { queryByText } = render(
+      <Provider store={mockStore}>
+        <CoinbasePaymentSection {...props} />
+      </Provider>
+    )
+    expect(queryByText('Coinbase Pay')).toBeFalsy()
+    expect(props.setNoPaymentMethods).toHaveBeenCalled()
+  })
+  it('shows nothign if coinbase is restricted and feature flag is true', async () => {
+    props.coinbaseProvider!.restricted = true
+    mockStore = createMockStore({
+      ...mockStore,
+      app: {
+        coinbasePayEnabled: true,
+      },
+    })
+    const { queryByText } = render(
+      <Provider store={mockStore}>
+        <CoinbasePaymentSection {...props} />
+      </Provider>
+    )
+    expect(queryByText('Coinbase Pay')).toBeFalsy()
+    expect(props.setNoPaymentMethods).toHaveBeenCalled()
+  })
+  it('shows card if coinbase is not restricted and feature flag is true', async () => {
+    props.coinbaseProvider!.restricted = false
+    mockStore = createMockStore({
+      ...mockStore,
+      app: {
+        coinbasePayEnabled: true,
+      },
+    })
+    const { queryByText } = render(
+      <Provider store={mockStore}>
+        <CoinbasePaymentSection {...props} />
+      </Provider>
+    )
     expect(queryByText('Coinbase Pay')).toBeTruthy()
     expect(props.setNoPaymentMethods).not.toHaveBeenCalled()
   })
