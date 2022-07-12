23a1f2d6791ecbda9692ea76a5d0d20fa13ab2d0
Samuel Shin <47931506+Samuel3Shin@users.noreply.github.com>
2022-07-12 08:54:34 -0700

    feat(In-app swaps): Update drawer menu items (#2664)
    ### Description
    - updated the order of existing drawer menu items and the icons of the menu item
    - added swap menu item in the drawer menu
    - showing the swap menu is controlled by remote config - showSwapMenuInDrawerMenu (default: not showing. This is because the swap screen is not yet implemented)
    
    current design <--------------------------------------------> updated design
    <img width="361" alt="Screen Shot 2022-06-30 at 5 44 07 PM" src="https://user-images.githubusercontent.com/47931506/176800992-9fdc6398-360c-454e-b71e-708146463d87.png"> <img width="362" alt="Screen Shot 2022-06-30 at 5 42 19 PM" src="https://user-images.githubusercontent.com/47931506/176801012-9a15437a-3f78-4eb6-8bda-b240539db40c.png"> 
    
    ### Other changes
    Updated `valora_branding_sha` in `sync_branding.sh` to the latest commit hash of `valora-app-branding` repo
    
    ### Tested
    Tested in iOS emulator with Firebase remote config.
    
    ### How others should test
    With iOS, Android emulators.
    
    ### Related issues
    - RET-319 (https://linear.app/valora/issue/RET-319/[in-app-swaps]-update-drawer-menu-items)
    
    ### Backwards compatibility
    Yes

--- src/firebase/remoteConfigValuesDefaults.ts
+++ src/firebase/remoteConfigValuesDefaults.ts
@@ -76,4 +76,5 @@ export const REMOTE_CONFIG_VALUES_DEFAULTS: Omit<
   dappConnectInfo: DappConnectInfo.Default,
   visualizeNFTsEnabledInHomeAssetsPage: false,
   coinbasePayEnabled: false,
+  showSwapMenuInDrawerMenu: false,
 }
