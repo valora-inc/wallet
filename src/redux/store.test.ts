7ad808ab9cf1dbe136cf1d58cad3f54dd51d14d6
Samuel3Shin <samuel3.shin@gmail.com>
2022-07-18 11:03:19 -0700

    changed redux store version to 62

--- src/redux/store.test.ts
+++ src/redux/store.test.ts
@@ -93,7 +93,7 @@ describe('store state', () => {
       Object {
         "_persist": Object {
           "rehydrated": true,
-          "version": 61,
+          "version": 62,
         },
         "account": Object {
           "acceptedTerms": false,
