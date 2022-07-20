4c61341f6cc0266d6b3988d90b1d92039b8499e5
Alvin Tsui <awtsui@uci.edu>
2022-07-20 11:30:53 -0700

    prettify

--- src/fiatExchanges/SelectProvider.test.tsx
+++ src/fiatExchanges/SelectProvider.test.tsx
@@ -189,5 +189,4 @@ describe(SelectProviderScreen, () => {
     expect(queryByText('selectProviderScreen.cryptoExchange')).toBeFalsy()
     expect(queryByText('selectProviderScreen.mobileMoney')).toBeFalsy()
   })
-
 })
