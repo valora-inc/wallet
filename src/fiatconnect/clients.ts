df2fd636d9b256642e31c5f455cf0239b304fee6
anthonydaoud <anthonydaoud01@gmail.com>
2022-08-15 17:32:49 -0400

    added api key change check

--- src/fiatconnect/clients.ts
+++ src/fiatconnect/clients.ts
@@ -6,7 +6,10 @@ import { getPassword } from 'src/pincode/authentication'
 import { UNLOCK_DURATION } from 'src/web3/consts'
 import { getWalletAsync } from 'src/web3/contracts'
 
-const fiatConnectClients: Record<string, { url: string; client: FiatConnectApiClient }> = {}
+const fiatConnectClients: Record<
+  string,
+  { url: string; apiKey: string | undefined; client: FiatConnectApiClient }
+> = {}
 
 export function getSigningFunction(wallet: UnlockableWallet): (message: string) => Promise<string> {
   return async function (message: string): Promise<string> {
@@ -24,11 +27,16 @@ export async function getFiatConnectClient(
   providerBaseUrl: string,
   providerApiKey?: string
 ): Promise<FiatConnectApiClient> {
-  if (!fiatConnectClients[providerId] || fiatConnectClients[providerId].url !== providerBaseUrl) {
+  if (
+    !fiatConnectClients[providerId] ||
+    fiatConnectClients[providerId].url !== providerBaseUrl ||
+    fiatConnectClients[providerId].apiKey !== providerApiKey
+  ) {
     const wallet = (await getWalletAsync()) as UnlockableWallet
     const [account] = wallet.getAccounts()
     fiatConnectClients[providerId] = {
       url: providerBaseUrl,
+      apiKey: providerApiKey,
       client: new FiatConnectClient(
         {
           baseUrl: providerBaseUrl,
