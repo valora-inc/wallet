1f41ddb4cfe84c137252632e7d9939205347f305
Satish Ravi <satish.ravi@valoraapp.com>
2022-08-15 23:53:02 -0700

    feat(ihl): use siwe auth

--- src/in-house-liquidity/index.test.ts
+++ src/in-house-liquidity/index.test.ts
@@ -1,23 +1,21 @@
-import { signAndFetch, verifyWalletAddress, createPersonaAccount } from 'src/in-house-liquidity'
 import { FetchMock } from 'jest-fetch-mock/types'
+import { createPersonaAccount } from 'src/in-house-liquidity'
 import networkConfig from 'src/web3/networkConfig'
 import { mockAccount } from 'test/values'
+
+const mockFetch = fetch as FetchMock
+
+jest.mock('src/in-house-liquidity/client', () => ({
+  getClient: jest.fn(() => ({
+    fetch: jest.fn(mockFetch),
+  })),
+}))
 
 const MOCK_USER = {
   walletAddress: mockAccount,
 }
 
 describe('In House Liquidity Calls', () => {
-  const mockFetch = fetch as FetchMock
-
-  beforeAll(() => {
-    jest.useFakeTimers()
-    jest.spyOn(Date, 'now').mockImplementation(() => 1641945400000)
-  })
-
-  afterAll(() => {
-    jest.useRealTimers()
-  })
   beforeEach(() => {
     mockFetch.resetMocks()
     jest.clearAllMocks()
@@ -44,50 +42,6 @@ describe('In House Liquidity Calls', () => {
       )
       // Returns nothing
       expect(response).toEqual(undefined)
-    })
-  })
-
-  describe('verifyWalletAddress', () => {
-    it('throws when walletAddress is null', () => {
-      expect(() =>
-        verifyWalletAddress({
-          walletAddress: null,
-        })
-      ).toThrow('Cannot call IHL because walletAddress is null')
-    })
-  })
-
-  describe('signAndFetch', () => {
-    it('calls fetch with the correct params', async () => {
-      mockFetch.mockResponseOnce(JSON.stringify({}), { status: 201 })
-      const body = { accountAddress: MOCK_USER.walletAddress }
-
-      const response = await signAndFetch({
-        path: '/persona/account/create',
-        walletAddress: MOCK_USER.walletAddress,
-        requestOptions: {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(body),
-        },
-      })
-
-      // Calls fetch correctly
-      expect(mockFetch).toHaveBeenCalledWith(
-        `${networkConfig.inHouseLiquidityURL}/persona/account/create`,
-        {
-          body: JSON.stringify(body),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          method: 'POST',
-        }
-      )
-
-      // Returns the response object
-      expect(response).toBeInstanceOf(Response)
     })
   })
 })
--- src/in-house-liquidity/index.ts
+++ src/in-house-liquidity/index.ts
@@ -1,13 +1,8 @@
+import { getClient } from 'src/in-house-liquidity/client'
 import networkConfig from 'src/web3/networkConfig'
 
 interface RequiredParams {
   walletAddress: string
-}
-
-interface SignAndFetchParams {
-  path: string
-  walletAddress: string
-  requestOptions: RequestInit
 }
 
 /**
@@ -18,55 +13,18 @@ interface SignAndFetchParams {
  */
 export const createPersonaAccount = async ({ walletAddress }: RequiredParams): Promise<void> => {
   const body = { accountAddress: walletAddress }
-  const response = await signAndFetch({
-    path: '/persona/account/create',
-    walletAddress,
-    requestOptions: {
+  const ihlClient = await getClient()
+  const response = await ihlClient.fetch(
+    `${networkConfig.inHouseLiquidityURL}/persona/account/create`,
+    {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(body),
-    },
-  })
+    }
+  )
   if (response.status !== 201 && response.status !== 409) {
     throw new Error(`IHL /persona/account/create failure status ${response.status}`)
-  }
-}
-
-/**
- * A fetch wrapper that adds in the signature needed for IHL authorization
- *
- *
- * @param {params.path} string like /persona/get/foo
- * @param {params.requestOptions} requestOptions all the normal fetch options
- * @returns {Response} response object from the fetch call
- */
-export const signAndFetch = async ({
-  path,
-  requestOptions,
-}: SignAndFetchParams): Promise<Response> => {
-  return fetch(`${networkConfig.inHouseLiquidityURL}${path}`, {
-    ...requestOptions,
-    headers: {
-      ...requestOptions.headers,
-      // Authorization: authHeader, // todo add this once auth scheme is refactored
-    },
-  })
-}
-
-// just checks that the wallet address is non null for now. keeping this since we may want it to do more once
-//  the new auth scheme is figured out (like checking if a wallet is unlocked or a token is truthy)
-export const verifyWalletAddress = ({
-  walletAddress,
-}: {
-  walletAddress: string | null
-}): RequiredParams => {
-  if (!walletAddress) {
-    throw new Error('Cannot call IHL because walletAddress is null')
-  }
-
-  return {
-    walletAddress: walletAddress,
   }
 }
