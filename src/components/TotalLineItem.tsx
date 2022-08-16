9ea15aa749d463000c58caf581409d0a4ff82466
Xiaoyao Xu <xiaoyao.xu@valoraapp.com>
2022-08-16 15:07:48 -0700

    TotalLineItem reales translation

--- src/components/TotalLineItem.tsx
+++ src/components/TotalLineItem.tsx
@@ -26,6 +26,7 @@ const totalAmountKey = {
   [Currency.Dollar]: 'totalInDollars',
   [Currency.Euro]: 'totalInEuros',
   [Currency.Celo]: 'totalInCelo',
+  ['cREAL']: 'totalInReales',
 }
 
 export default function TotalLineItem({
