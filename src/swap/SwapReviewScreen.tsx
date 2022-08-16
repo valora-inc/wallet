8fb8545b795317c79f6ce0059191e733a1a8c148
MuckT <mcgtom10@gmail.com>
2022-08-16 14:00:40 -0700

    Revert "fix: remove unneeded navOptions"
    This reverts commit a4c7a335213039b2faef30dea75fa44f1b427aa5.

--- src/swap/SwapReviewScreen.tsx
+++ src/swap/SwapReviewScreen.tsx
@@ -22,6 +22,7 @@ import CustomHeader from 'src/components/header/CustomHeader'
 import TokenDisplay, { formatValueToDisplay } from 'src/components/TokenDisplay'
 import Touchable from 'src/components/Touchable'
 import InfoIcon from 'src/icons/InfoIcon'
+import { noHeader } from 'src/navigator/Headers'
 import { Screens } from 'src/navigator/Screens'
 import { StackParamList } from 'src/navigator/types'
 import DisconnectBanner from 'src/shared/DisconnectBanner'
@@ -360,5 +361,9 @@ const styles = StyleSheet.create({
     textDecorationStyle: 'solid',
   },
 })
+
+SwapReviewScreen.navOptions = {
+  ...noHeader,
+}
 
 export default SwapReviewScreen
