0b393c2b8918511ff43e2d723ab325e754afca11
MuckT <mcgtom10@gmail.com>
2022-08-29 18:53:14 -0700

    fix: lint error prefer-const

--- src/onboarding/education/OnboardingEducationScreen.tsx
+++ src/onboarding/education/OnboardingEducationScreen.tsx
@@ -23,7 +23,7 @@ function useStep() {
   const shuffleArray = (arr: Array<any>) => {
     for (let i = arr.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1))
-      let temp = arr[i]
+      const temp = arr[i]
       arr[i] = arr[j]
       arr[j] = temp
     }
