3dc365bcfae25b64be9d2ad18c8d3b9d30e6a4c4
MuckT <mcgtom10@gmail.com>
2022-08-30 11:41:50 -0700

    fix: lint errors

--- src/utils/array.test.ts
+++ src/utils/array.test.ts
@@ -15,21 +15,21 @@ describe('shuffleArray', () => {
 
   it('results should be evenly distributed across many calls', () => {
     const array = [1, 2, 3]
-    let resultObj = {} as any
+    const resultObj = {} as any
     // Check that all random results are evenly distributed - 1% difference allowed
-    let lowerInclusive = 1566
-    let upperExclusive = 1766
+    const lowerInclusive = 1566
+    const upperExclusive = 1766
     for (let i = 0; i < 10000; i++) {
-      let result = shuffleArray(array).join('-')
+      const result = shuffleArray(array).join('-')
       if (resultObj[result]) {
         resultObj[result]++
       } else {
         resultObj[result] = 1
       }
     }
-    for (let key in resultObj) {
-      expect(resultObj[key]).toBeGreaterThanOrEqual(lowerInclusive)
-      expect(resultObj[key]).toBeLessThan(upperExclusive)
-    }
+    Object.entries(resultObj).forEach((entry) => {
+      expect(entry[1]).toBeGreaterThanOrEqual(lowerInclusive)
+      expect(entry[1]).toBeLessThan(upperExclusive)
+    })
   })
 })
