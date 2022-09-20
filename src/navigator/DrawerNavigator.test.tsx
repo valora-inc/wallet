a6acd0ddb51bb0181e04410f62135c50c7dfd94b
Xiaoyao Xu <xiaoyao.xu@valoraapp.com>
2022-09-20 15:57:03 -0700

    fix dom rendering for nav drawer

--- src/navigator/DrawerNavigator.test.tsx
+++ src/navigator/DrawerNavigator.test.tsx
@@ -6,11 +6,12 @@ import DrawerNavigator from 'src/navigator/DrawerNavigator'
 import MockedNavigator from 'test/MockedNavigator'
 import { createMockStore } from 'test/utils'
 
-// This avoids rendering WalletHome as we're mostly interested in testing the menu here
-jest.mock('src/home/WalletHome')
+// TODO avoid rendering WalletHome as we're mostly interested in testing the menu here
+
 describe('DrawerNavigator', () => {
   // TODO: need to add tests here now that the DrawerNavigator no longer displays balances
   it.todo('renders correctly')
+
   it('does not include username when username is empty', () => {
     const store = createMockStore({
       account: {
