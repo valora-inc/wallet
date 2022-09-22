c479c6c4861892de54fe1ca57543037957289c55
Xiaoyao Xu <xiaoyao.xu@valoraapp.com>
2022-09-22 08:44:53 -0700

    Add persona button to kyclanding

--- src/fiatconnect/KycLanding.tsx
+++ src/fiatconnect/KycLanding.tsx
@@ -1,9 +1,11 @@
-import { KycStatus as PersonaKycStatus } from 'src/account/reducer'
 import * as React from 'react'
 import { StyleSheet } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { CICOFlow } from 'src/fiatExchanges/utils'
+import Persona from 'src/account/Persona'
+import { KycStatus as PersonaKycStatus } from 'src/account/reducer'
 import FiatConnectQuote from 'src/fiatExchanges/quotes/FiatConnectQuote'
+import { CICOFlow } from 'src/fiatExchanges/utils'
+import { postKyc } from 'src/in-house-liquidity'
 
 export interface Props {
   personaKycStatus: PersonaKycStatus
@@ -12,7 +14,20 @@ export interface Props {
 }
 
 function KycLanding(_props: Props) {
-  return <SafeAreaView style={styles.container} />
+  const sendKYCSchema = async () => {
+    if (!_props.quote.quoteResponseKycSchema) {
+      throw 'expected KYC schema to send is nullish'
+    }
+    await postKyc({
+      providerInfo: _props.quote.quote.provider,
+      kycSchema: _props.quote.quoteResponseKycSchema.kycSchema,
+    })
+  }
+  return (
+    <SafeAreaView style={styles.container}>
+      <Persona kycStatus={_props.personaKycStatus} onSuccess={sendKYCSchema} />
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
