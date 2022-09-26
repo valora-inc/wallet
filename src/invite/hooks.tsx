40c277fdf5c3676b0a427ff545be54c91d48c65d
Jacob Turner <j.israel.turner@gmail.com>
2022-09-26 13:40:40 -0600

    Add error handling

--- src/invite/hooks.tsx
+++ src/invite/hooks.tsx
@@ -3,27 +3,40 @@ import { useState } from 'react'
 import { useAsync } from 'react-async-hook'
 import { useTranslation } from 'react-i18next'
 import { Share } from 'react-native'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { showError } from 'src/alert/actions'
 import { InviteEvents } from 'src/analytics/Events'
 import ValoraAnalytics from 'src/analytics/ValoraAnalytics'
-import { FIREBASE_ENABLED } from 'src/config'
+import Logger from 'src/utils/Logger'
 
+import { FIREBASE_ENABLED } from 'src/config'
 import { walletAddressSelector } from 'src/web3/selectors'
 
 import { createDynamicLink } from './utils'
+
+const TAG = 'InviteWithReferralURL'
 
 export function useShareUrl() {
   const [shareUrl, setShareUrl] = useState<string | null>(null)
 
   const address = useSelector(walletAddressSelector)
   const { t } = useTranslation()
+  const dispatch = useDispatch()
 
   const message = isNil(shareUrl) ? null : t('inviteWithUrl.share', { shareUrl })
 
   useAsync(async () => {
-    if (isNil(address) || !FIREBASE_ENABLED) return
-    const url = await createDynamicLink(address)
-    setShareUrl(url)
+    try {
+      if (isNil(address) || !FIREBASE_ENABLED) {
+        return
+      } else {
+        const url = await createDynamicLink(address)
+        setShareUrl(url)
+      }
+    } catch (e) {
+      Logger.debug(TAG, 'Error while creating a dynamic link', e)
+      dispatch(showError(t('inviteWithUrl.error')))
+    }
   }, [address])
 
   return isNil(message)
