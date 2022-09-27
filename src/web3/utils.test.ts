5e9ad2b759e8ee8ed4075022b2ce90c585d8eed8
MuckT <mcgtom10@gmail.com>
2022-09-26 17:59:08 -0700

    test(web3/utils): use const instead of let

--- src/web3/utils.test.ts
+++ src/web3/utils.test.ts
@@ -5,13 +5,13 @@ describe('applyChainIdWorkaround', () => {
     const preTx = {
       chainId: 42220,
     }
-    let tx = applyChainIdWorkaround(preTx, 0)
+    const tx = applyChainIdWorkaround(preTx, 0)
     expect(tx.chainId).toEqual('0xa4ec')
   })
 
   it('applyChainIdWorkaround should work with chainId param', async () => {
     const preTx = {}
-    let tx = applyChainIdWorkaround(preTx, 42220)
+    const tx = applyChainIdWorkaround(preTx, 42220)
     expect(tx.chainId).toEqual('0xa4ec')
   })
 })
