8eb9d5e8430dd7e5c29efa62e19b4ddc38f903e3
Satish Ravi <satish.ravi@valoraapp.com>
2022-09-30 19:49:33 -0700

    testing

--- src/account/__mocks__/Persona.tsx
+++ src/account/__mocks__/Persona.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
-import { Button } from 'react-native'
+import { Button, View } from 'react-native'
 
 const MockPersona = (props: any) => {
-  return <Button title={'mock Persona button'} testID="PersonaButton" /> // onPress not passed thru from props
+  return (
+    <>
+      <View testID="PersonaView"></View>
+      <Button
+        title={'mock Persona button'}
+        testID="PersonaButton"
+        onPress={() => {
+          console.log('something else')
+        }}
+      />
+    </>
+  ) // onPress not passed thru from props
 }
 
 export default MockPersona
--- src/fiatconnect/BugDemo.test.tsx
+++ src/fiatconnect/BugDemo.test.tsx
@@ -1,15 +1,39 @@
 import { fireEvent, render } from '@testing-library/react-native'
 import React from 'react'
+import { Provider } from 'react-redux'
+import { createMockStore } from 'test/utils'
+import { mockAccount, mockPrivateDEK } from 'test/values'
 import Persona from '../account/Persona'
 import { KycStatus } from '../account/reducer'
 
 jest.mock('src/account/Persona')
 describe('bug demo', () => {
+  const store = createMockStore({
+    web3: {
+      mtwAddress: mockAccount,
+      dataEncryptionKey: mockPrivateDEK,
+    },
+  })
   it('uses onClick provided', () => {
     const onPress = jest.fn()
-    const { getByTestId } = render(
-      <Persona onPress={onPress} kycStatus={KycStatus.Approved} disabled={false} />
+    const { debug, getByTestId } = render(
+      <Provider store={store}>
+        <Persona onPress={onPress} kycStatus={KycStatus.Approved} disabled={false} />
+      </Provider>
     )
+    debug()
+    fireEvent.press(getByTestId('PersonaView')) // this test fails (as one would expect, since the onPress handler is ignored in the mock component)
+    expect(onPress).toHaveBeenCalled()
+  })
+
+  it('uses onClick provided 2', () => {
+    const onPress = jest.fn()
+    const { debug, getByTestId } = render(
+      <Provider store={store}>
+        <Persona onPress={onPress} kycStatus={KycStatus.Approved} disabled={false} />
+      </Provider>
+    )
+    debug()
     fireEvent.press(getByTestId('PersonaButton')) // this test fails (as one would expect, since the onPress handler is ignored in the mock component)
     expect(onPress).toHaveBeenCalled()
   })
