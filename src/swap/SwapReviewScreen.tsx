97f0a4acf505895cc1a4b519989a6684c6b86e64
MuckT <mcgtom10@gmail.com>
2022-10-06 11:15:55 -0700

    chore: remove swap to celo workaround

--- src/swap/SwapReviewScreen.tsx
+++ src/swap/SwapReviewScreen.tsx
@@ -69,13 +69,6 @@ export function SwapReviewScreen() {
   const { t } = useTranslation()
   const dispatch = useDispatch()
 
-  // Workaround for buying Celo
-  const CELO_ADDRESS = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'
-  const toCeloWorkAround = (token: string) => {
-    const symbol = coreTokens.find((token) => token.address === toToken)?.symbol
-    return symbol === 'CELO' ? CELO_ADDRESS : token
-  }
-
   // Token Symbols
   const toTokenSymbol = coreTokens.find((token) => token.address === toToken)?.symbol
   const fromTokenSymbol = coreTokens.find((token) => token.address === fromToken)?.symbol
@@ -95,7 +88,7 @@ export function SwapReviewScreen() {
       const swapAmountInWei = multiplyByWei(swapAmount[updatedField]!)
       const swapAmountParam = updatedField === Field.FROM ? 'sellAmount' : 'buyAmount'
       const params = {
-        buyToken: toCeloWorkAround(toToken),
+        buyToken: toToken,
         sellToken: fromToken,
         [swapAmountParam]: swapAmountInWei.toString().split('.')[0],
         // Enable when supported by 0xAPI & valora-rest-api
