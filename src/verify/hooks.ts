81325b50b213c448bd9d585e07c053d804ded626
Kathy Luo <kathyluo18@gmail.com>
2022-10-07 16:43:49 +0200

    chore: augment request body of verifyPhoneNumber with extra params (#2960)
    ### Description
    
    Add client bundle id for
    https://valora-app.slack.com/archives/C044B8ULVC0/p1665055690215779
    Add DEK public key for comment encryption
    
    ### Other changes
    
    N/A
    
    ### Tested
    
    Updated tests, test verification manually
    
    ### How others should test
    
    N/A
    
    ### Related issues
    
    Relates RET-442
    
    ### Backwards compatibility
    
    N/A

--- src/verify/hooks.ts
+++ src/verify/hooks.ts
@@ -1,3 +1,5 @@
+import { hexToBuffer } from '@celo/utils/lib/address'
+import { compressedPubKey } from '@celo/utils/lib/dataEncryptionKey'
 import { useRef, useState } from 'react'
 import { useAsync } from 'react-async-hook'
 import { Platform } from 'react-native'
@@ -11,7 +13,7 @@ import { ErrorMessages } from 'src/app/ErrorMessages'
 import { retrieveSignedMessage } from 'src/pincode/authentication'
 import Logger from 'src/utils/Logger'
 import networkConfig from 'src/web3/networkConfig'
-import { walletAddressSelector } from 'src/web3/selectors'
+import { dataEncryptionKeySelector, walletAddressSelector } from 'src/web3/selectors'
 
 const TAG = 'verify/hooks'
 
@@ -50,6 +52,7 @@ export function useVerifyPhoneNumber(phoneNumber: string, countryCallingCode: st
 
   const dispatch = useDispatch()
   const address = useSelector(walletAddressSelector)
+  const privateDataEncryptionKey = useSelector(dataEncryptionKeySelector)
 
   const [verificationStatus, setVerificationStatus] = useState(PhoneNumberVerificationStatus.NONE)
   const [verificationId, setVerificationId] = useState('')
@@ -94,6 +97,10 @@ export function useVerifyPhoneNumber(phoneNumber: string, countryCallingCode: st
         return
       }
 
+      if (!privateDataEncryptionKey) {
+        throw new Error('No data encryption key was found in the store. This should never happen.')
+      }
+
       Logger.debug(`${TAG}/requestVerificationCode`, 'Initiating request to verifyPhoneNumber')
       const signedMessage = await retrieveSignedMessage()
 
@@ -107,6 +114,8 @@ export function useVerifyPhoneNumber(phoneNumber: string, countryCallingCode: st
           phoneNumber,
           clientPlatform: Platform.OS,
           clientVersion: DeviceInfo.getVersion(),
+          clientBundleId: DeviceInfo.getBundleId(),
+          publicDataEncryptionKey: compressedPubKey(hexToBuffer(privateDataEncryptionKey)),
         }),
       })
       if (response.ok) {
