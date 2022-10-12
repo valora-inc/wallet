a1823b1025c3086a57cc22ed77b3aada0a91e3a6
anthonydaoud <anthonydaoud01@gmail.com>
2022-10-11 19:41:28 -0500

    missed undo file

--- src/firebase/remoteConfigValuesDefaults.ts
+++ src/firebase/remoteConfigValuesDefaults.ts
@@ -9,14 +9,10 @@ export const REMOTE_CONFIG_VALUES_DEFAULTS: Omit<
   | 'celoEducationUri'
   | 'komenciAllowedDeployers'
   | 'dappListApiUrl'
-  | 'usernameAdjectiveBlocklist'
-  | 'usernameNounBlocklist'
   | 'sentryNetworkErrors'
   | 'superchargeTokenConfigByToken'
 > & {
   komenciAllowedDeployers: string
-  usernameAdjectiveBlocklist: string
-  usernameNounBlocklist: string
   sentryNetworkErrors: string
   superchargecUSDMin: number
   superchargecUSDMax: number
@@ -77,6 +73,4 @@ export const REMOTE_CONFIG_VALUES_DEFAULTS: Omit<
   inviteMethod: InviteMethodType.Escrow,
   showGuidedOnboardingCopy: false,
   centralPhoneVerificationEnabled: false,
-  usernameAdjectiveBlocklist: '',
-  usernameNounBlocklist: '',
 }
