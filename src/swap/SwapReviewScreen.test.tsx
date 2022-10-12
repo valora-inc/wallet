0ebcff37d134a8185448031323c0780aac8a468a
MuckT <mcgtom10@gmail.com>
2022-10-11 18:16:13 -0700

    test(swapReview): update expected value for fee display

--- src/swap/SwapReviewScreen.test.tsx
+++ src/swap/SwapReviewScreen.test.tsx
@@ -156,7 +156,7 @@ describe('SwapReviewScreen', () => {
       // Estimated Gas
       expect(getByTestId('EstimatedGas')).toHaveTextContent('0.00015 CELO')
       // Swap Fee
-      expect(getByTestId('SwapFee')).toHaveTextContent('Free')
+      expect(getByTestId('SwapFee')).toHaveTextContent('swapReviewScreen.free')
     })
   })
 
