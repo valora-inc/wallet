4b76621b4eb3275909c1cedcb97958dcf08d02f3
MuckT <mcgtom10@gmail.com>
2022-10-18 15:17:08 -0700

    test(KeyboardAwareScrollView): add test ID and scroll to submit

--- e2e/src/usecases/SecureSend.js
+++ e2e/src/usecases/SecureSend.js
@@ -1,5 +1,5 @@
 import { reloadReactNative } from '../utils/retries'
-import { enterPinUiIfNecessary, inputNumberKeypad, sleep } from '../utils/utils'
+import { enterPinUiIfNecessary, inputNumberKeypad, scrollIntoView, sleep } from '../utils/utils'
 const faker = require('@faker-js/faker')
 
 const PHONE_NUMBER = '+12057368924'
@@ -46,8 +46,8 @@ export default SecureSend = () => {
       await element(by.id(`SingleDigitInput/digit${index}`)).replaceText(character)
     }
 
-    // Tap to dismiss keyboard
-    await element(by.id('ConfirmAccountNumber/Title')).tap()
+    // Scroll to see submit button
+    await scrollIntoView('Submit', 'KeyboardAwareScrollView')
     await element(by.id('ConfirmAccountButton')).tap()
 
     // Write a comment.
