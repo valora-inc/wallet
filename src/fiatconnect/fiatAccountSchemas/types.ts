2d7ce14c9292bc78287f1db45dd5965fb3241a24
Jacob Waterman <jacobrwaterman@gmail.com>
2022-10-19 08:40:54 -0700

    refactor(fiatconnect): Refactor FiatDetails to prepare for more fiatAccountSchemas (#3009)
    ### Description
    
    There was feedback in the IBAN pr to split into multiple files. I
    thought it would be easier to do the bulk of that upfront.
    
    ### Tested
    
    refactor so no additional tests. Verified running locally
    
    Co-authored-by: jh2oman <jacobwaterman@MacBook-Pro.local>
    Co-authored-by: Tom McGuire <Mcgtom10@gmail.com>

---
+++ src/fiatconnect/fiatAccountSchemas/types.ts
@@ -0,0 +1,27 @@
+import { FiatAccountSchema, FiatAccountSchemas } from '@fiatconnect/fiatconnect-types'
+import { KeyboardType } from 'react-native'
+
+export interface FormFieldParam {
+  name: string
+  label: string
+  regex: RegExp
+  placeholderText: string
+  errorMessage?: string
+  keyboardType: KeyboardType
+}
+export interface ImplicitParam<T, K extends keyof T> {
+  name: string
+  value: T[K]
+}
+
+export interface ComputedParam<T, K extends keyof T> {
+  name: string
+  computeValue: (otherFields: Partial<T>) => T[K]
+}
+
+export type FiatAccountFormSchema<T extends FiatAccountSchema> = {
+  [Property in keyof FiatAccountSchemas[T]]:
+    | FormFieldParam
+    | ImplicitParam<FiatAccountSchemas[T], Property>
+    | ComputedParam<FiatAccountSchemas[T], Property>
+}
