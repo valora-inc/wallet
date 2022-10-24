96130b3d9ea32951d8cb631f9c1830ff510c84bf
Joseph Bergeron <jophish126@gmail.com>
2022-10-23 22:50:35 -0400

    fix test

--- src/fiatconnect/TransferStatusScreen.test.tsx
+++ src/fiatconnect/TransferStatusScreen.test.tsx
@@ -145,6 +145,7 @@ describe('TransferStatusScreen', () => {
           institutionName: 'some-bank',
           fiatAccountType: FiatAccountType.BankAccount,
           fiatAccountSchema: FiatAccountSchema.AccountNumber,
+          providerId: mockQuote.getProviderId(),
         },
         normalizedQuote: mockQuote,
         shouldRefetchQuote: true,
