6a5185e1ea8b4551df88ad7edf64ee73cef7ad40
anthonydaoud <anthonydaoud01@gmail.com>
2022-11-01 10:56:20 -0400

    added comments

--- e2e/src/usecases/FiatConnectTransferOut.js
+++ e2e/src/usecases/FiatConnectTransferOut.js
@@ -77,6 +77,12 @@ async function submitTransfer() {
   await expect(element(by.id('SendOrRequestBar'))).toBeVisible() // proxy for reaching home screen, imitating NewAccountOnboarding e2e test
 }
 
+/**
+ * Set the kycStatus for a wallet address on the mock provider.
+ *
+ * @param kycStatus: the kycStatus string
+ * @param walletAddress: the address string for which the kycStatus should be set
+ */
 async function setWalletKycStatus(kycStatus, walletAddress) {
   const requestOptions = {
     method: 'PUT',
@@ -95,7 +101,18 @@ async function setWalletKycStatus(kycStatus, walletAddress) {
   }
 }
 
-async function setupWalletForTransferOut(token, fundingAmount, cashOutAmount) {
+/**
+ * From a newly installed app, setup and start a transfer out.
+ *
+ * Onboards a new wallet and funds it before beginning a transfer out
+ *  with the mock FiatConnect provider.
+ *
+ * @param token: string of the token that gets funded to the wallet
+ * @param fundingAmount: token amount to be deposited
+ * @param cashOutAmount: token amount to be transfered out (doesn't include gas)
+ * @returns the address of the now-funded wallet
+ */
+async function onboardAndBeginTransferOut(token, fundingAmount, cashOutAmount) {
   const mnemonic = await generateMnemonic()
   const { address: walletAddress } = await generateKeys(mnemonic)
   await quickOnboarding(mnemonic) // ends on home screen
@@ -127,15 +144,26 @@ async function setupWalletForTransferOut(token, fundingAmount, cashOutAmount) {
   return walletAddress
 }
 
+/**
+ * From the FiatDetailsScreen, enter bank information and submit
+ *
+ * Currently assumes that the AccountNumber schema is used
+ *
+ * @return {{result: Error}}
+ */
 async function enterAccountInformation() {
-  // FiatDetailsScreen
-  // assumes AccountNumber quote schema
   await expect(element(by.text('Enter Bank Information'))).toBeVisible()
   await element(by.id('input-institutionName')).replaceText('My Bank Name')
   await element(by.id('input-accountNumber')).replaceText('1234567890')
   await element(by.id('submitButton')).tap()
 }
 
+/**
+ * From the home screen, navigate to FiatExchange and transfer out.
+ *
+ * @param token: string of the token that will be transferred out
+ * @param cashOutAmount: token amount to withdraw
+ */
 async function returnUserTransferOut(token, cashOutAmount) {
   await navigateToFiatExchangeScreen()
   await waitForElementId('cashOut')
@@ -153,7 +181,7 @@ export const fiatConnectNonKycTransferOut = () => {
     const gasAmount = 0.005
     const fundingAmount = `${2 * cashOutAmount + gasAmount}`
     const token = 'cUSD'
-    await setupWalletForTransferOut(token, fundingAmount, cashOutAmount)
+    await onboardAndBeginTransferOut(token, fundingAmount, cashOutAmount)
 
     // LinkAccountScreen
     await expect(element(by.text('Set Up Bank Account'))).toBeVisible()
@@ -177,7 +205,7 @@ export const fiatConnectKycTransferOut = () => {
     const gasAmount = 0.005
     const fundingAmount = `${2 * cashOutAmount + gasAmount}`
     const token = 'cUSD'
-    const walletAddress = await setupWalletForTransferOut(token, fundingAmount, cashOutAmount)
+    const walletAddress = await onboardAndBeginTransferOut(token, fundingAmount, cashOutAmount)
 
     // KycLanding
 
@@ -194,18 +222,25 @@ export const fiatConnectKycTransferOut = () => {
       .toBeVisible()
       .withTimeout(5000)
     await element(by.text('Begin verifying')).tap()
+
+    // Country of govt id screen
     await expect(element(by.text('Select'))).toBeVisible()
     await element(by.text('Select')).tap()
+
+    // Id select screen
     await expect(element(by.text('Driver License'))).toBeVisible()
     await element(by.text('Driver License')).tap()
     await expect(element(by.text('Enable camera'))).toBeVisible()
     await element(by.text('Enable camera')).tap()
 
-    //Manually wait for Take Photo button to appear, withTimeout didn't work
+    // Manually wait for Take Photo button to appear, withTimeout didn't work
     await sleep(10000)
 
+    // License photo
     await element(by.label('Take photo')).tap()
     await element(by.text('Use this photo')).tap()
+
+    // Selfie section
     await waitFor(element(by.text('Get started')))
       .toBeVisible()
       .withTimeout(5000)
@@ -214,12 +249,14 @@ export const fiatConnectKycTransferOut = () => {
     await element(by.label('shutter button')).tap()
     await element(by.label('shutter button')).tap()
 
+    // Name, number, address form
     await waitFor(element(by.text('Phone Number')))
       .toBeVisible()
       .withTimeout(5000)
     await element(by.type('PersonaPhoneNumberKit2.PhoneNumberTextField')).typeText('0123456789')
+
     await element(by.text('Continue')).tap()
-    await element(by.text('Done')).tap()
+    await element(by.text('Done')).tap() // End of Persona flow
 
     await setWalletKycStatus(KycStatus.KycApproved, walletAddress)
 
