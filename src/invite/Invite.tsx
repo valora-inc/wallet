a8fce24fa40dfd1f61504eb89c6f272fae0b7837
Jacob Turner <j.israel.turner@gmail.com>
2022-11-02 13:45:46 -0600

    TEMP

--- src/invite/Invite.tsx
+++ src/invite/Invite.tsx
@@ -1,26 +1,38 @@
-import { isNil, noop } from 'lodash'
 import React from 'react'
-import { useTranslation } from 'react-i18next'
+import { WebView } from 'react-native-webview'
 
 import { noHeader } from 'src/navigator/Headers'
-import { navigateBack } from 'src/navigator/NavigationService'
-
-import { useShareUrl } from './hooks'
-import InviteModal from './InviteModal'
 
 export default function Invite() {
-  const { t } = useTranslation()
-  const shareUrl = useShareUrl()
+  // const { t } = useTranslation()
+  // const shareUrl = useShareUrl()
 
   return (
-    <InviteModal
-      title={t('inviteWithUrl.title')}
-      description={t('inviteWithUrl.body')}
-      buttonLabel={t('inviteWithUrl.button')}
-      disabled={isNil(shareUrl)}
-      onClose={navigateBack}
-      onShareInvite={shareUrl ?? noop}
-    />
+    <>
+      <WebView
+        onError={(e) => console.warn(e)}
+        onHttpError={(e) => console.warn(e)}
+        onMessage={(m) => console.log(m)}
+        originWhitelist={['*']}
+        domStorageEnabled={true}
+        javaScriptEnabled={true}
+        loadsImagesAutomatically={true}
+        defaultTextEncodingName={true}
+        source={{
+          html: '<blockquote class="twitter-tweet"><p lang="en" dir="ltr">Happy 50th anniversary to the Wilderness Act! Here&#39;s a great wilderness photo from <a href="https://twitter.com/YosemiteNPS">@YosemiteNPS</a>. <a href="https://twitter.com/hashtag/Wilderness50?src=hash">#Wilderness50</a> <a href="http://t.co/HMhbyTg18X">pic.twitter.com/HMhbyTg18X</a></p>&mdash; US Dept of Interior (@Interior) <a href="https://twitter.com/Interior/status/507185938620219395">September 3, 2014</a></blockquote>n<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>',
+        }}
+      />
+      <WebView
+        originWhitelist={['*']}
+        domStorageEnabled={true}
+        javaScriptEnabled={true}
+        loadsImagesAutomatically={true}
+        defaultTextEncodingName={true}
+        source={{
+          html: '<blockquote class="twitter-tweet"><p lang="en" dir="ltr">Happy 50th anniversary to the Wilderness Act! Here&#39;s a great wilderness photo from <a href="https://twitter.com/YosemiteNPS">@YosemiteNPS</a>. <a href="https://twitter.com/hashtag/Wilderness50?src=hash">#Wilderness50</a> <a href="http://t.co/HMhbyTg18X">pic.twitter.com/HMhbyTg18X</a></p>&mdash; US Dept of Interior (@Interior) <a href="https://twitter.com/Interior/status/507185938620219395">September 3, 2014</a></blockquote>n<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>',
+        }}
+      />
+    </>
   )
 }
 
