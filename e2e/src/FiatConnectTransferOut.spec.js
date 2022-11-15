c687b1b209f5a8a0a6f32a4215cd193936caba8b
anthonydaoud <anthonydaoud01@gmail.com>
2022-11-15 16:55:09 -0500

    test(e2e): automated FiatConnect cash outs with KYC (#3056)
    ### Description
    
    Currently, there is an e2e test for cashing out crypto via FiatConnect
    without KYC. This PR adds another e2e test for cash out with KYC. The
    test onboards a new wallet through Persona's Inquiry process before
    setting up a fiat account and transferring out crypto. The test only
    runs on iOS and the return user cash out flow is also tested.
    
    For this test to work, the `.env.enc` file in the `e2e` directory is
    updated with two new values: `MOCK_PROVIDER_BASE_URL` and
    `MOCK_PROVIDER_API_KEY`.
    
    ### Other changes
    
    None
    
    ### Tested
    
    N/A
    
    ### How others should test
    
    N/A
    
    ### Related issues
    
    - Fixes ACT-495
    
    ### Backwards compatibility
    
    N/A
    
    Co-authored-by: Charlie Andrews-Jubelt <cajubelt@users.noreply.github.com>
    Co-authored-by: Satish Ravi <satish.ravi@valoraapp.com>

--- e2e/src/FiatConnectTransferOut.spec.js
+++ e2e/src/FiatConnectTransferOut.spec.js
@@ -1,6 +1,26 @@
-import { fiatConnectNonKycTransferOut } from './usecases/FiatConnectTransferOut'
+import {
+  fiatConnectNonKycTransferOut,
+  fiatConnectKycTransferOut,
+} from './usecases/FiatConnectTransferOut'
+import { MOCK_PROVIDER_BASE_URL, MOCK_PROVIDER_API_KEY } from 'react-native-dotenv'
+import { launchApp } from './utils/retries'
 
 describe('FiatConnect Transfer Out', () => {
   // deliberately not doing onboarding in beforeEach, since we'll want to re-use accounts, use fresh accounts, etc for these tests
+  beforeEach(async () => {
+    // uninstall and reinstall to obtain a fresh account
+    await device.uninstallApp()
+    await device.installApp()
+    await launchApp({
+      newInstance: false,
+      permissions: { notifications: 'YES', contacts: 'YES', camera: 'YES' },
+    })
+  })
   describe('Non KYC', fiatConnectNonKycTransferOut)
+
+  const platform = device.getPlatform()
+  // KYC test needs to be on iOS and needs Mock Provider info
+  if (platform == 'ios' && MOCK_PROVIDER_BASE_URL && MOCK_PROVIDER_API_KEY) {
+    describe('KYC', fiatConnectKycTransferOut)
+  }
 })
