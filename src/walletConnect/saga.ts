ce365da37100c73bdf6f8d67be2ac39f0e9aecd0
Kathy Luo <kathyluo18@gmail.com>
2022-11-29 14:08:33 +0100

    feat: add initialise client functionality for walletconnect v2 (#3162)
    ### Description
    
    This PR:
    - add plumbing to enable initialising a walletconnect v2 connection
    - there are sagas to handle initialising the v2 client, and initialise
    the pairing
    - nothing further works yet in this PR
    
    ### Other changes
    
    n/a
    
    ### Tested
    
    Ensure that after scanning QR, the client initialised + pairing
    initialised actions are dispatched
    
    ### How others should test
    
    n/a
    
    ### Related issues
    
    - Fixes RET-479
    
    ### Backwards compatibility
    
    Y

--- src/walletConnect/saga.ts
+++ src/walletConnect/saga.ts
@@ -1,5 +1,6 @@
 import '@react-native-firebase/database'
 import '@react-native-firebase/messaging'
+import '@walletconnect/react-native-compat'
 import { call, put, select, spawn } from 'redux-saga/effects'
 import { showMessage } from 'src/alert/actions'
 import { WalletConnectPairingOrigin } from 'src/analytics/types'
@@ -8,16 +9,19 @@ import { activeDappSelector } from 'src/dapps/selectors'
 import i18n from 'src/i18n'
 import Logger from 'src/utils/Logger'
 import { initialiseWalletConnectV1, walletConnectV1Saga } from 'src/walletConnect/v1/saga'
+import { initialiseWalletConnectV2, walletConnectV2Saga } from 'src/walletConnect/v2/saga'
 
 export function* walletConnectSaga() {
   yield spawn(walletConnectV1Saga)
+  yield spawn(walletConnectV2Saga)
 }
 
 export function* isWalletConnectEnabled(uri: string) {
-  const { v1 }: { v1: boolean } = yield select(walletConnectEnabledSelector)
+  const { v1, v2 }: { v1: boolean; v2: boolean } = yield select(walletConnectEnabledSelector)
   const [, , version] = uri.split(/[:@?]/)
   const versionEnabled: { [version: string]: boolean | undefined } = {
     '1': v1,
+    '2': v2,
   }
   return versionEnabled[version] ?? false
 }
@@ -34,6 +38,9 @@ export function* initialiseWalletConnect(uri: string, origin: WalletConnectPairi
   switch (version) {
     case '1':
       yield call(initialiseWalletConnectV1, uri, origin)
+      break
+    case '2':
+      yield call(initialiseWalletConnectV2, uri, origin)
       break
     default:
       throw new Error(`Unsupported WalletConnect version '${version}'`)
