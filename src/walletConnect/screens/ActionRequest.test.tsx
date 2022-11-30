83c05da8c5358e8ac5aeafb3057f8b4a40ad21e0
Kathy Luo <kathyluo18@gmail.com>
2022-11-30 13:24:01 +0100

    chore: add ActionRequest v2 tests

--- src/walletConnect/screens/ActionRequest.test.tsx
+++ src/walletConnect/screens/ActionRequest.test.tsx
@@ -1,4 +1,5 @@
 import { fireEvent, render } from '@testing-library/react-native'
+import { SignClientTypes } from '@walletconnect/types'
 import * as React from 'react'
 import 'react-native'
 import { Provider } from 'react-redux'
@@ -6,7 +7,7 @@ import { SupportedActions } from 'src/walletConnect/constants'
 import ActionRequest from 'src/walletConnect/screens/ActionRequest'
 import { createMockStore } from 'test/utils'
 
-describe('ActionRequest', () => {
+describe('ActionRequest with WalletConnect V1', () => {
   const peerId = 'c49968fd-9607-4a43-ac66-703402400ffa'
   const store = createMockStore({
     walletConnect: {
@@ -89,11 +90,142 @@ describe('ActionRequest', () => {
       expect(getByText('invalid hex')).toBeTruthy()
     })
 
-    it('shows request details with empty message', async () => {
+    it('shows request details with empty message', () => {
       action.params[0] = ''
       const { getByText } = render(
         <Provider store={store}>
           <ActionRequest version={1} pendingAction={{ action, peerId }} />
+        </Provider>
+      )
+      fireEvent.press(getByText('action.details'))
+      expect(getByText('action.emptyMessage')).toBeTruthy()
+    })
+  })
+})
+
+describe('ActionRequest with WalletConnect V2', () => {
+  const store = createMockStore({
+    walletConnect: {
+      v2: {
+        sessions: [
+          {
+            expiry: 1670411909,
+            self: {
+              metadata: {
+                icons: ['https://valoraapp.com//favicon.ico'],
+                description: 'A mobile payments wallet that works worldwide',
+                name: 'Valora',
+                url: 'https://valoraapp.com/',
+              },
+              publicKey: 'b991206845c62280479fd1f24087e9c6f0df3921b5f9d94f4619fbf995a81149',
+            },
+            relay: {
+              protocol: 'irn',
+            },
+            peer: {
+              metadata: {
+                name: 'WalletConnect Example',
+                description: '',
+                icons: [],
+                url: 'https://react-app.walletconnect.com',
+              },
+              publicKey: '3c78ff702b703e873a90a9619598effa0e3b01deb977cb277d3b0eecff3a0320',
+            },
+            controller: 'b991206845c62280479fd1f24087e9c6f0df3921b5f9d94f4619fbf995a81149',
+            namespaces: {
+              eip155: {
+                accounts: ['eip155:44787:0x047154ac4d7e01b1dc9ddeea9e8996b57895a747'],
+                methods: [
+                  'eth_sendTransaction',
+                  'eth_signTransaction',
+                  'eth_sign',
+                  'personal_sign',
+                  'eth_signTypedData',
+                ],
+                events: ['chainChanged', 'accountsChanged'],
+              },
+            },
+            acknowledged: true,
+            topic: 'd8afe1f5c3efa38bbb62c68005f572a7218afcd48703e4b02bdc5df2549ac5b5',
+            requiredNamespaces: {
+              eip155: {
+                methods: [
+                  'eth_sendTransaction',
+                  'eth_signTransaction',
+                  'eth_sign',
+                  'personal_sign',
+                  'eth_signTypedData',
+                ],
+                chains: ['eip155:44787'],
+                events: ['chainChanged', 'accountsChanged'],
+              },
+            },
+          },
+        ],
+      },
+    },
+  })
+
+  afterEach(() => {
+    store.clearActions()
+  })
+
+  describe('personal_sign', () => {
+    const pendingAction: SignClientTypes.EventArguments['session_request'] = {
+      id: 1669810746892321,
+      topic: 'd8afe1f5c3efa38bbb62c68005f572a7218afcd48703e4b02bdc5df2549ac5b5',
+      params: {
+        chainId: 'eip155:44787',
+        request: {
+          method: 'personal_sign',
+          params: [
+            '0x4d65737361676520746f207369676e', // hex of 'Message to sign'
+            '0x047154ac4d7e01b1dc9ddeea9e8996b57895a747',
+          ],
+        },
+      },
+    }
+
+    it('renders the correct elements', () => {
+      const { getByText } = render(
+        <Provider store={store}>
+          <ActionRequest version={2} pendingAction={pendingAction} />
+        </Provider>
+      )
+
+      expect(getByText('confirmTransaction, {"dappName":"WalletConnect Example"}')).toBeTruthy()
+      expect(getByText('action.askingV1_35, {"dappName":"WalletConnect Example"}')).toBeTruthy()
+      expect(getByText('action.sign')).toBeTruthy()
+      expect(getByText('allow')).toBeTruthy()
+      expect(getByText('cancel')).toBeTruthy()
+    })
+
+    it('shows request details with correct string on clicking details', () => {
+      const { getByText } = render(
+        <Provider store={store}>
+          <ActionRequest version={2} pendingAction={pendingAction} />
+        </Provider>
+      )
+      fireEvent.press(getByText('action.details'))
+      expect(getByText('Message to sign')).toBeTruthy()
+    })
+
+    it('shows request details with raw string if message cannot be decoded', () => {
+      pendingAction.params.request.params[0] = 'invalid hex'
+      const { getByText } = render(
+        <Provider store={store}>
+          <ActionRequest version={2} pendingAction={pendingAction} />
+        </Provider>
+      )
+      fireEvent.press(getByText('action.details'))
+      expect(getByText('invalid hex')).toBeTruthy()
+    })
+
+    it('shows request details with empty message', () => {
+      pendingAction.params.request.params[0] = ''
+      const { getByText } = render(
+        <Provider store={store}>
+          <ActionRequest version={2} pendingAction={pendingAction} />
         </Provider>
       )
       fireEvent.press(getByText('action.details'))
