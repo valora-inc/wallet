5cc093889b38797fbb937d83eab78651ea7331a1
Kathy Luo <kathyluo18@gmail.com>
2022-12-06 16:52:54 +0200

    test: add more tests, make tests more dry

--- e2e/src/usecases/WalletConnectV2.js
+++ e2e/src/usecases/WalletConnectV2.js
@@ -1,7 +1,12 @@
 import { newKit } from '@celo/contractkit'
+import {
+  hashMessageWithPrefix,
+  verifyEIP712TypedDataSigner,
+  verifySignature,
+} from '@celo/utils/lib/signatureUtils'
 import Client from '@walletconnect/sign-client'
 import fetch from 'node-fetch'
-import { formatUri } from '../utils/encoding'
+import { formatUri, utf8ToHex } from '../utils/encoding'
 import { enterPinUiIfNecessary, scrollIntoView, waitForElementId } from '../utils/utils'
 
 const jestExpect = require('expect')
@@ -47,38 +52,6 @@ async function formatTestTransaction(address) {
   }
 }
 
-let walletConnectClient, pairingUrl
-const initialiseClient = async () => {
-  walletConnectClient = await Client.init({
-    relayUrl: 'wss://relay.walletconnect.org',
-    projectId: DEFAULT_PROJECT_ID,
-    metadata: {
-      name: dappName,
-      description: 'WalletConnect Client',
-      url: 'https://valoraapp.com/',
-      icons: [],
-    },
-  })
-
-  const { uri } = await walletConnectClient.connect({
-    requiredNamespaces: {
-      eip155: {
-        methods: [
-          'eth_sendTransaction',
-          'eth_signTransaction',
-          'eth_sign',
-          'personal_sign',
-          'eth_signTypedData',
-        ],
-        chains: ['eip155:44787'],
-        events: ['chainChanged', 'accountsChanged'],
-      },
-    },
-  })
-
-  pairingUrl = uri
-}
-
 const verifySuccessfulConnection = async () => {
   await waitFor(element(by.text(`Success! Please go back to ${dappName} to continue`)))
     .toBeVisible()
@@ -88,9 +61,50 @@ const verifySuccessfulConnection = async () => {
     .withTimeout(15 * 1000)
 }
 
+const verifySuccessfulSign = async () => {
+  await waitFor(element(by.text(`${dappName} would like to confirm a transaction`)))
+    .toBeVisible()
+    .withTimeout(15 * 1000)
+  await waitFor(element(by.text('Sign a data payload')))
+    .toBeVisible()
+    .withTimeout(15 * 1000)
+  await element(by.text('Allow')).tap()
+  await enterPinUiIfNecessary()
+  await verifySuccessfulConnection()
+}
+
 export default WalletConnect = () => {
+  let walletConnectClient, pairingUrl
+
   beforeAll(async () => {
-    await initialiseClient()
+    walletConnectClient = await Client.init({
+      relayUrl: 'wss://relay.walletconnect.org',
+      projectId: DEFAULT_PROJECT_ID,
+      metadata: {
+        name: dappName,
+        description: 'WalletConnect Client',
+        url: 'https://valoraapp.com/',
+        icons: [],
+      },
+    })
+
+    const { uri } = await walletConnectClient.connect({
+      requiredNamespaces: {
+        eip155: {
+          methods: [
+            'eth_sendTransaction',
+            'eth_signTransaction',
+            'eth_sign',
+            'personal_sign',
+            'eth_signTypedData',
+          ],
+          chains: ['eip155:44787'],
+          events: ['chainChanged', 'accountsChanged'],
+        },
+      },
+    })
+
+    pairingUrl = uri
   })
 
   it('Then is able to establish a session', async () => {
@@ -124,12 +138,14 @@ export default WalletConnect = () => {
     await waitFor(element(by.text(`${dappName} would like to confirm a transaction`)))
       .toBeVisible()
       .withTimeout(15 * 1000)
+    await waitFor(element(by.text('Send a Celo transaction')))
+      .toBeVisible()
+      .withTimeout(15 * 1000)
 
     await element(by.id('ShowTransactionDetailsButton')).tap()
     await expect(element(by.id('DappData'))).toHaveText(`[${JSON.stringify(tx)}]`)
 
     await element(by.id('WalletConnectActionRequest/Allow')).tap()
-    await device.disableSynchronization()
     await enterPinUiIfNecessary()
     await verifySuccessfulConnection()
 
@@ -141,11 +157,107 @@ export default WalletConnect = () => {
     jestExpect(to).toStrictEqual(walletAddress)
   })
 
+  it('Then is able to sign a message (eth_sign)', async () => {
+    let signature
+    const message = hashMessageWithPrefix(`My email is valora.test@mailinator.com - ${+new Date()}`)
+    const params = [walletAddress, message]
+    const [session] = walletConnectClient.session.map.values()
+    walletConnectClient
+      .request({
+        topic: session.topic,
+        chainId: 'eip155:44787',
+        request: {
+          method: 'eth_sign',
+          params,
+        },
+      })
+      .then((result) => {
+        signature = result
+      })
+
+    await verifySuccessfulSign()
+
+    jestExpect(verifySignature(message, signature, walletAddress)).toStrictEqual(true)
+  })
+
+  it('Then is able to sign a personal message (personal_sign)', async () => {
+    let signature
+    const message = `My email is valora.test@mailinator.com - ${+new Date()}`
+    const params = [utf8ToHex(message), walletAddress]
+    const [session] = walletConnectClient.session.map.values()
+    walletConnectClient
+      .request({
+        topic: session.topic,
+        chainId: 'eip155:44787',
+        request: {
+          method: 'personal_sign',
+          params,
+        },
+      })
+      .then((result) => {
+        signature = result
+      })
+
+    await verifySuccessfulSign()
+
+    jestExpect(verifySignature(message, signature, walletAddress)).toStrictEqual(true)
+  })
+
+  it('Then is able to sign typed data (eth_signTypedData)', async () => {
+    let signature
+    const typedData = {
+      types: {
+        EIP712Domain: [
+          { name: 'name', type: 'string' },
+          { name: 'version', type: 'string' },
+          { name: 'chainId', type: 'uint256' },
+          { name: 'verifyingContract', type: 'address' },
+        ],
+        Person: [
+          { name: 'name', type: 'string' },
+          { name: 'wallet', type: 'address' },
+        ],
+        Mail: [
+          { name: 'from', type: 'Person' },
+          { name: 'to', type: 'Person' },
+          { name: 'contents', type: 'string' },
+        ],
+      },
+      primaryType: 'Mail',
+      domain: {
+        name: 'Ether Mail',
+        version: '1',
+        chainId: 1,
+        verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
+      },
+      message: {
+        from: { name: 'Cow', wallet: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826' },
+        to: { name: 'Bob', wallet: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB' },
+        contents: 'Hello, Bob!',
+      },
+    }
+    const params = [walletAddress, JSON.stringify(typedData)]
+    const [session] = walletConnectClient.session.map.values()
+    walletConnectClient
+      .request({
+        topic: session.topic,
+        chainId: 'eip155:44787',
+        request: {
+          method: 'eth_signTypedData',
+          params,
+        },
+      })
+      .then((result) => {
+        signature = result
+      })
+
+    await verifySuccessfulSign()
+
+    jestExpect(verifyEIP712TypedDataSigner(typedData, signature, walletAddress)).toStrictEqual(true)
+  })
+
   // TODO - Add the tests below
   // Then is able to sign a transaction
-  // Then is able to sign a personal message (personal_sign)
-  // Then is able to sign message (eth_sign)
-  // Then is able to sign typed data (eth_signTypedData)
   // Then is able to send custom request
 
   it('Then should be able to disconnect a session', async () => {
