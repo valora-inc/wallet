6d076d3b922c414d2934df6b77ee08ff56ab6e61
Kathy Luo <kathyluo18@gmail.com>
2022-12-16 13:13:18 +0100

    feat: add favouriting action that isn't hooked up to redux

--- src/dappsExplorer/DappCard.tsx
+++ src/dappsExplorer/DappCard.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Image, StyleSheet, Text, View } from 'react-native'
+import { useSelector } from 'react-redux'
 import Card from 'src/components/Card'
 import Touchable from 'src/components/Touchable'
+import { dappFavoritesEnabledSelector } from 'src/dapps/selectors'
 import { ActiveDapp, Dapp, DappSection } from 'src/dapps/types'
 import LinkArrow from 'src/icons/LinkArrow'
+import Star from 'src/icons/Star'
+import StarOutline from 'src/icons/StarOutline'
 import { Colors } from 'src/styles/colors'
 import fontStyles from 'src/styles/fonts'
 import { Shadow, Spacing } from 'src/styles/styles'
@@ -14,8 +18,23 @@ interface Props {
   onPressDapp: (dapp: ActiveDapp) => void
 }
 
+// Since this icon exists within a touchable, make the hitslop bigger than usual
+const favoriteIconHitslop = { top: 20, right: 20, bottom: 20, left: 20 }
+
 function DappCard({ dapp, section, onPressDapp }: Props) {
-  const onPress = () => onPressDapp({ ...dapp, openedFrom: section })
+  // TODO replace this state with a selector
+  const [isFavorited, setIsFavorited] = useState(false)
+
+  const dappFavoritesEnabled = useSelector(dappFavoritesEnabledSelector)
+
+  const onPress = () => {
+    onPressDapp({ ...dapp, openedFrom: section })
+  }
+
+  const onPressFavorite = () => {
+    // TODO replace with dispatch favourite dapp action
+    setIsFavorited((prev) => !prev)
+  }
 
   return (
     <Card style={styles.card} rounded={true} shadow={Shadow.SoftLight}>
@@ -26,7 +45,13 @@ function DappCard({ dapp, section, onPressDapp }: Props) {
             <Text style={styles.title}>{dapp.name}</Text>
             <Text style={styles.subtitle}>{dapp.description}</Text>
           </View>
-          <LinkArrow size={24} />
+          {dappFavoritesEnabled ? (
+            <Touchable onPress={onPressFavorite} hitSlop={favoriteIconHitslop}>
+              {isFavorited ? <Star /> : <StarOutline />}
+            </Touchable>
+          ) : (
+            <LinkArrow size={24} />
+          )}
         </>
       </Touchable>
     </Card>
