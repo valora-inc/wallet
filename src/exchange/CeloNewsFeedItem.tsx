ae8482b8d6c3a0e26f6dba1cdc5df94d3af6c05c
Jean Regisser <jean.regisser@gmail.com>
2022-12-19 13:09:48 +0100

    feat: celo news feed components (#3252)
    ### Description
    
    This PR adds the Celo news feed components. 
    It's using fake data for now, another PR will connect it to the API.
    
    ### Tested
    
    
    https://user-images.githubusercontent.com/57791/208106513-70f410b8-bf8d-403c-8e9f-45baa2c2c7b5.mp4
    
    ### How others should test
    
    N/A
    
    ### Related issues
    
    - RET-519
    
    ### Backwards compatibility
    
    Yes

---
+++ src/exchange/CeloNewsFeedItem.tsx
@@ -0,0 +1,81 @@
+import { formatDistanceToNowStrict } from 'date-fns'
+import * as React from 'react'
+import { Image, StyleSheet, Text, View } from 'react-native'
+import Touchable from 'src/components/Touchable'
+import { CeloNewsArticle } from 'src/exchange/types'
+import { navigate } from 'src/navigator/NavigationService'
+import { Screens } from 'src/navigator/Screens'
+import colors from 'src/styles/colors'
+import fontStyles from 'src/styles/fonts'
+import { Spacing } from 'src/styles/styles'
+
+const IMAGE_SIZE = 44
+
+interface Props {
+  article: CeloNewsArticle
+  testID?: string
+}
+
+export default function CeloNewsFeedItem({ article, testID }: Props) {
+  function onPress() {
+    navigate(Screens.WebViewScreen, { uri: article.link })
+  }
+
+  return (
+    <Touchable onPress={onPress} testID={testID}>
+      <View style={styles.contentContainer}>
+        <View style={styles.row}>
+          <Text style={styles.author} numberOfLines={1}>
+            {article.author}
+          </Text>
+          <Text style={styles.date} numberOfLines={1}>
+            {formatDistanceToNowStrict(new Date(article.createdAt))}
+          </Text>
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.title}>{article.title}</Text>
+          {!!article.articleImage && (
+            <Image
+              source={{ uri: article.articleImage }}
+              style={styles.image}
+              testID="CeloNewsFeedItemImage"
+            />
+          )}
+        </View>
+      </View>
+    </Touchable>
+  )
+}
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    marginHorizontal: Spacing.Thick24,
+    marginTop: Spacing.Regular16,
+    marginBottom: Spacing.Thick24,
+  },
+  author: {
+    ...fontStyles.label,
+    color: colors.goldDark,
+    marginBottom: Spacing.Small12,
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  title: {
+    ...fontStyles.regular,
+    flex: 1,
+  },
+  date: {
+    ...fontStyles.small,
+    fontSize: 13,
+    lineHeight: 16,
+    color: colors.gray5,
+    marginLeft: 4,
+  },
+  image: {
+    width: IMAGE_SIZE,
+    height: IMAGE_SIZE,
+    borderRadius: 4,
+    marginLeft: 16,
+  },
+})
