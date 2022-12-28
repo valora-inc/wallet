50a17ef0543c6946d0ec87f255ac43b01502ff44
Kathy Luo <kathyluo18@gmail.com>
2022-12-28 16:03:44 +0100

    test: add e2e tests for restoring with 12 word recovery phrase

--- e2e/src/usecases/RestoreAccountOnboarding.js
+++ e2e/src/usecases/RestoreAccountOnboarding.js
@@ -1,90 +1,90 @@
-import { EXAMPLE_NAME, SAMPLE_BACKUP_KEY } from '../utils/consts'
+import { getAddressChunks } from '@celo/utils/lib/address'
+import {
+  EXAMPLE_NAME,
+  SAMPLE_BACKUP_KEY,
+  SAMPLE_BACKUP_KEY_12_WORDS,
+  SAMPLE_WALLET_ADDRESS,
+  SAMPLE_WALLET_ADDRESS_12_WORDS,
+} from '../utils/consts'
 import { launchApp } from '../utils/retries'
-import { enterPinUi, sleep, waitForElementId } from '../utils/utils'
+import { enterPinUi, scrollIntoView, waitForElementId } from '../utils/utils'
 
 export default RestoreAccountOnboarding = () => {
-  beforeAll(async () => {
-    await device.terminateApp()
-    await sleep(5000)
+  beforeEach(async () => {
+    await device.uninstallApp()
+    await device.installApp()
     await launchApp({
-      delete: true,
       permissions: { notifications: 'YES', contacts: 'YES' },
     })
-    await sleep(5000)
   })
-  // Language is auto selected if it matches one of the available locale
-  // it('Language', async () => {
-  //   await element(by.id('ChooseLanguage/en-US')).tap()
-  // })
-  it('Onboarding Education', async () => {
-    // Onboarding education has 3 steps
-    for (let i = 0; i < 3; i++) {
-      await element(by.id('Education/progressButton')).tap()
+
+  it.each`
+    wordCount | phrase                        | walletAddress
+    ${'12'}   | ${SAMPLE_BACKUP_KEY_12_WORDS} | ${SAMPLE_WALLET_ADDRESS_12_WORDS}
+    ${'24'}   | ${SAMPLE_BACKUP_KEY}          | ${SAMPLE_WALLET_ADDRESS}
+  `(
+    'restores an existing wallet using a $wordCount word recovery phrase',
+    async ({ phrase, walletAddress }) => {
+      // Onboarding education has 3 steps
+      for (let i = 0; i < 3; i++) {
+        await element(by.id('Education/progressButton')).tap()
+      }
+
+      // choose restore flow
+      await element(by.id('RestoreAccountButton')).tap()
+
+      // accept terms
+      await element(by.id('scrollView')).scrollTo('bottom')
+      await expect(element(by.id('AcceptTermsButton'))).toBeVisible()
+      await element(by.id('AcceptTermsButton')).tap()
+
+      // enter name
+      await element(by.id('NameEntry')).replaceText(EXAMPLE_NAME)
+      await element(by.id('NameAndPictureContinueButton')).tap()
+
+      // Set and verify pin
+      await enterPinUi()
+      await enterPinUi()
+
+      // wait for connecting banner to go away
+      // TODO measure how long this take
+      await waitFor(element(by.id('connectingToCelo')))
+        .not.toBeVisible()
+        .withTimeout(20000)
+
+      // enter the recovery phrase
+      await element(by.id('ImportWalletBackupKeyInputField')).tap()
+      await element(by.id('ImportWalletBackupKeyInputField')).replaceText(phrase)
+      if (device.getPlatform() === 'ios') {
+        // On iOS, type one more space to workaround onChangeText not being triggered with replaceText above
+        // and leaving the restore button disabled
+        await element(by.id('ImportWalletBackupKeyInputField')).typeText('\n')
+      } else if (device.getPlatform() === 'android') {
+        // Press back button to close the keyboard
+        await device.pressBack()
+      }
+
+      // start wallet import
+      await waitFor(element(by.id('ImportWalletButton')))
+        .toBeVisible()
+        .withTimeout(1000 * 5)
+      await element(by.id('ImportWalletButton')).tap()
+
+      // verification step comes after restoring wallet, skip this step
+      await waitForElementId('PhoneVerificationSkipHeader')
+      await element(by.id('PhoneVerificationSkipHeader')).tap()
+      await element(by.id('PhoneVerificationSkipDialog/PrimaryAction')).tap()
+
+      // verify that we land on the home screen
+      await expect(element(by.id('SendOrRequestBar'))).toBeVisible()
+
+      // verify that the correct account was restored
+      await waitForElementId('Hamburger')
+      await element(by.id('Hamburger')).tap()
+      await scrollIntoView('Account Address', 'SettingsScrollView')
+
+      const addressString = '0x ' + getAddressChunks(walletAddress).join(' ')
+      await expect(element(by.text(addressString))).toBeVisible()
     }
-  })
-
-  it('Welcome', async () => {
-    await element(by.id('RestoreAccountButton')).tap()
-  })
-
-  it('Terms', async () => {
-    await element(by.id('scrollView')).scrollTo('bottom')
-    await expect(element(by.id('AcceptTermsButton'))).toBeVisible()
-    await element(by.id('AcceptTermsButton')).tap()
-  })
-
-  it('Name and Picture', async () => {
-    await element(by.id('NameEntry')).replaceText(EXAMPLE_NAME)
-    await element(by.id('NameAndPictureContinueButton')).tap()
-  })
-
-  it('Pin', async () => {
-    // Set pin
-    await enterPinUi()
-    // Verify pin
-    await enterPinUi()
-  })
-
-  // TODO(erdal) 2 new paths: using invite code, continue without
-  // TODO(erdal) get rid of sleeps if possible to make the tests faster
-
-  // Restore existing wallet
-  it('Restore Wallet Backup', async () => {
-    // wait for connecting banner to go away
-    // TODO measure how long this take
-    await waitFor(element(by.id('connectingToCelo')))
-      .not.toBeVisible()
-      .withTimeout(20000)
-
-    await element(by.id('ImportWalletBackupKeyInputField')).tap()
-    await element(by.id('ImportWalletBackupKeyInputField')).replaceText(`${SAMPLE_BACKUP_KEY}`)
-    if (device.getPlatform() === 'ios') {
-      // On iOS, type one more space to workaround onChangeText not being triggered with replaceText above
-      // and leaving the restore button disabled
-      await element(by.id('ImportWalletBackupKeyInputField')).typeText('\n')
-    } else if (device.getPlatform() === 'android') {
-      // Press back button to close the keyboard
-      await device.pressBack()
-    }
-
-    await waitFor(element(by.id('ImportWalletButton')))
-      .toBeVisible()
-      .withTimeout(1000 * 5)
-    await element(by.id('ImportWalletButton')).tap()
-  })
-
-  it('Verify Education', async () => {
-    await waitForElementId('PhoneVerificationSkipHeader')
-
-    // skip
-    await element(by.id('PhoneVerificationSkipHeader')).tap()
-    // confirmation popup skip
-    await element(by.id('PhoneVerificationSkipDialog/PrimaryAction')).tap()
-  })
-
-  it('Wallet Home', async () => {
-    await expect(element(by.id('SendOrRequestBar'))).toBeVisible()
-  })
-
-  // TODO(erdal): generate a new invite
+  )
 }
--- e2e/src/utils/consts.js
+++ e2e/src/utils/consts.js
@@ -3,6 +3,10 @@ export const SAMPLE_BACKUP_KEY =
   'general debate dial flock want basket local machine effort monitor stomach purity attend brand extend salon obscure soul open floor useful like cause exhaust'
 export const SAMPLE_PRIVATE_KEY = // corresponds to backup key above
   '0x34c78f42ec153668070bd55c0d237adcdfee0359f1ea473b8e16c8d5f99be14a'
+export const SAMPLE_WALLET_ADDRESS = '0x6131a6d616a4be3737b38988847270a64bc10caa' // corresponds to the backup key above
+export const SAMPLE_BACKUP_KEY_12_WORDS =
+  'embody siege middle glory soda solar nasty report swap now never any'
+export const SAMPLE_WALLET_ADDRESS_12_WORDS = '0x86b8f44386cb2d457db79c3dab8cf42f9d8a3fc0' // corresponds to the backup key above
 export const DEFAULT_PIN = '323122'
 export const ALTERNATIVE_PIN = '223121'
 export const EXAMPLE_NAME = 'Test Name'
