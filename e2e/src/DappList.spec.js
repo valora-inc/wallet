b55dac6c041ce22e949f3ae88a2a3e3688cf9e78
MuckT <mcgtom10@gmail.com>
2023-01-03 18:38:14 -0800

    test(e2e): relaunch app on device to ensure dapp list loads for tests

--- e2e/src/DappList.spec.js
+++ e2e/src/DappList.spec.js
@@ -1,9 +1,13 @@
 import DappListRecent from './usecases/DappListRecent'
 import { quickOnboarding } from './utils/utils'
+import { launchApp } from './utils/retries'
 
 describe('Dapp List', () => {
   beforeAll(async () => {
     await quickOnboarding()
+    // Relaunch app to ensure dapp list loads
+    // TODO: Remove this once we have a better way to ensure dapp list loads
+    await launchApp({ newInstance: true })
   })
 
   describe('Recent Dapps', DappListRecent)
