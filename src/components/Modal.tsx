41cb33e201624ae5c0b60970dd0ff55a00d3374d
Kathy Luo <kathyluo18@gmail.com>
2023-01-24 15:05:27 +0100

    refactor: use safe area context instead

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,14 +1,9 @@
 import * as React from 'react'
-import { Dimensions, Platform, StyleProp, StyleSheet, ViewStyle } from 'react-native'
+import { StyleProp, StyleSheet, ViewStyle } from 'react-native'
 import ReactNativeModal from 'react-native-modal'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { SafeAreaView, useSafeAreaFrame } from 'react-native-safe-area-context'
 import Card from 'src/components/Card'
 import colors from 'src/styles/colors'
-
-const deviceHeight =
-  Platform.OS === 'ios'
-    ? Dimensions.get('window').height
-    : require('react-native-extra-dimensions-android').get('REAL_WINDOW_HEIGHT')
 
 interface Props {
   children: React.ReactNode
@@ -19,13 +14,15 @@ interface Props {
 }
 
 export default function Modal({ children, isVisible, style, testID, onBackgroundPress }: Props) {
+  const { height } = useSafeAreaFrame()
+
   return (
     <ReactNativeModal
       testID={testID}
       isVisible={isVisible}
       backdropOpacity={0.1}
       onBackdropPress={onBackgroundPress}
-      deviceHeight={deviceHeight}
+      deviceHeight={height}
       // @ts-ignore statusBarTranslucent is supported since RN 0.62, but updated lib with the added prop hasn't been published yet
       statusBarTranslucent={true}
     >
