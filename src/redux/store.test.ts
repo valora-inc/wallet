1697c1fa43c78b13a9dd54be84fb224141ca843e
anthonydaoud <anthonydaoud01@gmail.com>
2023-01-25 18:27:03 -0500

    small fix to versions

--- src/redux/store.test.ts
+++ src/redux/store.test.ts
@@ -93,7 +93,7 @@ describe('store state', () => {
       Object {
         "_persist": Object {
           "rehydrated": true,
-          "version": 105,
+          "version": 106,
         },
         "account": Object {
           "acceptedTerms": false,
