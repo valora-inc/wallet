4a816cbf31fc0c4b16f72b0467038df7b4d68ebe
Joseph Bergeron <jophish126@gmail.com>
2023-01-31 11:16:42 -0500

    Remove unnecessary export

--- src/navigator/types.tsx
+++ src/navigator/types.tsx
@@ -32,7 +32,7 @@ import {
 import { QRCodeDataType, QRCodeStyle } from 'src/qrcode/schema'
 
 // Typed nested navigator params
-export type NestedNavigatorParams<ParamList> = {
+type NestedNavigatorParams<ParamList> = {
   [K in keyof ParamList]: undefined extends ParamList[K]
     ? { screen: K; params?: ParamList[K] }
     : { screen: K; params: ParamList[K] }
