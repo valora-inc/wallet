cc0bfc74bc27e80492b970f2aefc01171dbace96
MuckT <mcgtom10@gmail.com>
2023-02-13 19:23:02 -0800

    chore: update existing dapps favorites to support filters

--- src/dappsExplorer/FavoriteDappsSection.tsx
+++ src/dappsExplorer/FavoriteDappsSection.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { useSelector } from 'react-redux'
-import { favoriteDappsSelector } from 'src/dapps/selectors'
-import { ActiveDapp, DappSection } from 'src/dapps/types'
+import { dappsFilterAndSearchEnabledSelector, favoriteDappsSelector } from 'src/dapps/selectors'
+import { ActiveDapp, DappFilter, DappSection } from 'src/dapps/types'
 import DappCard from 'src/dappsExplorer/DappCard'
 import StarIllustration from 'src/icons/StarIllustration'
 import Colors from 'src/styles/colors'
@@ -12,12 +12,85 @@ import { Spacing } from 'src/styles/styles'
 
 interface Props {
   onPressDapp: (dapp: ActiveDapp) => void
+  filter?: DappFilter | null
+  removeFilter?: () => void
 }
 
-export function FavoriteDappsSection({ onPressDapp }: Props) {
+export function FavoriteDappsSection({ onPressDapp, filter, removeFilter }: Props) {
   const { t } = useTranslation()
   const favoriteDapps = useSelector(favoriteDappsSelector)
+  const dappsFilterAndSearchEnabled = useSelector(dappsFilterAndSearchEnabledSelector)
 
+  // If no favorites, display no favorites section
+  if (favoriteDapps.length === 0) {
+    return (
+      <View style={styles.container}>
+        <StarIllustration />
+        <View style={styles.contentContainer}>
+          <Text style={styles.title}>{t('dappsScreen.noFavorites.title')}</Text>
+          <Text style={styles.description}>{t('dappsScreen.noFavorites.description')}</Text>
+        </View>
+      </View>
+    )
+  }
+
+  // If dapp filter enabled display favorites matching filter or no results section
+  if (dappsFilterAndSearchEnabled) {
+    // Favorites dapps matching category filter
+    const favoritedDappsFiltered = favoriteDapps.filter(
+      (dapp) =>
+        dapp.categories && (dapp.categories.includes(filter?.id ?? '') || filter?.id === 'all')
+    )
+    // Matching favorite dapp(s) found
+    if (favoriteDapps.length > 0 && favoritedDappsFiltered.length > 0) {
+      return (
+        <View testID="DAppExplorerScreen/FavoriteDappsSection">
+          {favoritedDappsFiltered.map((favoriteDapp) => (
+            <DappCard
+              key={favoriteDapp.id}
+              dapp={favoriteDapp}
+              section={DappSection.FavoritesDappScreen}
+              onPressDapp={onPressDapp}
+            />
+          ))}
+        </View>
+      )
+    } else {
+      // No matching favorite dapp(s) found new return new no results section
+      return (
+        <>
+          <View style={styles.noMatchingFavorites}>
+            <View
+              style={{
+                flexDirection: 'row',
+                flexWrap: 'wrap',
+              }}
+            >
+              <Text style={styles.noResultsFilterText}>{filter?.name.toLocaleUpperCase()}</Text>
+              <Text style={[styles.noResultsFilterAppliedText, { paddingHorizontal: 4 }]}>
+                {t('dappsScreen.emptyResults.filterApplied')}
+              </Text>
+              <TouchableOpacity
+                onPress={removeFilter}
+                style={{
+                  paddingVertical: 0,
+                  paddingHorizontal: 8,
+                  backgroundColor: Colors.gray2,
+                  borderRadius: 16,
+                }}
+              >
+                <Text style={styles.noResultsRemoveFilterText}>
+                  {t('dappsScreen.emptyResults.removeFilter')}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </>
+      )
+    }
+  }
+
+  // If dappsFilterAndSearchEnabled is false (implied) and there are favorite dapp(s) then display old favorites section
   if (favoriteDapps.length > 0) {
     return (
       <View testID="DAppExplorerScreen/FavoriteDappsSection">
@@ -33,6 +106,7 @@ export function FavoriteDappsSection({ onPressDapp }: Props) {
     )
   }
 
+  // Catch all - don't return undefined for this component
   return (
     <View style={styles.container}>
       <StarIllustration />
@@ -61,6 +135,39 @@ const styles = StyleSheet.create({
   description: {
     ...fontStyles.small,
     color: Colors.gray4,
+  },
+  // No Results Section Search & Filter
+  filterName: {
+    ...fontStyles.regular600,
+  },
+  noMatchingFavorites: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    padding: 0,
+    gap: 8,
+    marginTop: Spacing.Thick24,
+  },
+  noResultsFilterText: {
+    ...fontStyles.regular500,
+    fontSize: 10,
+    lineHeight: 20,
+    fontWeight: '500',
+    color: Colors.blue,
+  },
+  noResultsFilterAppliedText: {
+    ...fontStyles.regular500,
+    fontWeight: '400',
+    fontSize: 10,
+    lineHeight: 20,
+    color: Colors.gray7,
+  },
+  noResultsRemoveFilterText: {
+    ...fontStyles.regular500,
+    fontWeight: '500',
+    fontSize: 10,
+    lineHeight: 20,
+    color: Colors.blue,
   },
 })
 
