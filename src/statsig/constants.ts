623874686d14b00887d9418e973513a8a0ac54bb
jh2oman <jacobwaterman@MacBook-Pro.local>
2023-02-15 17:30:38 -0800

    feat(onboarding): Change style of Name Screen

--- src/statsig/constants.ts
+++ src/statsig/constants.ts
@@ -1,24 +1,14 @@
 // TODO(any): consider making it more type safe
 import { SelectProviderExchangesLink, SelectProviderExchangesText } from 'src/fiatExchanges/types'
 import {
-  QRCodeStyle,
   QRCodeDataType,
+  QRCodeStyle,
   StatsigDynamicConfigs,
   StatsigExperiments,
   StatsigLayers,
 } from 'src/statsig/types'
 
 export const LayerParams = {
-  [StatsigLayers.NAME_AND_PICTURE_SCREEN]: {
-    showSkipButton: {
-      paramName: 'showSkipButton',
-      defaultValue: false,
-    },
-    nameType: {
-      paramName: 'nameType',
-      defaultValue: 'first_and_last',
-    },
-  },
   [StatsigLayers.SEND_RECEIVE_QR_CODE]: {
     qrCodeStyle: {
       paramName: 'qrCodeStyle',
