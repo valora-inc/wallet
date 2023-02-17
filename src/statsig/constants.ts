26faf135df44a33cde94c2f23d987823ccf40551
Jacob Waterman <jacobrwaterman@gmail.com>
2023-02-16 17:15:41 -0800

    feat(onboarding): Change style of Name Screen (#3450)
    ### Description
    
    Part of the wider onboarding changes.
    [Figma](https://www.figma.com/file/NAp0jPTvldgl8AVajej3ns/Onboarding-Updates?node-id=1801%3A16171&t=AupXj9tOKyTBqY20-0)
    
    
    Also removed lots of old experiment code. Restore flow now looks exactly
    the same as the new account flow
    
    | Empty | With Dwight |
    |:------:|:------:|
    | ![Simulator Screen Shot - iPhone 13 - 2023-02-15 at 17 22
    50](https://user-images.githubusercontent.com/8432644/219240375-f6c8da1e-b76c-49cb-bf30-c6c516444453.png)
    | ![Simulator Screen Shot - iPhone 13 - 2023-02-15 at 17 23
    02](https://user-images.githubusercontent.com/8432644/219240443-3db369ad-eca9-4532-b957-652eab616770.png)
    
    
    ### Test plan
    
    Unit tests updated
    
    ### Related issues
    
    https://linear.app/valora/issue/ACT-603/update-name-screen
    
    ---------
    
    Co-authored-by: jh2oman <jacobwaterman@MacBook-Pro.local>

--- src/statsig/constants.ts
+++ src/statsig/constants.ts
@@ -1,24 +1,8 @@
 // TODO(any): consider making it more type safe
 import { SelectProviderExchangesLink, SelectProviderExchangesText } from 'src/fiatExchanges/types'
-import {
-  QRCodeStyle,
-  QRCodeDataType,
-  StatsigDynamicConfigs,
-  StatsigExperiments,
-  StatsigLayers,
-} from 'src/statsig/types'
+import { QRCodeDataType, QRCodeStyle, StatsigExperiments, StatsigLayers } from 'src/statsig/types'
 
 export const LayerParams = {
-  [StatsigLayers.NAME_AND_PICTURE_SCREEN]: {
-    showSkipButton: {
-      paramName: 'showSkipButton',
-      defaultValue: false,
-    },
-    nameType: {
-      paramName: 'nameType',
-      defaultValue: 'first_and_last',
-    },
-  },
   [StatsigLayers.SEND_RECEIVE_QR_CODE]: {
     qrCodeStyle: {
       paramName: 'qrCodeStyle',
@@ -27,19 +11,6 @@ export const LayerParams = {
     qrCodeDataType: {
       paramName: 'qrCodeDataType',
       defaultValue: QRCodeDataType.ValoraDeepLink,
-    },
-  },
-}
-
-export const ConfigParams = {
-  [StatsigDynamicConfigs.USERNAME_BLOCK_LIST]: {
-    blockedAdjectives: {
-      paramName: 'blockedAdjectives',
-      defaultValue: [],
-    },
-    blockedNouns: {
-      paramName: 'blockedNouns',
-      defaultValue: [],
     },
   },
 }
