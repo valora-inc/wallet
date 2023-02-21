7e0eeb13582f4e0a8c259370b3ad33219495ff9e
Kathy Luo <kathyluo18@gmail.com>
2023-02-21 14:15:59 +0100

    fix: wrongful navigation to language screen on dev reload

--- src/redux/persist-helper.ts
+++ src/redux/persist-helper.ts
@@ -18,10 +18,8 @@ export function getRehydratePayload(action: RehydrateAction, key: string) {
 
 // Note: Shouldn't be necessary except for in one place in the root saga
 export function* waitForRehydrate() {
-  if (didRehydrate) {
-    return
+  if (__DEV__ || !didRehydrate) {
+    yield take(REHYDRATE)
+    didRehydrate = true
   }
-  yield take(REHYDRATE)
-  didRehydrate = true
-  return
 }
