301da56a43a5b96f81f1909dd27fa05fc0ce6291
Kathy Luo <kathyluo18@gmail.com>
2023-02-22 09:40:21 +0100

    refactor: remove abstraction

--- src/redux/persist-helper.ts
+++ src/redux/persist-helper.ts
@@ -1,10 +1,7 @@
 import { REHYDRATE } from 'redux-persist/es/constants'
-import { take } from 'redux-saga/effects'
 
 // Re-export for convenience
 export { REHYDRATE } from 'redux-persist/es/constants'
-
-let didRehydrate = false
 
 export interface RehydrateAction {
   type: typeof REHYDRATE
@@ -14,12 +11,4 @@ export interface RehydrateAction {
 
 export function getRehydratePayload(action: RehydrateAction, key: string) {
   return (action && action.payload && action.payload[key]) || {}
-}
-
-// Note: Shouldn't be necessary except for in one place in the root saga
-export function* waitForRehydrate() {
-  if (__DEV__ || !didRehydrate) {
-    yield take(REHYDRATE)
-    didRehydrate = true
-  }
 }
