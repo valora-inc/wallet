e5341bcc2662f046e048379299861b43e3205765
MuckT <mcgtom10@gmail.com>
2023-03-03 21:01:37 -0800

    chore: adjust fonts, touchable ripple and localization

--- src/dappsExplorer/NoResults.tsx
+++ src/dappsExplorer/NoResults.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Trans, useTranslation } from 'react-i18next'
 import { StyleSheet, Text, View } from 'react-native'
+import { useSelector } from 'react-redux'
 import { DappExplorerEvents } from 'src/analytics/Events'
 import ValoraAnalytics from 'src/analytics/ValoraAnalytics'
 import Touchable from 'src/components/Touchable'
 import { DappFilter } from 'src/dapps/types'
+import { currentLanguageSelector } from 'src/i18n/selectors'
 import Colors from 'src/styles/colors'
 import fontStyles from 'src/styles/fonts'
 import { Spacing } from 'src/styles/styles'
@@ -17,54 +19,56 @@ interface Props {
 
 export function NoResults({ filter, removeFilter, testID }: Props) {
   const { t } = useTranslation()
+  const language = useSelector(currentLanguageSelector)
 
-  const removePress = () => {
+  const onPress = () => {
     ValoraAnalytics.track(DappExplorerEvents.dapp_filter_remove)
     removeFilter()
   }
 
   return (
-    <View testID={`${testID}/NoResults`} style={styles.favorites}>
+    <View testID={`${testID}/NoResults`} style={styles.favoritesContainer}>
       <Text style={styles.filterAppliedText}>
         <Trans
           i18nKey={'dappsScreen.emptyResults.message'}
-          tOptions={{ filter: filter?.name.toUpperCase() }}
+          tOptions={{ filter: filter?.name.toLocaleUpperCase(language ?? 'en-US') }}
         >
           <Text style={styles.filterText} />
         </Trans>
       </Text>
-      <Touchable
-        onPress={removePress}
-        style={styles.removeFilterTouchable}
-        testID={`${testID}/NoResults/RemoveFilter`}
-      >
-        <Text style={styles.removeFilterText}>{t('dappsScreen.emptyResults.removeFilter')}</Text>
-      </Touchable>
+      <View style={styles.removeFilterTouchableContainer}>
+        <Touchable
+          hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}
+          onPress={onPress}
+          style={styles.removeFilterTouchable}
+          testID={`${testID}/NoResults/RemoveFilter`}
+        >
+          <Text style={styles.removeFilterText}>{t('dappsScreen.emptyResults.removeFilter')}</Text>
+        </Touchable>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  favorites: {
+  favoritesContainer: {
     flex: 1,
     flexDirection: 'row',
     flexWrap: 'wrap',
     marginTop: Spacing.Thick24,
   },
   filterText: {
-    ...fontStyles.regular500,
-    fontSize: 12,
-    lineHeight: 20,
-    fontWeight: '500',
+    ...fontStyles.small500,
     color: Colors.onboardingBlue,
   },
   filterAppliedText: {
-    ...fontStyles.regular500,
-    fontWeight: '400',
-    fontSize: 12,
-    lineHeight: 20,
+    ...fontStyles.small500,
     color: Colors.dark,
     paddingHorizontal: 4,
+  },
+  removeFilterTouchableContainer: {
+    borderRadius: 16,
+    overflow: 'hidden',
   },
   removeFilterTouchable: {
     paddingVertical: 0,
@@ -73,10 +77,7 @@ const styles = StyleSheet.create({
     borderRadius: 16,
   },
   removeFilterText: {
-    ...fontStyles.regular500,
-    fontWeight: '500',
-    fontSize: 12,
-    lineHeight: 20,
+    ...fontStyles.small500,
     color: Colors.onboardingBlue,
   },
 })
