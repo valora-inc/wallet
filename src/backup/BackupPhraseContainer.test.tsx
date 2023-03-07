b4cd1f284abaeec15e222b85e39094eb1127bdf6
anthonydaoud <anthonydaoud01@gmail.com>
2023-03-07 11:02:53 -0500

    feat: format 12 word seed phrase into a table design (#3500)
    ### Description
    
    Currently, the wallet displays both 12 and 24 word seed phrases as a
    list of words. This PR adds modifies the display of 12 word phrases so
    that they are now shown in a table.
    
    **Before**
    
    
    ![backupBefore](https://user-images.githubusercontent.com/20508929/223466844-3b125b0e-7d32-4f46-be19-614ce3d22dda.png)
    
    
    **After**
    
    
    ![backupAfter](https://user-images.githubusercontent.com/20508929/223466874-204c6eaf-7b7e-4087-86dc-daf5fbfb1a18.png)
    
    
    
    ### Test plan
    
    Updated e2e tests and jest snapshot.
    
    ### Related issues
    
    - ACT-649
    
    ### Backwards compatibility
    
    Yes.

--- src/backup/BackupPhraseContainer.test.tsx
+++ src/backup/BackupPhraseContainer.test.tsx
@@ -5,13 +5,24 @@ import BackupPhraseContainer, {
   BackupPhraseContainerMode,
   BackupPhraseType,
 } from 'src/backup/BackupPhraseContainer'
-import { mockMnemonic } from 'test/values'
+import { mockMnemonic, mockTwelveWordMnemonic } from 'test/values'
 
 describe(BackupPhraseContainer, () => {
-  it('renders correctly for readonly backup phrase', () => {
+  it('renders correctly for readonly backup 24-word phrase', () => {
     const tree = render(
       <BackupPhraseContainer
         value={mockMnemonic}
+        mode={BackupPhraseContainerMode.READONLY}
+        type={BackupPhraseType.BACKUP_KEY}
+      />
+    )
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders correctly for readonly backup 12-word phrase', () => {
+    const tree = render(
+      <BackupPhraseContainer
+        value={mockTwelveWordMnemonic}
         mode={BackupPhraseContainerMode.READONLY}
         type={BackupPhraseType.BACKUP_KEY}
       />
