aceb721f7bebcf079bc2f600ebd749a41c2ec31b
MuckT <mcgtom10@gmail.com>
2023-03-08 09:03:03 -0800

    chore: combine duplicate conditionals

--- src/dappsExplorer/DAppsExplorerScreenFilter.tsx
+++ src/dappsExplorer/DAppsExplorerScreenFilter.tsx
@@ -177,13 +177,10 @@ export function DAppsExplorerScreenFilter() {
                       removeFilter={removeFilter}
                       onPressDapp={onSelectDapp}
                     />
+                    <Text style={styles.sectionTitle}>
+                      {t('dappsScreen.allDapps').toLocaleUpperCase(language ?? 'en-US')}
+                    </Text>
                   </>
-                )}
-
-                {dappFavoritesEnabled && (
-                  <Text style={styles.sectionTitle}>
-                    {t('dappsScreen.allDapps').toLocaleUpperCase(language ?? 'en-US')}
-                  </Text>
                 )}
               </>
             }
