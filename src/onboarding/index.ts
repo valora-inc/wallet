46f0cb4d2eaada77dc624ca81aed364f72f3f0c8
Joseph Bergeron <jophish126@gmail.com>
2023-03-09 10:33:17 -0500

    Update comments

--- src/onboarding/index.ts
+++ src/onboarding/index.ts
@@ -1,14 +1,14 @@
 export interface OnboardingExperimentParams {
-  disableForcedBackup: boolean // Whether to enable/disable forced 24h backup.
+  disableForcedBackup: boolean // Whether to disable forced 24h backup.
   showRecoveryPhraseInOnboarding: boolean // Whether to show recovery phrase and backup options in onboarding.
-  showCloudBackupFakeDoor: boolean // Whether to show cloud backup as a fakedoor recovery option. Only has effect if showRecoveryPhraseInOnboarding is true.
+  showCloudBackupFakeDoor: boolean // Whether to show cloud backup as a fakedoor recovery option. No effect if showRecoveryPhraseInOnboarding is false.
   useNewBackupFlowCopy: boolean // Whether to use new copy in the backup flow.
   showBackupAlert: boolean // Whether to show an alert in the sidenav if the user hasn't backed up.
   useNewBackupHomecard: boolean // Whether to use new designs for backup homecard.
 }
 
 export function getOnboardingExperimentParams(): OnboardingExperimentParams {
-  // TODO (ACT-643): Replace function body with Statsig integration
+  // TODO (ACT-643): Replace function body with Statsig integration.
   return {
     disableForcedBackup: false,
     showRecoveryPhraseInOnboarding: false,
