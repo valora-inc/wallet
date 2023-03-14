911a3dce3f6d93816adc81ffc37228de9d2079d6
Jean Regisser <jean.regisser@gmail.com>
2023-03-14 10:05:28 +0100

    feat: logger improvements (#3509)
    ### Description
    
    This fixes a couple of issues with our logger:
    - log file not containing the full info, only the first argument was
    written to the file (while showing the full line in the packager logs).
    - warn and error levels treated as info. Making it easy to miss them in
    the packager logs. Though they were correctly already being written to
    the log file as `error` or `warn`
    - aligns the data in the log file so it's easier to parse visually
    
    ### Test plan
    
    **Log file (before)**:
    
    ```
    info [2023-03-07T18:16:57.964Z] sentry/Sentry/Sentry not enabled
    debug [2023-03-07T18:16:58.743Z] ValoraAnalytics/Queued call to identify, 0xa6d1e0bdb6960c3f1bda8ef8f1e91480cfc40dbb, [object Object]
    error [2023-03-07T18:16:58.744Z] The above error occurred in task appVersionSaga
    
    debug [2023-03-07T18:16:58.744Z] app/saga@monitorAppState/Starting monitor app state saga
    debug [2023-03-07T18:16:58.744Z] ValoraAnalytics/Queued call to track, init_contractkit_start
    info [2023-03-07T18:16:58.745Z] web3/contracts@initContractKit/Initializing contractkit
    info [2023-03-07T18:16:58.745Z] web3/contracts@initContractKit/Initializing wallet, {"address":"0xa6d1e0bdb6960c3f1bda8ef8f1e91480cfc40dbb","createdAt":"2023-03-06T12:01:38.576Z"}
    debug [2023-03-07T18:16:58.745Z] ValoraAnalytics/Queued call to track, init_contractkit_get_wallet_start
    debug [2023-03-07T18:16:58.745Z] ValoraAnalytics/Queued call to track, init_contractkit_get_wallet_finish
    debug [2023-03-07T18:16:58.747Z] web3/dataEncryptionKey@registerAccountDek/Skipping DEK registration because its already registered
    ```
    
    **Log file (after)**:
    
    ```
    INFO  [2023-03-07T18:23:25.607Z] sentry/Sentry/Sentry not enabled
    DEBUG [2023-03-07T18:23:26.572Z] ValoraAnalytics/Queued call to identify, 0xa6d1e0bdb6960c3f1bda8ef8f1e91480cfc40dbb, [object Object]
    ERROR [2023-03-07T18:23:26.572Z] [Error: You attempted to use 'firebase.database' but this module could not be found.
    
    Ensure you have installed and imported the '@react-native-firebase/database' package.]
    ERROR [2023-03-07T18:23:26.572Z] The above error occurred in task appVersionSaga
    
    DEBUG [2023-03-07T18:23:26.573Z] app/saga@monitorAppState/Starting monitor app state saga
    DEBUG [2023-03-07T18:23:26.709Z] ValoraAnalytics/Queued call to track, init_contractkit_start
    INFO  [2023-03-07T18:23:26.710Z] web3/contracts@initContractKit/Initializing contractkit
    INFO  [2023-03-07T18:23:26.710Z] web3/contracts@initContractKit/Initializing wallet, {"address":"0xa6d1e0bdb6960c3f1bda8ef8f1e91480cfc40dbb","createdAt":"2023-03-06T12:01:38.576Z"}
    DEBUG [2023-03-07T18:23:26.710Z] ValoraAnalytics/Queued call to track, init_contractkit_get_wallet_start
    DEBUG [2023-03-07T18:23:26.711Z] ValoraAnalytics/Queued call to track, init_contractkit_get_wallet_finish
    DEBUG [2023-03-07T18:23:26.711Z] web3/dataEncryptionKey@registerAccountDek/Skipping DEK registration because its already registered
    ```
    
    Notice how it actually includes the important error message about
    `firebase.database`, which wasn't present before, and complicating
    debugging from user provided logs.
    
    **Packager logs (before)**:
    
    <img width="1201" alt="Screenshot 2023-03-07 at 19 26 15"
    src="https://user-images.githubusercontent.com/57791/223515668-a93b4e33-730b-40fe-b7d8-bf58f8880f9a.png">
    
    **Packager logs (after)**:
    
    <img width="1098" alt="Screenshot 2023-03-07 at 19 29 04"
    src="https://user-images.githubusercontent.com/57791/223516384-54787ca6-929f-4ec0-922b-46eeb923ed73.png">
    
    It's much easier to see errors / warnings there.
    
    Note: I'll restore the timestamp there in a follow up PR.
    
    Also they now show clearly in-app too in `__DEV__` mode, forcing us to
    act on them.
    
    <img
    src="https://user-images.githubusercontent.com/57791/223516722-65adf6ed-b2f3-4728-9593-8e6ab6d67489.png"
    width="50%" />
    
    ### Related issues
    
    - Fixes RET-648
    
    ### Backwards compatibility
    
    Yes

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -46,8 +46,7 @@ class Logger {
     if (this.level < LoggerLevel.Warn) {
       return
     }
-    // console.warn would display yellow box, therefore, we will log to console.info instead.
-    console.info(`${tag}/${messages.join(', ')}`)
+    console.warn(`${tag}/${messages.join(', ')}`)
   }
 
   error = (
@@ -94,12 +93,9 @@ class Logger {
         Sentry.captureMessage(message, captureContext)
       }
     }
-    console.info(
+    console.error(
       `${tag} :: ${message} :: ${errorMsg} :: network connected ${this.isNetworkConnected}`
     )
-    if (__DEV__) {
-      console.info(console.trace())
-    }
   }
 
   setIsNetworkConnected = (isConnected: boolean) => {
@@ -258,23 +254,24 @@ class Logger {
     }
   }
 
-  // Anything being sent to console.log, console.warn, or console.error is piped into
-  // the logfile specified by getReactNativeLogsFilePath()
+  // Anything being sent to console.log, console.warn, console.error, etc is piped into
+  // the log file specified by getReactNativeLogsFilePath()
   overrideConsoleLogs = () => {
     const logFilePath = this.getReactNativeLogFilePath()
     console.debug('React Native logs will be piped to ' + logFilePath)
 
-    const consoleFns: { [key: string]: (message?: any, ...optionalParams: any[]) => void } = {
-      debug: console.debug,
-      log: console.log,
-      info: console.info,
-      // console.error displays a red box, therefore, we console.info instead
-      error: console.info,
-      // console.warn displays a yellow box, therefore, we console.info instead
-      warn: console.info,
+    // These are the levels used by the nativeLoggingHook
+    // See https://github.com/facebook/react-native/blob/419025df226dfad6a2be57c8d5515f103b96917b/packages/polyfills/console.js#L382-L387
+    // Here we pad the end of the string so logs are visually aligned in the file
+    // and is easier to parse for us humans :)
+    const NATIVE_LOG_LEVELS: Record<number, string | undefined> = {
+      0: 'DEBUG',
+      1: 'INFO ',
+      2: 'WARN ',
+      3: 'ERROR',
     }
 
-    const writeLog = async (level: string, message: string) => {
+    const writeLog = async (logLevel: number, message: string) => {
       try {
         // If log folder not present create it
         await RNFS.mkdir(this.getReactNativeLogsDir())
@@ -288,44 +285,28 @@ class Logger {
         // Ensure messages are converted to utf8 as some remote CTA's can have Non-ASCII characters
         await RNFS.appendFile(
           logFilePath,
-          `${level} [${timestamp}] ${Buffer.from(message, 'utf-8').toString()}\n`,
+          `${NATIVE_LOG_LEVELS[logLevel] || NATIVE_LOG_LEVELS[0]} [${timestamp}] ${Buffer.from(
+            message,
+            'utf-8'
+          ).toString()}\n`,
           'utf8'
         )
       } catch (error) {
-        consoleFns.debug(`Failed to write to ${logFilePath}`, error)
+        console.error(`Failed to write to ${logFilePath}`, error)
       }
     }
 
-    const log = (level: string, message?: any, ...optionalParams: any[]) => {
-      if (typeof message === 'string') {
-        const timestamp = Date.now()
-        const consoleMessage = `[${timestamp}] ${message}`
+    // Override the method used by React Native through which all logs go through
+    // See https://github.com/facebook/react-native/blob/419025df226dfad6a2be57c8d5515f103b96917b/packages/polyfills/console.js
+    // This way we don't need to override all console methods
+    const originalNativeLoggingHook = global.nativeLoggingHook
 
-        consoleFns[level](consoleMessage, ...optionalParams)
-        writeLog(level, message).catch((error) => consoleFns.debug(error))
-      } else {
-        consoleFns[level](message, ...optionalParams)
-      }
-    }
-
-    console.log = (message?: any, ...optionalParams: any[]) => {
-      log('log', message, ...optionalParams)
-    }
-
-    console.debug = (message?: any, ...optionalParams: any[]) => {
-      log('debug', message, ...optionalParams)
-    }
-
-    console.info = (message?: any, ...optionalParams: any[]) => {
-      log('info', message, ...optionalParams)
-    }
-
-    console.warn = (message?: any, ...optionalParams: any[]) => {
-      log('warn', message, ...optionalParams)
-    }
-
-    console.error = (message?: any, ...optionalParams: any[]) => {
-      log('error', message, ...optionalParams)
+    global.nativeLoggingHook = (message: string, logLevel: number) => {
+      // TODO: as an improvement, we could hook into the nativeLoggingHook from the native side
+      // to avoid the extra calls via the bridge to write to the file
+      // but for now it's simpler
+      writeLog(logLevel, message).catch((error) => console.error(error))
+      originalNativeLoggingHook(message, logLevel)
     }
   }
 }
