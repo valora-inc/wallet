5ad6b8fa85dfb1c7017f82cf3abd976124a5fe9f
Charlie Andrews <charlie.andrews@valoraapp.com>
2023-03-16 14:35:34 -0700

    added integration test

--- src/account/saga.test.ts
+++ src/account/saga.test.ts
@@ -10,6 +10,7 @@ import {
   handleChooseCreateAccount,
   handleUpdateAccountRegistration,
   initializeAccountSaga,
+  watchChooseCreateAccount,
 } from 'src/account/saga'
 import { choseToRestoreAccountSelector } from 'src/account/selectors'
 import { updateAccountRegistration } from 'src/account/updateAccountRegistration'
@@ -24,7 +25,7 @@ import { getOrCreateAccount, unlockAccount, UnlockResult } from 'src/web3/saga'
 import { walletAddressSelector } from 'src/web3/selectors'
 import { mockWallet } from 'test/values'
 import { mocked } from 'ts-jest/utils'
-import { initializeAccountSuccess, saveSignedMessage } from './actions'
+import { chooseCreateAccount, initializeAccountSuccess, saveSignedMessage } from './actions'
 import { Statsig } from 'statsig-react-native'
 
 const loggerErrorSpy = jest.spyOn(Logger, 'error')
@@ -233,5 +234,15 @@ describe('handleChooseCreateAccount', () => {
     await expectSaga(handleChooseCreateAccount).run()
     expect(Statsig.updateUser).toHaveBeenCalled()
     expect(loggerErrorSpy).toHaveBeenCalled()
+  })
+  it('integration test: Statsig.updateUser gets called when CHOOSE_CREATE_ACCOUNT is dispatched', async () => {
+    const fakeISODateTime = '2023-03-15T22:16:39.658Z'
+    jest.spyOn(Date.prototype, 'toISOString').mockReturnValueOnce(fakeISODateTime)
+    await expectSaga(watchChooseCreateAccount).dispatch(chooseCreateAccount()).silentRun()
+    expect(Statsig.updateUser).toHaveBeenCalledWith({
+      custom: {
+        startOnboardingTimestamp: fakeISODateTime,
+      },
+    })
   })
 })
