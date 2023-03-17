263de5fbc9435ef272ff4d9ab3b49dde2f78e143
Kathy Luo <kathyluo18@gmail.com>
2023-03-17 15:21:43 +0100

    feat: add haptic feedback on settings toggle and dapp favourite

---
+++ src/styles/hapticFeedback.ts
@@ -0,0 +1,18 @@
+import ReactNativeHapticFeedback from 'react-native-haptic-feedback'
+
+const options = {
+  enableVibrateFallback: false,
+  ignoreAndroidSystemSettings: false,
+}
+
+export const vibrate = () => {
+  ReactNativeHapticFeedback.trigger('impactMedium', options)
+}
+
+export const vibrateLight = () => {
+  ReactNativeHapticFeedback.trigger('impactMedium', options)
+}
+
+export const vibrateSuccess = () => {
+  ReactNativeHapticFeedback.trigger('notificationSuccess', options)
+}
