1b541a4e43da0a703d24a1b6b953c1000faab44f
MuckT <mcgtom10@gmail.com>
2023-03-18 20:35:23 -0700

    test(e2e): persona scroll fix attempt

--- e2e/src/usecases/FiatConnectTransferOut.js
+++ e2e/src/usecases/FiatConnectTransferOut.js
@@ -272,7 +272,7 @@ export const fiatConnectKycTransferOut = () => {
 
     // Scroll down to continue button - all params needed to scroll down on persona template
     // Last parameter is the start point of the scroll, 0.5 is the middle of the element
-    await element(by.type('UIScrollView')).atIndex(0).scroll(400, 'down', NaN, 0.5)
+    await element(by.type('UIScrollView')).atIndex(0).scroll(400, 'down', NaN, 0.0)
 
     await element(by.text('Continue')).tap()
 
