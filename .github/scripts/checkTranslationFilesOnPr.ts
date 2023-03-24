a6311b77b4835bda316460e24db7a94e34ca4c84
Kathy Luo <kathyluo18@gmail.com>
2023-03-24 16:05:40 +0100

    chore: tidy up script

--- .github/scripts/checkTranslationFilesOnPr.ts
+++ .github/scripts/checkTranslationFilesOnPr.ts
@@ -9,7 +9,6 @@ const TRANSLATION_FILES = new RegExp(
 
 echo('Checking modified files')
 const updatedFilenames = process.argv[2]
-echo(`argv 2 ${updatedFilenames}`)
 const modifiedTranslationFiles = updatedFilenames.split(',').filter((filename) => {
   return filename.match(TRANSLATION_FILES)
 })
