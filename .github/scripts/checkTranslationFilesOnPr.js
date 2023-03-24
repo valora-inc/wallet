07ce5e89f283eab2dcdfb4b6debff3702e5c0c7f
Kathy Luo <kathyluo18@gmail.com>
2023-03-24 14:25:58 +0100

    chore: add workflow to check that only base translations are modified

---
+++ .github/scripts/checkTranslationFilesOnPr.js
@@ -0,0 +1,24 @@
+import { config, echo, exec } from 'shelljs'
+
+config.fatal = true
+
+// matches all translation.json and InfoPlist.strings files except for the base ones
+const TRANSLATION_FILES = new RegExp(
+  /^locales\/(?!base\/)[^\/]+\/translation\.json$|^ios\/celo\/(?!Base\b)\w+\.lproj\/InfoPlist\.strings$/
+)
+
+echo('Checking modified files')
+const updatedFiles = exec('git diff main --name-only', { silent: true }).stdout.split(/\n/)
+const modifiedTranslationFiles = updatedFiles.filter((filename) => {
+  return filename.match(TRANSLATION_FILES)
+})
+
+if (modifiedTranslationFiles.length > 0) {
+  echo(
+    `❌ Only base translation files should be modified! The following files should not be modified:`
+  )
+  echo(modifiedTranslationFiles.join('\n'))
+  process.exit(1)
+}
+
+echo('✅ All good to go 🚀')
