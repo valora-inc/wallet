62731d1ed4b9b7fa9a2ed5d6b3466a2a60d04d98
Kathy Luo <kathyluo18@gmail.com>
2023-03-24 12:03:40 +0100

    chore: chain cases

--- src/consumerIncentives/slice.ts
+++ src/consumerIncentives/slice.ts
@@ -78,24 +78,25 @@ const slice = createSlice({
     }),
   },
   extraReducers: (builder) => {
-    builder.addCase(REHYDRATE, (state, action: RehydrateAction) => ({
-      ...state,
-      ...getRehydratePayload(action, 'supercharge'),
-      loading: false,
-      error: false,
-      fetchAvailableRewardsLoading: false,
-      fetchAvailableRewardsError: false,
-      availableRewards: [],
-    }))
-    builder.addCase(
-      AppActions.UPDATE_REMOTE_CONFIG_VALUES,
-      (state, action: UpdateConfigValuesAction) => {
-        state.superchargeV2Enabled = action.configValues.superchargeV2Enabled
-        state.superchargeRewardContractAddress =
-          action.configValues.superchargeRewardContractAddress
-        state.superchargeV1Addresses = action.configValues.superchargeV1Addresses
-      }
-    )
+    builder
+      .addCase(REHYDRATE, (state, action: RehydrateAction) => ({
+        ...state,
+        ...getRehydratePayload(action, 'supercharge'),
+        loading: false,
+        error: false,
+        fetchAvailableRewardsLoading: false,
+        fetchAvailableRewardsError: false,
+        availableRewards: [],
+      }))
+      .addCase(
+        AppActions.UPDATE_REMOTE_CONFIG_VALUES,
+        (state, action: UpdateConfigValuesAction) => {
+          state.superchargeV2Enabled = action.configValues.superchargeV2Enabled
+          state.superchargeRewardContractAddress =
+            action.configValues.superchargeRewardContractAddress
+          state.superchargeV1Addresses = action.configValues.superchargeV1Addresses
+        }
+      )
   },
 })
 
