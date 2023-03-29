081126375f44767bee7601dc58d9929edba849fa
MuckT <mcgtom10@gmail.com>
2023-03-29 13:59:06 -0700

    docs: add js doc for calculate search score function

--- src/dappsExplorer/utils.tsx
+++ src/dappsExplorer/utils.tsx
@@ -1,5 +1,15 @@
 import { DappV1, DappV2, isDappV2 } from 'src/dapps/types'
 
+/***
+ * Calculates a score based on the search term and dapp
+ * - +2 if the name matches the search term in a case sensitive way
+ * - +1 if the name matches the search term in a case insensitive way
+ * - +1 if the description matches the search term in a case insensitive way
+ * - +1 if the category matches the search term in a case insensitive way
+ * @param dapp
+ * @param searchTerm
+ * @returns {number} the score of the dapp 0 - 5
+ */
 export const calculateSearchScore = (dapp: DappV2 | DappV1, searchTerm: string) => {
   const nameMatchingCaseScore = dapp.name.includes(searchTerm) ? 2 : 0
   const nameScore = dapp.name.toLowerCase().includes(searchTerm.toLowerCase()) ? 1 : 0
