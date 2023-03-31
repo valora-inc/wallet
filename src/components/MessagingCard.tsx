e8beaf72ce0b1fd7572d00deac1fc81bd3b719cc
Satish Ravi <satish.ravi@valoraapp.com>
2023-03-31 09:53:31 -0700

    feat(home): new shadows for home card, dapps

--- src/components/MessagingCard.tsx
+++ src/components/MessagingCard.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { StyleSheet } from 'react-native'
 import Card, { Props } from 'src/components/Card'
+import { Shadow } from 'src/styles/styles'
 
 // Card used by all messaging cards
 export default function MessagingCard({ style, ...props }: Props) {
-  return <Card style={[styles.container, style]} rounded={true} {...props} />
+  return (
+    <Card style={[styles.container, style]} shadow={Shadow.BarShadow} rounded={true} {...props} />
+  )
 }
 
 const styles = StyleSheet.create({
