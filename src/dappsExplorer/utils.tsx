bccdcb8680e9a0f8ef4a2413165bb140ef3181b6
MuckT <mcgtom10@gmail.com>
2023-04-04 14:02:54 -0700

    chore: remove search support for dapp v1

--- src/dappsExplorer/utils.tsx
+++ src/dappsExplorer/utils.tsx
@@ -1,4 +1,4 @@
-import { DappV1WithCategoryName, DappV2WithCategoryNames, isDappV2 } from 'src/dapps/types'
+import { DappV2WithCategoryNames } from 'src/dapps/types'
 
 /***
  * Calculates a score based on the search term and dapp
@@ -6,25 +6,18 @@ import { DappV1WithCategoryName, DappV2WithCategoryNames, isDappV2 } from 'src/d
  * - +1 if the name matches the search term in a case insensitive way
  * - +1 if the description matches the search term in a case insensitive way
  * - +1 if the category matches the search term in a case insensitive way
- * @param dapp expects a dapp with a categoryName or categoryNames
- * @param searchTerm
+ * @param dapp: dapp with categoryNames
+ * @param searchTerm: the user input search string
  * @returns {number} the score of the dapp 0 - 5
  */
-export const calculateSearchScore = (
-  dapp: DappV1WithCategoryName | DappV2WithCategoryNames,
-  searchTerm: string
-) => {
+export const calculateSearchScore = (dapp: DappV2WithCategoryNames, searchTerm: string) => {
   if (searchTerm === '') return 0
   const nameMatchingCaseScore = dapp.name.includes(searchTerm) ? 2 : 0
   const nameScore = dapp.name.toLowerCase().includes(searchTerm.toLowerCase()) ? 1 : 0
   const descriptionScore = dapp.description.toLowerCase().includes(searchTerm.toLowerCase()) ? 1 : 0
-  const categoryScore = isDappV2(dapp)
-    ? dapp.categoryNames.some((category) =>
-        category.toLowerCase().includes(searchTerm.toLowerCase())
-      )
-      ? 1
-      : 0
-    : dapp.categoryName.toLowerCase().includes(searchTerm.toLowerCase())
+  const categoryScore = dapp.categoryNames.some((category) =>
+    category.toLowerCase().includes(searchTerm.toLowerCase())
+  )
     ? 1
     : 0
   return nameMatchingCaseScore + nameScore + descriptionScore + categoryScore
