c4fe85e5f5ba436fd7eafe206063d852a0a248b7
Tom McGuire <Mcgtom10@gmail.com>
2023-04-10 12:25:15 -0700

    feat: calculate dapp score and sort for search (#3629)
    ### Description
    
    An overhaul of #3598. This PR uses both exact match and fuzz match to
    determine the search score, for former having a having a heavier weight
    than that later. This implementation gives us several advantages to an
    exact match search only.
    
    It uses Levenshtein distance for fuzzy match which is the the minimum
    number of single-character edits (insertions, deletions or
    substitutions) required to change one word into the other. The library
    added is
    [fast-levenshtein](https://www.npmjs.com/package/fast-levenshtein) which
    as of this writing has as 30 million weekly downloads: we probably won't
    have to fix this library ourselves if it breaks.
    
    Some advantages of Levenshtien:
    - Allows for a match if a search term includes the string e.g. Searching
    "Swaps" would fuzzy match with "Swap" using exact match this would not
    match, same with "cambiar" and "cambia".
    - Allow for some misspellings in a language agonistic way. The allowed
    variance of a word from in proportional to the word length. "Swxp" would
    match "Swap", but "I" would not match "e".
    
    Some advantages of using exact match:
    - Works well when the searchTerm is a small subset of a larger string,
    where the levenshtien distance would be rather large in this case e.g.
    "Pool" & "PoolTogether"
    - Early returns if there is an exact match to avoid running unneeded
    calculations
    
    #### Levenshtein Distance Example
    ```
    const levenshteinThreshold = .7
    searchTerm = "swaps"
    haystackTerm = "swap"
    maxLength = Math.Max(searchTerm.length, haystackTerm.length) // 5
    const weightedDistance = get(searchTerm, haystackTerm, { useCollator: true }) / maxLength // .20
    return 1 - weightedDistance >= .7 // True in this case
    ```
    
    This PR includes a `searchDappList` method for sorting the dapp list
    based on a search the main advantage of this is we can reduce duplicate
    code and calculate the search score for each dapp only once per search
    term. This is more performant than the previous sort method which would
    calculate the dapp score each comparison of the sort.
    
    
    #### Some notes about other implementations tried
    
    - BM25 worked decent but was slower and more complex. Had some issues
    with smaller strings.
    - [flexsearch](https://www.npmjs.com/package/flexsearch#contextual)
    using - contextual indexing: worked well, but required additional
    stemmers, which are language specific, to fuzzy match well.
    Documentation was a bit unclear and it's unclear if this library is
    actively maintained.
    
    ### Test plan
    
    Tested locally on iOS and Android.
    Unit tests added for `scoreDapp` method and new selectors in:
    - `src/dappsExplorer/searchDappList.test.tsx`
    - `src/dapps/selectors.test.ts`
    
    ### Related issues
    
    - Fixes: RET-580
    
    ### Backwards compatibility
    
    Yes

---
+++ src/dappsExplorer/searchDappList.tsx
@@ -0,0 +1,111 @@
+import { get } from 'fast-levenshtein'
+import { DappV2WithCategoryNames } from 'src/dapps/types'
+
+const LEVENSHTEIN_THRESHOLD = 0.7 // levenshtein distance threshold
+
+enum ScoreValues {
+  High = 2.5,
+  Medium = 1.25,
+  Low = 0.625,
+}
+
+// Tokens are strings broken into lists of words by language agnostic regex
+const getTokens = (text: string, preserveCasing = false) => {
+  return preserveCasing
+    ? text.match(/[\p{L}]+/gu) ?? []
+    : text.toLowerCase().match(/[\p{L}]+/gu) ?? []
+}
+
+const getSimilarity = (a: string, b: string) => {
+  const maxLength = Math.max(a.length, b.length)
+  return 1 - get(a, b, { useCollator: true }) / maxLength
+}
+
+export const scoreDapp = (dapp: DappV2WithCategoryNames, searchTerm: string) => {
+  let score = 0
+  // Early return if there is no search term
+  if (searchTerm === '') return score
+
+  // Use a Map to keep take of the scores for each searchTerm
+  const scoreMap = new Map<string, number>()
+  const searchTokens = getTokens(searchTerm, true)
+  const searchTokensLower = getTokens(searchTerm)
+  const nameTokens = getTokens(dapp.name, true)
+  const descriptionTokens = getTokens(dapp.description)
+  // Lowercase categories as it was already an array of strings
+  const categoryTokens = dapp.categoryNames.map((category) => category.toLowerCase())
+
+  // Iterate through each word of and populate the scoreMap
+  // Use the searchTokens (Casing Preserved) to check for exact matches
+  for (const word of searchTokens) {
+    // Name scoring
+    if (nameTokens.some((token) => token.includes(word))) {
+      scoreMap.set(word, scoreMap.get(word) ?? 0 + ScoreValues.High)
+    } else {
+      for (const token of nameTokens) {
+        const similarity = getSimilarity(token, word)
+        if (similarity >= LEVENSHTEIN_THRESHOLD) {
+          scoreMap.set(word, (scoreMap.get(word) ?? 0) + ScoreValues.Medium)
+        }
+      }
+    }
+  }
+
+  // Use the lowercase search tokens to score the description and categories
+  for (const word of searchTokensLower) {
+    // Description scoring
+    if (descriptionTokens.some((token) => token.includes(word))) {
+      scoreMap.set(word, scoreMap.get(word) ?? 0 + ScoreValues.Medium)
+    } else {
+      for (const token of descriptionTokens) {
+        const similarity = getSimilarity(token, word)
+        if (similarity >= LEVENSHTEIN_THRESHOLD) {
+          scoreMap.set(word, (scoreMap.get(word) ?? 0) + ScoreValues.Low)
+        }
+      }
+    }
+
+    // Category scoring
+    if (categoryTokens.some((token) => token.includes(word))) {
+      scoreMap.set(word, scoreMap.get(word) ?? 0 + ScoreValues.Medium)
+    } else {
+      for (const token of categoryTokens) {
+        const similarity = getSimilarity(token, word)
+        if (similarity >= LEVENSHTEIN_THRESHOLD) {
+          scoreMap.set(word, (scoreMap.get(word) ?? 0) + ScoreValues.Low)
+        }
+      }
+    }
+  }
+
+  // Add the values from the map to the score
+  for (const value of scoreMap.values()) {
+    score += value
+  }
+
+  // Return the score
+  // If search gets slow due to lots of dapps consider capping the score
+  // and using a counting sort instead of a quicksort in searchDappList
+  return score
+}
+
+export const searchDappList = (dappList: DappV2WithCategoryNames[], searchTerm: string) => {
+  // A map of dapp id to score
+  const dappResults: Record<string, number> = {}
+
+  // Iterate through each dapp and score it populating the dappResults map with dapps that have a score > 0
+  dappList.forEach((dapp) => {
+    const score = scoreDapp(dapp, searchTerm)
+    if (score > 0) {
+      dappResults[dapp.id] = score
+    }
+  })
+
+  // Convert the map to an array, sort it by score and convert it back to a list of dapps
+  const dappResultsArray = Object.entries(dappResults).map(([id, score]) => ({ id, score }))
+  dappResultsArray.sort((a, b) => b.score - a.score)
+  const sortedDappList = dappResultsArray.map(({ id }) => dappList.find((dapp) => dapp.id === id))
+
+  // Return the sorted list of dapps or an empty list if there are no results
+  return sortedDappList ?? []
+}
