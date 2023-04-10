8e4cf177fc5be7e333edbb738db2519568f32dd7
Charlie Andrews <charlie.andrews@valoraapp.com>
2023-04-10 11:28:29 -0700

    fix build failure

--- src/firebase/notifications.test.ts
+++ src/firebase/notifications.test.ts
@@ -93,6 +93,7 @@ describe(handleNotification, () => {
 
   describe('with a payment received notification', () => {
     const message: FirebaseMessagingTypes.RemoteMessage = {
+      fcmOptions: { link: 'https://valoraapp.com' },
       notification: { title: 'My title', body: 'My Body' },
       data: {
         type: NotificationTypes.PAYMENT_RECEIVED,
