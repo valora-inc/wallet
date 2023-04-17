0501c828ca8390e8e17c6a72358a8dd006f74c21
anthonydaoud <anthonydaoud01@gmail.com>
2023-04-17 12:03:08 -0400

    feat: hide Swap and Add & Withdraw menu items for Home Screen Actions experiment (#3614)
    ### Description
    
    As part of an upcoming
    [experiment](https://www.notion.so/actions-on-the-homescreen-50725477376047a892f736d40eaa089f),
    one of the experiment variants will not have the Swap and Add & Withdraw
    actions visible in the hamburger menu. This PR sets up the logic to hide
    these items based on the experiment parameters obtained from Statsig.
    
    #### Menu without the actions
    
    
    ![menuWithoutSwapAddWithdraw](https://user-images.githubusercontent.com/20508929/229548130-7d57a332-9fbc-4adc-bc31-26de3c902dce.png)
    
    
    ### Test plan
    
    Added unit tests.
    
    ### Related issues
    
    - ACT-703
    
    ### Backwards compatibility
    
    Yes.

--- src/navigator/DrawerNavigator.test.tsx
+++ src/navigator/DrawerNavigator.test.tsx
@@ -3,12 +3,25 @@ import * as React from 'react'
 import 'react-native'
 import { Provider } from 'react-redux'
 import DrawerNavigator from 'src/navigator/DrawerNavigator'
+import { getExperimentParams } from 'src/statsig'
 import MockedNavigator from 'test/MockedNavigator'
 import { createMockStore } from 'test/utils'
+import { mocked } from 'ts-jest/utils'
+
+jest.mock('src/statsig', () => ({
+  getExperimentParams: jest.fn(),
+}))
 
 // TODO avoid rendering WalletHome as we're mostly interested in testing the menu here
 
 describe('DrawerNavigator', () => {
+  beforeEach(() => {
+    mocked(getExperimentParams).mockReturnValue({
+      showAddWithdrawOnMenu: true,
+      showSwapOnMenu: true,
+    })
+  })
+
   it('shows the expected menu items', () => {
     const store = createMockStore({
       app: {
@@ -63,6 +76,35 @@ describe('DrawerNavigator', () => {
       </Provider>
     )
     expect(queryByTestId('DrawerItem/dappsScreen.title')).toBeNull()
+  })
+
+  it('hides add/withdraw menu item based on statsig experiment param', () => {
+    mocked(getExperimentParams).mockReturnValue({
+      showAddWithdrawOnMenu: false,
+    })
+    const { queryByTestId } = render(
+      <Provider store={createMockStore()}>
+        <MockedNavigator component={DrawerNavigator}></MockedNavigator>
+      </Provider>
+    )
+    expect(queryByTestId('DrawerItem/addAndWithdraw')).toBeNull()
+  })
+
+  it('hides swap menu item based on statsig experiment param', () => {
+    mocked(getExperimentParams).mockReturnValue({
+      showSwapOnMenu: false,
+    })
+    const store = createMockStore({
+      app: {
+        showSwapMenuInDrawerMenu: true,
+      },
+    })
+    const { queryByTestId } = render(
+      <Provider store={store}>
+        <MockedNavigator component={DrawerNavigator}></MockedNavigator>
+      </Provider>
+    )
+    expect(queryByTestId('DrawerItem/swapScreen.title')).toBeNull()
   })
 
   describe('phone number in drawer', () => {
