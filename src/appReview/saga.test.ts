b9e65da9368bb4492a61b0a9b998433f883f77cb
MuckT <mcgtom10@gmail.com>
2023-04-28 18:35:29 -0700

    test: add app review saga test

---
+++ src/appReview/saga.test.ts
@@ -0,0 +1,31 @@
+import { expectSaga } from 'redux-saga-test-plan'
+import { call, select } from 'redux-saga/effects'
+import { initializeAppReview, updateAppReview } from 'src/appReview/actions'
+import { initAppReview, setAppReview } from 'src/appReview/saga'
+import { appReview } from 'src/appReview/selectors'
+
+jest.mock('react-native-in-app-review', () => ({
+  RequestInAppReview: jest.fn(),
+  isAvailable: jest.fn(),
+}))
+
+describe(initAppReview, () => {
+  it('should be able to initialize appReview', async () => {
+    await expectSaga(initAppReview).call(initializeAppReview).run()
+  })
+  it('should be able to update appReview', async () => {
+    await expectSaga(setAppReview)
+      .provide([
+        [
+          select(appReview),
+          {
+            isAvailable: true,
+            appRated: false,
+            lastInteractionTimestamp: null,
+          },
+        ],
+        [call(updateAppReview), false],
+      ])
+      .run()
+  })
+})
