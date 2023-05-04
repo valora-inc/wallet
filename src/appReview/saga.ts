0d3484804172e01f5e9af9c6a1a706380be85b36
MuckT <mcgtom10@gmail.com>
2023-05-03 17:56:07 -0700

    fix: use actions from slice and update saga

--- src/appReview/saga.ts
+++ src/appReview/saga.ts
@@ -1,18 +1,45 @@
 import InAppReview from 'react-native-in-app-review'
-import { call, takeLatest } from 'redux-saga/effects'
-import { Actions, initializeAppReview, updateAppReview } from 'src/appReview/actions'
+import { call, put, select, spawn, takeLatest, takeLeading } from 'redux-saga/effects'
+import { appReviewSelector } from 'src/appReview/selectors'
+import { setInitialized, setLastInteractionTimestamp } from 'src/appReview/slice'
+import { Actions as SendActions } from 'src/send/actions'
 import { safely } from 'src/utils/safely'
+import { ONE_DAY_IN_MILLIS } from 'src/utils/time'
+import { Actions as Web3Actions } from 'src/web3/actions'
 
 export function* initAppReview() {
-  yield call(initializeAppReview)
+  const { inAppRatingSupported, initialized } = yield select(appReviewSelector)
+  // Quick Return if we have already initialized or if the device does not support in app rating
+  if (initialized || !inAppRatingSupported) return
+  yield put(setInitialized(true))
 }
 
 export function* setAppReview() {
+  const { inAppRatingSupported, initialized, lastInteractionTimestamp } = yield select(
+    appReviewSelector
+  )
+
+  // Quick Return & check if we should show the review prompt
+  // if the device does not support in app review
+  // or we have not initialized
+  // or less than 7 days since last interaction
+  if (
+    !inAppRatingSupported ||
+    !initialized ||
+    lastInteractionTimestamp > Date.now() - ONE_DAY_IN_MILLIS * 7
+  )
+    return
+
+  // Will return true when the in-app review dialog is shown
   const result = yield call(InAppReview.RequestInAppReview)
-  yield call(updateAppReview, result)
+  if (result) yield put(setLastInteractionTimestamp(Date.now()))
+}
+
+export function* watchAppReview() {
+  yield takeLeading([Web3Actions.SET_ACCOUNT], safely(initAppReview))
+  yield takeLatest([SendActions.SEND_PAYMENT_SUCCESS], safely(setAppReview))
 }
 
 export function* appReviewSaga() {
-  yield takeLatest(Actions.INITIALIZE_APP_REVIEW, safely(initAppReview))
-  yield takeLatest(Actions.UPDATE_APP_REVIEW, safely(setAppReview))
+  yield spawn(watchAppReview)
 }
