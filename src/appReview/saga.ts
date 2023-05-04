62523b7ebf1393bf20a9180249cdd11f188a7663
MuckT <mcgtom10@gmail.com>
2023-05-04 11:02:53 -0700

    fix: remove init and store only what is needed in redux

--- src/appReview/saga.ts
+++ src/appReview/saga.ts
@@ -1,44 +1,28 @@
 import InAppReview from 'react-native-in-app-review'
-import { call, put, select, spawn, takeLatest, takeLeading } from 'redux-saga/effects'
-import { appReviewSelector } from 'src/appReview/selectors'
-import { setInitialized, setLastInteractionTimestamp } from 'src/appReview/slice'
+import { call, put, select, spawn, takeLatest } from 'redux-saga/effects'
+import { lastInteractionTimestampSelector } from 'src/appReview/selectors'
+import { setLastInteractionTimestamp } from 'src/appReview/slice'
 import { Actions as SendActions } from 'src/send/actions'
 import { safely } from 'src/utils/safely'
 import { ONE_DAY_IN_MILLIS } from 'src/utils/time'
-import { Actions as Web3Actions } from 'src/web3/actions'
-
-export function* initAppReview() {
-  const { inAppRatingSupported, initialized } = yield select(appReviewSelector)
-  // Quick Return if we have already initialized or if the device does not support in app rating
-  if (initialized || !inAppRatingSupported) return
-  yield put(setInitialized(true))
-}
 
 export function* setAppReview() {
-  const { inAppRatingSupported, initialized, lastInteractionTimestamp } = yield select(
-    appReviewSelector
-  )
+  // Quick return if the device does not support in app review
+  if (!InAppReview.isAvailable()) return
 
-  // Quick Return & check if we should show the review prompt
-  // if the device does not support in app review
-  // or we have not initialized
-  // or less than 7 days since last interaction
-  if (
-    !inAppRatingSupported ||
-    !initialized ||
-    lastInteractionTimestamp > Date.now() - ONE_DAY_IN_MILLIS * 7
-  ) {
-    return
+  const lastInteractionTimestamp = yield select(lastInteractionTimestampSelector)
+  const now = Date.now()
+
+  // If the last interaction was less than a quarter year ago or null
+  if (lastInteractionTimestamp <= now - ONE_DAY_IN_MILLIS * 91) {
+    const result = yield call(InAppReview.RequestInAppReview)
+    // If the in app review was shown, update the last interaction timestamp
+    if (result) yield put(setLastInteractionTimestamp(now))
   }
-
-  // Will return true when the in-app review dialog is shown
-  const result = yield call(InAppReview.RequestInAppReview)
-  if (result) yield put(setLastInteractionTimestamp(Date.now()))
 }
 
 export function* watchAppReview() {
   // TODO: add more actions to trigger app review
-  yield takeLeading([Web3Actions.SET_ACCOUNT], safely(initAppReview))
   yield takeLatest([SendActions.SEND_PAYMENT_SUCCESS], safely(setAppReview))
 }
 
--- src/appReview/slice.ts
+++ src/appReview/slice.ts
@@ -1,17 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit'
-import InAppReview from 'react-native-in-app-review'
 
 export interface State {
-  initialized: boolean
-  inAppRatingSupported: boolean
   lastInteractionTimestamp: number | null
 }
 
 const initialState: State = {
-  initialized: false,
-  // Android version >= 21 and iOS >= 10.3
-  // https://github.com/MinaSamir11/react-native-in-app-review#usage
-  inAppRatingSupported: !!InAppReview.isAvailable,
   lastInteractionTimestamp: null,
 }
 
@@ -19,18 +12,12 @@ export const slice = createSlice({
   name: 'appReview',
   initialState,
   reducers: {
-    setInitialized: (state, action) => {
-      state.initialized = action.payload
-    },
-    inAppRatingSupported: (state, action) => {
-      state.inAppRatingSupported = action.payload
-    },
     setLastInteractionTimestamp: (state, action) => {
       state.lastInteractionTimestamp = action.payload
     },
   },
 })
 
-export const { inAppRatingSupported, setLastInteractionTimestamp, setInitialized } = slice.actions
+export const { setLastInteractionTimestamp } = slice.actions
 
 export default slice.reducer
