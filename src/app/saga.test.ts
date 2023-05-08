b0b5d5ae5928f80b1780989d8fea71f7a685da40
Kathy Luo <kathyluo18@gmail.com>
2023-05-08 09:31:38 +0200

    chore(test): fix app init test which persisted spy mock across tests

--- src/app/saga.test.ts
+++ src/app/saga.test.ts
@@ -81,8 +81,6 @@ jest.mock('src/i18n', () => ({
 const mockedDEK = mocked(DEK)
 mockedDEK.compressedPubKey = jest.fn().mockReturnValue('publicKeyForUser')
 
-const loggerWarnSpy = jest.spyOn(Logger, 'warn')
-
 describe('handleDeepLink', () => {
   beforeEach(() => {
     jest.clearAllMocks()
@@ -484,7 +482,7 @@ describe('runCentralPhoneVerificationMigration', () => {
       },
       body: '{"clientPlatform":"android","clientVersion":"0.0.1","publicDataEncryptionKey":"publicKeyForUser","phoneNumber":"+31619777888","pepper":"somePepper","phoneHash":"somePhoneHash","mtwAddress":"0x123"}',
     })
-    expect(loggerWarnSpy).toHaveBeenCalled()
+    expect(Logger.warn).toHaveBeenCalled()
   })
 
   it('should not run if migration conditions are not met', async () => {
@@ -528,7 +526,7 @@ describe('runCentralPhoneVerificationMigration', () => {
       .run()
 
     expect(mockFetch).not.toHaveBeenCalled()
-    expect(loggerWarnSpy).toHaveBeenCalled()
+    expect(Logger.warn).toHaveBeenCalled()
   })
 })
 
@@ -541,7 +539,7 @@ describe('appInit', () => {
     [select(allowOtaTranslationsSelector), true],
     [select(otaTranslationsAppVersionSelector), '1'],
     [select(currentLanguageSelector), 'nl-NL'],
-    [select(sentryNetworkErrorsSelector), 'network error'],
+    [select(sentryNetworkErrorsSelector), ['network error']],
   ]
 
   it('should initialise the correct components, with the stored language', async () => {
