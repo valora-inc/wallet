abc34ce7a5c801ee378d3768f4c6bac4b5e05d97
Tom McGuire <Mcgtom10@gmail.com>
2023-05-09 11:04:14 -0700

    chore: remove app.celoNews.enabled (#3719)
    ### Description
    
    A follow up PR to: #3641 and  #3701.
    Removes removes enabled from the `CeloNewsConfig`.
    
    ### Test plan
    
    - Tested in CI.
    - `migrations.test.ts` updated. 
    
    ### Related issues
    
    - Fixes RET-624
    
    ### Backwards compatibility
    
    Yes

--- src/redux/migrations.test.ts
+++ src/redux/migrations.test.ts
@@ -11,6 +11,7 @@ import {
   v106Schema,
   v112Schema,
   v115Schema,
+  v124Schema,
   v13Schema,
   v14Schema,
   v15Schema,
@@ -870,6 +871,14 @@ describe('Redux persist migrations', () => {
     const migratedSchema = migrations[116](oldSchema)
     const expectedSchema: any = _.cloneDeep(oldSchema)
     expectedSchema.account.startOnboardingTime = undefined
+    expect(migratedSchema).toStrictEqual(expectedSchema)
+  })
+
+  it('works from v124 to v125', () => {
+    const oldSchema = v124Schema
+    const migratedSchema = migrations[125](oldSchema)
+    const expectedSchema: any = _.cloneDeep(oldSchema)
+    delete expectedSchema.app.celoNews.enabled
     expect(migratedSchema).toStrictEqual(expectedSchema)
   })
 })
