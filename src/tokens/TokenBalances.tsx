09e68ee78f2bf287f39f56d6bd1859e24623f48f
Kathy Luo <kathyluo18@gmail.com>
2023-05-11 13:58:21 +0200

    feat: update UI on token balances screen

--- src/tokens/TokenBalances.tsx
+++ src/tokens/TokenBalances.tsx
@@ -1,8 +1,18 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import BigNumber from 'bignumber.js'
-import React, { useLayoutEffect } from 'react'
+import { debounce } from 'lodash'
+import React, { useLayoutEffect, useRef, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Image, PixelRatio, ScrollView, StyleSheet, Text, View } from 'react-native'
+import {
+  FlatList,
+  Image,
+  LayoutChangeEvent,
+  NativeScrollEvent,
+  PixelRatio,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { useSelector } from 'react-redux'
 import { HomeEvents } from 'src/analytics/Events'
@@ -19,12 +29,11 @@ import { HeaderTitleWithSubtitle, headerWithBackButton } from 'src/navigator/Hea
 import { navigate } from 'src/navigator/NavigationService'
 import { Screens } from 'src/navigator/Screens'
 import { StackParamList } from 'src/navigator/types'
-import Positions from 'src/positions/Positions'
 import { totalPositionsBalanceUsdSelector } from 'src/positions/selectors'
 import Colors from 'src/styles/colors'
 import fontStyles from 'src/styles/fonts'
 import { Spacing } from 'src/styles/styles'
-import variables from 'src/styles/variables'
+import AssetBalanceFiat from 'src/tokens/AssetBalanceFiat'
 import {
   stalePriceSelector,
   tokensWithTokenBalanceSelector,
@@ -32,12 +41,15 @@ import {
   visualizeNFTsEnabledInHomeAssetsPageSelector,
 } from 'src/tokens/selectors'
 import { TokenBalance } from 'src/tokens/slice'
+import { sortByUsdBalance } from 'src/tokens/utils'
 import { ONE_DAY_IN_MILLIS } from 'src/utils/time'
 import networkConfig from 'src/web3/networkConfig'
 import { walletAddressSelector } from 'src/web3/selectors'
-import { sortByUsdBalance } from './utils'
 
 type Props = NativeStackScreenProps<StackParamList, Screens.TokenBalances>
+
+const HEADER_UPDATE_DEBOUNCE_TIME_MS = 100
+
 function TokenBalancesScreen({ navigation }: Props) {
   const { t } = useTranslation()
   const tokens = useSelector(tokensWithTokenBalanceSelector)
@@ -55,6 +67,9 @@ function TokenBalancesScreen({ navigation }: Props) {
   const totalPositionsBalanceLocal = useDollarsToLocalAmount(totalPositionsBalanceUsd)
   const totalBalanceLocal = totalTokenBalanceLocal?.plus(totalPositionsBalanceLocal ?? 0)
 
+  const balanceHeightRef = useRef(0)
+  const [showBalanceInHeader, setShowBalanceInHeader] = useState(false)
+
   useLayoutEffect(() => {
     const subTitle =
       !tokensAreStale && totalBalanceLocal.gte(0)
@@ -65,9 +80,13 @@ function TokenBalancesScreen({ navigation }: Props) {
         : `${localCurrencySymbol} -`
 
     navigation.setOptions({
-      headerTitle: () => <HeaderTitleWithSubtitle title={t('balances')} subTitle={subTitle} />,
+      title: '',
+      headerTitle: () =>
+        showBalanceInHeader ? (
+          <HeaderTitleWithSubtitle title={t('totalAssets')} subTitle={subTitle} />
+        ) : null,
     })
-  }, [navigation, totalBalanceLocal, localCurrencySymbol])
+  }, [navigation, totalBalanceLocal, localCurrencySymbol, showBalanceInHeader])
 
   function isHistoricalPriceUpdated(token: TokenBalance) {
     return (
@@ -77,7 +96,7 @@ function TokenBalancesScreen({ navigation }: Props) {
     )
   }
 
-  function getTokenDisplay(token: TokenBalance) {
+  function getTokenDisplay({ item: token }: { item: TokenBalance }) {
     return (
       <View key={`Token${token.address}`} style={styles.tokenContainer}>
         <View style={styles.row}>
@@ -127,6 +146,18 @@ function TokenBalancesScreen({ navigation }: Props) {
     })
   }
 
+  const handleMeasureBalanceHeight = (event: LayoutChangeEvent) => {
+    balanceHeightRef.current = event.nativeEvent.layout.height
+  }
+
+  const debouncedUpdateHeader = debounce((scrollOffset: number) => {
+    setShowBalanceInHeader(scrollOffset > balanceHeightRef.current)
+  }, HEADER_UPDATE_DEBOUNCE_TIME_MS)
+
+  const handleScroll = (event: { nativeEvent: NativeScrollEvent }) => {
+    debouncedUpdateHeader(event.nativeEvent.contentOffset.y)
+  }
+
   return (
     <>
       {shouldVisualizeNFTsInHomeAssetsPage && (
@@ -146,7 +177,7 @@ function TokenBalancesScreen({ navigation }: Props) {
             <Text style={styles.bannerText}>{t('nftViewer')}</Text>
             <View style={styles.rightInnerContainer}>
               <Text style={styles.bannerText}>{t('open')}</Text>
-              <OpenLinkIcon />
+              <OpenLinkIcon color={Colors.greenUI} />
             </View>
           </View>
         </Touchable>
@@ -156,18 +187,20 @@ function TokenBalancesScreen({ navigation }: Props) {
           <Text style={styles.lastDayText}>{t('lastDay')}</Text>
         </View>
       )}
-      <ScrollView
-        style={styles.scrollContainer}
+
+      <FlatList
+        style={styles.flatListContainer}
         contentContainerStyle={{
           paddingBottom: insets.bottom,
         }}
         // Workaround iOS setting an incorrect automatic inset at the top
         scrollIndicatorInsets={{ top: 0.01 }}
-      >
-        {tokens.sort(sortByUsdBalance).map(getTokenDisplay)}
-        {/* Temporary until we have finalized designs */}
-        <Positions />
-      </ScrollView>
+        data={[...tokens.sort(sortByUsdBalance), ...tokens.sort(sortByUsdBalance)]}
+        renderItem={getTokenDisplay}
+        keyExtractor={(item, index) => `${item.address}-${index}`}
+        ListHeaderComponent={<AssetBalanceFiat onLayout={handleMeasureBalanceHeight} />}
+        onScroll={handleScroll}
+      />
     </>
   )
 }
@@ -177,30 +210,33 @@ TokenBalancesScreen.navigationOptions = {
 }
 
 const styles = StyleSheet.create({
-  scrollContainer: {
-    paddingHorizontal: variables.contentPadding,
-  },
   tokenImg: {
-    width: 40,
-    height: 40,
+    width: 32,
+    height: 32,
     borderRadius: 20,
-    marginRight: 12,
+    marginRight: Spacing.Regular16,
   },
   tokenContainer: {
     flexDirection: 'row',
-    paddingTop: variables.contentPadding,
+    paddingBottom: Spacing.Large32,
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
   tokenLabels: {
     flexShrink: 1,
     flexDirection: 'column',
   },
   balances: {
-    flex: 9,
+    flex: 1,
     alignItems: 'flex-end',
   },
   row: {
-    flex: 11,
+    flex: 1,
     flexDirection: 'row',
+    alignItems: 'center',
+  },
+  flatListContainer: {
+    paddingHorizontal: Spacing.Thick24,
   },
   tokenName: {
     ...fontStyles.large600,
@@ -222,18 +258,20 @@ const styles = StyleSheet.create({
     flexDirection: 'row-reverse',
   },
   bannerContainer: {
+    marginTop: Spacing.Smallest8,
     paddingHorizontal: Spacing.Thick24,
     paddingVertical: 4,
     justifyContent: 'space-between',
     flexWrap: 'wrap',
     width: '100%',
     alignItems: 'center',
-    backgroundColor: Colors.greenUI,
+    backgroundColor: Colors.gray1,
     flexDirection: 'row',
   },
   bannerText: {
-    ...fontStyles.displayName,
-    color: Colors.light,
+    ...fontStyles.small500,
+    color: Colors.greenUI,
+    marginRight: 4,
   },
   rightInnerContainer: {
     flexDirection: 'row',
