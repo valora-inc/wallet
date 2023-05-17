21207f7642d0c0ed340330bcfb5aec7e185137d8
Satish Ravi <satish.ravi@valoraapp.com>
2023-05-17 16:27:04 -0700

    feat(keyless-backup): add sign in with google (#3740)
    ### Description
    
    Implements button click to initiate google sign in flow. Result saved on
    redux but not used. Also encrypts .env.* files since they now include
    client ids. See [this
    thread](https://valora-app.slack.com/archives/C025V1D6F3J/p1684179136241829)
    for more context.
    
    To be merged after #3741 
    
    ### Test plan
    
    Manually by running the following steps
    
    1. Enable backup locally by changing the feature flag to true (this line
    needs to change to true:
    https://github.com/valora-inc/wallet/blob/94fee8a/src/account/Settings.tsx#L374)
    2. Build and run app, navigate to settings
    3. Click on Keyless backup, click continue, then Sign In With Google and
    complete sign in
    4. Ensure idToken is saved in redux (can check logs for
    googleSignInCompleted event or look in redux debugger in flipper)
    
    iOS
    
    
    https://github.com/valora-inc/wallet/assets/5062591/e432cac0-9992-4a03-94f7-3af7e12f777f
    
    
    Android
    
    
    https://github.com/valora-inc/wallet/assets/5062591/4c7874f4-8a7d-4909-9b31-ac53e8e0c062
    
    
    
    ### Related issues
    
    - Fixes ACT-743
    
    ### Backwards compatibility
    
    Yes

---
+++ __mocks__/@react-native-google-signin/google-signin.ts
@@ -0,0 +1,6 @@
+export const GoogleSignin = {
+  configure: jest.fn(),
+  signOut: jest.fn(),
+  hasPlayServices: jest.fn(),
+  signIn: jest.fn(),
+}
--- src/keylessBackup/saga.ts
+++ src/keylessBackup/saga.ts
@@ -1,4 +1,6 @@
-import { put, spawn, takeLeading } from 'redux-saga/effects'
+import { GoogleSignin, User } from '@react-native-google-signin/google-signin'
+import { call, put, spawn, takeLeading } from 'redux-saga/effects'
+import { GOOGLE_OAUTH_CLIENT_ID } from 'src/config'
 import {
   googleSignInCompleted,
   googleSignInFailed,
@@ -10,8 +12,20 @@ const TAG = 'keylessBackupSaga'
 
 export function* handleGoogleSignInStarted() {
   try {
-    // TODO: start sign in with google flow
-    yield put(googleSignInCompleted({ idToken: 'dummy' }))
+    GoogleSignin.configure({ webClientId: GOOGLE_OAUTH_CLIENT_ID })
+    // signOut so no saved sessions are used. This is important on Android as
+    // otherwise this just succeeds with a screen flash and doesn't ask for the
+    // Google account to be used
+    yield call([GoogleSignin, 'signOut'])
+    // Ensures play services is available on Android: https://github.com/react-native-google-signin/google-signin#hasplayservicesoptions
+    yield call([GoogleSignin, 'hasPlayServices'])
+    const { idToken }: User = yield call([GoogleSignin, 'signIn'])
+
+    if (!idToken) {
+      throw new Error('got null idToken from GoogleSignIn')
+    }
+
+    yield put(googleSignInCompleted({ idToken }))
   } catch (err) {
     Logger.warn(TAG, 'Sign in with google failed', err)
     yield put(googleSignInFailed())
