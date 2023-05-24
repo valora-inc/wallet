937da6ab9e13ed6a3959c6358c62071eef9fb7ed
MuckT <mcgtom10@gmail.com>
2023-05-24 12:04:39 -0700

    feat: add Nft interface

---
+++ src/nfts/types.ts
@@ -0,0 +1,20 @@
+export interface Nft {
+  tokenId: string
+  contractAddress: string
+  tokenUri: string | null
+  ownerAddress: string | null
+  metadata: {
+    name: string
+    description: string
+    image: string
+    dna?: string
+    id?: number
+    date?: number
+    attributes?: [
+      {
+        trait_type: string
+        value: string
+      }
+    ]
+  } | null
+}
