171f496c467828b4a168292b7b06e6967eec7979
MuckT <mcgtom10@gmail.com>
2023-05-26 21:21:28 -0700

    test(NftFeedItem): add test for carousel navigation and skip existing

--- src/transactions/feed/NftFeedItem.test.tsx
+++ src/transactions/feed/NftFeedItem.test.tsx
@@ -11,6 +11,31 @@ import { createMockStore, RecursivePartial } from 'test/utils'
 
 const MOCK_TX_HASH = '0x006b866d20452a24d1d90c7514422188cc7c5d873e2f1ed661ec3f810ad5331c'
 const MOCK_ADDRESS = '0xFdd8bD58115FfBf04e47411c1d228eCC45E93075'.toLowerCase()
+const MOCK_NFT = {
+  contractAddress: '0x376f5039df4e9e9c864185d8fabad4f04a7e394a',
+  media: [
+    {
+      gateway: 'https://arweave.net/wLYAieibxIvlvCcbtHEOw5fK5_TGtrzn86QBQTyWyEI',
+      raw: 'https://arweave.net/wLYAieibxIvlvCcbtHEOw5fK5_TGtrzn86QBQTyWyEI',
+    },
+  ],
+  metadata: {
+    attributes: [
+      { trait_type: 'Character Set', value: 'Letters' },
+      { trait_type: 'Category', value: 'Quintuple' },
+      { trait_type: 'Rarity', value: 'Community' },
+    ],
+    date: null,
+    description: 'This is a soul name!',
+    dna: null,
+    id: null,
+    image: 'https://arweave.net/wLYAieibxIvlvCcbtHEOw5fK5_TGtrzn86QBQTyWyEI',
+    name: 'muckt.celo',
+  },
+  ownerAddress: MOCK_ADDRESS,
+  tokenId: '333670',
+  tokenUri: 'https://arweave.net/43bofqhlE-8Es0QjTOKvekV29dofxP-nlXOspkz8cO0',
+}
 
 describe('NftFeedItem', () => {
   function renderScreen({
@@ -38,6 +63,7 @@ describe('NftFeedItem', () => {
             timestamp: 1234,
             block: '2345',
             fees,
+            nfts: [MOCK_NFT],
           }}
         />
       </Provider>
@@ -49,7 +75,17 @@ describe('NftFeedItem', () => {
     }
   }
 
-  it('opens NFT Viewer correctly when NFT transaction item is clicked', () => {
+  it('opens NFT Info Carousel correctly when NFT transaction item is clicked', () => {
+    const tree = renderScreen({})
+
+    fireEvent.press(tree.getByTestId('NftFeedItem'))
+
+    expect(navigate).toHaveBeenCalledWith(Screens.NftInfoCarousel, {
+      nfts: [MOCK_NFT],
+    })
+  })
+
+  it.skip('opens NFT Viewer correctly when NFT transaction item is clicked and viewer is disabled in Statsig', () => {
     const tree = renderScreen({})
 
     fireEvent.press(tree.getByTestId('NftFeedItem'))
