ceb8362f53a51f7faa4ed753f34bda8404592aac
MuckT <mcgtom10@gmail.com>
2023-06-02 13:06:18 -0700

    test(nfts): add mock nfts to test values

--- src/transactions/feed/NftFeedItem.test.tsx
+++ src/transactions/feed/NftFeedItem.test.tsx
@@ -9,35 +9,10 @@ import NftFeedItem from 'src/transactions/feed/NftFeedItem'
 import { Fee, TokenTransactionTypeV2 } from 'src/transactions/types'
 import networkConfig from 'src/web3/networkConfig'
 import { createMockStore, RecursivePartial } from 'test/utils'
+import { mockAccount, mockNft } from 'test/values'
 import { mocked } from 'ts-jest/utils'
 
 const MOCK_TX_HASH = '0x006b866d20452a24d1d90c7514422188cc7c5d873e2f1ed661ec3f810ad5331c'
-const MOCK_ADDRESS = '0xFdd8bD58115FfBf04e47411c1d228eCC45E93075'.toLowerCase()
-const MOCK_NFT = {
-  contractAddress: '0x376f5039df4e9e9c864185d8fabad4f04a7e394a',
-  media: [
-    {
-      gateway: 'https://arweave.net/wLYAieibxIvlvCcbtHEOw5fK5_TGtrzn86QBQTyWyEI',
-      raw: 'https://arweave.net/wLYAieibxIvlvCcbtHEOw5fK5_TGtrzn86QBQTyWyEI',
-    },
-  ],
-  metadata: {
-    attributes: [
-      { trait_type: 'Character Set', value: 'Letters' },
-      { trait_type: 'Category', value: 'Quintuple' },
-      { trait_type: 'Rarity', value: 'Community' },
-    ],
-    date: null,
-    description: 'This is a soul name!',
-    dna: null,
-    id: null,
-    image: 'https://arweave.net/wLYAieibxIvlvCcbtHEOw5fK5_TGtrzn86QBQTyWyEI',
-    name: 'muckt.celo',
-  },
-  ownerAddress: MOCK_ADDRESS,
-  tokenId: '333670',
-  tokenUri: 'https://arweave.net/43bofqhlE-8Es0QjTOKvekV29dofxP-nlXOspkz8cO0',
-}
 
 jest.mock('src/statsig')
 
@@ -53,7 +28,7 @@ describe('NftFeedItem', () => {
   }) {
     const store = createMockStore({
       web3: {
-        account: MOCK_ADDRESS,
+        account: mockAccount,
       },
     })
 
@@ -67,7 +42,7 @@ describe('NftFeedItem', () => {
             timestamp: 1234,
             block: '2345',
             fees,
-            nfts: [MOCK_NFT],
+            nfts: [mockNft],
           }}
         />
       </Provider>
@@ -86,7 +61,7 @@ describe('NftFeedItem', () => {
     fireEvent.press(tree.getByTestId('NftFeedItem'))
 
     expect(navigate).toHaveBeenCalledWith(Screens.NftsInfoCarousel, {
-      nfts: [MOCK_NFT],
+      nfts: [mockNft],
     })
   })
 
@@ -97,7 +72,7 @@ describe('NftFeedItem', () => {
     fireEvent.press(tree.getByTestId('NftFeedItem'))
 
     expect(navigate).toHaveBeenCalledWith(Screens.WebViewScreen, {
-      uri: `${networkConfig.nftsValoraAppUrl}?address=${MOCK_ADDRESS}&hide-header=true`,
+      uri: `${networkConfig.nftsValoraAppUrl}?address=${mockAccount.toLowerCase()}&hide-header=true`,
     })
   })
 })
