e10311cea3ceafc9073b9269b01e437cd704f3ba
dievazqu <diego.vazquez@valoraapp.com>
2023-06-20 13:59:56 -0300

    feat(swaps): Adding swap versioning (#3881)

--- src/tokens/slice.ts
+++ src/tokens/slice.ts
@@ -13,8 +13,9 @@ interface BaseToken {
   // This field is for tokens that are part of the core contracts that allow paying for fees and
   // making transfers with a comment.
   isCoreToken?: boolean
+  // Deprecated: This flag enables swapping the token in all the releases, use minimumAppVersionToSwap instead.
   isSwappable?: boolean
-  isSwappableWithAnyDecimals?: boolean
+  minimumAppVersionToSwap?: string
 }
 
 interface HistoricalUsdPrices {
