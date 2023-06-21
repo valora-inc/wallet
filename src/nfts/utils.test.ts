84f253d13dc62f12de6668d6d1472daac5853111
MuckT <mcgtom10@gmail.com>
2023-06-20 17:11:35 -0700

    test(nfts): analytics image load error

--- src/nfts/utils.test.ts
+++ src/nfts/utils.test.ts
@@ -4,11 +4,15 @@ import { getGatewayUrl, onImageLoad } from 'src/nfts/utils'
 import { mockNftAllFields } from 'test/values'
 
 describe('nfts/utils', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
   it('getGatewayUrl', () => {
     expect(getGatewayUrl(mockNftAllFields)).toEqual(mockNftAllFields.media[0].gateway)
   })
 
-  it('onImageLoad', () => {
+  it('onImageLoad success', () => {
     onImageLoad(mockNftAllFields, NftOrigin.TransactionFeed, false)
     expect(ValoraAnalytics.track).toBeCalledWith('nft_image_load', {
       tokenId: mockNftAllFields.tokenId,
@@ -17,5 +21,18 @@ describe('nfts/utils', () => {
       origin: NftOrigin.TransactionFeed,
       error: false,
     })
+    expect(ValoraAnalytics.track).toBeCalledTimes(1)
+  })
+
+  it('onImageLoad error', () => {
+    onImageLoad(mockNftAllFields, NftOrigin.NftsInfoCarouselMain, true)
+    expect(ValoraAnalytics.track).toBeCalledWith('nft_image_load', {
+      tokenId: mockNftAllFields.tokenId,
+      contractAddress: mockNftAllFields.contractAddress,
+      url: mockNftAllFields.media[0].gateway,
+      origin: NftOrigin.NftsInfoCarouselMain,
+      error: true,
+    })
+    expect(ValoraAnalytics.track).toBeCalledTimes(1)
   })
 })
