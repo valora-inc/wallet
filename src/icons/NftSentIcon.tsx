243937885ff5854c9f880f900959f6c07210af2a
Tom McGuire <Mcgtom10@gmail.com>
2023-06-21 09:38:34 -0700

    feat(nfts): add nft icon in transaction feed (#3889)
    ### Description
    
    Adds the first NFT as an icon in the transaction feed item for a NFT
    transfer.
    
    ### Screenshot iOS
    #### Loaded / Image Load Error
    <p align="left">
    <img
    src="https://github.com/valora-inc/wallet/assets/26950305/e2f87813-8e78-4971-96db-94c6fea0e260"
    width="48%" height="auto" />
    <img
    src="https://github.com/valora-inc/wallet/assets/26950305/b1b76655-7258-4adc-aa56-3825f78672f8"
    width="48%" height="auto" />
    </p>
    
    ### Test plan
    
    - [x] Tested locally on iOS
    - [x] Tested locally on Android
    - [x] Tested in low network conditions
    
    ### Related issues
    
    - Fixes RET-595
    
    ### Backwards compatibility
    
    Yes

--- src/icons/NftSentIcon.tsx
+++ src/icons/NftSentIcon.tsx
@@ -3,7 +3,7 @@ import Svg, { Circle, Path } from 'react-native-svg'
 
 const NftSentIcon = () => (
   <Svg width={40} height={40}>
-    <Circle cx={20} cy={20} r={20} fill="#ECE6F7" />
+    <Circle cx={20} cy={20} r={20} fill="#ECE6F7" testID="NftSentIcon" />
     <Path
       d="M20 31c-6.069 0-11-4.931-11-11S13.931 9 20 9s11 4.931 11 11-4.931 11-11 11Zm0-19.977A8.979 8.979 0 0 0 11.023 20 8.979 8.979 0 0 0 20 28.977 8.98 8.98 0 0 0 28.977 20 8.98 8.98 0 0 0 20 11.023Z"
       fill="#452C75"
