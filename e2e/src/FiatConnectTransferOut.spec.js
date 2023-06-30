cfc541e45c20149bd94d08513fc04210ddf936ae
MuckT <mcgtom10@gmail.com>
2023-06-30 15:05:18 -0700

    test(e2e): skip tests instead of an empty suite

--- e2e/src/FiatConnectTransferOut.spec.js
+++ e2e/src/FiatConnectTransferOut.spec.js
@@ -5,7 +5,7 @@ import {
 import { MOCK_PROVIDER_BASE_URL, MOCK_PROVIDER_API_KEY } from 'react-native-dotenv'
 import { launchApp } from './utils/retries'
 
-describe('FiatConnect Transfer Out', () => {
+describe(':ios: FiatConnect Transfer Out', () => {
   // deliberately not doing onboarding in beforeEach, since we'll want to re-use accounts, use fresh accounts, etc for these tests
   beforeEach(async () => {
     // uninstall and reinstall to obtain a fresh account
@@ -19,12 +19,10 @@ describe('FiatConnect Transfer Out', () => {
 
   const platform = device.getPlatform()
   // disable test for android until we fix the bottom sheet issue
-  if (platform === 'ios') {
-    describe('Non KYC', fiatConnectNonKycTransferOut)
-  }
+  describe('Non KYC', fiatConnectNonKycTransferOut)
 
   // KYC test needs to be on iOS and needs Mock Provider info
-  if (platform == 'ios' && MOCK_PROVIDER_BASE_URL && MOCK_PROVIDER_API_KEY) {
+  if (MOCK_PROVIDER_BASE_URL && MOCK_PROVIDER_API_KEY) {
     describe('KYC', fiatConnectKycTransferOut)
   }
 })
