0b4ed9f512a2c3a30dc35d4ac63daf96842e56fc
jh2oman <jacobrwaterman@gmail.com>
2023-07-26 17:04:38 -0700

    typo

--- src/utils/random.test.ts
+++ src/utils/random.test.ts
@@ -3,12 +3,12 @@ import { shuffle } from './random'
 
 describe('shuffle', () => {
   it('should run through all permutations', () => {
-    const varients = new Set()
-    while (varients.size < 24) {
+    const variants = new Set()
+    while (variants.size < 24) {
       const shuffled = shuffle(['add', 'dapp', 'profile', 'learn'], uniqueId())
-      varients.add(shuffled.join(''))
+      variants.add(shuffled.join(''))
     }
-    expect(varients.size).toBe(24)
+    expect(variants.size).toBe(24)
   })
   it('should shuffle the same for the same seed', () => {
     const seed = uniqueId()
