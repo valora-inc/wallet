451337405c3a58b41e66876521665aad7ab648fd
Tom McGuire <Mcgtom10@gmail.com>
2023-08-03 15:38:55 -0700

    chore(nfts): cleanup for gallery and assets banner (#4031)
    ### Description
    
    - Filters out NFTs without metadata from displaying in the gallery view
    - Adjust banner NFT banner in assets:
    - Redirects to gallery if and adjusts text and icon if
    `SHOW_IN_APP_NFT_GALLERY` is enabled in Statsig.
    - Small fix for Android touchable ripple - previously was showing below
    banner.
    - Prevent ripple from extending outside of the border radius in gallery
    view.
       
    | Android Assets Before | Android Assets After |
    | :---: | :---: |
    |
    ![](https://github.com/valora-inc/wallet/assets/26950305/f29a29da-346e-46d5-be52-ad1bf39abb8a
    "Android Assets Before") |
    ![](https://github.com/valora-inc/wallet/assets/26950305/db1d9cfc-7b71-44f1-82c6-fd993586554c
    "Android Assets After") |
    
    ### Test plan
    
    - Tested Locally on iOS & Android
    - Unit tests updated for metadata filtering
    
    ### Related issues
    
    N/A
    
    ### Backwards compatibility
    
    Yes

--- src/nfts/selectors.ts
+++ src/nfts/selectors.ts
@@ -1,3 +1,4 @@
+import { createSelector } from 'reselect'
 import { RootState } from 'src/redux/reducers'
 
 export const nftsLoadingSelector = (state: RootState) => state.nfts.nftsLoading
@@ -5,3 +6,7 @@ export const nftsLoadingSelector = (state: RootState) => state.nfts.nftsLoading
 export const nftsErrorSelector = (state: RootState) => state.nfts.nftsError
 
 export const nftsSelector = (state: RootState) => state.nfts.nfts
+
+export const nftsWithMetadataSelector = createSelector(nftsSelector, (nfts) =>
+  nfts.filter((nft) => nft.metadata)
+)
