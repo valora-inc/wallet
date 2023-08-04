d5d4b4495a19b4c023c3334a14eae831105801e6
Charlie Andrews <charlie.andrews@valoraapp.com>
2023-08-04 16:22:29 -0700

    move torus network configs to config.ts

--- src/keylessBackup/web3auth.ts
+++ src/keylessBackup/web3auth.ts
@@ -1,41 +1,25 @@
-import NodeDetailManager, { TORUS_NETWORK, TORUS_NETWORK_TYPE } from '@toruslabs/fetch-node-details'
+import NodeDetailManager from '@toruslabs/fetch-node-details'
 import Torus from '@toruslabs/torus.js'
 import jwtDecode from 'jwt-decode'
 import Logger from 'src/utils/Logger'
-
-const NetworkToContractAddress: Record<TORUS_NETWORK_TYPE, string> = {
-  [TORUS_NETWORK.MAINNET]: NodeDetailManager.PROXY_ADDRESS_MAINNET,
-  [TORUS_NETWORK.TESTNET]: NodeDetailManager.PROXY_ADDRESS_TESTNET,
-  [TORUS_NETWORK.CYAN]: NodeDetailManager.PROXY_ADDRESS_CYAN,
-  [TORUS_NETWORK.AQUA]: NodeDetailManager.PROXY_ADDRESS_AQUA,
-  [TORUS_NETWORK.CELESTE]: NodeDetailManager.PROXY_ADDRESS_CELESTE,
-}
-
-const SIGNER_MAP: Record<TORUS_NETWORK_TYPE, string> = {
-  [TORUS_NETWORK.MAINNET]: 'https://signer.tor.us',
-  [TORUS_NETWORK.TESTNET]: 'https://signer.tor.us',
-  [TORUS_NETWORK.CYAN]: 'https://signer-polygon.tor.us',
-  [TORUS_NETWORK.AQUA]: 'https://signer-polygon.tor.us',
-  [TORUS_NETWORK.CELESTE]: 'https://signer-polygon.tor.us',
-}
+import { TORUS_NETWORK, TORUS_NETWORK_CONTRACT_ADDRESS, TORUS_SIGNER_BASE_URL } from 'src/config'
 
 const TAG = 'keylessBackup/torus'
 
 export async function getTorusPrivateKey({ verifier, jwt }: { verifier: string; jwt: string }) {
-  const network = 'testnet' // todo get from config
   // largely copied from CustomAuth triggerLogin
   Logger.debug(TAG, `decoding jwt ${jwt}`)
   const sub = jwtDecode<{ sub: string }>(jwt).sub
   const nodeDetailManager = new NodeDetailManager({
-    network,
-    proxyAddress: NetworkToContractAddress[network],
+    network: TORUS_NETWORK,
+    proxyAddress: TORUS_NETWORK_CONTRACT_ADDRESS,
   })
   const torus = new Torus({
     enableOneKey: false, // same as default from CustomAuth
     metadataHost: 'https://metadata.tor.us',
-    allowHost: `${SIGNER_MAP[network]}/api/allow`,
-    signerHost: `${SIGNER_MAP[network]}/api/sign`,
-    network,
+    allowHost: `${TORUS_SIGNER_BASE_URL}/api/allow`,
+    signerHost: `${TORUS_SIGNER_BASE_URL}/api/sign`,
+    network: TORUS_NETWORK,
   })
   Logger.debug(TAG, `getting node details for verifier ${verifier} and sub ${sub}`)
   const { torusNodeEndpoints, torusNodePub, torusIndexes } = await nodeDetailManager.getNodeDetails(
