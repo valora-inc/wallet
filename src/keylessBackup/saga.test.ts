63ac43b6f9ec98ca85609ae1a929fa675efc7d3a
Charlie Andrews <charlie.andrews@valoraapp.com>
2023-08-04 17:43:05 -0700

    add test for handleGoogleSignInCompleted (failure case)

--- src/keylessBackup/saga.test.ts
+++ src/keylessBackup/saga.test.ts
@@ -2,16 +2,36 @@ import { expectSaga } from 'redux-saga-test-plan'
 import { handleGoogleSignInCompleted } from 'src/keylessBackup/saga'
 import { call } from 'redux-saga/effects'
 import { getTorusPrivateKey } from 'src/keylessBackup/web3auth'
-import { googleSignInCompleted, torusKeyshareIssued } from 'src/keylessBackup/slice'
+import {
+  googleSignInCompleted,
+  keylessBackupFailed,
+  torusKeyshareIssued,
+} from 'src/keylessBackup/slice'
+import { throwError } from 'redux-saga-test-plan/providers'
 
 describe('keylessBackup saga', () => {
-  it('handleGoogleSignInCompleted', async () => {
-    const mockJwt = 'abc.def.ghi'
+  const mockJwt = 'abc.def.ghi'
+  it('handleGoogleSignInCompleted: success case', async () => {
+    const mockTorusKeyshare = 'my-torus-keyshare'
     await expectSaga(handleGoogleSignInCompleted, googleSignInCompleted({ idToken: mockJwt }))
       .provide([
-        [call(getTorusPrivateKey, { verifier: 'valora-auth0', jwt: mockJwt }), 'my-torus-keyshare'],
+        [call(getTorusPrivateKey, { verifier: 'valora-auth0', jwt: mockJwt }), mockTorusKeyshare],
       ])
-      .put(torusKeyshareIssued({ keyshare: 'my-torus-keyshare' }))
+      .put(torusKeyshareIssued({ keyshare: mockTorusKeyshare }))
+      .run()
+  })
+  it('handleGoogleSignInCompleted: failure case', async () => {
+    await expectSaga(handleGoogleSignInCompleted, googleSignInCompleted({ idToken: mockJwt }))
+      .provide([
+        [
+          call(getTorusPrivateKey, {
+            verifier: 'valora-auth0',
+            jwt: mockJwt,
+          }),
+          throwError(new Error('(test) Error fetching Torus private key')),
+        ],
+      ])
+      .put(keylessBackupFailed())
       .run()
   })
 })
