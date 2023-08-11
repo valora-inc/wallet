3c6cb5b3fe4725358a586a1d8228116c355b7c95
MuckT <mcgtom10@gmail.com>
2023-08-11 12:20:45 -0700

    feat: handle touchable ripple within component

--- src/components/Touchable.tsx
+++ src/components/Touchable.tsx
@@ -1,15 +1,22 @@
 import * as React from 'react'
-import { TouchableWithoutFeedbackProps } from 'react-native'
+import { TouchableWithoutFeedbackProps, View } from 'react-native'
 import PlatformTouchable from 'react-native-platform-touchable'
 
 export interface Props extends TouchableWithoutFeedbackProps {
   borderless?: boolean
   children: React.ReactNode // must only have one direct child. see https://github.com/react-native-community/react-native-platform-touchable#touchable
+  borderRadius?: number
 }
 
-export default function Touchable({ borderless, ...passThroughProps }: Props) {
+export default function Touchable({ borderless, borderRadius = 0, ...passThroughProps }: Props) {
   const background = borderless
     ? PlatformTouchable.SelectableBackgroundBorderless()
     : PlatformTouchable.SelectableBackground()
-  return <PlatformTouchable {...passThroughProps} background={background} />
+  return borderRadius === 0 ? (
+    <PlatformTouchable {...passThroughProps} background={background} />
+  ) : (
+    <View style={[{ borderRadius }, !borderless && { overflow: 'hidden' }]}>
+      <PlatformTouchable {...passThroughProps} background={background} />
+    </View>
+  )
 }
