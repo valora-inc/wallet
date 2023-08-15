930d025314de4668abd86b4861ab9e0b0f2a3515
Charlie Andrews <charlie.andrews@valoraapp.com>
2023-08-15 14:45:02 -0700

    WIP

--- src/keylessBackup/saga.ts
+++ src/keylessBackup/saga.ts
@@ -4,14 +4,63 @@ import {
   keylessBackupStarted,
   torusKeyshareIssued,
 } from 'src/keylessBackup/slice'
-import { call, put, spawn, takeLeading } from 'typed-redux-saga'
+import { call, delay, put, select, spawn, takeLeading } from 'typed-redux-saga'
 import { getTorusPrivateKey } from 'src/keylessBackup/web3auth'
 import Logger from 'src/utils/Logger'
+import { KeylessBackupFlow } from 'src/keylessBackup/types'
+import { getSecp256K1KeyPair } from 'src/keylessBackup/encryption'
 
 const TAG = 'keylessBackup/saga'
 
+const DELAY_INTERVAL_MS = 500 // how long to wait between checks for keyshares
+const WAIT_FOR_KEYSHARES_TIMEOUT_MS = 25 * 1000 // how long to wait for keyshares before failing
+
 export function* handleKeylessBackupStarted({ payload }: ReturnType<typeof keylessBackupStarted>) {
-  // TODO(ACT-684?): Implement backup/restore flow
+  if (payload.keylessBackupFlow === KeylessBackupFlow.Setup) {
+    yield* handleKeylessBackupSetup()
+  } else {
+    Logger.error(TAG, 'Unsupported keyless backup flow', payload.keylessBackupFlow)
+    yield* put(keylessBackupFailed())
+  }
+}
+
+export function* handleKeylessBackupSetup() {
+  try {
+    const { torusKeyshare, valoraKeyshare } = yield* waitForIssuedKeyshares()
+    const { publicKey: encryptionAddress } = yield* call(getSecp256K1KeyPair, [
+      Buffer.from(torusKeyshare, 'hex'),
+      Buffer.from(valoraKeyshare, 'hex'),
+    ])
+    // TODO get encrypted mnemonic using encryptPassphrase
+    // TODO post encrypted mnemonic to cloud-account-backup service
+  } catch (error) {
+    Logger.error(TAG, 'Error handling keyless backup setup', error)
+    yield* put(keylessBackupFailed())
+    return
+  }
+}
+
+export function* waitForIssuedKeyshares() {
+  const startTime = Date.now()
+  let torusKeyshare: string | null = null
+  let valoraKeyshare: string | null = null
+  while (
+    Date.now() - startTime < WAIT_FOR_KEYSHARES_TIMEOUT_MS &&
+    !torusKeyshare &&
+    !valoraKeyshare
+  ) {
+    torusKeyshare = yield* select((state) => state.keylessBackup.torusKeyshare)
+    valoraKeyshare = yield* select((state) => state.keylessBackup.valoraKeyshare)
+    if (!torusKeyshare || !valoraKeyshare) {
+      yield* delay(DELAY_INTERVAL_MS)
+    }
+  }
+  if (!torusKeyshare || !valoraKeyshare) {
+    throw new Error(
+      `Timed out waiting for keyshares. torusKeyshare obtained: ${!!torusKeyshare}, valoraKeyshare obtained: ${!!valoraKeyshare}`
+    )
+  }
+  return { torusKeyshare, valoraKeyshare }
 }
 
 export function* handleGoogleSignInCompleted({
