719afab59068916dcf4ad49dc51af081e736694c
Alex Bakoushin <alex@bakoush.in>
2023-08-15 13:52:41 +0200

    flatten previously grouped notifications

--- src/home/NotificationCenter.tsx
+++ src/home/NotificationCenter.tsx
@@ -4,17 +4,113 @@ import { useTranslation } from 'react-i18next'
 import { LayoutChangeEvent, StyleSheet, Text, View } from 'react-native'
 import Animated, { useAnimatedScrollHandler, useSharedValue } from 'react-native-reanimated'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import SimpleMessagingCard from 'src/components/SimpleMessagingCard'
+import EscrowedPaymentListItem from 'src/escrow/EscrowedPaymentListItem'
+import { getReclaimableEscrowPayments } from 'src/escrow/reducer'
 import type { Notification } from 'src/home/NotificationBox'
-import { useNotifications } from 'src/home/NotificationBox'
+import {
+  INCOMING_PAYMENT_REQUESTS_PRIORITY,
+  INVITES_PRIORITY,
+  OUTGOING_PAYMENT_REQUESTS_PRIORITY,
+  useSimpleActions,
+} from 'src/home/NotificationBox'
 import ThumbsUpIllustration from 'src/icons/ThumbsUpIllustration'
 import { Screens } from 'src/navigator/Screens'
 import useScrollAwareHeader from 'src/navigator/ScrollAwareHeader'
 import { StackParamList } from 'src/navigator/types'
+import IncomingPaymentRequestListItem from 'src/paymentRequest/IncomingPaymentRequestListItem'
+import OutgoingPaymentRequestListItem from 'src/paymentRequest/OutgoingPaymentRequestListItem'
+import { cancelPaymentRequest, updatePaymentRequestNotified } from 'src/paymentRequest/actions'
+import {
+  getIncomingPaymentRequests,
+  getOutgoingPaymentRequests,
+} from 'src/paymentRequest/selectors'
+import { getRecipientFromAddress } from 'src/recipients/recipient'
+import { recipientInfoSelector } from 'src/recipients/reducer'
+import useSelector from 'src/redux/useSelector'
 import colors from 'src/styles/colors'
 import fontStyles from 'src/styles/fonts'
 import { Spacing } from 'src/styles/styles'
 
 type NotificationsProps = NativeStackScreenProps<StackParamList, Screens.NotificationCenter>
+
+export function useNotifications() {
+  const notifications: Notification[] = []
+
+  // Pending outgoing invites in escrow
+  const reclaimableEscrowPayments = useSelector(getReclaimableEscrowPayments)
+  if (reclaimableEscrowPayments && reclaimableEscrowPayments.length) {
+    for (const payment of reclaimableEscrowPayments) {
+      const itemPriority = Number(`${INVITES_PRIORITY}.${payment.timestamp.toString()}`)
+
+      notifications.push({
+        element: <EscrowedPaymentListItem payment={payment} />,
+        priority: !Number.isNaN(itemPriority) ? itemPriority : INVITES_PRIORITY,
+        id: `reclaimInvite/${payment.paymentID}`,
+      })
+    }
+  }
+
+  // Incoming payment requests
+  const incomingPaymentRequests = useSelector(getIncomingPaymentRequests)
+  if (incomingPaymentRequests && incomingPaymentRequests.length) {
+    for (const request of incomingPaymentRequests) {
+      if (!request.uid) {
+        continue
+      }
+
+      const itemPriority = Number(`${INCOMING_PAYMENT_REQUESTS_PRIORITY}.${request.createdAt ?? 0}`)
+
+      notifications.push({
+        element: <IncomingPaymentRequestListItem paymentRequest={request} />,
+        priority: !Number.isNaN(itemPriority) ? itemPriority : INCOMING_PAYMENT_REQUESTS_PRIORITY,
+        id: `incomingPaymentRequest/${request.uid}`,
+      })
+    }
+  }
+
+  // Outgoing payment requests
+  const outgoingPaymentRequests = useSelector(getOutgoingPaymentRequests)
+  if (outgoingPaymentRequests && outgoingPaymentRequests.length) {
+    const recipientInfo = useSelector(recipientInfoSelector)
+
+    for (const request of outgoingPaymentRequests) {
+      if (!request.uid) {
+        continue
+      }
+
+      const requestee = getRecipientFromAddress(request.requesteeAddress, recipientInfo)
+
+      const itemPriority = Number(`${OUTGOING_PAYMENT_REQUESTS_PRIORITY}.${request.createdAt ?? 0}`)
+
+      notifications.push({
+        element: (
+          <OutgoingPaymentRequestListItem
+            id={request.uid}
+            amount={request.amount}
+            requestee={requestee}
+            comment={request.comment}
+            cancelPaymentRequest={cancelPaymentRequest}
+            updatePaymentRequestNotified={updatePaymentRequestNotified}
+          />
+        ),
+        priority: !Number.isNaN(itemPriority) ? itemPriority : OUTGOING_PAYMENT_REQUESTS_PRIORITY,
+        id: `outgoingPaymentRequest/${request.uid}`,
+      })
+    }
+  }
+
+  const simpleActions = useSimpleActions()
+  notifications.push(
+    ...simpleActions.map((notification) => ({
+      element: <SimpleMessagingCard testID={notification.id} {...notification} />,
+      priority: notification.priority,
+      id: notification.id,
+    }))
+  )
+
+  return notifications.sort((n1, n2) => n2.priority - n1.priority)
+}
 
 export default function Notifications({ navigation }: NotificationsProps) {
   const safeAreaInsets = useSafeAreaInsets()
