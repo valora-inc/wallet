87272b224f865253a9babeabb026f7b8b1d001ec
Alex Bakoushin <alex@bakoush.in>
2023-08-29 14:31:07 +0200

    tidy up

--- src/home/NotificationCenter.tsx
+++ src/home/NotificationCenter.tsx
@@ -207,7 +207,6 @@ export default function Notifications({ navigation }: NotificationsProps) {
 
   return (
     <Animated.FlatList
-      testID={'NotificationCenter/List'}
       style={styles.container}
       ListHeaderComponent={renderHeader}
       ListEmptyComponent={renderEmptyState}
