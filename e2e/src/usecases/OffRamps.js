cba37b71253cc17becb7a9bfc3367b34572b2097
jh2oman <jacobrwaterman@gmail.com>
2023-09-19 17:14:07 +0200

    e2e

--- e2e/src/usecases/OffRamps.js
+++ e2e/src/usecases/OffRamps.js
@@ -95,10 +95,10 @@ export default offRamps = () => {
         await waitForElementId('FiatExchangeInput')
         await element(by.id('FiatExchangeInput')).replaceText('20')
         await element(by.id('FiatExchangeNextButton')).tap()
-        await expect(element(by.text('Select Payment Method'))).toBeVisible()
+        await expect(element(by.text('Select Withdraw Method'))).toBeVisible()
         await waitForElementId('Exchanges')
         await element(by.id('Exchanges')).tap()
-        await waitForElementId('accountBox')
+        await waitForElementId('SendBar')
         // Exchanges start at index 0
         await waitForElementId(`provider-${exchanges.minExpected - 1}`)
       }
