d8604110c477c9fe93cb5c2cc15e916058c75d07
MuckT <mcgtom10@gmail.com>
2023-09-25 15:49:46 -0700

    test: add test for network name and reset mock token between tests

--- src/tokens/TokenBalanceItem.test.tsx
+++ src/tokens/TokenBalanceItem.test.tsx
@@ -2,23 +2,28 @@ import { render } from '@testing-library/react-native'
 import BigNumber from 'bignumber.js'
 import React from 'react'
 import { Provider } from 'react-redux'
+import { TokenBalance } from 'src/tokens/slice'
 import { TokenBalanceItem } from 'src/tokens/TokenBalanceItem'
 import { createMockStore } from 'test/utils'
 import { mockCusdAddress } from 'test/values'
 
+let mockTokenInfo: TokenBalance
+
 describe('TokenBalanceItem', () => {
-  let mockTokenInfo = {
-    balance: new BigNumber('10'),
-    usdPrice: new BigNumber('1'),
-    lastKnownUsdPrice: new BigNumber('1'),
-    symbol: 'cUSD',
-    address: mockCusdAddress,
-    isCoreToken: true,
-    priceFetchedAt: Date.now(),
-    decimals: 18,
-    name: 'Celo Dollar',
-    imageUrl: '',
-  } as any
+  beforeEach(() => {
+    mockTokenInfo = {
+      balance: new BigNumber('10'),
+      usdPrice: new BigNumber('1'),
+      lastKnownUsdPrice: new BigNumber('1'),
+      symbol: 'cUSD',
+      address: mockCusdAddress,
+      isCoreToken: true,
+      priceFetchedAt: Date.now(),
+      decimals: 18,
+      name: 'Celo Dollar',
+      imageUrl: '',
+    } as any
+  })
 
   it('displays correctly', () => {
     const { getByText, queryByTestId } = render(
@@ -30,8 +35,8 @@ describe('TokenBalanceItem', () => {
     expect(getByText('Celo Dollar')).toBeTruthy()
     expect(getByText('10.00 cUSD')).toBeTruthy()
     expect(getByText('₱13.30')).toBeTruthy()
-    expect(getByText('Celo Network')).toBeTruthy()
     expect(queryByTestId('BridgeLabel')).toBeFalsy()
+    expect(queryByTestId('NetworkLabel')).toBeFalsy()
   })
 
   it('displays correctly when token is bridged', () => {
@@ -45,7 +50,21 @@ describe('TokenBalanceItem', () => {
     expect(getByText('Celo Dollar')).toBeTruthy()
     expect(getByText('10.00 cUSD')).toBeTruthy()
     expect(getByText('₱13.30')).toBeTruthy()
-    expect(getByText('Celo Network')).toBeTruthy()
     expect(getByTestId('BridgeLabel')).toBeTruthy()
+  })
+
+  it('displays correctly when the token network is provided', () => {
+    mockTokenInfo.networkName = 'Celo'
+
+    const { getByText, getByTestId } = render(
+      <Provider store={createMockStore({})}>
+        <TokenBalanceItem token={mockTokenInfo} />
+      </Provider>
+    )
+
+    expect(getByText('Celo Dollar')).toBeTruthy()
+    expect(getByText('10.00 cUSD')).toBeTruthy()
+    expect(getByText('₱13.30')).toBeTruthy()
+    expect(getByTestId('NetworkLabel')).toBeTruthy()
   })
 })
