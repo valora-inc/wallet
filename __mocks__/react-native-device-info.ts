ce1b7979ce4b36c3f35ce96b8d9ced8b1f4b5600
Kathy Luo <kathyluo18@gmail.com>
2023-10-06 13:49:10 +0200

    chore: add OS version to support requests

--- __mocks__/react-native-device-info.ts
+++ __mocks__/react-native-device-info.ts
@@ -3,6 +3,7 @@ import mockRNDeviceInfo from 'react-native-device-info/jest/react-native-device-
 mockRNDeviceInfo.getBundleId.mockImplementation(() => 'org.celo.mobile.debug')
 mockRNDeviceInfo.getVersion.mockImplementation(() => '0.0.1')
 mockRNDeviceInfo.getBuildNumber.mockImplementation(() => '1')
+mockRNDeviceInfo.getSystemVersion.mockImplementation(() => '7.1')
 mockRNDeviceInfo.getUniqueId.mockImplementation(() => Promise.resolve('abc-def-123'))
 mockRNDeviceInfo.getUniqueIdSync.mockImplementation(() => 'abc-def-123')
 mockRNDeviceInfo.getDeviceId.mockImplementation(() => 'someDeviceId')
