1b8914be0da4e0fbb93b7a3d49083da8f033d049
Satish Ravi <satish.ravi@valoraapp.com>
2023-10-08 23:39:06 -0700

    add git fetch

--- .github/scripts/checkPodfileAndUpdateRenovatePr.ts
+++ .github/scripts/checkPodfileAndUpdateRenovatePr.ts
@@ -26,6 +26,7 @@ if (
   console.log('Renovate PR, pushing Podfile changes')
   $.exec('git remote set-url origin git@github.com:valora-inc/wallet.git')
   $.exec(`git checkout -b ${branchName}`)
+  $.exec('git fetch')
   $.exec(`git reset --hard origin/${branchName}`)
   // this assumes the diff is from Podfile.lock only
   $.exec('git add ios/Podfile.lock')
