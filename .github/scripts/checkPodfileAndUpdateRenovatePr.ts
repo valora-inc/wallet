442731d2ec2fe1473a16b734ff159499c202e9a0
Satish Ravi <satish.ravi@valoraapp.com>
2023-10-08 23:34:40 -0700

    try reset --hard

--- .github/scripts/checkPodfileAndUpdateRenovatePr.ts
+++ .github/scripts/checkPodfileAndUpdateRenovatePr.ts
@@ -25,10 +25,8 @@ if (
 ) {
   console.log('Renovate PR, pushing Podfile changes')
   $.exec('git remote set-url origin git@github.com:valora-inc/wallet.git')
-  $.exec('git log')
   $.exec(`git checkout -b ${branchName}`)
-  $.exec('git log')
-  $.exec(`git pull origin ${branchName} --ff-only`)
+  $.exec(`git reset --hard origin/${branchName}`)
   // this assumes the diff is from Podfile.lock only
   $.exec('git add ios/Podfile.lock')
   $.exec('git config user.email "valorabot@valoraapp.com"')
