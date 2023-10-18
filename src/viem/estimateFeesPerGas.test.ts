d14300aaea64158de05b4a2a35fd2ecc90805bdc
Jean Regisser <jean.regisser@gmail.com>
2023-10-18 18:01:13 +0200

    Add test

---
+++ src/viem/estimateFeesPerGas.test.ts
@@ -0,0 +1,29 @@
+import { estimateFeesPerGas } from 'src/viem/estimateFeesPerGas'
+
+describe(estimateFeesPerGas, () => {
+  it('should return the correct fees per gas', async () => {
+    const client = {
+      request: jest.fn(async ({ method, params }) => {
+        expect(params).toBeUndefined()
+        if (method === 'eth_gasPrice') return '100'
+        if (method === 'eth_maxPriorityFeePerGas') return '10'
+        throw new Error(`Unknown method ${method}`)
+      }),
+    }
+    const fees = await estimateFeesPerGas(client as any)
+    expect(fees).toEqual({ maxFeePerGas: BigInt(110), maxPriorityFeePerGas: BigInt(10) })
+  })
+
+  it('when fee currency is specified, it should return the correct fees per gas', async () => {
+    const client = {
+      request: jest.fn(async ({ method, params }) => {
+        expect(params).toEqual(['0x123'])
+        if (method === 'eth_gasPrice') return '100'
+        if (method === 'eth_maxPriorityFeePerGas') return '10'
+        throw new Error(`Unknown method ${method}`)
+      }),
+    }
+    const fees = await estimateFeesPerGas(client as any, '0x123')
+    expect(fees).toEqual({ maxFeePerGas: BigInt(110), maxPriorityFeePerGas: BigInt(10) })
+  })
+})
