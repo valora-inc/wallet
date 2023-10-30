c95a0c05bce22bef07f70b84f81ff80001864c49
Satish Ravi <satish.ravi@valoraapp.com>
2023-10-30 15:22:34 -0700

    test(e2e): add assets screen tests

---
+++ e2e/src/Assets.spec.js
@@ -0,0 +1,6 @@
+import { AssetsOnWalletWithBalance, AssetsOnWalletWithNoBalance } from './usecases/Assets'
+
+describe('Assets', () => {
+  describe('For wallet with balance', AssetsOnWalletWithBalance)
+  describe('For wallet with no balance', AssetsOnWalletWithNoBalance)
+})
