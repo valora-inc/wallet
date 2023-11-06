a24d0aba83b25bdc11452ad133d50e722965b102
Jean Regisser <jean.regisser@gmail.com>
2023-11-06 18:54:40 +0100

    Add test

---
+++ src/viem/preparedTransactionSerialization.test.ts
@@ -0,0 +1,66 @@
+import {
+  getPreparedTransactions,
+  getSerializablePreparedTransactions,
+} from 'src/viem/preparedTransactionSerialization'
+
+describe(getSerializablePreparedTransactions, () => {
+  it('should turn bigints into strings for known bigint keys', () => {
+    expect(
+      getSerializablePreparedTransactions([
+        {
+          from: '0x123',
+          to: '0x456',
+          value: BigInt(123),
+          gas: BigInt(456),
+          maxFeePerGas: BigInt(789),
+          maxPriorityFeePerGas: BigInt(1011),
+          gatewayFee: BigInt(1213),
+          // @ts-expect-error: unknown key, will be ignored, but TS will prevent in normal use
+          unknown: BigInt(1415),
+        },
+      ])
+    ).toStrictEqual([
+      {
+        from: '0x123',
+        to: '0x456',
+        value: '123',
+        gas: '456',
+        maxFeePerGas: '789',
+        maxPriorityFeePerGas: '1011',
+        gatewayFee: '1213',
+        unknown: BigInt(1415), // not touched
+      },
+    ])
+  })
+})
+
+describe(getPreparedTransactions, () => {
+  it('should turn strings into bigints for known bigint keys', () => {
+    expect(
+      getPreparedTransactions([
+        {
+          from: '0x123',
+          to: '0x456',
+          value: '123',
+          gas: '456',
+          maxFeePerGas: '789',
+          maxPriorityFeePerGas: '1011',
+          gatewayFee: '1213',
+          // @ts-expect-error: unknown key, will be ignored, but TS will prevent in normal use
+          unknown: BigInt(1415), // not touched
+        },
+      ])
+    ).toStrictEqual([
+      {
+        from: '0x123',
+        to: '0x456',
+        value: BigInt(123),
+        gas: BigInt(456),
+        maxFeePerGas: BigInt(789),
+        maxPriorityFeePerGas: BigInt(1011),
+        gatewayFee: BigInt(1213),
+        unknown: BigInt(1415), // not touched
+      },
+    ])
+  })
+})
