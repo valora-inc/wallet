dac4ea5e404b72d4f6c3a5cf2b51092d88be66f5
Kathy Luo <kathyluo18@gmail.com>
2023-11-06 10:07:08 +0100

    feat: add UI for swap transaction details (#4418)
    ### Description
    
    This PR just adds the new UI, and a dummy feature flag that prevents it
    from being shown. The plumbing for getting the fee estimate particularly
    will be a big ish change so breaking this part out to keep PRs small.
    
    ### Test plan
    
    
    
    https://github.com/valora-inc/wallet/assets/20150449/f40e9217-19d7-41dc-9b82-b8e8e985a386
    
    
    
    ### Related issues
    
    Related to RET-865
    
    ### Backwards compatibility
    
    Y

---
+++ src/swap/SwapTransactionDetails.tsx
@@ -0,0 +1,108 @@
+import BigNumber from 'bignumber.js'
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { StyleSheet, Text, View } from 'react-native'
+import { BottomSheetRefType } from 'src/components/BottomSheet'
+import TokenDisplay from 'src/components/TokenDisplay'
+import Touchable from 'src/components/Touchable'
+import InfoIcon from 'src/icons/InfoIcon'
+import { NETWORK_NAMES } from 'src/shared/conts'
+import colors from 'src/styles/colors'
+import { typeScale } from 'src/styles/fonts'
+import { Spacing } from 'src/styles/styles'
+import { NetworkId } from 'src/transactions/types'
+
+interface Props {
+  networkId?: NetworkId
+  networkFee: BigNumber
+  networkFeeInfoBottomSheetRef: React.RefObject<BottomSheetRefType>
+  slippagePercentage: number
+  feeTokenId: string
+}
+
+export function SwapTransactionDetails({
+  networkId,
+  networkFee,
+  networkFeeInfoBottomSheetRef,
+  feeTokenId,
+  slippagePercentage,
+}: Props) {
+  const { t } = useTranslation()
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.row}>
+        <Touchable
+          style={styles.touchableRow}
+          onPress={() => {
+            networkFeeInfoBottomSheetRef.current?.snapToIndex(0)
+          }}
+          disabled={!networkId}
+          testID="SwapTransactionDetails/NetworkFeeMoreInfo"
+        >
+          <>
+            <Text style={styles.label}>
+              {networkId
+                ? t('swapScreen.transactionDetails.networkFee', {
+                    networkName: NETWORK_NAMES[networkId],
+                  })
+                : t('swapScreen.transactionDetails.networkFeeNoNetwork')}
+            </Text>
+            {networkId && (
+              <InfoIcon
+                size={14}
+                color={colors.gray4}
+                testID="SwapTransactionDetails/NetworkFeeMoreInfo/Icon"
+              />
+            )}
+          </>
+        </Touchable>
+        <TokenDisplay
+          style={styles.value}
+          amount={networkFee}
+          tokenId={feeTokenId}
+          showLocalAmount={true}
+        />
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.label}>{t('swapScreen.transactionDetails.swapFee')}</Text>
+        <Text testID={'SwapFee'} style={styles.value}>
+          {t('swapScreen.transactionDetails.swapFeeWaived')}
+        </Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.label}>{t('swapScreen.transactionDetails.slippagePercentage')}</Text>
+        <Text testID={'SwapFee'} style={styles.value}>
+          {`${slippagePercentage}%`}
+        </Text>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: Spacing.Tiny4,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingBottom: Spacing.Small12,
+  },
+  touchableRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  value: {
+    ...typeScale.bodyXSmall,
+    color: colors.gray4,
+    fontWeight: '600',
+  },
+  label: {
+    ...typeScale.bodyXSmall,
+    color: colors.gray4,
+    marginRight: Spacing.Tiny4,
+  },
+})
+
+export default SwapTransactionDetails
