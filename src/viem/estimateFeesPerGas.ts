23ea75678644eae117ced9a4e526b96870c5ff67
Jean Regisser <jean.regisser@gmail.com>
2023-11-07 14:01:00 +0100

    Revert "Update with latest recommendation"
    This reverts commit 5ec20cd59613636fc13567a1a55e3d8a46949765.

--- src/viem/estimateFeesPerGas.ts
+++ src/viem/estimateFeesPerGas.ts
@@ -1,18 +1,14 @@
 import { Address, Client, hexToBigInt } from 'viem'
 
-// Custom function for Celo that can be removed once it's supported in viem
+// Custom function that can be removed once it's supported in viem
 // See https://github.com/wagmi-dev/viem/discussions/914
 export async function estimateFeesPerGas(client: Client, feeCurrency?: Address) {
-  // The gasPrice returned on Celo is already roughly 2x baseFeePerGas
-  // so we can just use that as maxFeePerGas, no need for an explicit maxPriorityFeePerGas
-  // See this interesting thread for more details:
-  // https://valora-app.slack.com/archives/CNJ7KTHQU/p1697717100995909?thread_ts=1697647756.662059&cid=CNJ7KTHQU
-  const gasPrice = await getGasPrice(client, feeCurrency)
-  const maxFeePerGas = gasPrice
-  // Here we still return maxPriorityFeePerGas as undefined to be consistent with viem
-  // and so consumer TXs are correctly serialized as CIP-42 TXs
-  // See https://github.com/wagmi-dev/viem/blob/5c95fafceffe7f399b5b5ee32119e2d78a0c8acd/src/chains/celo/serializers.ts#L105-L106
-  return { maxFeePerGas, maxPriorityFeePerGas: undefined }
+  const [gasPrice, maxPriorityFeePerGas] = await Promise.all([
+    getGasPrice(client, feeCurrency),
+    getMaxPriorityFeePerGas(client, feeCurrency),
+  ])
+  const maxFeePerGas = gasPrice + maxPriorityFeePerGas
+  return { maxFeePerGas, maxPriorityFeePerGas }
 }
 
 // Get gas price with optional fee currency, this is Celo specific
@@ -23,4 +19,13 @@ export async function getGasPrice(client: Client, feeCurrency: Address | undefin
     ...((feeCurrency ? { params: [feeCurrency] } : {}) as object),
   })
   return hexToBigInt(priceHex)
+}
+
+// Get max priority fee per gas with optional fee currency, this is Celo specific
+export async function getMaxPriorityFeePerGas(client: Client, feeCurrency?: Address) {
+  const maxPriorityFeePerGasHex = await client.request({
+    method: 'eth_maxPriorityFeePerGas',
+    ...((feeCurrency ? { params: [feeCurrency] } : {}) as object),
+  })
+  return hexToBigInt(maxPriorityFeePerGasHex)
 }
