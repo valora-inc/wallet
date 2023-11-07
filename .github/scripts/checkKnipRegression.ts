6c99205a56977fa8cd29e06d5768e19b32a962c8
Joe Bergeron <jbergero@alum.mit.edu>
2023-11-07 17:55:56 -0500

    feat(ci): Replace depcheck with knip, add dead code regression to CI (#4430)
    ### Description
    
    This PR does two things:
    * Removes depcheck, and replaces our depcheck CI step with knip instead,
    using only its dependency checking
    * Adds in a new regression CI step using knip, which basically runs all
    of knip's checks against the PR branch and main, and for each check,
    ensures that the PR branch has no more errors than main.
    
    See a successful run of the regression step
    [here](https://github.com/valora-inc/wallet/actions/runs/6741018767/job/18324961630);
    you can see an overview of the diff between PR branch and main near the
    end of the main output.
    
    (re: the knip CI script -- knip doesn't expose a programmatic API; it's
    only available as a CLI tool, so it required a bit of hacky regex'ing to
    parse the results)
    
    ### Test plan
    
    CI
    
    ### Related issues
    
    N/A
    
    ### Backwards compatibility
    
    Yes.

---
+++ .github/scripts/checkKnipRegression.ts
@@ -0,0 +1,60 @@
+import * as $ from 'shelljs'
+
+const SECTION_REGEX = /[a-zA-Z ]+\([0-9]+\)/
+const CATEGORY_REGEX = /[a-zA-Z ]+/
+const PARENS_REGEX = /\(|\)| /
+
+function parseKnipOutput(knipOutput: string): Record<string, number> {
+  const lines = knipOutput.split('\n')
+  const result: Record<string, number> = {}
+  for (const line of lines) {
+    if (SECTION_REGEX.test(line)) {
+      const tokens = line.split(PARENS_REGEX)
+      const category = (line.match(CATEGORY_REGEX) as RegExpMatchArray)[0]
+      result[category] = parseInt(tokens[tokens.length - 2])
+    }
+  }
+  return result
+}
+
+function compareKnipResults(
+  mainKnipResults: Record<string, number>,
+  branchKnipResults: Record<string, number>
+): boolean {
+  let shouldFail = false
+  const rows: Array<Record<string, string | number>> = []
+  for (const [category, mainProblems] of Object.entries(mainKnipResults)) {
+    const branchProblems = branchKnipResults[category] ?? 0
+    rows.push({
+      'Issue Category': category.trim(),
+      'Main Branch': mainProblems,
+      'PR Branch': branchProblems,
+    })
+    if (branchProblems > mainProblems) {
+      shouldFail = true
+    }
+  }
+  console.table(rows)
+  return shouldFail
+}
+
+if (process.env.GITHUB_EVENT_NAME === 'pull_request') {
+  const branchName = process.env.GITHUB_HEAD_REF
+
+  const branchKnipOutput = $.exec('yarn knip --no-gitignore').stdout.trim()
+  const branchKnipResults = parseKnipOutput(branchKnipOutput)
+
+  $.exec('git checkout main')
+
+  const mainKnipOutput = $.exec('yarn knip --no-gitignore').stdout.trim()
+  const mainKnipResults = parseKnipOutput(mainKnipOutput)
+
+  $.exec(`git checkout ${branchName}`)
+
+  if (compareKnipResults(mainKnipResults, branchKnipResults)) {
+    console.log('Knip check failed. Branch reported more problems than main.')
+    process.exit(1)
+  }
+}
+
+process.exit(0)
