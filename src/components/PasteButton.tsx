e6ce40ab47dad972956e8898c7fd988bf563e6e5
Dennis <147835134+denvalora@users.noreply.github.com>
2023-11-14 19:50:23 -0500

    Added optional right element

---
+++ src/components/PasteButton.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { GestureResponderEvent, StyleSheet, Text } from 'react-native'
+import Touchable from 'src/components/Touchable'
+import { Colors } from 'src/styles/colors'
+import { typeScale } from 'src/styles/fonts'
+import { useClipboard } from 'src/utils/useClipboard'
+
+interface Props {
+  value: string
+  setValue: React.Dispatch<React.SetStateAction<string>>
+  onPress?: (event: GestureResponderEvent) => void
+}
+
+export const PasteButton = ({ value, setValue, onPress }: Props) => {
+  // when a value is already set, remove the button
+  if (value) return null
+
+  const { t } = useTranslation()
+  const [, , getFreshClipboardContent] = useClipboard()
+
+  const pasteClipboard = async (event: GestureResponderEvent) => {
+    const content = await getFreshClipboardContent()
+    setValue(content)
+    onPress?.(event)
+  }
+
+  return (
+    <Touchable onPress={pasteClipboard}>
+      <Text style={styles.text}>{t('paste')}</Text>
+    </Touchable>
+  )
+}
+
+const styles = StyleSheet.create({
+  text: {
+    textAlign: 'center',
+    ...typeScale.semiBoldMedium,
+    color: Colors.greenUI,
+  },
+})
