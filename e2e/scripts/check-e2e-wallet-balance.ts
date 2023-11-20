67cd20a913adf60abb5e842431163bc1e1bdec35
Kathy Luo <kathyluo18@gmail.com>
2023-11-20 10:56:33 +0100

    chore: add balance check

--- e2e/scripts/check-e2e-wallet-balance.ts
+++ e2e/scripts/check-e2e-wallet-balance.ts
@@ -1,7 +1,11 @@
-import { E2E_TEST_WALLET } from './consts'
+import { E2E_TEST_WALLET, E2E_TEST_WALLET_SECURE_SEND } from './consts'
 import { checkBalance, getBalance } from './utils'
 ;(async () => {
   console.log(E2E_TEST_WALLET)
   console.table(await getBalance(E2E_TEST_WALLET))
   await checkBalance(E2E_TEST_WALLET)
+
+  console.log(E2E_TEST_WALLET_SECURE_SEND)
+  console.table(await getBalance(E2E_TEST_WALLET_SECURE_SEND))
+  await checkBalance(E2E_TEST_WALLET_SECURE_SEND)
 })()
