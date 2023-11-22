1752aa38101766f041ee2840408c1f5fc99e5a5a
Kathy Luo <kathyluo18@gmail.com>
2023-11-22 15:22:28 +0100

    chore: replace yelloFaint

--- src/styles/colors.tsx
+++ src/styles/colors.tsx
@@ -9,7 +9,6 @@ export enum Colors {
   goldUI = '#EEB93C',
   goldDark = '#9C6E00',
   beige = '#F1F0EB',
-  yellowFaint = '#FFF9EA',
   dark_0_2 = '#2E333833', // same as dark but with alpha 0.2 (20% opacity)
   gray6 = '#DFDBCE',
   onboardingBrownLight = '#A49B80',
