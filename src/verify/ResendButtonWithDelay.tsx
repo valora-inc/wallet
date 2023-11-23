69761222d0bbd3e712c2a6d305031446fe0189c7
Kathy Luo <kathyluo18@gmail.com>
2023-11-23 11:55:15 +0100

    chore: replace gray6 with gray2 - hardly any noticable diff

--- src/verify/ResendButtonWithDelay.tsx
+++ src/verify/ResendButtonWithDelay.tsx
@@ -41,7 +41,7 @@ function ResendButtonWithDelay({ onPress }: Props) {
       testID="PhoneVerificationResendSmsBtn"
       disabled={disabled}
       style={{
-        color: disabled ? colors.gray6 : colors.onboardingBrownLight,
+        color: disabled ? colors.gray2 : colors.onboardingBrownLight,
         fontVariant: ['tabular-nums'],
       }}
       onPress={handleOnPress}
