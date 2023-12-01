422abf0773438181ae6c01bfe007de6413a5d660
Joe Bergeron <jbergero@alum.mit.edu>
2023-12-01 13:14:32 -0800

    feat(send): Implement new Send Search UI (#4468)
    ### Description
    
    Part of ACT-950. Implements the UI components of the new search flow,
    including the paste button, search result logic, and send/invite button.
    
    Currently, this does _not_ account for the new address verification
    scheme we have planned. (e.g., we still consider all addresses to be
    "verified" and should in theory show the Valora V).
    
    ### Test plan
    
    Unit and manual tested. See videos below.
    
    #### Search for Verified Phone Number 
    
    
    https://github.com/valora-inc/wallet/assets/569401/f65c32c0-a382-498d-ae38-9671f4b90b82
    
    #### Search for Unverified Phone Number
    
    
    https://github.com/valora-inc/wallet/assets/569401/f843343e-921b-48c2-8a9b-7a436af6f679
    
    #### Search for Address (includes paste button behavior & empty state)
    
    
    https://github.com/valora-inc/wallet/assets/569401/4f0e6b51-402c-43d6-85a8-d3b1a4371b55
    
    #### Search for Nomspace Address
    
    
    https://github.com/valora-inc/wallet/assets/569401/ffccb9b6-19f1-4678-a76d-9e4916bf60ca
    
    ### Related issues
    
    - Fixes ACT-952
    
    ### Backwards compatibility
    
    Yes.
    
    ---------
    
    Co-authored-by: Tom McGuire <Mcgtom10@gmail.com>
    Co-authored-by: Jacob Waterman <jacobrwaterman@gmail.com>

---
+++ src/send/PasteAddressButton.tsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { useTranslation } from 'react-i18next'
+import { Spacing } from 'src/styles/styles'
+import Touchable from 'src/components/Touchable'
+import { PasteAwareWrappedElementProps, withPasteAware } from 'src/components/WithPasteAware'
+import Clipboard from 'src/icons/Clipboard'
+import CircledIcon from 'src/icons/CircledIcon'
+import colors from 'src/styles/colors'
+import { typeScale } from 'src/styles/fonts'
+
+type Props = PasteAwareWrappedElementProps
+
+function PasteAddressButton(props: Props) {
+  const { isPasteIconVisible, onPressPaste, clipboardContent } = props
+  const { t } = useTranslation()
+  // TODO: We need to check clipboardContent here since for iOS 14+
+  // isPasteIconVisible is set to true while clipboardContent is always null.
+  if (!(isPasteIconVisible && clipboardContent)) {
+    return null
+  }
+  return (
+    <View style={styles.wrapper}>
+      <Touchable
+        testID="PasteAddressButton"
+        borderRadius={10}
+        style={styles.buttonContainer}
+        onPress={onPressPaste}
+      >
+        <>
+          <CircledIcon radius={40} backgroundColor={colors.gray2}>
+            <Clipboard />
+          </CircledIcon>
+          <View style={styles.textSection}>
+            <Text style={styles.title}>{t('sendSelectRecipient.paste')}</Text>
+            <Text style={styles.address}>{clipboardContent}</Text>
+          </View>
+        </>
+      </Touchable>
+    </View>
+  )
+}
+const Wrapper = withPasteAware(PasteAddressButton)
+export default Wrapper
+
+const styles = StyleSheet.create({
+  wrapper: {
+    borderRadius: 10,
+    backgroundColor: colors.gray1,
+    marginVertical: Spacing.Small12,
+    marginHorizontal: Spacing.Thick24,
+  },
+  buttonContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    paddingVertical: Spacing.Regular16,
+    paddingHorizontal: Spacing.Small12,
+    alignItems: 'center',
+    borderRadius: 10,
+  },
+  textSection: {
+    paddingLeft: Spacing.Regular16,
+    display: 'flex',
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+    flexShrink: 1,
+  },
+  address: {
+    ...typeScale.bodyXSmall,
+    color: colors.gray4,
+    flexWrap: 'wrap',
+    flexShrink: 1,
+    maxWidth: '100%',
+  },
+  title: {
+    ...typeScale.labelSmall,
+  },
+})
