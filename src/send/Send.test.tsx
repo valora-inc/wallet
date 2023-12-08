26e39aceb0ab8c7bc29b7d6004eaf11c518c831c
Joe Bergeron <jbergero@alum.mit.edu>
2023-12-08 11:00:55 -0800

    feat(send): Show Valora V icon for address-based recipients depending on phone verification (#4586)
    ### Description
    
    Updates the behavior of the Valora V icon to show only for address-based
    recipients if the address is associated with a phone number verified
    with Valora. "Address-based recipients" here means either
    `RecipientType.Address` or `Recipient.Nomspace`, since previously, the
    Valora V icon behaviors for both were treated identically.
    
    ### Test plan
    
    Unit and manual tested. See videos below:
    
    #### Address has no associated phone number
    
    
    https://github.com/valora-inc/wallet/assets/569401/6ff32d95-8230-4ba4-aaf0-78cffb0f6612
    
    #### Address does have associated phone number
    
    
    https://github.com/valora-inc/wallet/assets/569401/88afbedc-6395-40bd-be20-68beabd5cd94
    
    ### Related issues
    
    - Fixes #[issue number here]
    
    ### Backwards compatibility
    
    <!-- Brief explanation of why these changes are/are not backwards
    compatible. -->

--- src/send/Send.test.tsx
+++ src/send/Send.test.tsx
@@ -55,6 +55,11 @@ const defaultStore = {
       },
     },
   },
+  identity: {
+    addressToVerificationStatus: {
+      [mockRecipient.address]: true,
+    },
+  },
 }
 
 jest.mock('src/statsig', () => {
--- src/send/useFetchRecipientVerificationStatus.ts
+++ src/send/useFetchRecipientVerificationStatus.ts
@@ -1,10 +1,16 @@
 import { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { fetchAddressesAndValidate } from 'src/identity/actions'
-import { e164NumberToAddressSelector } from 'src/identity/selectors'
+import { fetchAddressesAndValidate, fetchAddressVerification } from 'src/identity/actions'
+import {
+  e164NumberToAddressSelector,
+  addressToVerificationStatusSelector,
+} from 'src/identity/selectors'
 import { RecipientVerificationStatus } from 'src/identity/types'
 import { getRecipientVerificationStatus, Recipient } from 'src/recipients/recipient'
 import useSelector from 'src/redux/useSelector'
+import { getFeatureGate } from 'src/statsig'
+import { StatsigFeatureGates } from 'src/statsig/types'
+import { phoneNumberVerifiedSelector } from 'src/app/selectors'
 
 const useFetchRecipientVerificationStatus = () => {
   const [recipient, setRecipient] = useState<Recipient | null>(null)
@@ -12,7 +18,13 @@ const useFetchRecipientVerificationStatus = () => {
     RecipientVerificationStatus.UNKNOWN
   )
 
+  const useNewAddressVerificationBehavior = getFeatureGate(
+    StatsigFeatureGates.USE_NEW_RECIPIENT_SCREEN
+  )
+
   const e164NumberToAddress = useSelector(e164NumberToAddressSelector)
+  const addressToVerificationStatus = useSelector(addressToVerificationStatusSelector)
+  const phoneNumberVerified = useSelector(phoneNumberVerifiedSelector)
   const dispatch = useDispatch()
 
   const unsetSelectedRecipient = () => {
@@ -22,23 +34,33 @@ const useFetchRecipientVerificationStatus = () => {
 
   const setSelectedRecipient = (selectedRecipient: Recipient) => {
     setRecipient(selectedRecipient)
-    setRecipientVerificationStatus(
-      selectedRecipient?.address
-        ? RecipientVerificationStatus.VERIFIED
-        : RecipientVerificationStatus.UNKNOWN
-    )
+    setRecipientVerificationStatus(RecipientVerificationStatus.UNKNOWN)
 
     if (selectedRecipient?.e164PhoneNumber) {
       dispatch(fetchAddressesAndValidate(selectedRecipient.e164PhoneNumber))
+    } else if (selectedRecipient?.address) {
+      if (
+        addressToVerificationStatus[selectedRecipient.address] ||
+        !useNewAddressVerificationBehavior
+      ) {
+        setRecipientVerificationStatus(RecipientVerificationStatus.VERIFIED)
+      } else if (phoneNumberVerified) {
+        dispatch(fetchAddressVerification(selectedRecipient.address))
+      } else {
+        setRecipientVerificationStatus(RecipientVerificationStatus.UNVERIFIED)
+      }
     }
   }
 
   useEffect(() => {
     if (recipient && recipientVerificationStatus === RecipientVerificationStatus.UNKNOWN) {
-      // e164NumberToAddress is updated after a successful phone number lookup
-      setRecipientVerificationStatus(getRecipientVerificationStatus(recipient, e164NumberToAddress))
+      // e164NumberToAddress is updated after a successful phone number lookup,
+      // addressToVerificationStatus is updated after a successful address lookup
+      setRecipientVerificationStatus(
+        getRecipientVerificationStatus(recipient, e164NumberToAddress, addressToVerificationStatus)
+      )
     }
-  }, [e164NumberToAddress, recipient, recipientVerificationStatus])
+  }, [e164NumberToAddress, addressToVerificationStatus, recipient, recipientVerificationStatus])
 
   return {
     recipient,
