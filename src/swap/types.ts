c923ae9f4e2ae57874c2625f36d3485dd9be6528
Jean Regisser <jean.regisser@gmail.com>
2023-12-18 17:31:51 +0100

    Sync SwapTransaction with our API type

--- src/swap/types.ts
+++ src/swap/types.ts
@@ -23,75 +23,26 @@ export interface SwapUserInput {
   updatedField: Field
 }
 
-interface Fill {
-  adjustedOutput: string
-  gas: number
-  input: string
-  outPut: string
-}
-
-interface FillData {
-  router: string
-  tokenAddress: Array<string>
-  makerAmount?: string
-  makerToken?: string
-  takerAmount?: string
-  takerToken?: string
-  type?: number
-}
-
-interface Order {
-  type: number
-  source: string
-  makerToken: string
-  takeToken: string
-  makerAmount: string
-  takerAmount: string
-  fill: Fill
-  fillData: FillData
-}
-
-interface Source {
-  name: string
-  proportion: string
-}
-
 export interface SwapTransaction {
-  allowanceTarget: string
-  buyAmount: string
-  buyTokenAddress: string
-  buyTokenToEthRate: string
   chainId: number
-  data: string
-  decodedUniqueId: string
-  estimatedGas: string
+  buyAmount: string
+  sellAmount: string
+  buyTokenAddress: string
+  sellTokenAddress: string
+  // be careful -- price means different things when using sellAmount vs buyAmount
+  price: string
+  guaranteedPrice: string
   /**
    * In percentage, between 0 and 100
    */
   estimatedPriceImpact: string | null
-  expectedSlippage: string | null
-  from: string
   gas: string
   gasPrice: string
-  guaranteedPrice: string
-  minimumProtocolFee: string
-  orders: Array<Order>
-  // be careful -- price means different things when using sellAmount vs buyAmount
-  price: string
-  protocolFee: string
-  sellAmount: string
-  sellTokenAddress: string
-  sellTokenToEthRate: string
-  sources: Array<Source>
   to: string
   value: string
-}
-export interface ApproveTransaction {
-  chainId: number
   data: string
   from: string
-  gas: string
-  to: string
+  allowanceTarget: string
 }
 
 export interface SwapInfo {
@@ -108,7 +59,6 @@ export interface SwapInfo {
 }
 
 export interface FetchQuoteResponse {
-  approveTransaction: ApproveTransaction
   unvalidatedSwapTransaction: SwapTransaction
   details: {
     swapProvider: string
