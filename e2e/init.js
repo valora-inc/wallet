77f07fd565caab52c5c7fd8675d6a49e7bbea087
MuckT <mcgtom10@gmail.com>
2024-01-05 00:44:17 -0800

    test(e2e): remove before all app launch

--- e2e/init.js
+++ e2e/init.js
@@ -1,10 +1,4 @@
-import { launchApp } from './src/utils/retries'
-
 beforeAll(async () => {
   // Install app if not present
   await device.installApp()
-  await launchApp({
-    newInstance: false,
-    permissions: { notifications: 'YES', contacts: 'YES', camera: 'YES' },
-  })
 })
