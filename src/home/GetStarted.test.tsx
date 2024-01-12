9d89f9a65132e310c8d26ef423e781dda8a616a6
MuckT <mcgtom10@gmail.com>
2024-01-12 15:02:43 -0800

    test(getStarted): check text and analytic events

---
+++ src/home/GetStarted.test.tsx
@@ -0,0 +1,66 @@
+import { fireEvent, render } from '@testing-library/react-native'
+import React from 'react'
+import { Provider } from 'react-redux'
+import { FiatExchangeEvents } from 'src/analytics/Events'
+import ValoraAnalytics from 'src/analytics/ValoraAnalytics'
+import GetStarted from 'src/home/GetStarted'
+import { createMockStore } from 'test/utils'
+
+jest.mock('src/analytics/ValoraAnalytics')
+
+describe('GetStarted', () => {
+  it('should display the correct text', () => {
+    const { getByText } = render(
+      <Provider
+        store={createMockStore({
+          app: {
+            superchargeApy: 12,
+          },
+        })}
+      >
+        <GetStarted />
+      </Provider>
+    )
+
+    expect(getByText('getStarted')).toBeTruthy()
+    expect(getByText('transactionFeed.getStarted.title')).toBeTruthy()
+    expect(getByText('transactionFeed.getStarted.body, {"apy":12}')).toBeTruthy()
+  })
+
+  it('should trigger impression analytics event', () => {
+    render(
+      <Provider
+        store={createMockStore({
+          app: {
+            superchargeApy: 12,
+          },
+        })}
+      >
+        <GetStarted />
+      </Provider>
+    )
+
+    expect(ValoraAnalytics.track).toHaveBeenCalledWith(
+      FiatExchangeEvents.cico_add_get_started_impression
+    )
+  })
+
+  it('should trigger button tap analytics event', () => {
+    const { getByText } = render(
+      <Provider
+        store={createMockStore({
+          app: {
+            superchargeApy: 12,
+          },
+        })}
+      >
+        <GetStarted />
+      </Provider>
+    )
+
+    fireEvent.press(getByText('addFunds'))
+    expect(ValoraAnalytics.track).toHaveBeenCalledWith(
+      FiatExchangeEvents.cico_add_get_started_selected
+    )
+  })
+})
