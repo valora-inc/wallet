e01e5ff88160e01cbbb16df957dca5aaaa18b99a
MuckT <mcgtom10@gmail.com>
2024-01-25 09:25:50 -0800

    test(e2e): add confirm transaction helper util

--- e2e/src/utils/utils.js
+++ e2e/src/utils/utils.js
@@ -313,6 +313,33 @@ export async function addComment(comment) {
   }
 }
 
+/**
+ * Confirms a transaction by checking that the comment is present in the feed and navigating to the transaction details
+ * @param {string} commentText
+ */
+export async function confirmTransaction(commentText) {
+  try {
+    // Comment should be present in the feed
+    const { elements } = await element(by.id('TransferFeedItem/subtitle')).getAttributes()
+    jestExpect(elements.some((element) => element.text === commentText)).toBeTruthy()
+
+    // Scroll to transaction
+    await waitFor(element(by.text(commentText)))
+      .toBeVisible()
+      .whileElement(by.id('TransactionList'))
+      .scroll(100, 'down')
+
+    // Navigate to transaction details
+    await element(by.text(commentText)).tap()
+
+    // Completed and comment present in transaction details
+    await waitForElementByText(commentText)
+    await waitForElementByText('Completed')
+  } catch (error) {
+    throw new Error(`utils/confirmTransaction failed: ${error}`)
+  }
+}
+
 export async function dismissCashInBottomSheet() {
   try {
     await waitFor(element(by.id('CashInBottomSheet')))
