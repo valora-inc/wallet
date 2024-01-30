b1fc29a36b5fb6d244a9e854fcc6409fb778c2ef
Finnian Jacobson-Schulte <140328381+finnian0826@users.noreply.github.com>
2024-01-30 14:07:10 -0700

    add test

---
+++ src/keylessBackup/LinkPhoneNumber.test.tsx
@@ -0,0 +1,40 @@
+import { fireEvent, render } from '@testing-library/react-native'
+import React from 'react'
+import { Provider } from 'react-redux'
+import LinkPhoneNumber from 'src/keylessBackup/LinkPhoneNumber'
+import { navigate, navigateHome } from 'src/navigator/NavigationService'
+import { Screens } from 'src/navigator/Screens'
+import { createMockStore, getMockStackScreenProps } from 'test/utils'
+
+jest.mock('src/navigator/NavigationService')
+
+describe('LinkPhoneNumber', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('navigates to verification start screen', async () => {
+    const { getByTestId } = render(
+      <Provider store={createMockStore()}>
+        <LinkPhoneNumber {...getMockStackScreenProps(Screens.LinkPhoneNumber)} />
+      </Provider>
+    )
+    fireEvent.press(getByTestId('LinkPhoneNumberButton'))
+
+    expect(navigate).toHaveBeenCalledTimes(1)
+    expect(navigate).toHaveBeenCalledWith(Screens.VerificationStartScreen, {
+      hideOnboardingStep: true,
+    })
+  })
+
+  it('navigates to home on later', async () => {
+    const { getByTestId } = render(
+      <Provider store={createMockStore()}>
+        <LinkPhoneNumber {...getMockStackScreenProps(Screens.LinkPhoneNumber)} />
+      </Provider>
+    )
+    fireEvent.press(getByTestId('LinkePhoneNumberLater'))
+
+    expect(navigateHome).toHaveBeenCalledTimes(1)
+  })
+})
