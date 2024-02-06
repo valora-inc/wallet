5775d01e971b966681d476af17975ee93dc44fff
Alex Bakoushin <alex@bakoush.in>
2024-02-06 19:52:34 +0100

    fix attention icon viewbox

--- src/icons/Attention.tsx
+++ src/icons/Attention.tsx
@@ -11,7 +11,7 @@ const AttentionIcon = ({
   size?: number
   testId?: string
 }) => (
-  <Svg width={size} height={size} fill="none" testID={testId}>
+  <Svg width={size} height={size} fill="none" testID={testId} viewBox="0 0 16 16">
     <Path
       d="M7.2 10.4h1.6V12H7.2v-1.6Zm0-6.4h1.6v4.8H7.2V4Zm.792-4C3.576 0 0 3.584 0 8s3.576 8 7.992 8C12.416 16 16 12.416 16 8s-3.584-8-8.008-8ZM8 14.4A6.398 6.398 0 0 1 1.6 8c0-3.536 2.864-6.4 6.4-6.4 3.536 0 6.4 2.864 6.4 6.4 0 3.536-2.864 6.4-6.4 6.4Z"
       fill={color}
