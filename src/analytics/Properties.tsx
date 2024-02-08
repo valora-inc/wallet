b9367897d89fd7723d952a5966079a0bfbb4f78d
Tom McGuire <Mcgtom10@gmail.com>
2024-02-07 18:08:59 -0800

    feat: keyless backup intro (#4848)
    ### Description
    
    Sets up a `KeylessBackupIntro.tsx` screen which handles both setup and
    restore.
    
    | iOS Setup | iOS |
    | ---- | ----- | 
    |
    ![](https://github.com/valora-inc/wallet/assets/26950305/79e5d243-f801-45dd-9cb8-14fa50586f62
    "iOS Setup") |
    ![](https://github.com/valora-inc/wallet/assets/26950305/ba0236c6-ae6c-4d39-85f9-b12ee8eb9ed2
    "iOS Restore") |
    
    
    ### Test plan
    
    - Tested locally on iOS
    - Unit tests added 
    
    ### Related issues
    
    - Fixes ACT-838
    
    ### Backwards compatibility
    
    Yes
    
    ---------
    
    Co-authored-by: Jacob Waterman <jacobrwaterman@gmail.com>

--- src/analytics/Properties.tsx
+++ src/analytics/Properties.tsx
@@ -228,13 +228,13 @@ interface CommonKeylessBackupProps {
 
 interface KeylessBackupEventsProperties {
   [KeylessBackupEvents.wallet_security_primer_get_started]: undefined
-  [KeylessBackupEvents.cab_setup_continue]: undefined
   [KeylessBackupEvents.cab_setup_recovery_phrase]: undefined
   [KeylessBackupEvents.cab_sign_in_with_google]: CommonKeylessBackupProps
   [KeylessBackupEvents.cab_sign_in_with_google_success]: CommonKeylessBackupProps
   [KeylessBackupEvents.cab_sign_in_with_email_screen_cancel]: CommonKeylessBackupProps
   [KeylessBackupEvents.cab_enter_phone_number_continue]: CommonKeylessBackupProps
   [KeylessBackupEvents.cab_enter_phone_number_cancel]: CommonKeylessBackupProps
+  [KeylessBackupEvents.cab_intro_continue]: CommonKeylessBackupProps
   [KeylessBackupEvents.cab_issue_sms_code_start]: CommonKeylessBackupProps
   [KeylessBackupEvents.cab_issue_sms_code_success]: CommonKeylessBackupProps
   [KeylessBackupEvents.cab_issue_sms_code_error]: CommonKeylessBackupProps
