c0ce87351584e2a7ca2fd8b063a4352027e098ca
Alex Bakoushin <alex@bakoush.in>
2024-03-15 17:00:11 +0100

    add unit test

---
+++ src/jumpstart/JumpstartClaimStatusError.test.tsx
@@ -0,0 +1,48 @@
+import { fireEvent, render } from '@testing-library/react-native'
+import * as React from 'react'
+import { Provider } from 'react-redux'
+import JumpstartClaimStatusError from 'src/jumpstart/JumpstartClaimStatusError'
+import { jumpstartErrorDismissed } from 'src/jumpstart/slice'
+import { navigate } from 'src/navigator/NavigationService'
+import { Screens } from 'src/navigator/Screens'
+import { createMockStore } from 'test/utils'
+
+jest.mock('src/navigator/NavigationService', () => ({
+  navigate: jest.fn(),
+}))
+
+function renderComponent() {
+  const store = createMockStore({
+    jumpstart: {
+      claimStatus: 'error',
+    },
+  })
+  store.dispatch = jest.fn()
+
+  const screen = render(
+    <Provider store={store}>
+      <JumpstartClaimStatusError />
+    </Provider>
+  )
+
+  return { store, screen, ...screen }
+}
+
+describe('JumpstartClaimStatusError', () => {
+  it('handles dismiss correctly', () => {
+    const { store, getByText } = renderComponent()
+
+    fireEvent.press(getByText('jumpstartStatus.error.dismiss'))
+
+    expect(store.dispatch).toHaveBeenCalledWith(jumpstartErrorDismissed())
+  })
+
+  it('handles contact support correctly', () => {
+    const { store, getByText } = renderComponent()
+
+    fireEvent.press(getByText('jumpstartStatus.error.contactSupport'))
+
+    expect(store.dispatch).toHaveBeenCalledWith(jumpstartErrorDismissed())
+    expect(navigate).toHaveBeenCalledWith(Screens.SupportContact)
+  })
+})
