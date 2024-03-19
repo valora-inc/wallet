8912b98823f0313713b0a37978d091317d883bcc
Satish Ravi <satish.ravi@valoraapp.com>
2024-03-18 20:23:20 -0700

    test(e2e): update tests to run against tab navigator

--- e2e/src/usecases/OnRamps.js
+++ e2e/src/usecases/OnRamps.js
@@ -5,6 +5,7 @@ export default onRamps = () => {
   beforeAll(async () => {
     await launchApp({
       newInstance: true,
+      launchArgs: { statsigGateOverrides: `use_tab_navigator=true` },
     })
   })
   beforeEach(async () => {
