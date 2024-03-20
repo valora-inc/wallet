6a8eaefff4d592458385bb81eb81963d41810bc2
Finnian Jacobson-Schulte <140328381+finnian0826@users.noreply.github.com>
2024-03-20 16:03:00 -0600

    add cleanup todo

--- src/backup/BackupComplete.tsx
+++ src/backup/BackupComplete.tsx
@@ -29,6 +29,7 @@ function BackupComplete({ route }: Props) {
   useEffect(() => {
     const timer = setTimeout(() => {
       if (settingsScreen) {
+        // TODO(ACT-1133): change settingsScreen props to isAccountRemoval boolean to know if we need to go back to settings to show promptConfirmRemovalModal
         navigate(settingsScreen === Screens.Settings ? Screens.Settings : Screens.SettingsDrawer, {
           promptConfirmRemovalModal: true,
           isTabNav: settingsScreen === Screens.Settings,
