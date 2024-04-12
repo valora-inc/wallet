52124560f279d98646f4f199d437d960c9cbfd3d
Chris Keating <ckeating.nh@gmail.com>
2024-04-12 16:59:23 -0700

    Remove red background (used for easy testing purposes)

--- src/navigator/TopBarIconButtonV2.tsx
+++ src/navigator/TopBarIconButtonV2.tsx
@@ -76,7 +76,6 @@ export function TopBarIconButtonV2(props: TopBarIconButtonProps) {
 const styles = StyleSheet.create({
   button: {
     padding: Spacing.Small12,
-    backgroundColor: 'red',
   },
   container: {
     justifyContent: 'center',
