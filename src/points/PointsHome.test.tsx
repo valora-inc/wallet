7fe7042b5e384fdba8a76d96e5c2fb9341e4f903
Kathy Luo <kathyluo18@gmail.com>
2024-04-20 00:45:52 +0200

    chore(points): add UI for no supported activities (#5279)
    ### Description
    
    As the title.
    [Context](https://valora-app.slack.com/archives/C0684TXDR3K/p1713471273331519?thread_ts=1713256160.962269&cid=C0684TXDR3K).
    
    ### Test plan
    
    When the activities defined in the app version has no overlap with the
    live points config:
    ![Simulator Screenshot - iPhone 14 Pro - 2024-04-19 at 10 23
    42](https://github.com/valora-inc/wallet/assets/20150449/a0d1b675-5c97-48e8-9444-8216228a2d10)
    
    
    ### Related issues
    
    - Fixes RET-1044
    
    ### Backwards compatibility
    
    Y
    
    ### Network scalability
    
    Y
    
    ---------
    
    Co-authored-by: Jean Regisser <jean.regisser@gmail.com>

--- src/points/PointsHome.test.tsx
+++ src/points/PointsHome.test.tsx
@@ -7,17 +7,23 @@ import { navigate } from 'src/navigator/NavigationService'
 import { Screens } from 'src/navigator/Screens'
 import PointsHome from 'src/points/PointsHome'
 import { getHistoryStarted, getPointsConfigRetry } from 'src/points/slice'
+import { PointsActivity } from 'src/points/types'
 import { createMockStore, getMockStackScreenProps } from 'test/utils'
 
 const mockScreenProps = () => getMockStackScreenProps(Screens.PointsHome)
 
 const renderPointsHome = (
-  pointsConfigStatus: 'idle' | 'loading' | 'error' | 'success' = 'success'
+  pointsConfigStatus: 'idle' | 'loading' | 'error' | 'success' = 'success',
+  activitiesById?: {
+    [activityId in PointsActivity]?: {
+      pointsAmount: number
+    }
+  }
 ) => {
   const store = createMockStore({
     points: {
       pointsConfig: {
-        activitiesById: {
+        activitiesById: activitiesById ?? {
           swap: {
             pointsAmount: 50,
           },
@@ -92,6 +98,20 @@ describe(PointsHome, () => {
     expect(getByTestId('PointsActivityCard-more-coming-20')).toBeTruthy()
     expect(getByTestId('PointsActivityCard-create-wallet-20')).toBeTruthy()
 
+    expect(queryByText('points.loading.title')).toBeFalsy()
+    expect(queryByText('points.error.title')).toBeFalsy()
+  })
+
+  it('renders only the balance if there are no supported activities', async () => {
+    const { getByTestId, getByText, queryByText } = renderPointsHome('success', {})
+
+    expect(getByText('points.title')).toBeTruthy()
+    expect(getByText('50')).toBeTruthy() // balance
+    expect(getByTestId('PointsActivityButton')).toBeTruthy()
+    expect(getByText('points.noActivities.title')).toBeTruthy()
+    expect(getByText('points.noActivities.body')).toBeTruthy()
+
+    expect(queryByText('points.infoCard.title')).toBeFalsy()
     expect(queryByText('points.loading.title')).toBeFalsy()
     expect(queryByText('points.error.title')).toBeFalsy()
   })
