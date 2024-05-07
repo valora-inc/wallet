8f7a45d88831650b5dacd966baeed5d078271a41
Tom McGuire <mcgtom10@gmail.com>
2024-05-07 12:56:43 -0700

    feat: add aave pool fetch user balance function

--- src/earn/poolInfo.ts
+++ src/earn/poolInfo.ts
@@ -1,11 +1,12 @@
 import BigNumber from 'bignumber.js'
 import AavePool from 'src/abis/AavePoolV3'
+import erc20 from 'src/abis/IERC20'
 import { Network } from 'src/transactions/types'
 import Logger from 'src/utils/Logger'
 import { ensureError } from 'src/utils/ensureError'
 import { publicClient } from 'src/viem'
 import networkConfig from 'src/web3/networkConfig'
-import { Address } from 'viem'
+import { Address, formatUnits, getContract } from 'viem'
 
 const TAG = 'earn/poolInfo'
 
@@ -13,6 +14,9 @@ const COMPOUND_PERIOD = 365 * 24 * 60 * 60 // 1 year in seconds
 
 export async function fetchAavePoolInfo(assetAddress: Address) {
   try {
+    if (!assetAddress) {
+      throw new Error('No asset address provided')
+    }
     Logger.debug(TAG, 'Fetching Aave pool info for asset', assetAddress)
     const result = await publicClient[Network.Arbitrum].readContract({
       abi: AavePool,
@@ -29,6 +33,49 @@ export async function fetchAavePoolInfo(assetAddress: Address) {
   } catch (error) {
     const err = ensureError(error)
     Logger.error(TAG, 'Failed to fetch Aave pool info', err)
+    throw err
+  }
+}
+
+export async function fetchAavePoolUserBalance({
+  assetAddress,
+  walletAddress,
+}: {
+  assetAddress: Address
+  walletAddress: Address
+}) {
+  try {
+    if (!assetAddress) {
+      throw new Error('No asset address provided')
+    }
+    if (!walletAddress) {
+      throw new Error('No wallet address provided')
+    }
+    Logger.debug(TAG, 'Fetching Aave pool user balance', assetAddress)
+    const result = await publicClient[Network.Arbitrum].readContract({
+      abi: AavePool,
+      address: networkConfig.arbAavePoolV3ContractAddress,
+      functionName: 'getReserveData',
+      args: [assetAddress],
+    })
+
+    const { aTokenAddress } = result as { aTokenAddress: `0x${string}` }
+    const aaveUSDCContract = getContract({
+      abi: erc20.abi,
+      address: aTokenAddress,
+      client: {
+        public: publicClient[Network.Arbitrum],
+      },
+    })
+
+    const balance = await aaveUSDCContract.read.balanceOf([walletAddress])
+    const decimals = await aaveUSDCContract.read.decimals()
+    const balanceInDecimal = formatUnits(balance, decimals)
+
+    return { balanceInDecimal }
+  } catch (error) {
+    const err = ensureError(error)
+    Logger.error(TAG, 'Failed to fetch Aave pool user balance', err)
     throw err
   }
 }
