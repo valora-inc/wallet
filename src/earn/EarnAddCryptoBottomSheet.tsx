07035ab25935fb2c8712eb859389c4897cd75de6
Finnian Jacobson-Schulte <140328381+finnian0826@users.noreply.github.com>
2024-05-07 11:21:24 -0600

    clean up

--- src/earn/EarnAddCryptoBottomSheet.tsx
+++ src/earn/EarnAddCryptoBottomSheet.tsx
@@ -86,7 +86,7 @@ export default function EarnAddCryptoBottomSheet({
   )
 }
 
-export const getActions = (
+const getActions = (
   token: TokenBalance,
   tokenAmount: BigNumber,
   showAdd: boolean,
