a328140c29da9684e9c2ccd442a4407a17f09304
Tom McGuire <mcgtom10@gmail.com>
2024-05-14 22:17:16 -0700

    feat: earn feed transaction feed item and details

---
+++ src/transactions/feed/EarnFeedItem.tsx
@@ -0,0 +1,188 @@
+import BigNumber from 'bignumber.js'
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { StyleSheet, Text, View } from 'react-native'
+import TokenDisplay from 'src/components/TokenDisplay'
+import Touchable from 'src/components/Touchable'
+import { navigate } from 'src/navigator/NavigationService'
+import { Screens } from 'src/navigator/Screens'
+import { getDynamicConfigParams } from 'src/statsig'
+import { DynamicConfigs } from 'src/statsig/constants'
+import { StatsigDynamicConfigs } from 'src/statsig/types'
+import colors, { Colors } from 'src/styles/colors'
+import { typeScale } from 'src/styles/fonts'
+import { Spacing } from 'src/styles/styles'
+import variables from 'src/styles/variables'
+import TransactionFeedItemImage from 'src/transactions/feed/TransactionFeedItemImage'
+import {
+  EarnClaimReward,
+  EarnDeposit,
+  EarnWithdraw,
+  TransactionStatus,
+} from 'src/transactions/types'
+
+interface AmountProps {
+  transaction: EarnWithdraw | EarnDeposit | EarnClaimReward
+}
+
+function Amount({ transaction }: AmountProps) {
+  switch (transaction.__typename) {
+    case 'EarnDeposit':
+      return (
+        <View style={styles.amountContainer}>
+          <TokenDisplay
+            amount={new BigNumber(-transaction.outAmount.value)}
+            tokenId={transaction.outAmount.tokenId}
+            showLocalAmount={false}
+            showSymbol={true}
+            showExplicitPositiveSign={true}
+            hideSign={false}
+            style={styles.amountTitle}
+            testID={'EarnFeedItem/outAmount-crypto'}
+          />
+          <TokenDisplay
+            amount={new BigNumber(-transaction.outAmount.value)}
+            tokenId={transaction.outAmount.tokenId}
+            showLocalAmount={true}
+            showSymbol={true}
+            showExplicitPositiveSign={true}
+            hideSign={true}
+            style={styles.amountSubtitle}
+            testID={'EarnFeedItem/outAmount-local'}
+          />
+        </View>
+      )
+    case 'EarnWithdraw':
+      return (
+        <View style={styles.amountContainer}>
+          <TokenDisplay
+            amount={new BigNumber(transaction.inAmount.value)}
+            tokenId={transaction.inAmount.tokenId}
+            showLocalAmount={false}
+            showSymbol={true}
+            showExplicitPositiveSign={true}
+            hideSign={false}
+            style={[styles.amountTitle, { color: Colors.primary }]}
+            testID={'EarnFeedItem/inAmount-crypto'}
+          />
+          <TokenDisplay
+            amount={new BigNumber(transaction.inAmount.value)}
+            tokenId={transaction.inAmount.tokenId}
+            showLocalAmount={true}
+            showSymbol={true}
+            showExplicitPositiveSign={true}
+            hideSign={true}
+            style={styles.amountSubtitle}
+            testID={'EarnFeedItem/inAmount-local'}
+          />
+        </View>
+      )
+    case 'EarnClaimReward':
+      return (
+        <View style={styles.amountContainer}>
+          <TokenDisplay
+            amount={new BigNumber(transaction.amount.value)}
+            tokenId={transaction.amount.tokenId}
+            showLocalAmount={false}
+            showSymbol={true}
+            showExplicitPositiveSign={true}
+            hideSign={false}
+            style={[styles.amountTitle, { color: Colors.primary }]}
+            testID={'EarnFeedItem/amount-crypto'}
+          />
+          <TokenDisplay
+            amount={new BigNumber(transaction.amount.value)}
+            tokenId={transaction.amount.tokenId}
+            showLocalAmount={true}
+            showSymbol={true}
+            showExplicitPositiveSign={true}
+            hideSign={true}
+            style={styles.amountSubtitle}
+            testID={'EarnFeedItem/amount-local'}
+          />
+        </View>
+      )
+    default:
+      return null
+  }
+}
+
+interface Props {
+  transaction: EarnWithdraw | EarnDeposit | EarnClaimReward
+}
+
+export default function EarnFeedItem({ transaction }: Props) {
+  const { t } = useTranslation()
+  const { providerName } = getDynamicConfigParams(
+    DynamicConfigs[StatsigDynamicConfigs.EARN_STABLECOIN_CONFIG]
+  )
+  const isDeposit = transaction.__typename === 'EarnDeposit'
+  const isWithdraw = transaction.__typename === 'EarnWithdraw'
+  const isClaim = transaction.__typename === 'EarnClaimReward'
+
+  return (
+    <Touchable
+      testID={`EarnFeedItem/${transaction.transactionHash}`}
+      onPress={() => {
+        navigate(Screens.TransactionDetailsScreen, {
+          transaction: { ...transaction, status: TransactionStatus.Complete },
+        })
+      }}
+    >
+      <View style={styles.container}>
+        <TransactionFeedItemImage
+          status={transaction.status}
+          transactionType={transaction.__typename}
+          networkId={transaction.networkId}
+        />
+        <View style={styles.contentContainer}>
+          <Text style={styles.title} testID={'EarnFeedItem/title'} numberOfLines={1}>
+            {isDeposit && t('earnFlow.transactionFeed.earnDepositTitle')}
+            {isWithdraw && t('earnFlow.transactionFeed.earnWithdrawTitle')}
+            {isClaim && t('earnFlow.transactionFeed.earnClaimTitle')}
+          </Text>
+          <Text style={styles.subtitle} testID={'EarnFeedItem/subtitle'} numberOfLines={1}>
+            {isDeposit && t('earnFlow.transactionFeed.earnDepositSubtitle', { providerName })}
+            {isWithdraw && t('earnFlow.transactionFeed.earnWithdrawSubtitle', { providerName })}
+            {isClaim && t('earnFlow.transactionFeed.earnClaimSubtitle', { providerName })}
+          </Text>
+        </View>
+        <Amount transaction={transaction} />
+      </View>
+    </Touchable>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    paddingVertical: Spacing.Small12,
+    paddingHorizontal: variables.contentPadding,
+    alignItems: 'center',
+  },
+  contentContainer: {
+    flex: 1,
+    paddingHorizontal: variables.contentPadding,
+  },
+  title: {
+    ...typeScale.labelMedium,
+  },
+  subtitle: {
+    ...typeScale.bodySmall,
+    color: colors.gray4,
+  },
+  amountContainer: {
+    maxWidth: '50%',
+  },
+  amountTitle: {
+    ...typeScale.labelMedium,
+    flexWrap: 'wrap',
+    textAlign: 'right',
+  },
+  amountSubtitle: {
+    ...typeScale.bodySmall,
+    color: colors.gray4,
+    flexWrap: 'wrap',
+    textAlign: 'right',
+  },
+})
