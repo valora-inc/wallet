16aa133988a508a628bdd9f9bde8e841595bb178
Tom McGuire <mcgtom10@gmail.com>
2024-05-15 16:24:09 -0700

    chore: add colors to text

--- src/transactions/feed/EarnFeedItem.tsx
+++ src/transactions/feed/EarnFeedItem.tsx
@@ -138,6 +138,7 @@ const styles = StyleSheet.create({
   },
   title: {
     ...typeScale.labelMedium,
+    color: Colors.black,
   },
   subtitle: {
     ...typeScale.bodySmall,
@@ -148,6 +149,7 @@ const styles = StyleSheet.create({
   },
   amountTitle: {
     ...typeScale.labelMedium,
+    color: Colors.black,
     flexWrap: 'wrap',
     textAlign: 'right',
   },
