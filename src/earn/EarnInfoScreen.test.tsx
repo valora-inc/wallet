37eb4372b96a782b60e2dbc9fbe52de654490514
Tom McGuire <mcgtom10@gmail.com>
2024-05-24 00:20:54 -0700

    test(earn-info): unit tests sans analytics for earn info

---
+++ src/earn/EarnInfoScreen.test.tsx
@@ -0,0 +1,104 @@
+import { fireEvent, render } from '@testing-library/react-native'
+import React from 'react'
+import { Provider } from 'react-redux'
+import { EARN_STABLECOINS_LEARN_MORE } from 'src/config'
+import EarnInfoScreen from 'src/earn/EarnInfoScreen'
+import { navigate } from 'src/navigator/NavigationService'
+import { Screens } from 'src/navigator/Screens'
+import { getFeatureGate } from 'src/statsig'
+import { StatsigFeatureGates } from 'src/statsig/types'
+import networkConfig from 'src/web3/networkConfig'
+import MockedNavigator from 'test/MockedNavigator'
+import { createMockStore } from 'test/utils'
+
+jest.mock('src/statsig', () => ({
+  getFeatureGate: jest.fn(),
+}))
+
+const store = createMockStore({})
+
+describe('EarnInfoScreen', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('should render correctly when no gas subsidy', async () => {
+    const { getByText, queryByText } = render(
+      <Provider store={store}>
+        <MockedNavigator
+          component={EarnInfoScreen}
+          params={{ tokenId: networkConfig.arbUsdcTokenId }}
+        />
+      </Provider>
+    )
+
+    // First details item - includes subsidy code
+    expect(getByText('earnFlow.earnInfo.title')).toBeTruthy()
+    expect(getByText('earnFlow.earnInfo.details.earn.title')).toBeTruthy()
+    expect(queryByText('earnFlow.earnInfo.details.earn.titleGasSubsidy')).toBeFalsy()
+    expect(queryByText('earnFlow.earnInfo.details.earn.footnoteSubsidy')).toBeFalsy()
+
+    // Second details item
+    expect(getByText('earnFlow.earnInfo.details.manage.title')).toBeTruthy()
+    expect(getByText('earnFlow.earnInfo.details.manage.subtitle')).toBeTruthy()
+
+    // Third details item
+    expect(getByText('earnFlow.earnInfo.details.access.title')).toBeTruthy()
+    expect(getByText('earnFlow.earnInfo.details.access.subtitle')).toBeTruthy()
+
+    // Buttons
+    expect(getByText('earnFlow.earnInfo.action.learn')).toBeTruthy()
+    expect(getByText('earnFlow.earnInfo.action.earn')).toBeTruthy()
+  })
+
+  it('should render correctly when gas subsidy enabled', () => {
+    jest
+      .mocked(getFeatureGate)
+      .mockImplementation((gate) => gate === StatsigFeatureGates.SUBSIDIZE_STABLECOIN_EARN_GAS_FEES)
+
+    const { getByText, queryByText } = render(
+      <Provider store={store}>
+        <MockedNavigator
+          component={EarnInfoScreen}
+          params={{ depositTokenId: networkConfig.arbUsdcTokenId }}
+        />
+      </Provider>
+    )
+
+    expect(getByText('earnFlow.earnInfo.details.earn.titleGasSubsidy')).toBeTruthy()
+    expect(getByText('earnFlow.earnInfo.details.earn.footnoteSubsidy')).toBeTruthy()
+    expect(queryByText('earnFlow.earnInfo.details.earn.title')).toBeFalsy()
+  })
+
+  it('should navigate to correctly on Learn More button press', () => {
+    const { getByText } = render(
+      <Provider store={store}>
+        <MockedNavigator
+          component={EarnInfoScreen}
+          params={{ depositTokenId: networkConfig.arbUsdcTokenId }}
+        />
+      </Provider>
+    )
+
+    fireEvent.press(getByText('earnFlow.earnInfo.action.learn'))
+    expect(navigate).toHaveBeenCalledWith(Screens.WebViewScreen, {
+      uri: EARN_STABLECOINS_LEARN_MORE,
+    })
+  })
+
+  it('should navigate to correctly on Start Earning button press', () => {
+    const { getByText } = render(
+      <Provider store={store}>
+        <MockedNavigator
+          component={EarnInfoScreen}
+          params={{ depositTokenId: networkConfig.arbUsdcTokenId }}
+        />
+      </Provider>
+    )
+
+    fireEvent.press(getByText('earnFlow.earnInfo.action.earn'))
+    expect(navigate).toHaveBeenCalledWith(Screens.EarnEnterAmount, {
+      tokenId: networkConfig.arbUsdcTokenId,
+    })
+  })
+})
