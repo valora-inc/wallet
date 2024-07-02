c0bb3662de030c77f2e3128619b63022e4b7b052
Alex Bakoushin <alex@bakoush.in>
2024-07-02 18:43:07 +0200

    update branding hash

--- scripts/sync_branding.sh
+++ scripts/sync_branding.sh
@@ -21,7 +21,7 @@ echo $mobile_root
 cd "$mobile_root"
 
 # Please update the sha when valora branding updates are needed
-valora_branding_sha=e3162c016f448d476ace235242cf143602c84cfb
+valora_branding_sha=fa62d95cd35faa1f3cc87a11dd22e990ae2330d4
 
 if [[ "$branding" == "valora" ]]; then
   # prevents git from asking credentials
