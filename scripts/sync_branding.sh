81374f3df91d7afc8ed01a23081ac5f2a079f53e
Alex Bakoushin <alex@bakoush.in>
2024-07-03 11:46:09 +0200

    chore(points): points discover card redesign (#5608)
    ### Description
    
    Implement recent designs for points discover card.
    
    Figma:
    https://www.figma.com/design/rXBDplfMHHqYmuu6EkgMEo/Gamification-experiments?node-id=1907-11167&t=llLXJVN4HRpgRqIr-0
    
    ### Test plan
    
    * Tested manually
    
    ### Related issues
    
    - Related to RET-1103
    
    ### Backwards compatibility
    
    Y
    
    ### Network scalability
    
    If a new NetworkId and/or Network are added in the future, the changes
    in this PR will:
    
    - [x] Continue to work without code changes, OR trigger a compilation
    error (guaranteeing we find it when a new network is added)

--- scripts/sync_branding.sh
+++ scripts/sync_branding.sh
@@ -21,7 +21,7 @@ echo $mobile_root
 cd "$mobile_root"
 
 # Please update the sha when valora branding updates are needed
-valora_branding_sha=bcf249c4b07521cee12afae86eb7d35b7772df3c
+valora_branding_sha=fa62d95cd35faa1f3cc87a11dd22e990ae2330d4
 
 if [[ "$branding" == "valora" ]]; then
   # prevents git from asking credentials
