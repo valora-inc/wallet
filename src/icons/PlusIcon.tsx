9c4a7c3c864a0763f1d8a78d791d350962aff38c
Myroslav Sviderok <myroslav.sviderok@gmail.com>
2024-08-14 16:10:15 +0300

    chore: replace hardcoded color with Colors enum for PlusIcon

--- src/icons/PlusIcon.tsx
+++ src/icons/PlusIcon.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react'
 import Svg, { Path } from 'react-native-svg'
+import Colors from 'src/styles/colors'
 
 const PlusIcon = () => (
   <Svg width={24} height={24} fill="none" viewBox="0 0 24 24">
     <Path
       d="M19 12.998H13V18.998H11V12.998H5V10.998H11V4.99805H13V10.998H19V12.998Z"
-      fill="#2E3338"
+      fill={Colors.black}
     />
   </Svg>
 )
