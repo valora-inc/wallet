9c9b201a436e4deaeb4d5d3cd9d313db799661b8
Tom McGuire <mcgtom10@gmail.com>
2024-08-16 12:21:23 -0400

    docs: remove old comment about test that is now enabled

--- e2e/src/QRScanner.spec.js
+++ e2e/src/QRScanner.spec.js
@@ -1,8 +1,6 @@
 import { reloadReactNative, launchApp } from './utils/retries'
 import { quickOnboarding, waitForElementId } from './utils/utils'
 
-// Re-enable for android when react-native-reanimated is updated to v3
-// https://linear.app/valora/issue/ENG-76/[wallet]-update-react-native-reanimated-to-v3
 describe('Given QR Scanner', () => {
   beforeAll(async () => {
     await quickOnboarding()
