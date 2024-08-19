e22a2f1065e1741928b0d4c7ad3db9614ec798a6
Kathy Luo <kathyluo18@gmail.com>
2024-08-19 09:58:10 +0200

    refactor: update branded icons

--- branding/src/icons/GuideKeyHomeCardIcon.tsx
+++ branding/src/icons/GuideKeyHomeCardIcon.tsx
@@ -14,12 +14,7 @@ export default class GuideKeyIcon extends React.PureComponent<Props> {
 
   render() {
     return (
-      <Svg
-        width={this.props.width}
-        height={this.props.height}
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
+      <Svg width={this.props.width} height={this.props.height} fill="none">
         <Path
           d="M55.21 45.03H43.809m-7.602 6.923v-6.922h7.602m0 0v6.922"
           stroke="#2E3338"
@@ -29,30 +24,18 @@ export default class GuideKeyIcon extends React.PureComponent<Props> {
         <Path
           d="M51.31 9.975C32.23 9.123 12.923 18.71.555 35.59.555 12.447 27.2-.95 49.688.053c22.486 1.004 42.651 17.855 41.948 33.46C79.3 19.364 70.389 10.827 51.31 9.974Z"
           fill="#FFE768"
-          style={{
-            mixBlendMode: 'multiply',
-          }}
         />
         <Path
           d="M44.094 74.222c18.46 4.876 30.448-.389 46.14-14.26-9.533 17.387-29.03 29.826-50.787 24.08-21.756-5.747-30.592-27.23-22.944-44.767 2.318 14.306 9.13 30.071 27.59 34.947Z"
           fill="#42D689"
-          style={{
-            mixBlendMode: 'multiply',
-          }}
         />
         <Path
           d="M20.831 34.857c-6.733 14.522-3.984 31.144 4.585 46.605C9.704 73.626 4.652 50.175 12.586 33.06c7.936-17.115 29.068-23.775 40.862-17.893l-.043.009c-11.618 2.327-25.85 5.177-32.574 19.681Z"
           fill="#FF6F43"
-          style={{
-            mixBlendMode: 'multiply',
-          }}
         />
         <Path
           d="M78.36 44.298c.798-16.3-5.86-29.026-20.368-39.65 17.916.869 30.129 17.227 30.129 39.65s-21.78 36.46-35.228 35.807C66.617 72.58 77.563 60.6 78.36 44.298Z"
           fill="#92D8FF"
-          style={{
-            mixBlendMode: 'multiply',
-          }}
         />
       </Svg>
     )
--- branding/src/icons/GuideKeyIcon.tsx
+++ branding/src/icons/GuideKeyIcon.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import Svg, { Path, Ellipse } from 'svgs'
+import Svg, { Ellipse, Path } from 'svgs'
 
 interface Props {
   height?: number
@@ -14,12 +14,7 @@ export default class GuideKeyIcon extends React.PureComponent<Props> {
 
   render() {
     return (
-      <Svg
-        width={this.props.width}
-        height={this.props.height}
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
+      <Svg width={this.props.width} height={this.props.height} fill="none">
         <Path
           d="M85.88 60.094H71.31m-9.713 8.846v-8.846h9.713m0 0v8.846"
           stroke="#2E3338"
@@ -29,30 +24,18 @@ export default class GuideKeyIcon extends React.PureComponent<Props> {
         <Path
           d="M80.896 15.302c-24.38-1.09-49.05 11.16-64.854 32.729 0-29.571 34.048-46.692 62.78-45.408 28.733 1.283 54.5 22.815 53.601 42.754-15.761-18.078-27.148-28.986-51.527-30.075Z"
           fill="#FFE768"
-          style={{
-            mixBlendMode: 'multiply',
-          }}
         />
         <Path
           d="M71.675 97.395c23.588 6.231 38.906-.497 58.958-18.222-12.183 22.218-37.095 38.111-64.895 30.769-27.8-7.343-39.09-34.793-29.318-57.202 2.962 18.28 11.667 38.425 35.255 44.655Z"
           fill="#42D689"
-          style={{
-            mixBlendMode: 'multiply',
-          }}
         />
         <Path
           d="M41.95 47.095c-8.603 18.556-5.09 39.795 5.859 59.551-20.077-10.012-26.532-39.977-16.393-61.847 10.14-21.87 37.142-30.378 52.212-22.863l-.056.011c-14.845 2.973-33.03 6.616-41.622 25.148Z"
           fill="#FF6F43"
-          style={{
-            mixBlendMode: 'multiply',
-          }}
         />
         <Path
           d="M115.46 59.159c1.019-20.829-7.488-37.09-26.026-50.664 22.892 1.11 38.498 22.012 38.498 50.664s-27.83 46.586-45.013 45.753c17.536-9.616 31.523-24.925 32.541-45.753Z"
           fill="#92D8FF"
-          style={{
-            mixBlendMode: 'multiply',
-          }}
         />
       </Svg>
     )
