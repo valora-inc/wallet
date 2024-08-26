b88589c7e76ba6dd7b02eaeaaa2fbdf21a050e59
Kathy Luo <kathyluo18@gmail.com>
2024-08-26 13:42:25 +0200

    chore: document require context

--- metro.config.js
+++ metro.config.js
@@ -25,7 +25,7 @@ const config = {
         inlineRequires: true,
       },
     }),
-    unstable_allowRequireContext: true,
+    unstable_allowRequireContext: true, // used to enable rendering of all app assets dynamically in debug assets screen
   },
   resolver: {
     assetExts: [...defaultAssetExts, 'txt'],
