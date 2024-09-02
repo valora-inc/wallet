4c14de50d01c3b61d0c715db27cf13a8e2e060bb
Myroslav Sviderok <myroslav.sviderok@gmail.com>
2024-09-02 11:22:43 +0300

    Add missing "address.ts" functions from @celo/utils

--- src/utils/address.ts
+++ src/utils/address.ts
@@ -1,9 +1,51 @@
-// Turns '0xce10ce10ce10ce10ce10ce10ce10ce10ce10ce10'
-// into ['ce10','ce10','ce10','ce10','ce10','ce10','ce10','ce10','ce10','ce10']
+import { privateToAddress, privateToPublic, toChecksumAddress } from '@ethereumjs/util'
+import { Address } from 'viem'
+
+/**
+ * Turns '0xce10ce10ce10ce10ce10ce10ce10ce10ce10ce10'
+ * into ['ce10','ce10','ce10','ce10','ce10','ce10','ce10','ce10','ce10','ce10']
+ */
 export function getAddressChunks(address: string) {
   return trimLeading0x(address).match(/.{1,4}/g) || []
 }
 
 export function trimLeading0x(address: string) {
   return address.startsWith('0x') ? address.slice(2) : address
+}
+
+export const normalizeAddressWith0x = (a: string) => ensureLeading0x(a).toLowerCase() as Address
+
+export const ensureLeading0x = (input: string): Address =>
+  input.startsWith('0x') ? (input as Address) : (`0x${input}` as const)
+
+export const hexToBuffer = (input: string) => Buffer.from(trimLeading0x(input), 'hex')
+
+export const privateKeyToAddress = (privateKey: string) =>
+  toChecksumAddress(
+    ensureLeading0x(privateToAddress(hexToBuffer(privateKey)).toString('hex'))
+  ) as Address
+
+export const privateKeyToPublicKey = (privateKey: string) =>
+  toChecksumAddress(ensureLeading0x(privateToPublic(hexToBuffer(privateKey)).toString('hex')))
+
+export const eqAddress = (a: string, b: string) => normalizeAddress(a) === normalizeAddress(b)
+
+export const normalizeAddress = (a: string) => trimLeading0x(a).toLowerCase()
+
+export const isValidAddress = (input: string): input is Address => {
+  if ('string' !== typeof input) {
+    return false
+  }
+  if (!/^(0x)?[0-9a-f]{40}$/i.test(input)) {
+    return false
+  }
+  if (/^(0x|0X)?[0-9A-F]{40}$/.test(input.toUpperCase())) {
+    return true
+  }
+
+  if (toChecksumAddress(input) === input) {
+    return true
+  }
+
+  return false
 }
