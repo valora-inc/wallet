4a7bef8e598bf318c32c8b262dc438953befc9ef
Jean Regisser <jean.regisser@gmail.com>
2024-09-02 10:17:24 +0200

    refactor: copy account module from @celo/cryptographic-utils@3.2.0 (#5861)
    ### Description
    
    This is the first step to remove `@celo/cryptographic-utils`.
    
    It copies the
    [accounts](https://github.com/celo-org/celo-monorepo/blob/sdks-3.2.0/packages/sdk/cryptographic-utils/src/account.ts)
    module from it.
    
    I've copied the exact same version used from the celo SDK: `3.2.0`.
    And listed the exact same NPM packages it was using internally.
    This is to ensure it behaves the same.
    We'll upgrade/remove these in a follow up PR.
    
    I've slightly modified it to directly include some sub modules, which we
    didn't need to share.
    - `@celo/base/lib/account` (some type definitions)
    - `@celo/base/lib/string` (only 1 function used)
    
    ### Test plan
    
    - Tests pass
    
    ### Related issues
    
    - Part of RET-1185
    
    ### Backwards compatibility
    
    Yes
    
    ### Network scalability
    
    If a new NetworkId and/or Network are added in the future, the changes
    in this PR will:
    
    - [x] Continue to work without code changes, OR trigger a compilation
    error (guaranteeing we find it when a new network is added)

--- src/import/saga.ts
+++ src/import/saga.ts
@@ -1,9 +1,3 @@
-import {
-  invalidMnemonicWords,
-  normalizeMnemonic,
-  suggestMnemonicCorrections,
-  validateMnemonic,
-} from '@celo/cryptographic-utils'
 import { privateKeyToAddress } from '@celo/utils/lib/address'
 import { Task } from '@redux-saga/types'
 import * as bip39 from 'react-native-bip39'
@@ -11,8 +5,8 @@ import { setBackupCompleted } from 'src/account/actions'
 import { initializeAccountSaga } from 'src/account/saga'
 import { recoveringFromStoreWipeSelector } from 'src/account/selectors'
 import { showError } from 'src/alert/actions'
-import { AppEvents, OnboardingEvents } from 'src/analytics/Events'
 import AppAnalytics from 'src/analytics/AppAnalytics'
+import { AppEvents, OnboardingEvents } from 'src/analytics/Events'
 import { ErrorMessages } from 'src/app/ErrorMessages'
 import { countMnemonicWords, generateKeysFromMnemonic, storeMnemonic } from 'src/backup/utils'
 import { refreshAllBalances } from 'src/home/actions'
@@ -26,8 +20,14 @@ import { navigate } from 'src/navigator/NavigationService'
 import { Screens } from 'src/navigator/Screens'
 import { goToNextOnboardingScreen, onboardingPropsSelector } from 'src/onboarding/steps'
 import { FetchedTokenBalance, fetchTokenBalancesForAddress } from 'src/tokens/saga'
-import { ensureError } from 'src/utils/ensureError'
 import Logger from 'src/utils/Logger'
+import {
+  invalidMnemonicWords,
+  normalizeMnemonic,
+  suggestMnemonicCorrections,
+  validateMnemonic,
+} from 'src/utils/account'
+import { ensureError } from 'src/utils/ensureError'
 import { safely } from 'src/utils/safely'
 import { assignAccountFromPrivateKey } from 'src/web3/saga'
 import {
