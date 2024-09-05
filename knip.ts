341e6cc26d63a297910b8d1c3b60692fdf15c3df
Jean Regisser <jean.regisser@gmail.com>
2024-09-05 10:30:19 +0200

    chore: remove `node-libs-react-native` and obsolete polyfills

--- knip.ts
+++ knip.ts
@@ -11,7 +11,6 @@ const config: KnipConfig = {
   ],
   project: ['src/**/*.ts!', 'src/**/*.tsx!', 'src/**/*.js!'],
   ignoreDependencies: [
-    'Base64',
     '@actions/github',
     '@babel/runtime', // enforce specific version for react-native
     '@babel/plugin-transform-private-methods', // used in babel.config.js to build. not imported, so knip doesn't understand it is used
@@ -34,8 +33,6 @@ const config: KnipConfig = {
     '@types/isomorphic-fetch',
     '@types/jest',
     'husky',
-    'react-native-randombytes', // not sure we need this; only referenced in iOS Podfile.lock
-    '@celo/contractkit', // Temporary until we remove the dependency
   ],
   ignore: ['src/utils/inputValidation.ts', 'src/utils/country.json'],
 }
--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,5 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config')
 const path = require('path')
-const nodeLibs = require('node-libs-react-native')
 const exclusionList = require('metro-config/src/defaults/exclusionList')
 const escapeStringRegexp = require('escape-string-regexp')
 const isE2E = process.env.CELO_TEST_CONFIG === 'e2e'
@@ -33,17 +32,11 @@ const config = {
       isE2E ? blist : blist.concat([RegExp(`${escapedRoot}\/e2e\/mocks/.*`)])
     ),
     extraNodeModules: {
-      ...nodeLibs,
       // This is the crypto module we want to use moving forward (unless something better comes up).
       // It is implemented natively using OpenSSL.
       crypto: require.resolve('react-native-quick-crypto'),
       fs: require.resolve('react-native-fs'),
       'isomorphic-fetch': require.resolve('cross-fetch'),
-      // We don't need the `net` module for now.
-      // This doesn't actually provide any implementation,
-      // but avoids an error when require('net') is used (in ContractKit for instance).
-      net: require.resolve('node-libs-react-native/mock/net'),
-      vm: require.resolve('vm-browserify'),
     },
     sourceExts: isE2E ? ['e2e.ts', 'e2e.js'].concat(defaultSourceExts) : defaultSourceExts,
   },
