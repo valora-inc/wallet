1a83fc6da8df7181a277447860d2d83692811c33
Tom McGuire <mcgtom10@gmail.com>
2024-09-09 19:06:39 -0700

    style: avoid unneeded changes

--- __mocks__/@gorhom/bottom-sheet.ts
+++ __mocks__/@gorhom/bottom-sheet.ts
@@ -3,7 +3,7 @@ import { FlatList, ScrollView, SectionList } from 'react-native'
 module.exports = {
   __esModule: true,
   ...require('@gorhom/bottom-sheet/mock'),
+  BottomSheetScrollView: ScrollView,
   BottomSheetFlatList: FlatList,
   BottomSheetSectionList: SectionList,
-  BottomSheetScrollView: ScrollView,
 }
