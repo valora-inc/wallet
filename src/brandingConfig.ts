85dacf127328fe00a5b3604af87b8b4cf0395d95
Kathy Luo <kathyluo18@gmail.com>
2024-09-11 09:27:19 +0200

    chore: replace swap links

--- src/brandingConfig.ts
+++ src/brandingConfig.ts
@@ -1,6 +1,4 @@
 export const APP_NAME = 'Valora'
-export const SWAP_LEARN_MORE = 'https://valora.xyz/support/swap-learn-more'
-export const TRANSACTION_FEES_LEARN_MORE = 'https://valora.xyz/support/transaction-fees-learn-more'
 export const INVITE_REWARDS_NFTS_LEARN_MORE =
   'https://valora.xyz/support/invite-rewards-nfts-learn-more'
 export const INVITE_REWARDS_STABLETOKEN_LEARN_MORE =
