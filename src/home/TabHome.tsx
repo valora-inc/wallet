f922c87d97fab469d983670d676f438cf3520524
Myroslav Sviderok <myroslav.sviderok@gmail.com>
2024-09-11 12:37:14 +0300

    chore: remove "Welcome" title from Home Tab (#5971)
    ### Description
    Remove "Welcome" title from Home Tab
    
    ### Test plan
    Adjust existing TabHome tests
    
    ### Related issues
    
    - Relates to RET-1189
    
    ### Backwards compatibility
    Yes
    
    ### Network scalability
    
    If a new NetworkId and/or Network are added in the future, the changes
    in this PR will:
    
    - [x] Continue to work without code changes, OR trigger a compilation
    error (guaranteeing we find it when a new network is added)

--- src/home/TabHome.tsx
+++ src/home/TabHome.tsx
@@ -1,16 +1,9 @@
 import { useIsFocused } from '@react-navigation/native'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import _ from 'lodash'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
-import {
-  LayoutChangeEvent,
-  RefreshControl,
-  RefreshControlProps,
-  SectionList,
-  StyleSheet,
-  Text,
-} from 'react-native'
+import { RefreshControl, RefreshControlProps, SectionList, StyleSheet } from 'react-native'
 import Animated, { useAnimatedScrollHandler, useSharedValue } from 'react-native-reanimated'
 import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context'
 import { showMessage } from 'src/alert/actions'
@@ -29,14 +22,11 @@ import NftReward from 'src/home/celebration/NftReward'
 import { showNftCelebrationSelector, showNftRewardSelector } from 'src/home/selectors'
 import { importContacts } from 'src/identity/actions'
 import { Screens } from 'src/navigator/Screens'
-import useScrollAwareHeader from 'src/navigator/ScrollAwareHeader'
 import { StackParamList } from 'src/navigator/types'
 import { phoneRecipientCacheSelector } from 'src/recipients/reducer'
 import { useDispatch, useSelector } from 'src/redux/hooks'
 import { initializeSentryUserContext } from 'src/sentry/actions'
 import colors from 'src/styles/colors'
-import { typeScale } from 'src/styles/fonts'
-import { Spacing } from 'src/styles/styles'
 import TransactionFeed from 'src/transactions/feed/TransactionFeed'
 import { hasGrantedContactsPermission } from 'src/utils/contacts'
 
@@ -44,7 +34,7 @@ const AnimatedSectionList = Animated.createAnimatedComponent(SectionList)
 
 type Props = NativeStackScreenProps<StackParamList, Screens.TabHome>
 
-function TabHome({ navigation }: Props) {
+function TabHome(_props: Props) {
   const { t } = useTranslation()
 
   const appState = useSelector(appStateSelector)
@@ -81,22 +71,9 @@ function TabHome({ navigation }: Props) {
 
   // Scroll Aware Header
   const scrollPosition = useSharedValue(0)
-  const [titleHeight, setTitleHeight] = useState(0)
-
-  const handleMeasureTitleHeight = (event: LayoutChangeEvent) => {
-    setTitleHeight(event.nativeEvent.layout.height)
-  }
 
   const handleScroll = useAnimatedScrollHandler((event) => {
     scrollPosition.value = event.contentOffset.y
-  })
-
-  useScrollAwareHeader({
-    navigation,
-    title: t('bottomTabsNavigator.home.title'),
-    scrollPosition,
-    startFadeInPosition: titleHeight - titleHeight * 0.33,
-    animationDistance: titleHeight * 0.33,
   })
 
   const tryImportContacts = async () => {
@@ -141,15 +118,6 @@ function TabHome({ navigation }: Props) {
     <RefreshControl refreshing={isLoading} onRefresh={onRefresh} colors={[colors.primary]} />
   ) as React.ReactElement<RefreshControlProps>
 
-  const homeTabTitleSection = {
-    data: [{}],
-    renderItem: () => (
-      <Text onLayout={handleMeasureTitleHeight} style={styles.homeTabTitle}>
-        {t('bottomTabsNavigator.home.title')}
-      </Text>
-    ),
-  }
-
   const notificationBoxSection = {
     data: [{}],
     renderItem: () => (
@@ -170,12 +138,7 @@ function TabHome({ navigation }: Props) {
     renderItem: () => <TransactionFeed key={'TransactionList'} />,
   }
 
-  const sections = [
-    homeTabTitleSection,
-    actionsCarouselSection,
-    notificationBoxSection,
-    transactionFeedSection,
-  ]
+  const sections = [actionsCarouselSection, notificationBoxSection, transactionFeedSection]
 
   return (
     <SafeAreaView testID="WalletHome" style={styles.container} edges={[]}>
@@ -203,13 +166,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     position: 'relative',
-  },
-  homeTabTitle: {
-    ...typeScale.titleMedium,
-    color: colors.black,
-    marginHorizontal: Spacing.Regular16,
-    marginTop: Spacing.Regular16,
-    marginBottom: Spacing.Large32,
   },
 })
 
