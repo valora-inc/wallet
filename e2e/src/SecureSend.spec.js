b7469efe64401733202569798b08972acb205ee8
Joseph Bergeron <jophish126@gmail.com>
2024-09-12 09:44:27 -0400

    Fix test

--- e2e/src/SecureSend.spec.js
+++ e2e/src/SecureSend.spec.js
@@ -1,6 +1,5 @@
 import SecureSend from './usecases/SecureSend'
 
 describe('Given', () => {
-  // TODO(mobilestack): Un-skip this usecase if we support CPV
-  // describe('SecureSend with CPV', SecureSend)
+  describe('SecureSend with CPV', SecureSend)
 })
