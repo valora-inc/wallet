b9cb0fc8613a73a9a1dc71d980ad0bd62e589110
Satish Ravi <satish.ravi@valoraapp.com>
2024-09-19 10:18:42 -0700

    feat(earn): consistent analytics props for deposit (#6044)
    ### Description
    
    As the title
    
    ### Test plan
    
    Unit tests
    
    ### Related issues
    
    - Fixes ACT-1358
    
    ### Backwards compatibility
    
    Yes
    
    ### Network scalability
    
    If a new NetworkId and/or Network are added in the future, the changes
    in this PR will:
    
    - [x] Continue to work without code changes, OR trigger a compilation
    error (guaranteeing we find it when a new network is added)

--- src/analytics/Properties.tsx
+++ src/analytics/Properties.tsx
@@ -1552,7 +1552,12 @@ interface EarnCommonProperties {
 }
 
 interface EarnDepositProperties extends EarnCommonProperties {
-  tokenAmount: string
+  depositTokenAmount: string
+  mode: EarnDepositMode
+  // the below are mainly for swap-deposit. For deposit, this would just be
+  // same as the depositTokenAmount and depositTokenId
+  fromTokenAmount: string
+  fromTokenId: string
 }
 
 interface EarnWithdrawProperties extends EarnCommonProperties {
@@ -1601,10 +1606,6 @@ interface EarnEventsProperties {
   [EarnEvents.earn_enter_amount_continue_press]: {
     amountInUsd: string
     amountEnteredIn: AmountEnteredIn
-    // TODO(ACT-1358): these could be moved to EarnDepositProperties
-    depositTokenAmount: string
-    sourceTokenId: string
-    mode: EarnDepositMode
   } & EarnDepositProperties
   [EarnEvents.earn_enter_amount_info_more_pools]: undefined
   [EarnEvents.earn_exit_pool_press]: {
