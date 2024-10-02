c6eed278338f08ad5d8cf7babfd0cce31d54786b
Alex Bakoushin <alex@bakoush.in>
2024-10-02 21:19:43 +0200

    temp disable most of the test suite

--- e2e/src/Verify.spec.js
+++
@@ -1,5 +0,0 @@
-import NewAccountPhoneVerification from './usecases/NewAccountPhoneVerification'
-
-describe('Given Phone Verification', () => {
-  describe('When New Account', NewAccountPhoneVerification)
-})
