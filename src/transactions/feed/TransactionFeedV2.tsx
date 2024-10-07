8bfe6d61dbd3a74d2809785af47c9e71ce69b821
Myroslav Sviderok <myroslav.sviderok@gmail.com>
2024-10-07 10:25:21 +0300

    Merge standBy transactions into the feed

--- src/transactions/feed/TransactionFeedV2.tsx
+++ src/transactions/feed/TransactionFeedV2.tsx
@@ -3,10 +3,12 @@ import { ActivityIndicator, SectionList, StyleSheet, View } from 'react-native'
 import SectionHead from 'src/components/SectionHead'
 import GetStarted from 'src/home/GetStarted'
 import { useSelector } from 'src/redux/hooks'
+import { RootState } from 'src/redux/reducers'
 import { getFeatureGate, getMultichainFeatures } from 'src/statsig'
 import { StatsigFeatureGates } from 'src/statsig/types'
 import colors from 'src/styles/colors'
 import { Spacing } from 'src/styles/styles'
+import { getSupportedNetworkIdsForApprovalTxsInHomefeed } from 'src/tokens/utils'
 import NoActivity from 'src/transactions/NoActivity'
 import { useTransactionFeedV2Query } from 'src/transactions/api'
 import EarnFeedItem from 'src/transactions/feed/EarnFeedItem'
@@ -14,9 +16,11 @@ import NftFeedItem from 'src/transactions/feed/NftFeedItem'
 import SwapFeedItem from 'src/transactions/feed/SwapFeedItem'
 import TokenApprovalFeedItem from 'src/transactions/feed/TokenApprovalFeedItem'
 import TransferFeedItem from 'src/transactions/feed/TransferFeedItem'
-import { pendingStandbyTransactionsSelector } from 'src/transactions/reducer'
-import { NetworkId, type TokenTransaction } from 'src/transactions/types'
-import { groupFeedItemsInSections } from 'src/transactions/utils'
+import { NetworkId, TransactionStatus, type TokenTransaction } from 'src/transactions/types'
+import {
+  groupFeedItemsInSections,
+  standByTransactionToTokenTransaction,
+} from 'src/transactions/utils'
 import { walletAddressSelector } from 'src/web3/selectors'
 
 type PaginatedData = {
@@ -29,6 +33,82 @@ const FIRST_PAGE_TIMESTAMP = 0
 
 function getAllowedNetworkIdsForTransfers() {
   return getMultichainFeatures().showTransfers.join(',').split(',') as NetworkId[]
+}
+
+/**
+ * Every page of paginated data includes a limited amount of transactions within a certain period.
+ * When the new page is added - we concatenate all the pages into a single array and sort them.
+ * Currently, the sorting is necessary to ensure that approvals are always shown first if the
+ * corresponding transaction has identical timestamp.
+ *
+ * In standByTransactions we might have transactions from months ago. Whenever we load a new page
+ * we only want to add those stand by transactions that are within the time period of the new page.
+ * Otherwise, if we merge all the stand by transactins into the page it will cause more late transactions
+ * that were already merged to be removed from the top of the list and move them to the bottom.
+ * This will cause the screen "shift".
+ */
+function mergeStandByTransactionsInRange(
+  transactions: TokenTransaction[],
+  standBy: TokenTransaction[]
+) {
+  if (transactions.length === 0) return []
+
+  const allowedNetworks = getAllowedNetworkIdsForTransfers()
+  const max = transactions[0].timestamp
+  const min = transactions.at(-1)!.timestamp
+
+  const standByInRange = standBy.filter((tx) => tx.timestamp >= min && tx.timestamp <= max)
+  const mergedTransactions = [...transactions, ...standByInRange].reduce(
+    (acc, tx) => {
+      if (!allowedNetworks.includes(tx.networkId)) return acc
+
+      if (!acc.used[tx.transactionHash]) {
+        acc.used[tx.transactionHash] = true
+        acc.list.push(tx)
+      }
+      return acc
+    },
+    { list: [] as TokenTransaction[], used: {} as { [hash: string]: true } }
+  )
+
+  const sortedTransactions = mergedTransactions.list.sort((a, b) => b.timestamp - a.timestamp)
+  return sortedTransactions
+}
+
+/**
+ * Current inmplementation of allStandbyTransactionsSelector has function
+ * getSupportedNetworkIdsForApprovalTxsInHomefeed in its' selectors list which triggers a lot of
+ * unnecessary re-renders. This can be avoided if we join it's result in a string and memoize it.
+ *
+ * Not using existing selectors for pending/confirmed stand by transaction only cause they are
+ * dependant on the un-memoized allStandbyTransactionsSelector selector which will break the flow
+ * of the new pagination management.
+ */
+function useStandByTransactions() {
+  const supportedNetworkrsForApproval = getSupportedNetworkIdsForApprovalTxsInHomefeed().join(',')
+  const standByTransactions = useSelector(
+    (state: RootState) => state.transactions.standbyTransactions
+  )
+
+  return useMemo(() => {
+    const networkIds = supportedNetworkrsForApproval.split(',') as NetworkId[]
+    return standByTransactions
+      .filter((tx) => {
+        return tx.__typename === 'TokenApproval' ? networkIds.includes(tx.networkId) : true
+      })
+      .reduce(
+        (acc, tx) => {
+          if (tx.status === TransactionStatus.Pending) {
+            acc.pending.push(standByTransactionToTokenTransaction(tx))
+          } else {
+            acc.confirmed.push(tx)
+          }
+
+          return acc
+        },
+        { pending: [] as TokenTransaction[], confirmed: [] as TokenTransaction[] }
+      )
+  }, [standByTransactions, supportedNetworkrsForApproval])
 }
 
 function renderItem({ item: tx }: { item: TokenTransaction; index: number }) {
@@ -52,7 +132,7 @@ function renderItem({ item: tx }: { item: TokenTransaction; index: number }) {
 
 export default function TransactionFeedV2() {
   const address = useSelector(walletAddressSelector)
-  const pendingStandByTransactions = useSelector(pendingStandbyTransactionsSelector)
+  const standByTransactions = useStandByTransactions()
   const [endCursor, setEndCursor] = useState(0)
   const [paginatedData, setPaginatedData] = useState<PaginatedData>({})
   const { pageData, isFetching, error } = useTransactionFeedV2Query(
@@ -103,16 +183,21 @@ export default function TransactionFeedV2() {
         return prev
       }
 
-      return { ...prev, [pageData.currentCursor]: pageData.transactions }
+      const transactions = mergeStandByTransactionsInRange(
+        pageData.transactions,
+        standByTransactions.confirmed
+      )
+
+      return { ...prev, [pageData.currentCursor]: transactions }
     })
-  }, [isFetching, pageData])
+  }, [isFetching, pageData, standByTransactions.confirmed])
 
   const pendingTransactions = useMemo(() => {
     const allowedNetworks = getAllowedNetworkIdsForTransfers()
-    return pendingStandByTransactions.filter((tx) => {
+    return standByTransactions.pending.filter((tx) => {
       return allowedNetworks.includes(tx.networkId)
     })
-  }, [pendingStandByTransactions])
+  }, [standByTransactions.pending])
 
   const confirmedTransactions = useMemo(() => {
     return Object.values(paginatedData)
