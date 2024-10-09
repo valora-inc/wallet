a98c1cccd8b0eef4953a24d33c6d4096fc8aee83
Finnian Jacobson-Schulte <140328381+finnian0826@users.noreply.github.com>
2024-10-09 12:00:19 -0600

    updates

--- src/tokens/PositionItem.tsx
+++ src/tokens/PositionItem.tsx
@@ -1,15 +1,17 @@
 import BigNumber from 'bignumber.js'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { TouchableWithoutFeedback } from 'react-native-gesture-handler'
+import { Platform, StyleSheet, Text, View } from 'react-native'
 import { useSelector } from 'react-redux'
 import AppAnalytics from 'src/analytics/AppAnalytics'
 import { AssetsEvents } from 'src/analytics/Events'
+import { openUrl } from 'src/app/actions'
 import LegacyTokenDisplay from 'src/components/LegacyTokenDisplay'
+import Touchable from 'src/components/Touchable'
 import { navigate } from 'src/navigator/NavigationService'
 import { Screens } from 'src/navigator/Screens'
 import { earnPositionIdsSelector } from 'src/positions/selectors'
 import { EarnPosition, Position } from 'src/positions/types'
+import { useDispatch } from 'src/redux/hooks'
 import Colors from 'src/styles/colors'
 import { typeScale } from 'src/styles/fonts'
 import { Spacing } from 'src/styles/styles'
@@ -23,6 +25,8 @@ export const PositionItem = ({
   position: Position
   hideBalances?: boolean
 }) => {
+  const dispatch = useDispatch()
+
   const balanceInDecimal =
     position.type === 'contract-position' ? undefined : new BigNumber(position.balance)
   const balanceUsd =
@@ -44,54 +48,54 @@ export const PositionItem = ({
     earnPositionIds.includes(position.positionId)
       ? navigate(Screens.EarnPoolInfoScreen, { pool: position as EarnPosition })
       : position.displayProps.manageUrl
-        ? navigate(Screens.WebViewScreen, {
-            uri: position.displayProps.manageUrl,
-          })
+        ? Platform.OS === 'android'
+          ? navigate(Screens.WebViewScreen, {
+              uri: position.displayProps.manageUrl,
+            })
+          : dispatch(openUrl(position.displayProps.manageUrl, true))
         : null
   }
 
   return (
-    <TouchableWithoutFeedback
-      testID="PositionItem"
-      style={styles.positionsContainer}
-      onPress={onPress}
-    >
-      <View style={styles.row}>
-        <PositionIcon position={position} />
+    <Touchable testID="PositionItem" style={styles.positionsContainer} onPress={onPress}>
+      <>
+        <View style={styles.row}>
+          <PositionIcon position={position} />
 
-        <View style={styles.tokenLabels}>
-          <Text style={styles.tokenName} numberOfLines={1}>
-            {position.displayProps.title}
-          </Text>
-          <Text style={styles.subtext}>{position.displayProps.description}</Text>
+          <View style={styles.tokenLabels}>
+            <Text style={styles.tokenName} numberOfLines={1}>
+              {position.displayProps.title}
+            </Text>
+            <Text style={styles.subtext}>{position.displayProps.description}</Text>
+          </View>
         </View>
-      </View>
-      {!hideBalances && (
-        <View style={styles.balances}>
-          {balanceUsd.gt(0) || balanceUsd.lt(0) ? (
-            <LegacyTokenDisplay
-              amount={balanceUsd}
-              currency={Currency.Dollar}
-              style={styles.tokenAmt}
-            />
-          ) : (
-            // If the balance is 0 / NaN, display a dash instead
-            // as it means we don't have a price for at least one of the underlying tokens
-            <Text style={styles.tokenAmt}>-</Text>
-          )}
-          {balanceInDecimal && (
-            <LegacyTokenDisplay
-              amount={balanceInDecimal}
-              // Hack to display the token balance without having said token in the base token list
-              currency={Currency.Celo}
-              style={styles.subtext}
-              showLocalAmount={false}
-              showSymbol={false}
-            />
-          )}
-        </View>
-      )}
-    </TouchableWithoutFeedback>
+        {!hideBalances && (
+          <View style={styles.balances}>
+            {balanceUsd.gt(0) || balanceUsd.lt(0) ? (
+              <LegacyTokenDisplay
+                amount={balanceUsd}
+                currency={Currency.Dollar}
+                style={styles.tokenAmt}
+              />
+            ) : (
+              // If the balance is 0 / NaN, display a dash instead
+              // as it means we don't have a price for at least one of the underlying tokens
+              <Text style={styles.tokenAmt}>-</Text>
+            )}
+            {balanceInDecimal && (
+              <LegacyTokenDisplay
+                amount={balanceInDecimal}
+                // Hack to display the token balance without having said token in the base token list
+                currency={Currency.Celo}
+                style={styles.subtext}
+                showLocalAmount={false}
+                showSymbol={false}
+              />
+            )}
+          </View>
+        )}
+      </>
+    </Touchable>
   )
 }
 
