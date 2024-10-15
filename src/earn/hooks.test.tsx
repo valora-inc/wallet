af46c0ddf11829b24246318d8d381a6c2f6afb03
Finnian Jacobson-Schulte <140328381+finnian0826@users.noreply.github.com>
2024-10-15 13:43:07 -0600

    fix unit tests

--- src/earn/hooks.test.tsx
+++ src/earn/hooks.test.tsx
@@ -1,13 +1,59 @@
-import BigNumber from 'bignumber.js'
+import { render } from '@testing-library/react-native'
+import React from 'react'
+import { Text, View } from 'react-native'
+import { Provider } from 'react-redux'
 import { useEarnPositionBalanceValues } from 'src/earn/hooks'
+import { EarnPosition } from 'src/positions/types'
+import { createMockStore } from 'test/utils'
 import { mockEarnPositions } from 'test/values'
 
+function TestComponent({ pools }: { pools: EarnPosition[] }) {
+  const poolBalances = useEarnPositionBalanceValues({ pools })
+
+  return (
+    <View>
+      <Text testID="poolBalanceInUsd">{poolBalances[0].poolBalanceInUsd.toNumber()}</Text>
+      <Text testID="poolBalanceInDepositToken">
+        {poolBalances[0].poolBalanceInDepositToken.toNumber()}
+      </Text>
+      {poolBalances[1]?.poolBalanceInUsd && (
+        <Text testID="poolBalanceInUsdSecondPool">
+          {poolBalances[1].poolBalanceInUsd.toNumber()}
+        </Text>
+      )}
+      {poolBalances[1]?.poolBalanceInDepositToken && (
+        <Text testID="poolBalanceInDepositTokenSecondPool">
+          {poolBalances[1].poolBalanceInDepositToken.toNumber()}
+        </Text>
+      )}
+    </View>
+  )
+}
+
 describe('useEarnPositionBalanceValues', () => {
-  it('should return the correct USD and depositToken crypto balances for a pool', () => {
-    const { poolBalanceInUsd, poolBalanceInDepositToken } = useEarnPositionBalanceValues({
-      pools: [{ ...mockEarnPositions[0], balance: '100' }],
-    })[0]
-    expect(poolBalanceInUsd).toEqual(new BigNumber(120))
-    expect(poolBalanceInDepositToken).toEqual(new BigNumber(110))
+  it('should return the correct USD and depositToken crypto balances for a single pool', () => {
+    const { getByTestId } = render(
+      <Provider store={createMockStore()}>
+        <TestComponent pools={[{ ...mockEarnPositions[0], balance: '100' }]} />
+      </Provider>
+    )
+    expect(getByTestId('poolBalanceInUsd').props.children).toEqual(120)
+    expect(getByTestId('poolBalanceInDepositToken').props.children).toEqual(110)
+  })
+  it('should return the correct USD and depositToken crypto balances for multiple pools', () => {
+    const { getByTestId } = render(
+      <Provider store={createMockStore()}>
+        <TestComponent
+          pools={[
+            { ...mockEarnPositions[0], balance: '100' },
+            { ...mockEarnPositions[1], balance: '2', priceUsd: '500' },
+          ]}
+        />
+      </Provider>
+    )
+    expect(getByTestId('poolBalanceInUsd').props.children).toEqual(120)
+    expect(getByTestId('poolBalanceInDepositToken').props.children).toEqual(110)
+    expect(getByTestId('poolBalanceInUsdSecondPool').props.children).toEqual(1000)
+    expect(getByTestId('poolBalanceInDepositTokenSecondPool').props.children).toEqual(2)
   })
 })
