b66cb70b3245ecb93afc09c3556000b8b3d9e2c2
Alex Bakoushin <alex@bakoush.in>
2024-10-16 16:49:30 +0200

    chore(e2e): re-enable home feed e2e test (#6158)
    Reverts #6101 as the GraphQL endpoint for Alfajores has been fixed.
    
    Context:
    https://valora-app.slack.com/archives/C01AEKH9XGX/p1728460043801219?thread_ts=1727801385.833739&cid=C01AEKH9XGX
    
    CI succeeds
    
    NA
    
    NA
    
    NA

---
+++ e2e/src/HomeFeed.spec.js
@@ -0,0 +1,45 @@
+import { quickOnboarding, waitForElementId } from './utils/utils'
+import { sleep } from '../../src/utils/sleep'
+import jestExpect from 'expect'
+
+beforeAll(async () => {
+  await quickOnboarding()
+})
+
+describe('Home Feed', () => {
+  it('should show correct information on tap of feed item', async () => {
+    // Load Wallet Home
+    await waitForElementId('WalletHome')
+    const items = await element(by.id('TransferFeedItem')).getAttributes()
+
+    // Tap top TransferFeedItem
+    await element(by.id('TransferFeedItem')).atIndex(0).tap()
+
+    // Assert on text based on elements returned earlier
+    const address = items.elements[0].label.split(' ')[0]
+    const amount = items.elements[0].label.match(/(\d+\.\d+)/)[1]
+    await expect(element(by.text(address)).atIndex(0)).toBeVisible()
+    await expect(element(by.text(`$${amount}`)).atIndex(0)).toBeVisible()
+  })
+
+  it('should load more items on scroll', async () => {
+    // Tap back button if present form previous test
+    try {
+      await element(by.id('BackChevron')).tap()
+    } catch {}
+
+    // Load Wallet Home
+    await waitForElementId('WalletHome')
+    const startingItems = await element(by.id('TransferFeedItem')).getAttributes()
+
+    // Scroll to bottom - Android will scroll forever so we set a static value
+    device.getPlatform() === 'ios'
+      ? await element(by.id('WalletHome/FlatList')).scrollTo('bottom')
+      : await element(by.id('WalletHome/FlatList')).scroll(2000, 'down')
+    await sleep(5000)
+
+    // Compare initial number of items to new number of items after scroll
+    const endingItems = await element(by.id('TransferFeedItem')).getAttributes()
+    jestExpect(endingItems.elements.length).toBeGreaterThan(startingItems.elements.length)
+  })
+})
