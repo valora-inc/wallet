08f21b0247f596dcfa998304932cfdd6bef2d615
Myroslav Sviderok <myroslav.sviderok@gmail.com>
2024-10-18 09:14:46 +0300

    fix typo

--- src/redux/reducersList.ts
+++ src/redux/reducersList.ts
@@ -21,7 +21,7 @@ import { recipientsReducer as recipients } from 'src/recipients/reducer'
 import { sendReducer as send } from 'src/send/reducers'
 import swapReducer from 'src/swap/slice'
 import tokenReducer from 'src/tokens/slice'
-import transactionsSlice from 'src/transactions/slice'
+import transactionsReducer from 'src/transactions/slice'
 import { reducer as walletConnect } from 'src/walletConnect/reducer'
 import { reducer as web3 } from 'src/web3/reducer'
 
@@ -32,7 +32,7 @@ export const reducersList = {
   alert,
   send,
   home,
-  transactions: transactionsSlice,
+  transactions: transactionsReducer,
   web3,
   identity,
   account,
