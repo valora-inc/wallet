26d57378f43b80076c56bbc0aaa75059759ef423
Tom McGuire <Mcgtom10@gmail.com>
2024-10-21 12:29:42 -0700

    style: use Spacing instead of variables.contentPadding

--- src/earn/poolInfoScreen/EarnPoolInfoScreen.tsx
+++ src/earn/poolInfoScreen/EarnPoolInfoScreen.tsx
@@ -546,7 +546,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
-    padding: variables.contentPadding,
+    padding: Spacing.Thick24,
   },
   learnMoreText: {
     ...typeScale.labelSemiBoldSmall,
