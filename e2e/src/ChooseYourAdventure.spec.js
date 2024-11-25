0631acabb2ea852b68d336b1cd79d3841d03f782
Tom McGuire <Mcgtom10@gmail.com>
2024-11-25 14:56:22 -0800

    test(e2e): move cya to it's own spec to prevent long runs

---
+++ e2e/src/ChooseYourAdventure.spec.js
@@ -0,0 +1,57 @@
+import { launchApp } from './utils/retries'
+import { quickOnboarding, waitForElementByText, waitForElementId } from './utils/utils'
+
+describe('Choose Your Adventure', () => {
+  beforeEach(async () => {
+    await device.uninstallApp()
+    await device.installApp()
+    await launchApp({
+      newInstance: true,
+      launchArgs: {
+        onboardingOverrides: 'EnableBiometry,ProtectWallet,PhoneVerification,CloudBackup',
+      },
+    })
+    await quickOnboarding({ stopOnCYA: true, cloudBackupEnabled: true })
+  })
+
+  it('learn about points navigates to points journey page', async () => {
+    await waitForElementByText({ text: `Learn about Valora Points`, tap: true })
+
+    // Check that we are on the Points journey page
+    await waitForElementByText({ text: 'Earn points effortlessly', index: 0 })
+
+    // Back should go to the home screen
+    await element(by.id('BackChevron')).tap()
+    await waitForElementId('HomeAction-Send')
+  })
+
+  it('build your profile navigates to profile page', async () => {
+    await waitForElementByText({ text: 'Build your profile', tap: true })
+
+    // Check that we are on the profile page
+    await waitForElementId('ProfileEditName')
+
+    // Back should go to the home screen
+    await element(by.id('BackButton')).tap()
+    await waitForElementId('HomeAction-Send')
+  })
+
+  it('add funds to your wallet navigates to home and opens the token bottom sheet', async () => {
+    await waitForElementByText({ text: 'Add funds to your wallet', tap: true })
+
+    // Check that we are on the bottom sheet
+    await waitForElementId('TokenBottomSheet')
+
+    // dismissing the bottom sheet should show home screen
+    await element(by.id('TokenBottomSheet')).swipe('down')
+    await waitForElementId('HomeAction-Send')
+  })
+
+  it('explore earning opportunities navigates to stablecoins info page', async () => {
+    await waitForElementByText({ text: 'Explore earning opportunities', tap: true })
+
+    await waitForElementId('EarnInfoScreen/Title')
+    // Check that we are on the Earn On Your Stablecoins page
+    await expect(element(by.text('Earn on your\ncrypto')).atIndex(0)).toBeVisible()
+  })
+})
