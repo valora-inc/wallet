d77be26d113fd7451939d4bd046c8e119701e397
Tom McGuire <Mcgtom10@gmail.com>
2024-11-26 14:10:43 -0800

    test(e2e): remove url blocklist

--- e2e/src/utils/retries.js
+++ e2e/src/utils/retries.js
@@ -25,9 +25,7 @@ export const launchApp = async (customArgs = {}) => {
       }
     },
     { retries: 5, delay: 10 * 1000, timeout: 30 * 10000 }
-  ).then(async () => {
-    await device.setURLBlacklist(['.*blockchain-api-dot-celo-mobile-mainnet.*'])
-  })
+  )
 }
 
 export const reloadReactNative = async () => {
