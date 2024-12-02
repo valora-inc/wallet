295b9f5282fd60e7d0fa99f3d855ac06b9c0cdaf
Myroslav Sviderok <myroslav.sviderok@gmail.com>
2024-12-02 14:16:45 +0200

    feat(TokenEnterAmount): add new flow to EnterAmount.tsx (#6245)
    ### Description
    3/5 PR for new Enter Amount component. This PR uses the new component
    and hook in the Send flow. A lot of tests are adjusted to follow the new
    flow.
    
    `AmountInput` component was moved purely for compatibility with
    `EarnEnterAmount` component. This is an outdated boilerplate code which
    will be removed in the next PR (4/5). Most of the line changes is this
    specific component and a lot of tests' adjusting. There's really not
    many changes other than replacing boilerplate code with the same code
    from a reusable hook.
    
    ### Test plan
    Changes a bunch of test files to relate to the new changes. All the
    tests related to `MAX` button are skipped for now.
    
    <video
    src="https://github.com/user-attachments/assets/6d3e286e-393a-4061-85e8-6f5d04713dba"
    width="500"></video>
    
    ### Related issues
    
    - Relates to RET-1208
    ### Backwards compatibility
    
    Yes
    ### Network scalability
    
    If a new NetworkId and/or Network are added in the future, the changes
    in this PR will:
    
    - [x] Continue to work without code changes, OR trigger a compilation
    error (guaranteeing we find it when a new network is added)
    
    ---------
    
    Co-authored-by: Kathy Luo <kathyluo18@gmail.com>

--- src/components/TokenEnterAmount.test.tsx
+++ src/components/TokenEnterAmount.test.tsx
@@ -13,6 +13,8 @@ import TokenEnterAmount, {
   formatNumber,
   getDisplayLocalAmount,
   getDisplayTokenAmount,
+  roundFiatValue,
+  unformatNumberForProcessing,
   useEnterAmount,
 } from './TokenEnterAmount'
 
@@ -65,6 +67,15 @@ describe('TokenEnterAmount', () => {
       .mocked(getNumberFormatSettings)
       .mockReturnValue({ decimalSeparator: '.', groupingSeparator: ',' })
 
+    expect(unformatNumberForProcessing('')).toBe('')
+    expect(unformatNumberForProcessing('0.25')).toBe('0.25')
+    expect(unformatNumberForProcessing('1,234.34567')).toBe('1234.34567')
+
+    expect(roundFiatValue(null)).toBe('')
+    expect(roundFiatValue(new BigNumber('0.01'))).toBe('0.01')
+    expect(roundFiatValue(new BigNumber('0.000001'))).toBe('0.000001')
+    expect(roundFiatValue(new BigNumber('1234.34567'))).toBe('1234.35')
+
     expect(formatNumber('')).toBe('')
     expect(formatNumber('123')).toBe('123')
     expect(formatNumber('1234')).toBe('1,234')
@@ -73,8 +84,6 @@ describe('TokenEnterAmount', () => {
     expect(formatNumber('123456789012345')).toBe('123,456,789,012,345')
     expect(formatNumber('12.34567')).toBe('12.34567')
     expect(formatNumber('-1234567.89')).toBe('-1,234,567.89')
-    expect(formatNumber('1234abc')).toBe('1,234abc')
-    expect(formatNumber('1234.56abc')).toBe('1,234.56abc')
 
     const { token } = defaultProps
     expect(getDisplayTokenAmount(null, token)).toBe('')
@@ -107,6 +116,15 @@ describe('TokenEnterAmount', () => {
       .mocked(getNumberFormatSettings)
       .mockReturnValue({ decimalSeparator: ',', groupingSeparator: '.' })
 
+    expect(unformatNumberForProcessing('')).toBe('')
+    expect(unformatNumberForProcessing('0,25')).toBe('0.25')
+    expect(unformatNumberForProcessing('1.234,34567')).toBe('1234.34567')
+
+    expect(roundFiatValue(null)).toBe('')
+    expect(roundFiatValue(new BigNumber('0.01'))).toBe('0.01')
+    expect(roundFiatValue(new BigNumber('0.000001'))).toBe('0.000001')
+    expect(roundFiatValue(new BigNumber('1234.34567'))).toBe('1234.35')
+
     expect(formatNumber('')).toBe('')
     expect(formatNumber('123')).toBe('123')
     expect(formatNumber('1234')).toBe('1.234')
@@ -115,8 +133,6 @@ describe('TokenEnterAmount', () => {
     expect(formatNumber('123456789012345')).toBe('123.456.789.012.345')
     expect(formatNumber('12.34567')).toBe('12,34567')
     expect(formatNumber('-1234567.89')).toBe('-1.234.567,89')
-    expect(formatNumber('1234abc')).toBe('1.234abc')
-    expect(formatNumber('1234.56abc')).toBe('1.234,56abc')
 
     const { token } = defaultProps
     expect(getDisplayTokenAmount(null, token)).toBe('')
@@ -163,6 +179,7 @@ describe('TokenEnterAmount', () => {
           decimals: 6,
           tokenId: mockUSDCTokenId,
           priceUsd: new BigNumber(1.001),
+          balance: new BigNumber(1500.76),
         } as TokenBalance,
       })
 
@@ -173,12 +190,10 @@ describe('TokenEnterAmount', () => {
       expect(result.current.amount).toBe('1234.678')
       expect(result.current.processedAmounts).toStrictEqual({
         token: {
-          amount: '1234.678',
           bignum: new BigNumber('1234.678'),
           displayAmount: '1,234.678 USDC',
         },
         local: {
-          amount: '1235.91',
           bignum: new BigNumber('1235.912678'),
           displayAmount: '$1,235.91',
         },
@@ -193,6 +208,7 @@ describe('TokenEnterAmount', () => {
           decimals: 6,
           tokenId: mockUSDCTokenId,
           priceUsd: new BigNumber(1.001),
+          balance: new BigNumber(1500.76),
         } as TokenBalance,
       })
 
@@ -203,12 +219,10 @@ describe('TokenEnterAmount', () => {
       expect(result.current.amount).toBe('1234.67')
       expect(result.current.processedAmounts).toStrictEqual({
         local: {
-          amount: '1234.67',
           bignum: new BigNumber('1234.67'),
           displayAmount: '$1,234.67',
         },
         token: {
-          amount: '1233.436563',
           bignum: new BigNumber('1233.436563'),
           displayAmount: '1,233.436563 USDC',
         },
