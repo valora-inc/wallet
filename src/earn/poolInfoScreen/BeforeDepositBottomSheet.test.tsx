fad126def5b53b5681135425a7511fc8f95c46a2
Finnian Jacobson-Schulte <140328381+finnian0826@users.noreply.github.com>
2024-12-11 08:07:54 -0700

    clean up tests more

--- src/earn/poolInfoScreen/BeforeDepositBottomSheet.test.tsx
+++ src/earn/poolInfoScreen/BeforeDepositBottomSheet.test.tsx
@@ -2,8 +2,6 @@ import { fireEvent, render } from '@testing-library/react-native'
 import BigNumber from 'bignumber.js'
 import React from 'react'
 import { Provider } from 'react-redux'
-import AppAnalytics from 'src/analytics/AppAnalytics'
-import { EarnEvents } from 'src/analytics/Events'
 import BeforeDepositBottomSheet from 'src/earn/poolInfoScreen/BeforeDepositBottomSheet'
 import { CICOFlow } from 'src/fiatExchanges/types'
 import { navigate } from 'src/navigator/NavigationService'
@@ -58,7 +56,7 @@ describe('BeforeDepositBottomSheet', () => {
     expect(getByTestId('Earn/ActionCard/AddMore')).toBeTruthy()
   })
   it('show bottom sheet correctly when hasDepositToken is true, token(s) on differnet chain', () => {
-    const { getByText, getByTestId } = render(
+    const { getByTestId } = render(
       <Provider store={createMockStore()}>
         <BeforeDepositBottomSheet
           forwardedRef={{ current: null }}
@@ -72,17 +70,6 @@ describe('BeforeDepositBottomSheet', () => {
         />
       </Provider>
     )
-    fireEvent.press(getByText('earnFlow.poolInfoScreen.deposit'))
-    expect(AppAnalytics.track).toHaveBeenCalledWith(EarnEvents.earn_pool_info_tap_deposit, {
-      providerId: 'aave',
-      poolId: 'arbitrum-sepolia:0x460b97bd498e1157530aeb3086301d5225b91216',
-      networkId: 'arbitrum-sepolia',
-      depositTokenId: mockEarnPositions[0].dataProps.depositTokenId,
-      hasDepositToken: true,
-      hasTokensOnSameNetwork: false,
-      hasTokensOnOtherNetworks: true,
-    })
-
     expect(getByTestId('Earn/ActionCard/Deposit')).toBeTruthy()
     expect(getByTestId('Earn/ActionCard/Swap')).toBeTruthy()
     expect(getByTestId('Earn/ActionCard/AddMore')).toBeTruthy()
@@ -184,7 +171,6 @@ describe('BeforeDepositBottomSheet', () => {
     expect(navigate).toHaveBeenCalledWith(Screens.EarnEnterAmount, {
       pool: mockEarnPositions[0],
     })
-    expect(AppAnalytics.track).toHaveBeenCalledTimes(2)
   })
 
   it('navigates correctly when swap and deposit action item is tapped', () => {
@@ -208,7 +194,6 @@ describe('BeforeDepositBottomSheet', () => {
       pool: mockEarnPositions[0],
       mode: 'swap-deposit',
     })
-    expect(AppAnalytics.track).toHaveBeenCalledTimes(2)
   })
 
   it('navigates correctly when cross chain swap action item is tapped', () => {
@@ -231,7 +216,6 @@ describe('BeforeDepositBottomSheet', () => {
     expect(navigate).toHaveBeenCalledWith(Screens.SwapScreenWithBack, {
       toTokenId: mockEarnPositions[0].dataProps.depositTokenId,
     })
-    expect(AppAnalytics.track).toHaveBeenCalledTimes(2)
   })
 
   it('navigates correctly when add more action item is tapped', () => {
@@ -256,7 +240,6 @@ describe('BeforeDepositBottomSheet', () => {
       flow: CICOFlow.CashIn,
       tokenSymbol: 'USDC',
     })
-    expect(AppAnalytics.track).toHaveBeenCalledTimes(2)
   })
 
   it('navigates correctly when add action item is tapped', () => {
@@ -281,7 +264,6 @@ describe('BeforeDepositBottomSheet', () => {
       flow: CICOFlow.CashIn,
       tokenSymbol: 'USDC',
     })
-    expect(AppAnalytics.track).toHaveBeenCalledTimes(2)
   })
 
   it('navigates correctly when swap action item is tapped', () => {
@@ -304,7 +286,6 @@ describe('BeforeDepositBottomSheet', () => {
     expect(navigate).toHaveBeenCalledWith(Screens.SwapScreenWithBack, {
       toTokenId: mockEarnPositions[0].dataProps.depositTokenId,
     })
-    expect(AppAnalytics.track).toHaveBeenCalledTimes(2)
   })
 
   it('navigates correctly when transfer action item is tapped', () => {
@@ -328,6 +309,5 @@ describe('BeforeDepositBottomSheet', () => {
       flow: CICOFlow.CashIn,
       exchanges: [],
     })
-    expect(AppAnalytics.track).toHaveBeenCalledTimes(2)
   })
 })
