valora-inc/wallet

commit 10967879aeeb8270cdddcea22b9871daac79ad98

chore(naming): Remove (almost) all in-code references to Valora (#5692)
### Description

Second step in
[RET-1154](https://linear.app/valora/issue/RET-1154/%5Bvalora%5D-remove-in-code-valora-references).
Refactors the repository, replacing in-code references to `Valora` with
generic counterparts. There were a couple of kinds of things that needed
to change:

* Variable, type, etc. names - these were straightforward changes
* String literal values. In cases where these can be changed in-place
(where the actual value of `"valora"` doesn't matter), they are. In
other places where the actual `"valora"` value is relevant, the value
has been pulled out into a config variable, so that it can potentially
be modified down the line in MS apps.

Some references to valora in the app were _not_ changed, however. This
is mainly evident in the deeplink/universal link feature, where pulling
those out to be configurable will be done in a separate ticket. Certain
code comments that link to Valora repos/Slack threads also remain.

You can get a decent feeling of the valora references that remain with
the following `grep` command:

```bash
grep -v -w "//" -r src/ --exclude=\*networkConfig.ts --exclude=\*config.ts --exclude=\*brandingConfig.ts | grep -i -e "valora"
```

Which gives the following output:

```
src/send/hooks.test.tsx:import { NameResolution, ResolutionKind } from '@valora/resolve-kit'
src/send/hooks.ts:import { NameResolution, ResolutionKind } from '@valora/resolve-kit'
src/components/BottomSheetLegacy.tsx: * @deprecated use https://www.npmjs.com/package/@gorhom/bottom-sheet instead, see example here https://github.com/valora-inc/wallet/blob/09c4d4ce98181480f1ed0a299709070d535221ad/src/dappsExplorer/useDappInfoBottomSheet.tsx#L47
src/analytics/InjectTraits.ts: * TODO eventually: remove dependencies and delete this plugin, see https://linear.app/valora/issue/ENG-60/remove-dependencies-on-context-traits-

src/fiatExchanges/BidaliScreen.tsx
@@ -41,7 +41,7 @@ function useInitialJavaScript(
     // When a new url needs to be open (currently for FAQ, Terms of Service), `openUrl` is called by Bidali.
     // See also the comment in the `onMessage` handler
     setInitialJavaScript(`
-      window.valora = {
+      window.walletApp = {
         paymentCurrency: "${currency.toUpperCase()}",
         phoneNumber: ${JSON.stringify(e164PhoneNumber)},
         balances: ${jsonBalances},
@@ -84,10 +84,10 @@ function BidaliScreen({ route, navigation }: Props) {
         // These 2 callbacks needs to be called to notify Bidali of the status of the payment request
         // so it can update the WebView accordingly.
         const onPaymentSent = () => {
-          webViewRef.current?.injectJavaScript(`window.valora.paymentSent();`)
+          webViewRef.current?.injectJavaScript(`window.walletApp.paymentSent();`)
         }
         const onCancelled = () => {
-          webViewRef.current?.injectJavaScript(`window.valora.paymentCancelled();`)
+          webViewRef.current?.injectJavaScript(`window.walletApp.paymentCancelled();`)
         }
         dispatch(
           bidaliPaymentRequested(
@@ -135,7 +135,7 @@ function BidaliScreen({ route, navigation }: Props) {
   // Update balances when they change
   useEffect(() => {
     webViewRef.current?.injectJavaScript(`
-      window.valora.balances = ${jsonBalances}
+      window.walletApp.balances = ${jsonBalances}
     `)
   }, [jsonBalances])
 

src/fiatExchanges/BidaliScreen.test.tsx
@@ -14,14 +14,14 @@ const mockScreenProps = getMockStackScreenProps(Screens.BidaliScreen, {
 
 declare global {
   interface Window {
-    valora: any
+    walletApp: any
   }
 }
 
 describe(BidaliScreen, () => {
   beforeEach(() => {
     // Reset injected JS effect
-    window.valora = undefined
+    window.walletApp = undefined
   })
 
   it('renders correctly when no phone number is provided', () => {
@@ -51,7 +51,7 @@ describe(BidaliScreen, () => {
     expect(webView).toBeDefined()
     // eslint-disable-next-line no-eval
     expect(eval(webView.props.injectedJavaScriptBeforeContentLoaded)).toBe(true)
-    expect(window.valora).toMatchInlineSnapshot(`
+    expect(window.walletApp).toMatchInlineSnapshot(`
       {
         "balances": {
           "CEUR": "5",
@@ -91,7 +91,7 @@ describe(BidaliScreen, () => {
     expect(webView).toBeDefined()
     // eslint-disable-next-line no-eval
     expect(eval(webView.props.injectedJavaScriptBeforeContentLoaded)).toBe(true)
-    expect(window.valora).toMatchInlineSnapshot(`
+    expect(window.walletApp).toMatchInlineSnapshot(`
       {
         "balances": {
           "CEUR": "5",
@@ -136,7 +136,7 @@ describe(BidaliScreen, () => {
     // eslint-disable-next-line no-eval
     expect(eval(webView.props.injectedJavaScriptBeforeContentLoaded)).toBe(true)
     // `paymentCurrency` is CEUR here because it has the highest balance in the local currency
-    expect(window.valora).toMatchInlineSnapshot(`
+    expect(window.walletApp).toMatchInlineSnapshot(`
       {
         "balances": {
           "CEUR": "9",

commit 14214947a293ba05aa27b5d682cf7dbd3409ee55

fix: broken custom payment with Bidali (#6383)
### Description

This fixes the custom payment integration with Bidali that was broken by
https://github.com/valora-inc/wallet/pull/5692
Because it renamed `window.valora` to `window.walletApp`.

Note: it would be nice to make this Bidali payment flow work multichain
too. Right now it's only for the Celo network.

### Test plan

**Before**


https://github.com/user-attachments/assets/f2d0946a-376e-4b2b-b029-d25e5659ef74

It's not showing the custom payment integration. Only WalletConnect or
the QR code (which is not really usable as is in the mobile-to-mobile
flow).
Note: the payment QR code causes an incorrect conversion, but that's
another story, we'll tackle that separately.

**After**


https://github.com/user-attachments/assets/abe723c9-0a33-4737-81ac-de1834ab02b9

It's showing the custom payment integration "Pay with Valora".

### Related issues

- Part of RET-1292

### Backwards compatibility

Yes

### Network scalability

If a new NetworkId and/or Network are added in the future, the changes
in this PR will:

- [x] Continue to work without code changes, OR trigger a compilation
error (guaranteeing we find it when a new network is added)

src/fiatExchanges/BidaliScreen.tsx
@@ -40,8 +40,9 @@ function useInitialJavaScript(
     // When a payment request is needed, Bidali calls the provided `onPaymentRequest` method.
     // When a new url needs to be open (currently for FAQ, Terms of Service), `openUrl` is called by Bidali.
     // See also the comment in the `onMessage` handler
+    // IMPORTANT: DO NOT RENAME window.valora to anything else, it is used by Bidali for the custom integration.
     setInitialJavaScript(`
-      window.walletApp = {
+      window.valora = {
         paymentCurrency: "${currency.toUpperCase()}",
         phoneNumber: ${JSON.stringify(e164PhoneNumber)},
         balances: ${jsonBalances},
@@ -84,10 +85,10 @@ function BidaliScreen({ route, navigation }: Props) {
         // These 2 callbacks needs to be called to notify Bidali of the status of the payment request
         // so it can update the WebView accordingly.
         const onPaymentSent = () => {
-          webViewRef.current?.injectJavaScript(`window.walletApp.paymentSent();`)
+          webViewRef.current?.injectJavaScript(`window.valora.paymentSent();`)
         }
         const onCancelled = () => {
-          webViewRef.current?.injectJavaScript(`window.walletApp.paymentCancelled();`)
+          webViewRef.current?.injectJavaScript(`window.valora.paymentCancelled();`)
         }
         dispatch(
           bidaliPaymentRequested(
@@ -135,7 +136,7 @@ function BidaliScreen({ route, navigation }: Props) {
   // Update balances when they change
   useEffect(() => {
     webViewRef.current?.injectJavaScript(`
-      window.walletApp.balances = ${jsonBalances}
+      window.valora.balances = ${jsonBalances};
     `)
   }, [jsonBalances])
 

src/fiatExchanges/BidaliScreen.test.tsx
@@ -14,14 +14,14 @@ const mockScreenProps = getMockStackScreenProps(Screens.BidaliScreen, {
 
 declare global {
   interface Window {
-    walletApp: any
+    valora: any
   }
 }
 
 describe(BidaliScreen, () => {
   beforeEach(() => {
     // Reset injected JS effect
-    window.walletApp = undefined
+    window.valora = undefined
   })
 
   it('renders correctly when no phone number is provided', () => {
@@ -51,7 +51,7 @@ describe(BidaliScreen, () => {
     expect(webView).toBeDefined()
     // eslint-disable-next-line no-eval
     expect(eval(webView.props.injectedJavaScriptBeforeContentLoaded)).toBe(true)
-    expect(window.walletApp).toMatchInlineSnapshot(`
+    expect(window.valora).toMatchInlineSnapshot(`
       {
         "balances": {
           "CEUR": "5",
@@ -91,7 +91,7 @@ describe(BidaliScreen, () => {
     expect(webView).toBeDefined()
     // eslint-disable-next-line no-eval
     expect(eval(webView.props.injectedJavaScriptBeforeContentLoaded)).toBe(true)
-    expect(window.walletApp).toMatchInlineSnapshot(`
+    expect(window.valora).toMatchInlineSnapshot(`
       {
         "balances": {
           "CEUR": "5",
@@ -136,7 +136,7 @@ describe(BidaliScreen, () => {
     // eslint-disable-next-line no-eval
     expect(eval(webView.props.injectedJavaScriptBeforeContentLoaded)).toBe(true)
     // `paymentCurrency` is CEUR here because it has the highest balance in the local currency
-    expect(window.walletApp).toMatchInlineSnapshot(`
+    expect(window.valora).toMatchInlineSnapshot(`
       {
         "balances": {
           "CEUR": "9",