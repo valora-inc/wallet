fdd9e6fefede33555ec64211786a9c9e1f8d2bd2
Finnian Jacobson-Schulte <140328381+finnian0826@users.noreply.github.com>
2024-12-23 10:45:02 -0700

    add in description and cta label

--- src/components/GasFeeWarning.tsx
+++ src/components/GasFeeWarning.tsx
@@ -24,6 +24,22 @@ function GasFeeWarning({
 }) {
   const { t } = useTranslation()
 
+  const flowToActionString = {
+    [GasFeeWarningFlow.Send]: t('gasFeeWarning.actions.sending'),
+    [GasFeeWarningFlow.Swap]: t('gasFeeWarning.actions.swapping'),
+    [GasFeeWarningFlow.Deposit]: t('gasFeeWarning.actions.depositing'),
+    [GasFeeWarningFlow.Withdraw]: t('gasFeeWarning.actions.withdrawing'),
+    [GasFeeWarningFlow.Dapp]: undefined,
+  }
+
+  const flowToVerbString = {
+    [GasFeeWarningFlow.Send]: t('gasFeeWarning.verb.send'),
+    [GasFeeWarningFlow.Swap]: t('gasFeeWarning.verb.swap'),
+    [GasFeeWarningFlow.Deposit]: t('gasFeeWarning.verb.deposit'),
+    [GasFeeWarningFlow.Withdraw]: t('gasFeeWarning.verb.withdraw'),
+    [GasFeeWarningFlow.Dapp]: undefined,
+  }
+
   if (prepareTransactionsResult.type === 'possible') {
     return null
   }
@@ -34,16 +50,39 @@ function GasFeeWarning({
     prepareTransactionsResult.type === 'not-enough-balance-for-gas'
       ? prepareTransactionsResult.feeCurrencies[0].symbol
       : prepareTransactionsResult.feeCurrency.symbol
+
+  const amountToAdd = flow === GasFeeWarningFlow.Dapp ? 0 : undefined // TODO: Get amount here
+
+  const title =
+    flow === GasFeeWarningFlow.Dapp
+      ? t('gasFeeWarning.titleDapp')
+      : t('gasFeeWarning.title', { feeTokenSymbol: feeCurrencySymbol })
+  const description =
+    flow === GasFeeWarningFlow.Dapp
+      ? t('gasFeeWarning.descriptionDapp', { amount: amountToAdd, tokenSymbol: feeCurrencySymbol })
+      : prepareTransactionsResult.type === 'not-enough-balance-for-gas'
+        ? t('gasFeeWarning.descriptionNotEnoughGas', {
+            action: flowToActionString[flow],
+            tokenSymbol: feeCurrencySymbol,
+          })
+        : t('gasWarning.descriptionMaxAmount', {
+            action: flowToActionString[flow],
+            tokenSymbol: feeCurrencySymbol,
+          })
+  const ctaLabel =
+    flow === GasFeeWarningFlow.Dapp
+      ? undefined
+      : prepareTransactionsResult.type === 'not-enough-balance-for-gas'
+        ? t('gasFeeWarning.cta', { tokenSymbol: feeCurrencySymbol })
+        : t('gasWarning.ctaGasToken', {
+            verb: flowToVerbString[flow],
+          })
   return (
     <InLineNotification
       variant={NotificationVariant.Warning}
-      title={
-        flow === GasFeeWarningFlow.Dapp
-          ? t('gasFeeWarning.titleDapp')
-          : t('gasFeeWarning.title', { feeTokenSymbol: feeCurrencySymbol })
-      }
-      description={}
-      ctaLabel={}
+      title={title}
+      description={description}
+      ctaLabel={ctaLabel}
       onPressCta={onPressCta}
       style={styles.warning}
       testID="EarnEnterAmount/NotEnoughForGasWarning"
