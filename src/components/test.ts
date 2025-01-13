17dd5899ecc0fde92647f6260454b705bbc3bfd9
Kathy Luo <kathyluo18@gmail.com>
2025-01-13 11:43:22 +0100

    fix: remove test

--- src/components/test.ts
+++
@@ -1,9 +0,0 @@
-function groupNumber(value: string) {
-  return value.replace(/\B(?=(\d{3})+(?!\d))(?<!\.\d*)/g, 'group')
-}
-
-function actualTest(value: string, decimalSeparator: string, groupingSeparator: string) {
-  return groupNumber(value).replaceAll('.', decimalSeparator).replaceAll('group', groupingSeparator)
-}
-
-console.log(actualTest('1.234.567,9999', ',', '.')) // 1,234,567.9999
