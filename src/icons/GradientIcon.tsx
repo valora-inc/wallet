2fefba8999473474f06dd4d3ae7dfcc8ca16c618
Kathy Luo <kathyluo18@gmail.com>
2025-01-17 15:35:33 +0100

    fix(colors): avoid hard coded colors in GradientIcon

--- src/icons/GradientIcon.tsx
+++ src/icons/GradientIcon.tsx
@@ -10,6 +10,15 @@ interface Props {
 }
 
 export default function GradientIcon({ radius = 50, borderWidth = 1, children }: Props) {
+  const gradientBackgroundStyles = [
+    styles.background,
+    {
+      borderRadius: radius,
+      height: radius - borderWidth * 2,
+      width: radius - borderWidth * 2,
+    },
+  ]
+
   return (
     <View
       style={[
@@ -31,19 +40,14 @@ export default function GradientIcon({ radius = 50, borderWidth = 1, children }:
         />
       )}
       <LinearGradient
-        colors={['#e8fbf2', '#fffaea']}
+        colors={[Colors.gradientBorderLeft, Colors.gradientBorderRight]}
         locations={[0.1085, 1]}
         useAngle={true}
         angle={90}
-        style={[
-          styles.background,
-          {
-            borderRadius: radius,
-            height: radius - borderWidth * 2,
-            width: radius - borderWidth * 2,
-          },
-        ]}
+        style={gradientBackgroundStyles}
       />
+      {/* Apply a semi-transparent white overlay to tone down the brand gradient. react-native-linear-gradient doesn't support semi-transparent gradients with different colors. */}
+      <View style={[gradientBackgroundStyles, styles.mask]} />
       {children}
     </View>
   )
@@ -58,5 +62,11 @@ const styles = StyleSheet.create({
     position: 'absolute',
     width: '100%',
     height: '100%',
+  },
+  mask: {
+    // White is intentionally hardcoded as it effectively softens the gradient
+    // without altering its color balance, ensuring the design remains consistent.
+    backgroundColor: 'white',
+    opacity: 0.9,
   },
 })
