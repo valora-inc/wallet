9dfca43f6b3e007a1c0aed5779b9734c9148f2fc
Tom McGuire <mcgtom10@gmail.com>
2025-01-30 11:43:29 -0800

    chore: remove default props from back button

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -3,25 +3,28 @@ import { StyleSheet, View } from 'react-native'
 import BackChevron, { Props as BackChevronProps } from 'src/icons/BackChevron'
 import { navigateBack } from 'src/navigator/NavigationService'
 import { TopBarIconButton, TopBarIconButtonProps } from 'src/navigator/TopBarButton'
+import Colors from 'src/styles/colors'
 import { Spacing } from 'src/styles/styles'
 
 type Props = Omit<TopBarIconButtonProps, 'icon'> & BackChevronProps
 
-function BackButton(props: Props) {
+function BackButton({
+  onPress = navigateBack,
+  color = Colors.contentPrimary,
+  height = 16,
+  style,
+  ...props
+}: Props) {
   return (
-    <View style={[styles.container, props.style]}>
+    <View style={[styles.container, style]}>
       <TopBarIconButton
         {...props}
         style={styles.button}
-        icon={<BackChevron color={props.color} height={props.height} />}
+        icon={<BackChevron color={color} height={height} />}
+        onPress={onPress}
       />
     </View>
   )
-}
-
-BackButton.defaultProps = {
-  onPress: navigateBack,
-  ...BackChevron.defaultProps,
 }
 
 const styles = StyleSheet.create({
@@ -34,6 +37,7 @@ const styles = StyleSheet.create({
     // I tried removing the parent view, but it didn't help either
     paddingHorizontal: Spacing.Regular16,
     paddingVertical: Spacing.Small12, // vertical padding slightly smaller so the ripple effect isn't cut off
+    paddingVertical: Spacing.Small12,
     left: -Spacing.Regular16,
   },
 })
