6c5b7017b0b9d38075433904b2efb722c6fb1c3e
Kathy Luo <kathyluo18@gmail.com>
2025-02-04 14:30:59 +0100

    chore: reduce changes

--- src/i18n/saga.ts
+++ src/i18n/saga.ts
@@ -17,52 +17,50 @@ import { call, put, select, spawn, takeLatest } from 'typed-redux-saga'
 
 const TAG = 'i18n/saga'
 const otaClient = new OtaClient(CROWDIN_DISTRIBUTION_HASH)
+const allowOtaTranslations = ENABLE_OTA_TRANSLATIONS
 
 export function* handleFetchOtaTranslations() {
-  if (!ENABLE_OTA_TRANSLATIONS) {
-    Logger.debug(TAG, 'OTA translations disabled')
-    return
-  }
+  if (allowOtaTranslations) {
+    try {
+      const currentLanguage = yield* select(currentLanguageSelector)
+      if (!currentLanguage) {
+        // this is true on first app install if the language cannot be
+        // automatically detected, we should not proceed without a language
+        return
+      }
 
-  try {
-    const currentLanguage = yield* select(currentLanguageSelector)
-    if (!currentLanguage) {
-      // this is true on first app install if the language cannot be
-      // automatically detected, we should not proceed without a language
-      return
+      const lastFetchLanguage = yield* select(otaTranslationsLanguageSelector)
+      const lastFetchTime = yield* select(otaTranslationsLastUpdateSelector)
+      const timestamp = yield* call([otaClient, otaClient.getManifestTimestamp])
+      const lastFetchAppVersion = yield* select(otaTranslationsAppVersionSelector)
+
+      if (
+        lastFetchLanguage !== currentLanguage ||
+        lastFetchTime !== timestamp ||
+        DeviceInfo.getVersion() !== lastFetchAppVersion
+      ) {
+        const languageMappings = yield* call([otaClient, otaClient.getLanguageMappings])
+        const customMappedLanguage = _.findKey(languageMappings, { locale: currentLanguage })
+
+        const translations = yield* call(
+          [otaClient, otaClient.getStringsByLocale],
+          undefined,
+          customMappedLanguage || currentLanguage
+        )
+        i18n.addResourceBundle(currentLanguage, 'translation', translations, true, true)
+
+        yield* call(saveOtaTranslations, { [currentLanguage]: translations })
+        yield* put(
+          otaTranslationsUpdated({
+            otaTranslationsLastUpdate: timestamp,
+            otaTranslationsAppVersion: DeviceInfo.getVersion(),
+            otaTranslationsLanguage: currentLanguage,
+          })
+        )
+      }
+    } catch (error) {
+      Logger.error(`${TAG}@handleFetchOtaTranslations`, 'Failed to fetch OTA translations', error)
     }
-
-    const lastFetchLanguage = yield* select(otaTranslationsLanguageSelector)
-    const lastFetchTime = yield* select(otaTranslationsLastUpdateSelector)
-    const timestamp = yield* call([otaClient, otaClient.getManifestTimestamp])
-    const lastFetchAppVersion = yield* select(otaTranslationsAppVersionSelector)
-
-    if (
-      lastFetchLanguage !== currentLanguage ||
-      lastFetchTime !== timestamp ||
-      DeviceInfo.getVersion() !== lastFetchAppVersion
-    ) {
-      const languageMappings = yield* call([otaClient, otaClient.getLanguageMappings])
-      const customMappedLanguage = _.findKey(languageMappings, { locale: currentLanguage })
-
-      const translations = yield* call(
-        [otaClient, otaClient.getStringsByLocale],
-        undefined,
-        customMappedLanguage || currentLanguage
-      )
-      i18n.addResourceBundle(currentLanguage, 'translation', translations, true, true)
-
-      yield* call(saveOtaTranslations, { [currentLanguage]: translations })
-      yield* put(
-        otaTranslationsUpdated({
-          otaTranslationsLastUpdate: timestamp,
-          otaTranslationsAppVersion: DeviceInfo.getVersion(),
-          otaTranslationsLanguage: currentLanguage,
-        })
-      )
-    }
-  } catch (error) {
-    Logger.error(`${TAG}@handleFetchOtaTranslations`, 'Failed to fetch OTA translations', error)
   }
 }
 
