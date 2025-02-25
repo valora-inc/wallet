a3387e026ae7f2ad7bfb942b1500f93da15f5dda
Tom McGuire <mcgtom10@gmail.com>
2025-02-24 16:17:48 -0800

    fix(e2e): remove unused imports

--- e2e/src/CeloPage.spec.js
+++ e2e/src/CeloPage.spec.js
@@ -1,6 +1,4 @@
 import CeloEducation from './usecases/CeloEducation'
-import CeloNews from './usecases/CeloNews'
-import PriceChart from './usecases/PriceChart'
 import { quickOnboarding } from './utils/utils'
 
 describe('Celo page', () => {
