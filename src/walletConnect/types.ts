fb74afecbb5e50e2ac679ffcb7bedb9a26b8d1b7
Alex Bakoushin <alex@bakoush.in>
2025-02-26 10:16:15 +0100

    get rid of legacy types

--- src/walletConnect/types.ts
+++ src/walletConnect/types.ts
@@ -1,37 +1,3 @@
-import {
-  IClientMeta,
-  IJsonRpcSubscription,
-  IWalletConnectSession,
-} from '@walletconnect/legacy-types'
-
-export interface WalletConnectSessionRequest {
-  id: number
-  jsonrpc: string
-  method: string
-  uri?: string
-  params: {
-    peerId: string
-    peerMeta: IClientMeta
-    chainId: number | null
-  }[]
-}
-
-export type WalletConnectSession = IWalletConnectSession
-
-export type WalletConnectPayloadRequest = IJsonRpcSubscription
-
-export type PendingAction = { version: 1; action: WalletConnectPayloadRequest; peerId: string }
-
-export type Session = {
-  version: 1
-  session: WalletConnectSession
-}
-
-export type PendingSession = {
-  version: 1
-  session: WalletConnectSessionRequest
-}
-
 export enum WalletConnectRequestType {
   Loading,
   Session,
