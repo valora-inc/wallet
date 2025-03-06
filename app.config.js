ce4f7a558567bcfdb435c6eaac6bd8910d8df09d
Alex Bakoushin <alex@bakoush.in>
2025-03-06 14:42:38 +0100

    chore: migrate to divvi framework (#6530)
    ### Description
    
    Migrate the codebase to the [divvi mobile
    framework](https://github.com/divvi-xyz/divvi-mobile/tree/main)
    
    The app configuration is defined through these two files
    * `app.config.js` - details related to expo framework
    * `index.tsx` - details related to divvi framework
    
    ### Running this branch
    
    ```bash
    yarn reset
    yarn prebuild
    yarn ios 
    yarn android
    ```
    
    ### TODO
    * Add [redux dev tools
    plugin](https://docs.expo.dev/debugging/devtools-plugins/#redux) to the
    framework
    * Support Alfajores (once the framework allows it)
    * Update setup and release docs
    * Cleanup `package.json` and root directory
    
    ### Known issues
    
    * Expo doesn't support a full-screen splash for Android (TODO: figure
    out next steps with Kayla)
    * Strange splash of green background on startup (TODO: same in the beefy
    app; probably requires framework tuning)
    
    ### Test plan
    
    * No unit/e2e tests for now, since all tests currently are within the
    framework repo
    * Added e2e boilerplate same as in the [Beefy
    app](https://github.com/valora-inc/beefy/tree/main/.github/workflows).
    All it does is build the app
    
    ### Related issues
    
    - Related to RET-1321
    
    ### Backwards compatibility
    
    Y
    
    ### Network scalability
    
    NA

---
+++ app.config.js
@@ -0,0 +1,237 @@
+const fs = require('fs')
+const { version } = require('./package.json')
+
+const mainnetSettings = {
+  easProjectId: '8593729d-4d16-40aa-b712-7f96b2293c9f',
+  showTestnetBanner: false,
+  networks: {
+    'celo-mainnet': true,
+    'ethereum-mainnet': true,
+    'arbitrum-one': true,
+    'op-mainnet': true,
+    'base-mainnet': true,
+  },
+}
+
+const testnetSettings = {
+  easProjectId: 'TODO',
+  showTestnetBanner: true,
+  networks: {
+    'celo-alfajores': true,
+    'ethereum-sepolia': true,
+    'arbitrum-one': true,
+    'op-sepolia': true,
+    'base-sepolia': true,
+  },
+}
+
+// Firebase credentials
+const GOOGLE_SERVICE_INFO_PLIST =
+  process.env.GOOGLE_SERVICE_INFO_PLIST ?? `${process.env.PWD}/.eas/.env/GOOGLE_SERVICES_PLIST`
+const GOOGLE_SERVICES_JSON =
+  process.env.GOOGLE_SERVICES_JSON ?? `${process.env.PWD}/.eas/.env/GOOGLE_SERVICES_JSON`
+const firebaseEnabled =
+  fs.existsSync(GOOGLE_SERVICE_INFO_PLIST) && fs.existsSync(GOOGLE_SERVICES_JSON)
+
+module.exports = () => {
+  const appVariant = process.env.APP_VARIANT ?? 'mainnet-dev'
+
+  const getAppConfig = () => {
+    switch (appVariant) {
+      case 'mainnet':
+        return {
+          ...mainnetSettings,
+          name: 'Valora',
+          appStoreId: '1520414263',
+          bundleId: 'co.clabs.valora',
+          auth0Domain: 'auth.valora.xyz',
+        }
+      case 'mainnet-dev':
+        return {
+          ...mainnetSettings,
+          name: 'Valora (dev)',
+          appStoreId: '1520414263',
+          bundleId: 'co.clabs.valora.dev',
+          auth0Domain: 'auth.valora.xyz',
+        }
+      case 'mainnet-nightly':
+        return {
+          ...mainnetSettings,
+          name: 'Valora (nightly)',
+          appStoreId: '1599290566',
+          bundleId: 'co.clabs.valora.nightly',
+          auth0Domain: 'auth.valora.xyz',
+        }
+      case 'alfajores':
+        return {
+          ...testnetSettings,
+          name: 'Alfajores',
+          appStoreId: '1482389446',
+          bundleId: 'org.celo.mobile.alfajores',
+          auth0Domain: 'auth.alfajores.valora.xyz',
+        }
+      case 'alfajores-dev':
+        return {
+          ...testnetSettings,
+          name: 'Alfajores (dev)',
+          appStoreId: '1482389446',
+          bundleId: 'org.celo.mobile.alfajores.dev',
+          auth0Domain: 'auth.alfajores.valora.xyz',
+        }
+      case 'alfajores-nightly':
+        return {
+          ...testnetSettings,
+          name: 'Alfajores (nightly)',
+          appStoreId: '1599287614',
+          bundleId: 'org.celo.mobile.alfajores.nightly',
+          auth0Domain: 'auth.alfajores.valora.xyz',
+        }
+      default:
+        throw new Error(`Unknown app variant: ${appVariant}`)
+    }
+  }
+
+  const { name, appStoreId, bundleId, auth0Domain, networks, showTestnetBanner, easProjectId } =
+    getAppConfig()
+
+  return {
+    expo: {
+      name,
+      slug: 'valora',
+      scheme: 'celo',
+      version,
+      orientation: 'portrait',
+      icon: './assets/icon/icon.png',
+      userInterfaceStyle: 'light',
+      // disable for now as it causes an Android build error with react-native-auth0
+      // See https://github.com/auth0/react-native-auth0/issues/879
+      newArchEnabled: false,
+      ios: {
+        icon: './assets/icon/icon.png',
+        supportsTablet: false,
+        bundleIdentifier: bundleId,
+        associatedDomains: ['applinks:vlra.app', 'applinks:valoraapp.com'],
+        infoPlist: {
+          NSCameraUsageDescription: 'Connecting your camera allows you to scan codes for payments.',
+          NSContactsUsageDescription:
+            'Adding your contacts makes it easy to send and request payments with your friends.',
+          NSPhotoLibraryAddUsageDescription:
+            'Connecting your photo library allows you to save your code to your photos.',
+          NSPhotoLibraryUsageDescription: 'This is required for you to choose a profile picture.',
+          NSUserTrackingUsageDescription:
+            'We use the advertising identifier to accurately attribute app installs from ad campaigns.',
+          NSFaceIDUsageDescription:
+            'This is required for you to use Face ID to secure your account.',
+          NSLocationWhenInUseUsageDescription:
+            'This app requires location access to provide location-based features.',
+          ITSAppUsesNonExemptEncryption: false,
+        },
+        entitlements: {
+          'aps-environment': 'production',
+        },
+        ...(firebaseEnabled && {
+          googleServicesFile: GOOGLE_SERVICE_INFO_PLIST,
+        }),
+      },
+      android: {
+        adaptiveIcon: {
+          foregroundImage: './assets/icon/adaptive-foreground.png',
+          backgroundImage: './assets/icon/adaptive-background.png',
+        },
+        package: bundleId,
+        permissions: [
+          'android.permission.CAMERA',
+          'android.permission.ACCESS_NETWORK_STATE',
+          'android.permission.INTERNET',
+          'android.permission.POST_NOTIFICATIONS',
+        ],
+        ...(firebaseEnabled && {
+          googleServicesFile: GOOGLE_SERVICES_JSON,
+        }),
+      },
+      plugins: [
+        [
+          'expo-splash-screen',
+          {
+            android: {
+              image: './assets/splash-icon.png',
+              imageWidth: 200,
+              resizeMode: 'contain',
+            },
+            ios: {
+              image: './assets/splash-gradient.png',
+              enableFullScreenImage_legacy: true,
+              resizeMode: 'cover',
+            },
+          },
+        ],
+        [
+          'expo-font',
+          {
+            fonts: [
+              'fonts/Inter-Bold.ttf',
+              'fonts/Inter-Medium.ttf',
+              'fonts/Inter-Regular.ttf',
+              'fonts/Inter-SemiBold.ttf',
+            ],
+          },
+        ],
+        [
+          'expo-build-properties',
+          {
+            ios: {
+              // Minimum iOS version we support
+              deploymentTarget: '15.1',
+              useFrameworks: 'static',
+            },
+            android: {
+              // For Persona SDK
+              extraMavenRepos: ['https://sdk.withpersona.com/android/releases'],
+              // Ad hoc fix for Android build error
+              kotlinVersion: '1.9.24',
+            },
+          },
+        ],
+        [
+          'react-native-permissions',
+          {
+            iosPermissions: ['Camera', 'AppTrackingTransparency', 'Contacts'],
+          },
+        ],
+        [
+          'react-native-auth0',
+          {
+            domain: auth0Domain,
+          },
+        ],
+        '@divvi/mobile',
+        './plugins/withConditionalDetox',
+        [
+          './plugins/withCustomGradleProperties',
+          {
+            'org.gradle.jvmargs': '-Xmx4096m -XX:+HeapDumpOnOutOfMemoryError',
+          },
+        ],
+        ...(firebaseEnabled
+          ? [
+              '@react-native-firebase/app',
+              '@react-native-firebase/auth',
+              '@react-native-firebase/dynamic-links',
+              '@react-native-firebase/messaging',
+            ]
+          : []),
+      ],
+      extra: {
+        appStoreId,
+        networks,
+        showTestnetBanner,
+        auth0Domain,
+        firebaseEnabled,
+        eas: {
+          projectId: easProjectId,
+        },
+      },
+      owner: 'divvi',
+    },
+  }
+}
