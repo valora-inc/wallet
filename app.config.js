ba34157668723a00e84ca3f920287c400cbab138
Tom McGuire <mcgtom10@gmail.com>
2025-09-08 15:27:07 -0700

    feat(deps): use @divvi/mobile ^1.0.0-alpha.91

--- app.config.js
+++ app.config.js
@@ -152,6 +152,9 @@ module.exports = () => {
         [
           'expo-build-properties',
           {
+            android: {
+              targetSdkVersion: 35 // Use 35 as edgeToEdge support is required for SDK 35+
+            },
             ios: {
               // Minimum iOS version we support
               deploymentTarget: '15.1',
